a40d5b8d8796a8787a00c520ce2c6ce31b45924e
Og B. Maciel <ogmaciel@gnome.org>
2012-04-13 17:32:15 -0400

    Minor tweaks to make the login test work.

--- src/lib/login.py
+++ src/lib/login.py
@@ -9,31 +9,43 @@ from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from robot.utils import asserts
 import time
 
-class Login(object):
+class login(object):
 
     ROBOT_LIBRARY_SCOPE = 'TEST_CASE'
     __version__ = '0.1'
 
 
-    def __init__(self):
-        print "hi"
-
     def start_browser(self, login_url, browser):
 
-        # Create a new instance of the Firefox driver
-        self.driver = webdriver.Firefox()
+        if browser == "firefox":
+            # Create a new instance of the Firefox driver
+            self.driver = webdriver.Firefox()
+        else:
+            # Create a new instance of the Chrome driver
+            self.driver = webdriver.Chrome()
 
         # go to the google home page
         self.driver.get(login_url)
 
+
+    def stop_browser(self):
+        # Quit the browser.
+        self.driver.quit()
+
     def login_user(self, username, password):
         # find the element that's name attribute is 'username'
         usernameElement = self.driver.find_element_by_name("username")
-        # type in the search
+        # type in the username
         usernameElement.send_keys(username)
         # find the element that's name attribute is 'password'
         passwordElement = self.driver.find_element_by_name("password")
+        # type in the password
+        passwordElement.send_keys(password)
         # find the submit button
-        inputElement = self.driver.find_element_by_name("Log In")
+        inputElement = self.driver.find_element_by_name("commit")
         # submit the form
         inputElement.click()
+
+    def success_login(self):
+        success = self.driver.find_element_by_css("div.jnotify-notification.jnotify-notification-success")
+
