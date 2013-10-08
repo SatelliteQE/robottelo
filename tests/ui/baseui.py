39f3960c55be0ae746c15a2a3c61c0b48b477c75
Og B. Maciel <ogmaciel@gnome.org>
2013-10-08 10:03:57 -0400

    Take a screenshot when a test fails.

--- tests/ui/baseui.py
+++ tests/ui/baseui.py
@@ -2,9 +2,12 @@
 # -*- encoding: utf-8 -*-
 # vim: ts=4 sw=4 expandtab ai
 
+import base64
+import datetime
 import logging
 import logging.config
 import os
+import selenium
 import unittest
 
 from splinter import Browser
@@ -27,7 +30,37 @@ class BaseUI(unittest.TestCase):
         self.browser = Browser(driver_name=self.driver_name)
         self.browser.visit("%s/%s" % (self.host, self.project))
 
-    def tearDown(self):
+    # Borrowed from the following article:
+    #  http://engineeringquality.blogspot.com/2012/12/python-selenium-capturing-screenshot-on.html
+    def take_screenshot(self, webdriver, file_name="error.png"):
+            """
+            @param webdriver: WebDriver.
+            @type webdriver: WebDriver
+            @param file_name: Name to label this screenshot.
+            @type file_name: str
+            """
+            if isinstance(webdriver, selenium.webdriver.remote.webdriver.WebDriver):
+                # Get Screenshot over the wire as base64
+                base64_data = webdriver.get_screenshot_as_base64()
+                screenshot_data = base64.decodestring(base64_data)
+                screenshot_file = open(file_name, "w")
+                screenshot_file.write(screenshot_data)
+                screenshot_file.close()
+            else:
+                webdriver.save_screenshot(filename)
+
+    def run(self, result=None):
+        super(BaseUI, self).run(result)
+
+        if result.failures:
+            fname = str(self).replace("(", "").replace(")", "").replace(" ", "_")
+            fmt='%y-%m-%d_%H.%M.%S'
+            fdate = datetime.datetime.now().strftime(fmt)
+            filename = "%s_%s.png" % (fdate, fname)
+            self.take_screenshot(self.browser.driver, filename)
+
         self.browser.quit()
         self.browser = None
+
+        return result
 
