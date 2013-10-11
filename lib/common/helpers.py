08b601b0d8038e5ff1a47ae908debe0fddf94b39
Og B. Maciel <ogmaciel@gnome.org>
2013-10-11 16:57:11 -0400

    Adding helper module.

---
+++ lib/common/helpers.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# vim: ts=4 sw=4 expandtab ai
+
+import random
+import string
+
+def generate_name(min=4, max=8):
+
+    if min <= 0:
+        min = 4
+    if max < min:
+        max = min
+
+    r = random.SystemRandom()
+    pool1 = string.ascii_lowercase + string.digits
+
+    return "%s" % str().join(r.choice(pool1) for x in range(random.randint(min, max)))
+
+def valid_names_list():
+
+    VALID_NAMES = [
+        generate_name(5,5),
+        generate_name(255),
+        "%s-%s" % (generate_name(4), generate_name(4)),
+        "%s.%s" % (generate_name(4), generate_name(4)),
+        u"նոր օգտվող-%s" % generate_name(2),
+        u"新用戶-%s" % generate_name(2),
+        u"नए उपयोगकर्ता-%s" % generate_name(2),
+        u"нового пользователя-%s" % generate_name(2),
+        u"uusi käyttäjä-%s" % generate_name(2),
+        u"νέος χρήστης-%s" % generate_name(2),
+        "foo@!#$^&*( ) %s" % generate_name(),
+        "<blink>%s</blink>" % generate_name(),
+        "bar+{}|\"?hi %s" % generate_name(),
+    ]
+
+    return VALID_NAMES
+
+def invalid_names_list():
+
+    INVALID_NAMES = [
+        " ",
+        generate_name(256),
+        " " + generate_name(),
+        generate_name() + " ",
+    ]
+
+    return INVALID_NAMES
