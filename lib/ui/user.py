c30567386fca2626f20dcd82d2cad752223d09b8
Og B. Maciel <ogmaciel@gnome.org>
2013-10-13 17:27:52 -0400

    Fixing Users tests. Still not fully green.

--- lib/ui/user.py
+++ lib/ui/user.py
@@ -12,27 +12,33 @@ class User():
     def new_user(self, username, email=None, password1=None, password2=None):
         self.browser.find_by_id(locators["users.new"]).click()
 
-        if self.browser.is_element_present_by_id(locators["users.username"]):
+        if self.browser.is_element_present_by_id(locators["users.username"], 10):
             self.browser.find_by_id(locators["users.username"]).fill(username)
             # The following fields are not available via LDAP auth
-            if self.browser.is_element_present_by_id(locators["users.email"]):
+            if self.browser.is_element_present_by_id(locators["users.email"], 10):
                 self.browser.find_by_id(locators["users.email"]).fill(email)
+            if self.browser.is_element_present_by_id(locators["users.password1"], 10):
                 self.browser.find_by_id(locators["users.password1"]).fill(password1)
+            if self.browser.is_element_present_by_id(locators["users.password2"], 10):
                 self.browser.find_by_id(locators["users.password2"]).fill(password2)
             self.browser.find_by_id(locators["users.save"]).click()
 
     def find_user(self, username):
-        return self.browser.find_by_xpath(locators["users.user"] % username)
+        return self.browser.find_by_xpath(locators["users.user"] % username, 10)
 
     def remove_user(self, username, really=False):
         user = self.find_user(username)
 
         if user:
-            self.browser.find_by_xpath(locators["users.remove"]).click()
-            if really:
-                self.browser.find_by_xpath(locators["dialog.yes"]).click()
-            else:
-                self.browser.find_by_xpath(locators["dialog.no"]).click()
+            user.click()
+            #TODO: Need to wait until the edit panel is visible so we
+            #can interact with it.
+            if self.browser.is_element_present_by_xpath(locators["users.remove"], 10):
+                self.browser.find_by_xpath(locators["users.remove"]).click()
+                if really:
+                    self.browser.find_by_xpath(locators["dialog.yes"]).click()
+                else:
+                    self.browser.find_by_xpath(locators["dialog.no"]).click()
 
     def update_locale(self, lang='pt-BR'):
         self.browser.select(locators["users.locale"], lang)
--- tests/ui/test_User.py
+++ tests/ui/test_User.py
@@ -4,6 +4,7 @@
 
 from baseui import BaseUI
 from robottelo.lib.common.helpers import generate_name
+from robottelo.lib.ui.locators import *
 
 class User(BaseUI):
 
@@ -17,6 +18,7 @@ class User(BaseUI):
         email = email or "%s@example.com" % name
 
         self.user.new_user(name, email, passwd1, passwd2)
+        self.assertTrue(self.browser.is_element_present_by_css(locators["notif.success"]))
         user = self.user.find_user(name)
         self.assertEqual(user.text, name)
 
@@ -24,7 +26,7 @@ class User(BaseUI):
         "Successfully creates a new user"
         self.users_page()
 
-        password = generate_name()
+        password = generate_name(6)
         self._create_user(None, None, password, password)
 
     def test_create_user_2(self):
@@ -32,10 +34,12 @@ class User(BaseUI):
         self.users_page()
 
         name = generate_name()
-        email = email "%s@example.com" % name
-        password = generate_name()
-        self._create_user(None, None, password, password)
+        email = "%s@example.com" % name
+        password = generate_name(6)
+        self._create_user(name, email, password, password)
 
+        # Revisit Users page
+        self.navigator.go_to_users()
         self.user.new_user(name, email, password, password)
         self.assertTrue(self.browser.is_element_present_by_css(locators["notif.error"]))
 
@@ -43,7 +47,7 @@ class User(BaseUI):
         "Create then edit new user's password"
         self.users_page()
 
-        password = generate_name()
+        password = generate_name(6)
         self._create_user(None, None, password, password)
 
         new_password = generate_name()
@@ -56,7 +60,7 @@ class User(BaseUI):
         "Create then edit new user's locale"
         self.users_page()
 
-        password = generate_name()
+        password = generate_name(6)
         self._create_user(None, None, password, password)
         # Change locale to pt_BR (default)
         self.user.update_locale()
@@ -69,15 +73,20 @@ class User(BaseUI):
         "Creates and immediately deletes a user"
         self.users_page()
 
-        password = generate_name()
-        self._create_user(None, None, password, password)
+        name = generate_name()
+        password = generate_name(6)
+        self._create_user(name, None, password, password)
 
         # Attempt to remove a user but change mind
+        # Revisit Users page
+        self.navigator.go_to_users()
         self.user.remove_user(name, False)
         user = self.user.find_user(name)
         self.assertEqual(user.text, name)
 
         # Now delete it for real
+        # Revisit Users page
+        self.navigator.go_to_users()
         self.user.remove_user(name, True)
         user = self.user.find_user(name)
         self.assertEqual(user, [])
