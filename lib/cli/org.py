7a6332f19b74e6f3c2bef45d9f7540688df3eace
Og B. Maciel <ogmaciel@gnome.org>
2013-11-02 21:22:03 -0400

    Adding base support for Org CLI module.

---
+++ lib/cli/org.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# vim: ts=4 sw=4 expandtab ai
+
+from base import Base
+
+FIELDS = ['id', 'name', 'description', 'default service level', 'available service levels', 'default system info keys']
+
+class Org(Base):
+
+    def __init__(self, conn):
+        self.conn = conn
+
+    def attach_all_systems(self):
+        pass
+
+    def create(self, name=None, label=None, description=None):
+        cmd = "org create --name='%s' --label='%s'"
+
+        if name is None:
+            name = generate_name()
+        if description is None:
+            description = "Automatically generated"
+
+        cmd = cmd % (name, label, description)
+
+        stdout, stderr = self.execute(cmd)
+
+        return False if stderr else True
+
+    def default_info(self):
+        pass
+
+    def delete(self, name):
+        cmd = "org delete --name='%s'"
+
+        stdout, stderr = self.execute(cmd % name)
+
+        return False if stderr else True
+
+    def info(self, name):
+        cmd = "org info --name='%s'"
+
+        org = {}
+
+        stdout, stderr = self.execute(cmd % name)
+
+        if stdout:
+            for key, value in zip(FIELDS, "".join(stdout).split()):
+                org[key] = value
+        return org
+
+    def list(self):
+        cmd = "org list"
+
+        orgs = []
+
+        stdout, stderr = self.execute(cmd)
+
+        if stdout:
+            for entry in stdout:
+                org = {}
+                for key, value in zip(FIELDS, "".join(entry).split()):
+                    org[key] = value
+                orgs.append(user)
+        return orgs
+
+    def subscriptions(self):
+        pass
+
+    def uebercert(self):
+        pass
+
+    def update(self, name, description=None, service_level=None):
+        cmd = "org update --name='%s'" % name
+
+        if description:
+            cmd += " --description='%s'" % description
+        if service_level:
+            cmd += " --servicelevel='%s'" % service_level
+
+        stdout, stderr = self.execute(cmd)
+
+        return False if stderr else True
+
