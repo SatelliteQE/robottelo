7477035d65b2339ae55d4351d36be05e9979c520
Og B. Maciel <ogmaciel@gnome.org>
2013-11-02 20:48:29 -0400

    Added new web locators for Organizations UI.

--- lib/ui/locators.py
+++ lib/ui/locators.py
@@ -38,6 +38,40 @@ locators = {
     "users.remove": (By.XPATH, "//a[@class='remove_item']"),
     "users.locale": (By.ID, "locale_locale"),
 
+    # Organizations
+    "orgs.search": (By.ID, "search"),
+    "orgs.new": (By.ID, "new"),
+    "orgs.name": (By.ID, "organization_name"),
+    "orgs.label": (By.ID, "organization_label"),
+    "orgs.description": (By.ID, "organization_description"),
+    "orgs.env_name": (By.ID, "environment_name"),
+    "orgs.env_label": (By.ID, "environment_label"),
+    "orgs.env_description": (By.ID, "environment_description"),
+    "orgs.save": (By.NAME, "commit"),
+    "orgs.remove": (By.XPATH, "//a[@class='remove_item']"),
+    "orgs.org": (By.XPATH, "//div[@title='%s']"),
+
+    # Organization Details
+    "orgs.details_secion": (By.XPATH, "//li[@id='organization_details']/a"),
+    "org.name": (By.NAME, "organization[name]"),
+    "org.description": (By.NAME, "organization[description]"),
+    "org.sla": (By.NAME, "organization[service_level]"),
+
+    # Organization Defaults
+    "orgs.default_info_section": (By.XPATH, "//li[@id='organization_default_info']/a"),
+    # Organization System Default Info
+    "org.system_default_link": (By.XPATH, "//li[@id='org_system_default_info']/a"),
+    # Organization Distributor Default Info
+    "org.distributor_default_link": (By.XPATH, "//li[@id='org_distributor_default_info']/a"),
+
+    "org.default_keyname_field": (By.ID "new_default_info_keyname"),
+    "org.default_keyname_save": (By.ID, "add_default_info_button"),
+    "org.default_keyname_apply": (By.ID, "apply_default_info_button"),
+    "org.default_keyname_remove": (By.XPATH, "//input[@data-id='default_info_%s']"),
+
+    # Organization History
+    "orgs.history_section": (By.XPATH, "//li[@id='organization_history']/a"),
+
     # Menus
 
     ## Administer
