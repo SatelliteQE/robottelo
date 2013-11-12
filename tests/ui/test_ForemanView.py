18874331e4d77f4c434ea99282326fba421c897e
Corey Welton <cwelton@redhat.com>
2013-11-12 14:15:25 -0500

    more attempts

--- tests/ui/test_ForemanView.py
+++ tests/ui/test_ForemanView.py
@@ -6,9 +6,11 @@ from baseui import BaseUI
 #from robottelo.lib.common.helpers import generate_name
 from robottelo.lib.ui.locators import *
 
-class Foreman(BaseUI):
+class ForemanView(BaseUI):
 
     def test_switch_to_foreman_view(self):
         "Switches to foreman view"
         self.login.login("admin","admin")
         self.navigator.go_to_foreman()
+        self.assertTrue(self.user.wait_until_element(locators["menu.foreman_dropdown1"]))
+#        self.assertTrue(self.user.wait_until_element(locators["notif.success"]))
