809e29521ffd2a1725d8e7a8cfa61c549e8f7e97
Corey Welton <cswiii@gmail.com>
2013-11-13 11:20:03 -0500

    function for creating Latin-1 and multibyte strings

--- lib/common/helpers.py
+++ lib/common/helpers.py
@@ -47,3 +47,32 @@ def invalid_names_list():
     ]
 
     return INVALID_NAMES
+
+
+def generate_i18n_string(str_type, length):
+    '''
+    This function will allow creation of unicode or latin-1 strings of
+    arbitrary length.  Presently the unicode strings are CJK-only but
+    should suffice for the purposes of most multibyte testing.
+    '''
+    # approximate range of CJK Unified Ideographs
+    # It does not include extensions.
+    utf8_range_begin = '4E00'
+    utf8_range_end = '9FFF'
+    # range of latin-1 characters/letters (ISO 8859-1)
+    # It does not include unicode Latin Extensions.
+    latin1_range_begin = '00C0'
+    latin1_range_end = '00F0'
+    output_array = []
+    if str_type == "latin1":
+        range_begin = latin1_range_begin
+        range_end = latin1_range_end
+    elif str_type == "unicode":
+        range_begin = utf8_range_begin
+        range_end = utf8_range_end
+    else:
+        raise Exception('Unexpected output type, valid types are "latin1", "unicode"')
+    for i in range(int(range_begin, 16), int(range_end, 16)):
+        output_array.append(i)
+    output_string = ''.join(unichr(random.choice(output_array)) for x in xrange(length))
+    return output_string
