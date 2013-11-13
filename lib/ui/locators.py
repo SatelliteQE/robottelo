160aa5bf63a18ba525af8ed6fbf89bd2f7fa317d
Og B. Maciel <ogmaciel@gnome.org>
2013-11-13 13:52:31 -0500

    Added locators and navigation for all menus.

--- lib/ui/locators.py
+++ lib/ui/locators.py
@@ -79,12 +79,65 @@ locators = {
 
     # Menus
 
+    #TODO: Get DEV to add UNIQUE IDS to these menus
+
+    # Dashboard
+    "menu.dashboard": (By.XPATH, "//nav[@class='nav left']/ul/li[1]/a/span"),
+    # Content
+    "menu.content": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/a/span"),
+    ## Subscriptions
+    "menu.subscriptions": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[1]/a/span"),
+    ### Red Hat Subscriptions
+    "menu.redhatsubs": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[1]/ul/li[1]/a"),
+    ### Subscription Management Applications
+    "menu.distributors": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[1]/ul/li[2]/a"),
+    ### Activation Keys
+    "menu.actkeys": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[1]/ul/li[3]/a"),
+    ### Import History
+    "menu.importhistory": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[1]/ul/li[4]/a"),
+    ## Repositories
+    "menu.repos": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[2]/a/span"),
+    ## Red Hat Repos
+    "menu.redhatrepos": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[2]/ul/li[1]/a"),
+    ## Products
+    "menu.products": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[2]/ul/li[2]/a"),
+    ## GPG Keys
+    "menu.gpgkeys": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[2]/ul/li[3]/a"),
+    ## Synchronization
+    "menu.sync": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[3]/a/span"),
+    ### Sync Status
+    "menu.syncstatus": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[3]/ul/li[1]/a"),
+    ### Sync Plans
+    "menu.syncplans": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[3]/ul/li[2]/a"),
+    ### Sync Schedule
+    "menu.syncschedule": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[3]/ul/li[3]/a"),
+    ## Content View Definitions
+    "menu.contentview": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[4]/a/span"),
+    ## Content Search
+    "menu.contentsearch": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[5]/a/span"),
+    ## Changesets
+    "menu.changesets": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[6]/a/span"),
+    ### Changeset Management
+    "menu.changesetmanage": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[6]/ul/li[1]/a"),
+    ### Changeset History
+    "menu.changesethistory": (By.XPATH, "//nav[@class='nav left']/ul/li[2]/ul/li[6]/ul/li[2]/a"),
+    ## Systems
+    "menu.systems": (By.XPATH, "//nav[@class='nav left']/ul/li[3]/a/span"),
+    ### All Systems
+    "menu.systemsall": (By.XPATH, "//nav[@class='nav left']/ul/li[3]/ul/li[1]/a"),
+    ### System Groups
+    "menu.systemgroups": (By.XPATH, "//nav[@class='nav left']/ul/li[3]/ul/li[2]/a"),
     ## Administer
-    "menu.administer": (By.XPATH, "//nav[@class='nav admin_menu right']/ul/li/a/span[@class='ng-binding']"),
-    "submenu.organizations": (By.XPATH, "//ul[@class='dropdown dropdown-right dropdown-active']/li/a[contains(@href,'/organizations')]"),
-    "submenu.users": (By.XPATH, "//ul[@class='dropdown dropdown-right dropdown-active']/li/a[contains(@href,'/users')]"),
-    "submenu.roles": (By.XPATH, "//ul[@class='dropdown dropdown-right dropdown-active']/li/a[contains(@href,'/roles')]"),
-    "submenu.about": (By.XPATH, "//ul[@class='dropdown dropdown-right dropdown-active']/li/a[contains(@href,'/about')]"),
-    "submenu.reports": (By.XPATH, "//ul[@class='dropdown dropdown-right dropdown-active']/li/a[contains(@href,'/filters')]")
+    "menu.administer": (By.XPATH, "//nav[@class='nav admin_menu right']/ul/li/a/span"),
+    ### Organizations
+    "menu.organizations": (By.XPATH, "//ul/li/a[contains(@href,'/organizations')]"),
+    ### Users
+    "menu.users": (By.XPATH, "//ul/li/a[contains(@href,'/users')]"),
+    ### Roles
+    "menu.roles": (By.XPATH, "//ul/li/a[contains(@href,'/roles')]"),
+    ### About
+    "menu.about": (By.XPATH, "//ul/li/a[contains(@href,'/about')]"),
+    ### Reports
+    "menu.reports": (By.XPATH, "//ul/li/a[contains(@href,'/filters')]")
 
 }
--- lib/ui/navigator.py
+++ lib/ui/navigator.py
@@ -3,7 +3,8 @@
 # vim: ts=4 sw=4 expandtab ai
 
 from base import Base
-from locators import *
+from locators import locators
+
 
 class Navigator(Base):
     """
@@ -12,6 +13,85 @@ class Navigator(Base):
 
     def __init__(self, browser):
         self.browser = browser
+
+    def go_to_dashboard(self):
+        self.find_element(locators["menu.dashboard"]).click()
+
+    def go_to_subscriptions(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.subscriptions"]).click()
+        self.find_element(locators["menu.redhatsubs"]).click()
+
+    def go_to_distributors(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.subscriptions"]).click()
+        self.find_element(locators["menu.distributors"]).click()
+
+    def go_to_activation_keys(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.subscriptions"]).click()
+        self.find_element(locators["menu.actkeys"]).click()
+
+    def go_to_import_history(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.subscriptions"]).click()
+        self.find_element(locators["menu.importhistory"]).click()
+
+    def go_to_repositories(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.repos"]).click()
+        self.find_element(locators["menu.redhatrepos"]).click()
+
+    def go_to_products(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.repos"]).click()
+        self.find_element(locators["menu.products"]).click()
+
+    def go_to_gpg_keys(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.repos"]).click()
+        self.find_element(locators["menu.gpgkeys"]).click()
+
+    def go_to_sync_status(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.sync"]).click()
+        self.find_element(locators["menu.syncstatus"]).click()
+
+    def go_to_sync_plans(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.sync"]).click()
+        self.find_element(locators["menu.syncplans"]).click()
+
+    def go_to_sync_schedule(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.sync"]).click()
+        self.find_element(locators["menu.syncschedule"]).click()
+
+    def go_to_content_views(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.contentview"]).click()
+
+    def go_to_content_search(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.contentsearch"]).click()
+
+    def go_to_changesets_management(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.changesets"]).click()
+        self.find_element(locators["menu.changesetsmanage"]).click()
+
+    def go_to_changesets_history(self):
+        self.find_element(locators["menu.content"]).click()
+        self.find_element(locators["menu.changesets"]).click()
+        self.find_element(locators["menu.changesetshistory"]).click()
+
+    def go_to_systems_all(self):
+        self.find_element(locators["menu.systems"]).click()
+        self.find_element(locators["menu.systemsall"]).click()
+
+    def go_to_system_groups(self):
+        self.find_element(locators["menu.systems"]).click()
+        self.find_element(locators["menu.systemgroups"]).click()
 
     def go_to_organizations(self):
         self.find_element(locators["menu.administer"]).click()
