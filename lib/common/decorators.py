81f0d999933016228b0f84ed09af8d0428a41ff3
Blair Morrison <blairmorrison@gmail.com>
2013-11-14 09:33:32 -0500

    Adds a bzbug decorator for skipping tests based on bugzilla

--- lib/common/decorators.py
+++ lib/common/decorators.py
@@ -4,7 +4,13 @@
 
 import os
 import unittest
+import bugzilla
+import logging
 
+bugzilla_log = logging.getLogger("bugzilla")
+bugzilla_log.setLevel(logging.WARNING)
+
+BUGZILLA_URL = "https://bugzilla.redhat.com/xmlrpc.cgi"
 
 def runIf(project):
     "Decorator to skip tests based on server mode"
@@ -17,3 +23,12 @@ def runIf(project):
         return lambda func: func
     return unittest.skip("%s specific test." % project)
 
+def bzbug(bz_id):
+    mybz = bugzilla.RHBugzilla()
+    mybz.connect(BUGZILLA_URL)
+    mybug = mybz.getbugsimple(bz_id)
+    if (mybug.status == 'NEW') or (mybug.status == 'ASSIGNED'):
+        logging.debug(mybug)
+        return unittest.skip(mybug)
+    else:
+        return lambda func: func
