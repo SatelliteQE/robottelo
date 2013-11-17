339536d2fdc2703ec862630b3a1f8e5226cafa65
Og B. Maciel <ogmaciel@gnome.org>
2013-11-17 12:12:23 -0500

    Updated Navigator functions to go directly to submenu.
    Attempting to navigate through the web elements is not working for
    submenus of the More menu. Even though one can find the submenu
    elements with WebDriver, they are not "visible" to WebDriver and
    therefore cannot be interacted with.

--- lib/ui/navigator.py
+++ lib/ui/navigator.py
@@ -16,8 +16,8 @@ class Navigator(Base):
 
     def click_menu(self, menu_name, position=1):
         script = "return document.getElementsByClassName('%s')[1];" % menu_name
-        menu_element = self.execute_script(script)
-        menu_element. find_element_by_tag_name('a').click()
+        menu_element = self.browser.execute_script(script)
+        menu_element.find_element_by_tag_name('a').click()
 
         return menu_element
 
@@ -48,76 +48,90 @@ class Navigator(Base):
     def go_to_configuration(self):
         menu = self.go_to_more()
         submenu = menu.find_elements_by_xpath("//ul/li[@class='dropdown-submenu']/a")[3]
-        ActionChains(self.browser).move_to_element("submenu").perform()
+        ActionChains(self.browser).move_to_element(submenu).perform()
 
     def go_to_environments(self):
         self.go_to_configuration()
-        self.browser.find_element_by_xpath("//a[@href='/environments']").click()
+        self.browser.execute_script("window.location.href='/environments'")
+        #self.browser.find_element_by_xpath("//a[@href='/environments']").click()
 
     def go_to_global_parameters(self):
         self.go_to_configuration()
-        self.browser.find_element_by_xpath("//a[@href='/common_parameters']").click()
+        self.browser.execute_script("window.location.href='/common_parameters'")
+        #self.browser.find_element_by_xpath("//a[@href='/common_parameters']").click()
 
     def go_to_host_groups(self):
         self.go_to_configuration()
-        self.browser.find_element_by_xpath("//a[@href='/hostgroups']").click()
+        self.browser.execute_script("window.location.href='/hostgroups'")
+        #self.browser.find_element_by_xpath("//a[@href='/hostgroups']").click()
 
     def go_to_puppet_classes(self):
         self.go_to_configuration()
-        self.browser.find_element_by_xpath("//a[@href='/puppetclasses']").click()
+        self.browser.execute_script("window.location.href='/puppetclasses'")
+        #self.browser.find_element_by_xpath("//a[@href='/puppetclasses']").click()
 
     def go_to_smart_variables(self):
         self.go_to_configuration()
-        self.browser.find_element_by_xpath("//a[@href='/lookup_keys']").click()
+        self.browser.execute_script("window.location.href='/lookup_keys'")
+        #self.browser.find_element_by_xpath("//a[@href='/lookup_keys']").click()
 
     def go_to_smart_proxies(self):
         self.go_to_configuration()
-        self.browser.find_element_by_xpath("//a[@href='/smart_proxies']").click()
+        self.browser.execute_script("window.location.href='/smart_proxies'")
+        #self.browser.find_element_by_xpath("//a[@href='/smart_proxies']").click()
 
     def go_to_provisioning(self):
-        self.go_to_configuration()
+        menu = self.go_to_more()
         submenu = menu.find_elements_by_xpath("//ul/li[@class='dropdown-submenu']/a")[4]
-        ActionChains(self.browser).move_to_element("submenu").perform()
+        ActionChains(self.browser).move_to_element(submenu).perform()
 
     def go_to_architectures(self):
         self.go_to_provisioning()
-        self.browser.find_element_by_xpath("//a[@href='/architectures']").click()
+        self.browser.execute_script("window.location.href='/architectures'")
+        #self.browser.find_element_by_xpath("//a[@href='/architectures']").click()
 
     def go_to_compute_resources(self):
         self.go_to_provisioning()
-        self.browser.find_element_by_xpath("//a[@href='/compute_resources']").click()
+        self.browser.execute_script("window.location.href='/compute_resources'")
+        #self.browser.find_element_by_xpath("//a[@href='/compute_resources']").click()
 
     def go_to_domains(self):
         self.go_to_provisioning()
-        self.browser.find_element_by_xpath("//a[@href='/domains']").click()
+        self.browser.execute_script("window.location.href='/domains'")
+        #self.browser.find_element_by_xpath("//a[@href='/domains']").click()
 
     def go_to_hardware_models(self):
         self.go_to_provisioning()
-        self.browser.find_element_by_xpath("//a[@href='/models']").click()
+        self.browser.execute_script("window.location.href='/models'")
+        #self.browser.find_element_by_xpath("//a[@href='/models']").click()
 
     def go_to_installation_media(self):
         self.go_to_provisioning()
-        self.browser.find_element_by_xpath("//a[@href='/media']").click()
+        self.browser.execute_script("window.location.href='/media'")
+        #self.browser.find_element_by_xpath("//a[@href='/media']").click()
 
     def go_to_operating_systems(self):
         self.go_to_provisioning()
-        self.browser.find_element_by_xpath("//a[@href='/operating_systems']").click()
+        self.browser.execute_script("window.location.href='/operating_systems'")
+        #self.browser.find_element_by_xpath("//a[@href='/operating_systems']").click()
 
     def go_to_partition_tables(self):
         self.go_to_provisioning()
-        self.browser.find_element_by_xpath("//a[@href='/ptables']").click()
+        self.browser.execute_script("window.location.href='/ptables'")
+        #self.browser.find_element_by_xpath("//a[@href='/ptables']").click()
 
     def go_to_provisioning_templates(self):
         self.go_to_provisioning()
-        self.browser.find_element_by_xpath("//a[@href='/config_templates']").click()
+        self.browser.execute_script("window.location.href='/config_templates'")
+        #self.browser.find_element_by_xpath("//a[@href='/config_templates']").click()
 
     def go_to_subnets(self):
         self.go_to_provisioning()
-        self.browser.find_element_by_xpath("//a[@href='/subnets']").click()
+        self.browser.execute_script("window.location.href='/subnets'")
+        #self.browser.find_element_by_xpath("//a[@href='/subnets']").click()
 
     def go_to_users(self):
         self.go_to_configuration()
         submenu = menu.find_elements_by_xpath("//ul/li[@class='dropdown-submenu']/a")[5]
-        ActionChains(self.browser).move_to_element("submenu").perform()
+        ActionChains(self.browser).move_to_element(submenu).perform()
 
-    
