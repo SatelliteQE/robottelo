5023ba3d4a50be6e8fbc493df9903e233a236b9a
Og B. Maciel <ogmaciel@gnome.org>
2013-11-23 18:35:43 -0500

    Updated locators and navigators for menus and submenus.
    Latest Foreman has unique IDs for all menus and submenus now.

--- lib/ui/navigator.py
+++ lib/ui/navigator.py
@@ -15,137 +15,168 @@ class Navigator(Base):
     def __init__(self, browser):
         self.browser = browser
 
-    def more_menu_move_to(self, menu_name):
-        menu_element = self.browser.find_element_by_xpath("//div[contains(@style,'static')]//ul[@id='menu2']//a[normalize-space(.)='{0}']".format(menu_name))
-        ActionChains(self.browser).move_to_element(menu_element).perform()
-        return menu_element
+    def menu_click(self, top_menu_locator, sub_menu_locator):
+        #import epdb; epdb.st()
+        menu_element = self.find_element(top_menu_locator)
 
-    def more_menu_click(self, menu_name):
-        menu_element = self.browser.find_element_by_xpath("//div[contains(@style,'static')]//ul[@id='menu2']//a[normalize-space(.)='{0}']".format(menu_name))
-        ActionChains(self.browser).move_to_element(menu_element).perform()
-        menu_element.click()
-
-    def click_menu(self, menu_name):
-        menu_element = self.browser.find_element_by_xpath("//div[contains(@style,'static')]//a[normalize-space(.)='{0}']".format(menu_name)).click()
-        return menu_element
-
-    def go_to_more(self):
-        self.find_element(locators["menu.more"]).click()
-        self.wait_for_ajax()
+        if menu_element:
+            ActionChains(self.browser).move_to_element(menu_element).perform()
+            submenu_element = self.find_element(sub_menu_locator)
+            if submenu_element:
+                submenu_element.click()
 
     def go_to_dashboard(self):
-        self.click_menu("Dashboard")
-
-    def go_to_hosts(self):
-        self.click_menu("Hosts")
+        self.menu_click(
+            locators['menu.monitor'], locators['menu.dashboard'],
+        )
 
     def go_to_reports(self):
-        self.click_menu("Reports")
+        self.menu_click(
+            locators['menu.monitor'], locators['menu.reports'],
+        )
 
     def go_to_facts(self):
-        self.click_menu("Facts")
-
-    def go_to_audits(self):
-        self.click_menu("Audits")
+        self.menu_click(
+            locators['menu.monitor'], locators['menu.facts'],
+        )
 
     def go_to_statistics(self):
-        self.click_menu("Statistics")
+        self.menu_click(
+            locators['menu.monitor'], locators['menu.statistics'],
+        )
 
     def go_to_trends(self):
-        self.click_menu("Trends")
+        self.menu_click(
+            locators['menu.monitor'], locators['menu.trends'],
+        )
 
-    def go_to_configuration(self):
-        menu = self.go_to_more()
-        self.more_menu_move_to('Configuration')
-        self.more_menu_move_to('Environments')
-        self.wait_for_ajax()
+    def go_to_audits(self):
+        self.menu_click(
+            locators['menu.monitor'], locators['menu.audits'],
+        )
 
-    def go_to_environments(self):
-        self.go_to_configuration()
-        self.more_menu_click('Environments')
-
-    def go_to_global_parameters(self):
-        self.go_to_configuration()
-        self.more_menu_click('Global Parameters')
-
-    def go_to_host_groups(self):
-        self.go_to_configuration()
-        self.more_menu_click('Host Groups')
-
-    def go_to_puppet_classes(self):
-        self.go_to_configuration()
-        self.more_menu_click('Puppet Classes')
-
-    def go_to_smart_variables(self):
-        self.go_to_configuration()
-        self.more_menu_click('Smart Variables')
-
-    def go_to_smart_proxies(self):
-        self.go_to_configuration()
-        self.more_menu_click('Smart Proxies')
-
-    def go_to_provisioning(self):
-        menu = self.go_to_more()
-        self.more_menu_move_to('Provisioning')
-        self.wait_for_ajax()
-        self.more_menu_move_to('Architectures')
-        self.wait_for_ajax()
-
-    def go_to_architectures(self):
-        self.go_to_provisioning()
-        self.more_menu_click('Architectures')
-        self.wait_for_ajax()
-
-    def go_to_compute_resources(self):
-        self.go_to_provisioning()
-        self.more_menu_click('Compute Resources')
-
-    def go_to_domains(self):
-        self.go_to_provisioning()
-        self.more_menu_click('Domains')
-
-    def go_to_hardware_models(self):
-        self.go_to_provisioning()
-        self.more_menu_click('Hardware Models')
-
-    def go_to_installation_media(self):
-        self.go_to_provisioning()
-        self.more_menu_click('Installation Media')
+    def go_to_hosts(self):
+        self.menu_click(
+            locators['menu.hosts'], locators['menu.all_hosts'],
+        )
 
     def go_to_operating_systems(self):
-        self.go_to_provisioning()
-        self.more_menu_click('Operating Systems')
-
-    def go_to_partition_tables(self):
-        self.go_to_provisioning()
-        self.more_menu_click('Partition Tables')
+        self.menu_click(
+            locators['menu.hosts'], locators['menu.operating_systems'],
+        )
 
     def go_to_provisioning_templates(self):
-        self.go_to_provisioning()
-        self.more_menu_click('Provisioning Templates')
+        self.menu_click(
+            locators['menu.hosts'], locators['menu.provisioning_templates'],
+        )
+
+    def go_to_partition_tables(self):
+        self.menu_click(
+            locators['menu.hosts'], locators['menu.partition_tables'],
+        )
+
+    def go_to_installation_media(self):
+        self.menu_click(
+            locators['menu.hosts'], locators['menu.installation_media'],
+        )
+
+    def go_to_hardware_models(self):
+        self.menu_click(
+            locators['menu.hosts'], locators['menu.hardware_models'],
+        )
+
+    def go_to_architectures(self):
+        self.menu_click(
+            locators['menu.hosts'], locators['menu.architectures'],
+        )
+
+    def go_to_host_groups(self):
+        self.menu_click(
+            locators['menu.configure'], locators['menu.host_groups'],
+        )
+
+    def go_to_global_parameters(self):
+        self.menu_click(
+            locators['menu.configure'], locators['menu.global_parameters'],
+        )
+
+    def go_to_environments(self):
+        self.menu_click(
+            locators['menu.configure'], locators['menu.environments'],
+        )
+
+    def go_to_puppet_classes(self):
+        self.menu_click(
+            locators['menu.configure'], locators['menu.puppet_classes'],
+        )
+
+    def go_to_smart_variables(self):
+        self.menu_click(
+            locators['menu.configure'], locators['menu.'],
+        )
+
+    def go_to_smart_proxies(self):
+        self.menu_click(
+            locators['menu.infrastructure'], locators['menu.smart_proxies'],
+        )
+
+    def go_to_compute_resources(self):
+        self.menu_click(
+            locators['menu.infrastructure'],
+            locators['menu.compute_resources'],
+        )
 
     def go_to_subnets(self):
-        self.go_to_provisioning()
-        self.more_menu_click('Subnets')
+        self.menu_click(
+            locators['menu.infrastructure'], locators['menu.subnets'],
+        )
 
-    def go_to_users_menu(self):
-        self.go_to_more()
-        self.more_menu_move_to('Users')
-        self.more_menu_move_to('LDAP Authentication')
-        self.wait_for_ajax()
+    def go_to_domains(self):
+        self.menu_click(
+            locators['menu.infrastructure'], locators['menu.domains'],
+        )
 
-    def go_to_ldap_authentication(self):
-        self.go_to_users_menu()
-        self.more_menu_click('LDAP Authentication')
+    def go_to_ldap_auth(self):
+        self.menu_click(
+            locators['menu.administer'], locators['menu.ldap_auth'],
+        )
 
     def go_to_users(self):
-        self.go_to_users_menu()
-        self.more_menu_click('Users')
+        self.menu_click(
+            locators['menu.administer'], locators['menu.users'],
+        )
 
     def go_to_user_groups(self):
-        self.go_to_users_menu()
-        self.more_menu_click('User Groups')
+        self.menu_click(
+            locators['menu.administer'], locators['menu.user_groups'],
+        )
 
     def go_to_roles(self):
-        self.go_to_users_menu()
-        self.more_menu_click('Roles')
+        self.menu_click(
+            locators['menu.administer'], locators['menu.roles'],
+        )
+
+    def go_to_bookmarks(self):
+        self.menu_click(
+            locators['menu.administer'], locators['menu.bookmarks'],
+        )
+
+    def go_to_settings(self):
+        self.menu_click(
+            locators['menu.administer'], locators['menu.settings'],
+        )
+
+    def go_to_about(self):
+        self.menu_click(
+            locators['menu.administer'], locators['menu.about'],
+        )
+
+    def go_to_sign_out(self):
+        self.menu_click(
+            locators['menu.account'], locators['menu.sign_out'],
+        )
+
+    def go_to_my_account(self):
+        self.menu_click(
+            locators['menu.account'], locators['menu.my_account'],
+        )
