f32b7dd50d3df7d1cbe40081408512d0cfcde3b4
sghai <sghai@redhat.com>
2013-11-25 19:27:41 +0530

    used hyphen to seperate dic keynames

--- lib/ui/operatingsys.py
+++ lib/ui/operatingsys.py
@@ -19,12 +19,12 @@ class OperatingSys(Base):
         if self.wait_until_element(locators["operatingsys.name"]):
             self.find_element(locators["operatingsys.name"]).send_keys(osname)
         
-            if self.wait_until_element(locators["operatingsys.majorversion"]):
-                self.find_element(locators["operatingsys.majorversion"]).send_keys(major_version)     
+            if self.wait_until_element(locators["operatingsys.major_version"]):
+                self.find_element(locators["operatingsys.major_version"]).send_keys(major_version)     
             
             if minor_version:
-                if self.wait_until_element(locators["operatingsys.minorversion"]):
-                    self.find_element(locators["operatingsys.minorversion"]).send_keys(minor_version)
+                if self.wait_until_element(locators["operatingsys.minor_version"]):
+                    self.find_element(locators["operatingsys.minor_version"]).send_keys(minor_version)
                  
             if os_family:
                 if self.wait_until_element(locators["operatingsys.family"]):
