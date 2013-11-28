4c7c2ed4362609f62ab2cb067cf9e7cbbc8ec325
sghai <sghai@redhat.com>
2013-11-28 21:47:39 +0530

    updated var name from osname to os_name

--- tests/ui/test_Architecture.py
+++ tests/ui/test_Architecture.py
@@ -10,36 +10,36 @@ from time import sleep
 
 class Architecture(BaseUI):
     
-    def create_os(self, osname=None, major_version=None,):
-        osname = osname or generate_name(6)
+    def create_os(self, os_name=None, major_version=None,):
+        os_name = os_name or generate_name(6)
         major_version = major_version or generate_string('numeric', 1)
         self.navigator.go_to_operating_systems() #go to operating system page
-        self.operatingsys.create(osname, major_version)
+        self.operatingsys.create(os_name, major_version)
     
     
     def test_create_arch(self):
         "Create new Arch"
         name = generate_name(4)
-        osname = generate_name(6)
+        os_name = generate_name(6)
         major_version = generate_string('numeric', 1)
         self.login.login(self.katello_user, self.katello_passwd)  # login
-        self.create_os(osname, major_version) #create os
+        self.create_os(os_name, major_version) #create os
         sleep(5)
         self.navigator.go_to_architectures() #go to architecture page
-        self.architecture.create(name, osname)
+        self.architecture.create(name, os_name)
         self.assertTrue(self, self.architecture.search(name)) #search the created arch
          
         
     def test_remove_arch(self):
         "Delete Arch"
         name = generate_name(4)
-        osname = generate_name(6)
+        os_name = generate_name(6)
         major_version = generate_string('numeric',1)
         self.login.login(self.katello_user, self.katello_passwd)  # login
-        self.create_os(osname, major_version)
+        self.create_os(os_name, major_version)
         sleep(5)
         self.navigator.go_to_architectures() #go to architecture page
-        self.architecture.create(name, osname)
+        self.architecture.create(name, os_name)
         self.architecture.remove(name, True)
         
     def test_update_arch(self):
