c4d63c5e1922cf228489fc2ea2f5a35e19ccf96f
sghai <sghai@redhat.com>
2013-11-28 22:27:36 +0530

    something went wrong while rename so manually updating the file

--- tests/ui/test_OperatingSys.py
+++ tests/ui/test_OperatingSys.py
@@ -4,13 +4,32 @@
 
 from baseui import BaseUI
 from lib.ui.locators import *
+from lib.common.helpers import generate_name
+from lib.common.helpers import generate_string
+
 
 class OperatingSys(BaseUI):
-    
+ 
     def test_create_os(self):
         "create new OS"
-        self.login.login(self.katello_user, self.katello_passwd)  # login
+        name = generate_name(6)
+        major_version = generate_string('numeric', 1)
+        minor_version = generate_string('numeric', 1)
+        os_family = "Redhat"
+        self.login.login(self.katello_user, self.katello_passwd) # login
         self.navigator.go_to_operating_systems() #go to operating system page
-        self.operatingsys.create("testOS", "6", "2", "Redhat")
+        self.operatingsys.create(name, major_version, minor_version, os_family)
+        #UI doesn't raise notification - Raise Bug
         #self.assertTrue(self.user.wait_until_element(locators["notif.success"]))
         
+    def test_remove_os(self):
+        "Delete OS "
+        name = generate_name(6)
+        major_version = generate_string('numeric', 1)
+        minor_version = generate_string('numeric', 1)
+        os_family = "Redhat"
+        self.login.login(self.katello_user, self.katello_passwd) # login
+        self.navigator.go_to_operating_systems() #go to operating system page
+        self.operatingsys.create(name, major_version, minor_version, os_family)
+        self.operatingsys.delete(name, really=True)
+        self.assertTrue(self.user.wait_until_element(locators["notif.success"]))
