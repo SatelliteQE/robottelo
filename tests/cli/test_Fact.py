f19ccc5f2729669a61a318d4d4d4bb5706e8954e
Og B. Maciel <ogmaciel@gnome.org>
2013-12-05 09:12:56 -0500

    Support for Hammer CLI fact command.
    * The command hammer fact only exposes the 'list' subcommand;
    * Added 9 data-driven tests for hammer list command;

---
+++ tests/cli/test_Fact.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# vim: ts=4 sw=4 expandtab ai
+
+from ddt import data
+from ddt import ddt
+from lib.cli.fact import Fact
+from lib.common.helpers import generate_name
+from nose.plugins.attrib import attr
+from tests.cli.basecli import BaseCLI
+
+
+@ddt
+class TestFact(BaseCLI):
+    """
+    Fact related tests.
+    """
+
+    @data(
+        'uptime', 'uptime_days', 'uptime_seconds', 'memoryfree', 'ipaddress',
+    )
+    @attr('cli', 'fact')
+    def test_list_success(self, fact):
+        """
+        basic `list` operation test.
+        """
+
+        args = {
+            'search': "fact='%s'" % fact,
+        }
+
+        _ret = Fact(self.conn).list(args)
+        self.assertEqual(_ret[0]['Fact'], fact)
+
+    @data(
+        generate_name(), generate_name(), generate_name(), generate_name(),
+    )
+    @attr('cli', 'fact')
+    def test_list_fail(self, fact):
+        """
+        basic `list` operation test.
+        """
+
+        args = {
+            'search': "fact='%s'" % fact,
+        }
+        self.assertFalse(Fact(self.conn).list(args))
