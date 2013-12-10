c7751fad624dc5739ad86c14deaba75d01f96476
Garik Khachikyan <gkhachik@redhat.com>
2013-12-10 14:06:27 +0100

    done with: #162 Waiting for another PR to become in place for making environment.update return code possible to assert.

--- lib/cli/subnet.py
+++ lib/cli/subnet.py
@@ -2,6 +2,22 @@
 # -*- encoding: utf-8 -*-
 # vim: ts=4 sw=4 expandtab ai
 
+"""
+Usage:
+    hammer subnet [OPTIONS] SUBCOMMAND [ARG] ...
+
+Parameters:
+    SUBCOMMAND                    subcommand
+    [ARG] ...                     subcommand arguments
+
+Subcommands:
+    create                        Create a subnet
+    info                          Show a subnet.
+    list                          List of subnets
+    update                        Update a subnet
+    delete                        Delete a subnet
+
+"""
 from lib.cli.base import Base
 from lib.common.helpers import generate_ipaddr
 from lib.common.helpers import generate_name
