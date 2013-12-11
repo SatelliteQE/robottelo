4a8f5059a7d881aa0c0964002d7c427e2ce8322f
Kedar Bidarkar <kbidarka@redhat.com>
2013-12-11 16:56:24 +0530

    fixes for pep8 and other minor fixes

--- tests/ui/test_template.py
+++ tests/ui/test_template.py
@@ -2,11 +2,13 @@
 # -*- encoding: utf-8 -*-
 # vim: ts=4 sw=4 expandtab ai
 
+import unittest
 from tests.ui.baseui import BaseUI
 
 
 class Template(BaseUI):
 
+    @unittest.skip("Test needs to create other required stuff")
     def test_create_template(self):
         "Create new Template"
         # Some hard coded values just for now to test the CRUD if needed
