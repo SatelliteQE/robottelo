b54a9e49e5cfacaec291dfb7dfaede4ad735326b
Og B. Maciel <omaciel@ogmaciel.com>
2013-12-13 12:08:54 -0500

    Updates to Base CLI and Factory.
    * Updated the Info method to return a single item from list if stdout
      exists or raise an exception if more than one result is returned;
    * Refactored Factory to be a bit more generic and added a new
      make_model method;

--- lib/cli/factory.py
+++ lib/cli/factory.py
@@ -2,51 +2,108 @@
 # -*- encoding: utf-8 -*-
 # vim: ts=4 sw=4 expandtab ai
 
+from lib.cli.model import Model
 from lib.cli.user import User
 from lib.common.helpers import generate_name
+
 import logging
 
 logger = logging.getLogger("robottelo")
 
 
-def make_user(self, override_args=None):
+def update_dictionary(default, updates):
     """
-    @param override_args: override dictionary object which may have one or more
-    of the following key items
-            login
-            firstname
-            lastname
-            mail
-            admin
-            password
-            auth-source-id
-    @return args: dictionary object with all the above key items
+    Updates default dictionary with elements from
+    optional dictionary.
+
+    @param default: A python dictionary containing the minimal
+    required arguments to create a CLI object.
+    @param updates: A python dictionary containing attributes
+    to overwrite on default dictionary.
+
+    @return default: The modified default python dictionary.
     """
+
+    if updates:
+        for key in set(default.keys()).intersection(set(updates.keys())):
+            default[key] = updates[key]
+
+    return default
+
+
+def create_object(cli_object, args):
+    """
+    Creates <object> with dictionary of arguments.
+
+    @param cli_object: A valid CLI object.
+    @param args: A python dictionary containing all valid
+    attributes for creating a new object.
+    @raise Exception: Raise an exception if object cannot be
+    created.
+    """
+
+    result = cli_object().create(args)
+
+    # If the object is not created, raise exception, stop the show.
+    if result.return_code != 0 and not object().exists(result.stdout['Name']):
+        raise Exception("Failed to create object.")
+
+
+def make_model(options=None):
+    """
+    Usage:
+        hammer model create [OPTIONS]
+
+    Options:
+        --name NAME
+        --info INFO
+        --vendor-class VENDOR_CLASS
+        --hardware-model HARDWARE_MODEL
+    """
+
+    args = {
+        'name': generate_name(),
+        'info': None,
+        'vendor-class': None,
+        'hardware-model': None,
+    }
+
+    # Override default dictionary with updated one
+    args = update_dictionary(args, options)
+    create_object(Model, args)
+
+    return args
+
+
+def make_user(options=None):
+    """
+    Usage:
+        hammer user create [OPTIONS]
+
+    Options:
+        --login LOGIN
+        --firstname FIRSTNAME
+        --lastname LASTNAME
+        --mail MAIL
+        --admin ADMIN                 Is an admin account?
+        --password PASSWORD
+        --auth-source-id AUTH_SOURCE_ID
+    """
+
     login = generate_name(6)
 
     #Assigning default values for attributes
     args = {
-            'login': login,
-            'firstname': generate_name(),
-            'lastname': generate_name(),
-            'mail': "%s@example.com" % login,
-            'admin': None,
-            'password': generate_name(),
-            'auth-source-id': 1,
-        }
+        'login': login,
+        'firstname': generate_name(),
+        'lastname': generate_name(),
+        'mail': "%s@example.com" % login,
+        'admin': None,
+        'password': generate_name(),
+        'auth-source-id': 1,
+    }
 
-    #Update the dict with user overrides if any
-    try:
-        if override_args:
-            args_keys = args.keys()
-            for key, val in override_args.items():
-                if key in args_keys:
-                    args[key] = val
-        result = User().create(args)
-        # Check 1 - retcode, Check 2 - checking .exists()
-        if result.return_code == 0 and User().exists(args['login']):
-            return args
-        raise Exception("Failed to create User from factory")
-    except Exception, e:
-            logger.error("ERROR: %s" % str(e))
-            return None
+    args = update_dictionary(args, options)
+    create_object(User, args)
+
+    return args
