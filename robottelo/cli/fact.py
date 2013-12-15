c2352d627b4a8329c42947cba15b9c32f63be78e
Og B. Maciel <omaciel@ogmaciel.com>
2013-12-15 11:16:10 -0500

    Some minor refactoring and lint love for CLI.
    * Handled some much needed pylint issues;
    * Removed python "executor" line from headers;
    * Moved some repeated class methods to base CLI class;
    * Removed robottelo/cli/org.py as it is old and refers to Katello;

--- robottelo/cli/fact.py
+++ robottelo/cli/fact.py
@@ -1,11 +1,29 @@
-#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 # vim: ts=4 sw=4 expandtab ai
+
+"""
+Usage:
+    hammer fact [OPTIONS] SUBCOMMAND [ARG] ...
+
+Parameters:
+    SUBCOMMAND                    subcommand
+    [ARG] ...                     subcommand arguments
+
+Subcommands:
+    list                          List all fact values.
+"""
 
 from robottelo.cli.base import Base
 
 
 class Fact(Base):
+    """
+    Searches Foreman's facts.
+    """
 
     def __init__(self):
+        """
+        Sets the base command for class.
+        """
+        Base.__init__(self)
         self.command_base = "fact"
