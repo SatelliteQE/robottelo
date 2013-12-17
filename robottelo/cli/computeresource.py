404c3e33de3f550af049aa4aca8ce3f367d69128
Garik Khachikyan <gkhachik@redhat.com>
2013-12-17 10:20:56 +0100

    compute_resource cli CRUD: done. #131

---
+++ robottelo/cli/computeresource.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# vim: ts=4 sw=4 expandtab ai
+
+"""
+Usage:
+    hammer compute_resource [OPTIONS] SUBCOMMAND [ARG] ...
+
+Parameters:
+    SUBCOMMAND                    subcommand
+    [ARG] ...                     subcommand arguments
+
+Subcommands:
+    create                        Create a compute resource.
+    info                          Show an compute resource.
+    list                          List all compute resources.
+    update                        Update a compute resource.
+    delete                        Delete a compute resource.
+    image                         View and manage compute resource's images
+"""
+from robottelo.cli.base import Base
+
+
+class ComputeResource(Base):
+
+    def __init__(self):
+        self.command_base = "compute_resource"
--- robottelo/common/constants.py
+++ robottelo/common/constants.py
@@ -3,3 +3,11 @@
 # vim: ts=4 sw=4 expandtab ai
 
 ROBOTTELO_PROPERTIES = "robottelo.properties"
+FOREMAN_PROVIDERS = {
+    'libvirt': 'Libvirt',
+    'ovirt': 'Ovirt',
+    'ec2': 'EC2',
+    'vmware': 'Vmware',
+    'openstack': 'Openstack',
+    'rackspace': 'Rackspace',
+    'gce': 'GCE'}
