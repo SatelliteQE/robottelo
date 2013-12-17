8afaaceaf40e50f5a7de5cbb102f59965eed888e
sghai <sghai@redhat.com>
2013-12-17 12:54:39 +0530

    removed unwanted sleep

--- robottelo/ui/medium.py
+++ robottelo/ui/medium.py
@@ -20,11 +20,17 @@ class Medium(Base):
             if self.wait_until_element(locators["medium.path"]):
                 self.find_element(locators["medium.path"]).send_keys(path)
             if os_family:
-                Select(self.find_element(locators["medium.os_family"])).select_by_visible_text(os_family)
+                Select(self.find_element(locators
+                                         ["medium.os_family"]
+                                         )).select_by_visible_text(os_family)
             self.find_element(locators["submit"]).click()
+            self.wait_for_ajax()
 
     def remove(self, name, really):
-        element = self.wait_until_element((locators["medium.delete"][0], locators["medium.delete"][1] % name))
+        element = self.wait_until_element((locators
+                                           ["medium.delete"][0],
+                                           locators
+                                           ["medium.delete"][1] % name))
         if element:
             element.click()
             if really:
@@ -33,6 +39,7 @@ class Medium(Base):
             else:
                 alert = self.browser.switch_to_alert()
                 alert.dismiss()
+            self.wait_for_ajax()
 
     def search(self, name):
         searchbox = self.wait_until_element(locators["search"])
@@ -40,24 +47,29 @@ class Medium(Base):
             searchbox.clear()
             searchbox.send_keys(name)
             searchbox.send_keys(Keys.RETURN)
-            medium = self.wait_until_element((locators["medium.medium_name"][0], locators["medium.medium_name"][1] % name))
+            medium = self.wait_until_element((locators
+                                              ["medium.medium_name"][0],
+                                              locators
+                                              ["medium.medium_name"][1] % name))
             if medium:
                 medium.click()
         return medium
 
     def update(self, oldname, newname=None, newpath=None, new_os_family=None):
-        element = self.wait_until_element((locators["medium.medium_name"][0], locators["medium.medium_name"][1] % oldname))
+        element = self.wait_until_element((locators
+                                           ["medium.medium_name"][0],
+                                           locators
+                                           ["medium.medium_name"][1] % oldname))
         if element:
             element.click()
         if self.wait_until_element(locators["medium.name"]):
-            txt_field = self.find_element(locators["medium.name"])
-            txt_field.clear()
-            txt_field.send_keys(newname)
+            self.field_update("medium.name", newname)
         if newpath:
             if self.wait_until_element(locators["medium.path"]):
-                txt_field = self.find_element(locators["medium.path"])
-                txt_field.clear()
-                txt_field.send_keys(newpath)
+                self.field_update("medium.path", newpath)
         if new_os_family:
-            Select(self.find_element(locators["medium.os_family"])).select_by_visible_text(new_os_family)
+            Select(self.find_element(locators
+                                     ["medium.os_family"]
+                                     )).select_by_visible_text(new_os_family)
         self.find_element(locators["submit"]).click()
+        self.wait_for_ajax()
