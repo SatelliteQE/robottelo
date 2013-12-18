84652615eb5e3f3d388801c33793928784ace7d1
tkolhar <tazimkolhar@gmail.com>
2013-12-18 14:26:23 +0530

    Added the decorator for BZ ID 1043830 and tried to resolve PEP8 issues

--- tests/cli/test_report.py
+++ tests/cli/test_report.py
@@ -22,20 +22,29 @@ Options:
 from robottelo.cli.report import Report
 from robottelo.common.helpers import generate_id, sleep_for_seconds
 from tests.cli.basecli import BaseCLI
+from robottelo.common.decorators import bzbug
 
 
 class TestReport(BaseCLI):
 
+    @bzbug('1043830')
     def test_info(self):
+        """
+        Displays info for puppet report.
+        """
         id = generate_id() 
-        result = Report().info({'id': id})
+        result = Report().info({'id': 3})
         self.assertEquals(
             len(result.stdout), 1, "Report info - return count"
         )
-        self.assertEquals(result.stdout[0]['ID'], name,
+        self.assertEquals(result.stdout['ID'], name,
                           "Report info - stdout contains 'ID'")
         
+    @bzbug('1043830')   
     def test_delete(self):
+        """
+        Deletes the report generated for puppet.
+        """
         id = generate_id()
         result = Report().delete({'id': id})
         self.assertTrue(result.return_code == 0,
@@ -45,7 +54,11 @@ class TestReport(BaseCLI):
         self.assertTrue(len(result.stdout) == 0,
                         "Report list - does not have deleted id")
         
+    @bzbug('1043830')    
     def test_list(self):
+        """
+        List the report for puppet.
+        """
         id = generate_id()
         result = Report().list({'search': id})
         self.assertTrue(len(result.stdout) == 1,
@@ -59,3 +72,4 @@ class TestReport(BaseCLI):
         result = Report().list({'per-page': id})
         self.assertTrue(len(result.stdout) == 1,
                         "Report list - stdout contains 'ID'")
+           
