7b945b4bf9233d509b41258aece2bf868a176e7e
Blair Morrison <blairmorrison@gmail.com>
2013-12-18 16:44:45 -0500

    PEP8 alphabetizing and newline

--- tests/ui/baseui.py
+++ tests/ui/baseui.py
@@ -29,7 +29,7 @@ from robottelo.ui.user import User
 from robottelo.ui.computeresource import ComputeResource
 from selenium import webdriver
 from selenium_factory.ParseSauceURL import ParseSauceURL
-from selenium_factory.SeleniumFactory import SeleniumFactory, SauceRest
+from selenium_factory.SeleniumFactory import SauceRest, SeleniumFactory
 
 SCREENSHOTS_DIR = os.path.join(
     os.path.abspath(os.path.curdir), 'screenshots')
