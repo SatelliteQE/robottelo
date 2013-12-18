00ebe0835753bbedead93f2a5cd68c6b99db5384
tkolhar <tazimkolhar@gmail.com>
2013-12-18 14:26:23 +0530

    Added the decorator for BZ ID 1043830 and tried to resolve PEP8 issues

--- robottelo/cli/report.py
+++ robottelo/cli/report.py
@@ -1,7 +1,7 @@
-#-*- encoding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 # vim: ts=4 sw=4 expandtab ai
+
 """
-# hammer report --help
 Usage:
     hammer report [OPTIONS] SUBCOMMAND [ARG] ...
 
@@ -10,24 +10,24 @@ Parameters:
     [ARG] ...                     subcommand arguments
 
 Subcommands:
-    info                          Show a report.
-    list                          List all reports.
-    delete                        Delete a report.
+    info                          Show info for report.
+    list                          List reports.
+    delete                        Delete report.
 
-Options:
-    -h, --help                    print help
 """
 
 from robottelo.cli.base import Base
 
+
 class Report(Base):
-      """
-          Report puppet info
-      """
-      
-      def __init__(self):
-          """
-                Sets the base command for class.
-          """
-          Base.__init__(self)
-          self.command_base = "report"
+    """
+    Manipulates Foreman's reports.
+    """
+
+    def __init__(self):
+        """
+        Sets the base command for class.
+        """
+        Base.__init__(self)
+        self.command_base = "report"
+                 
