dacf9bb507f91805422b31aa2de6e760ef5b01a7
Kedar Bidarkar <kbidarka@redhat.com>
2013-12-18 19:02:54 +0530

    Removed all the hardcoding

--- tests/ui/test_computeresource.py
+++ tests/ui/test_computeresource.py
@@ -3,18 +3,21 @@
 # vim: ts=4 sw=4 expandtab ai
 
 import unittest
-from tests.ui.baseui import BaseUI
-from robottelo.ui.locators import locators
+from robottelo.common import conf
 from robottelo.common.helpers import generate_name
+from robottelo.ui.locators import locators
+from tests.ui.baseui import BaseUI
 
 
 class ComputeResource(BaseUI):
 
+    # would require some libvirt configuration on foreman host.
     @unittest.skip("Test needs to create other required stuff")
     def test_create_resource(self):
         "Test to create a new libvirt Compute Resource"
         name = generate_name(8)
-        url = "qemu+tcp://xxx.yyy.com:16509/system"  # conf file needed
+        libvirt_url = "qemu+tcp://%s:16509/system"
+        url = (libvirt_url % conf.properties['main.server.hostname'])
         self.login.login(self.katello_user, self.katello_passwd)
         self.navigator.go_to_compute_resources()
         self.compute_resource.create(name, provider_type="Libvirt", url=url)
@@ -23,7 +26,8 @@ class ComputeResource(BaseUI):
     def test_remove_resource(self):
         "Test to delete a Compute Resource "
         name = generate_name(8)
-        url = "qemu+tcp://xxx.yyy.com:16509/system"  # conf file needed
+        libvirt_url = "qemu+tcp://%s:16509/system"
+        url = (libvirt_url % conf.properties['main.server.hostname'])
         self.login.login(self.katello_user, self.katello_passwd)
         self.navigator.go_to_compute_resources()
         self.compute_resource.create(name, provider_type="Libvirt", url=url)
