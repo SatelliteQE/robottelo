e918c341582a68b2f7123dc4de706b777c6d417c
Adam Saleh <asaleh@redhat.com>
2013-12-18 13:54:34 +0100

    refactored api tests

---
+++ robottelo/api/operating_systems.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+from robottelo.api.apicrud import ApiCrudMixin
+from robottelo.api.model import ApiModelMixin
+from random import randint, choice
+from lib.common.helpers import generate_name
+from lib.common.helpers import generate_email_address
+
+class OperatingSystemApi(ApiCrudMixin,ApiModelMixin):
+    """Implementation of /api/operatingsystems endpoint"""
+    @classmethod
+    def api_path(cls):
+        return "/api/operatingsystems/"
+
+    @classmethod
+    def id_from_json(cls, json):
+        return json[u'operatingsystem'][u'id']
+
+    def __init__(self, generate=False):
+        if generate:
+            self.name = unicode(generate_name(8))
+            self.major = unicode(randint(1, 8))
+            self.minor = unicode(randint(1, 30))
+            self.family = unicode(choice([ "Archlinux",
+                "Debian",
+                "Freebsd",
+                "Gentoo",
+                "Redhat",
+                "Solaris",
+                "Suse",
+                "Windows" ]))
+            self.release_name = unicode(generate_name(8))
+
+    def opts(self):
+        return {u'operatingsystem':self.to_json()}
+
+    def change(self):
+        self.release_name += "updated"
+        return self
---
+++ tests/api/positive_crud_tests.py
@@ -0,0 +1,65 @@
+# -*- encoding: utf-8 -*-
+
+class PositiveCrudTestMixin:
+    """Implements basic crud tests based oon supplied test class
+    implementing ApiCrudMixin and ApiModelMixin
+
+    """
+    def tested_class(self):
+        """Returns class implementing both ApiCrudMixin and ApiModelMixin"""
+        raise NotImplementedError("Tested class needs to be defined")
+
+    def post_result(self):
+        """Override might be required for some endpoint,
+        i.e POST /api/models returns 201 on success"""
+        return 200
+
+    def test_create(self):
+        """Basic positive create test"""
+        ModelApi = self.tested_class()
+        model = ModelApi(generate=True)
+        response = ModelApi.create(json=model.opts())
+        self.assertEqual(response.status_code, self.post_result())
+        self.assertFeaturing(model.result_opts(), response.json())
+
+    def test_read_model(self):
+        """Basic positive read test"""
+        ModelApi = self.tested_class()
+        model = ModelApi(generate=True)
+        response = ModelApi.create(json=model.opts())
+        uid = ModelApi.id_from_json(response.json())
+
+        response = ModelApi.show(uid=uid)
+        self.assertEqual(response.status_code, 200)
+        self.assertFeaturing(model.result_opts(), response.json())
+
+    def test_read_model_negative(self):
+        """We presume that on wrong id, api should return 404"""
+        ModelApi = self.tested_class()
+        response = ModelApi.show(uid=-1)
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_update_model(self):
+        """Basic positive update test"""
+        ModelApi = self.tested_class()
+        model = ModelApi(generate=True)
+        response = ModelApi.create(json=model.opts())
+        uid = ModelApi.id_from_json(response.json())
+        model = model.change()
+
+        response = ModelApi.update(uid=uid,json=model.opts())
+        self.assertEqual(response.status_code, 200)
+        self.assertFeaturing(model.result_opts(), response.json())
+
+    def test_remove_model(self):
+        """Basic remove test"""
+        ModelApi = self.tested_class()
+        model = ModelApi(generate=True)
+        response = ModelApi.create(json=model.opts())
+        uid = ModelApi.id_from_json(response.json())
+        response = ModelApi.delete(uid=uid)
+        self.assertEqual(response.status_code, 200)
+        response = ModelApi.show(uid=uid)
+        self.assertEqual(response.status_code, 404)
+
