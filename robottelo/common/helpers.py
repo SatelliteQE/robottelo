617f214580622e765d91281cd9cad23322a1307f
tkolhar <tazimkolhar@gmail.com>
2013-12-19 15:12:47 +0530

    Added the doc string and fixed the space issue

--- robottelo/common/helpers.py
+++ robottelo/common/helpers.py
@@ -32,19 +32,25 @@ def generate_name(minimum=4, maximum=8):
 
     return name
 
-def generate_id(min=4, max=8):
 
-    if min <= 0:
-        min = 4
-    if max < min:
-        max = min
+def generate_id(minimum=4, maximum=8):
+    """
+    Generates a unique id for reports
+    """
 
-    r = random.SystemRandom()
+    if minimum <= 0:
+        minimum = 4
+    if maximum < minimum:
+        maximum = minimum
+
+    rand = random.SystemRandom()
     pool1 = string.digits
-    
-    id = str().join(r.choice(pool1) for x in range(random.randint(min, max)))
-    
+
+    name = str().join(
+        rand.choice(pool1) for x in range(random.randint(minimum, maximum)))
+
     return id
+
 
 def generate_email_address(name_length=8, domain_length=6):
     """
