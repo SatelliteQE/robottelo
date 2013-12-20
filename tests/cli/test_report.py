d588f4bb2a357f70e1d1a643ee7dc6ff0657daf8
tkolhar <tazimkolhar@gmail.com>
2013-12-20 19:04:15 +0530

    Added the hammer report scenario as per discussed

--- tests/cli/test_report.py
+++ tests/cli/test_report.py
@@ -19,57 +19,68 @@ Subcommands:
 Options:
     -h, --help                    print help
 """
-from robottelo.cli.report import Report
-from robottelo.common.helpers import generate_id, sleep_for_seconds
+
 from tests.cli.basecli import BaseCLI
 from robottelo.common.decorators import bzbug
+from robottelo.common import ssh
 
 
 class TestReport(BaseCLI):
 
-    @bzbug('1043830')
+    def test_list(self):
+        """
+        Displays list for puppet report.
+        """
+
+        result = ssh.command('puppet agent -t')
+        self.assertEqual(result.return_code, 0)
+        self.assertFalse(result.stderr)
+        result = ssh.command('hammer report list --search')
+        self.assertEqual(result.return_code, 0)
+        self.assertFalse(result.stderr)
+        result = ssh.command('hammer report list --order')
+        self.assertEqual(result.return_code, 0)
+        self.assertFalse(result.stderr)
+        result = ssh.command('hammer report list --page')
+        self.assertEqual(result.return_code, 0)
+        self.assertFalse(result.stderr)
+        result = ssh.command('hammer report list --per-page')
+        self.assertEqual(result.return_code, 0)
+        self.assertFalse(result.stderr)
+
     def test_info(self):
         """
         Displays info for puppet report.
         """
 
-        id = generate_id()
-        result = Report().info({'id': id})
-        self.assertEquals(
-            len(result.stdout), 1, "Report info - return count")
-        self.assertEquals(result.stdout['ID'], name,
-                          "Report info - stdout contains 'ID'")
+        result = ssh.command('puppet agent -t')
+        self.assertEqual(result.return_code, 0)
+        self.assertFalse(result.stderr)
+        result = ssh.command('hammer report list')
+        self.assertEqual(result.return_code, 0)
+        self.assertFalse(result.stderr)
+        output = result.stdout
+        index = [c.strip() for c in output[1].split('|')].index('ID')
+        ids = [line.split('|')[index].strip() for line in output[3:-1]]
+        result = ssh.command('hammer report info --id ' + ids[0])
+        self.assertEqual(result.return_code, 0)
+        self.assertFalse(result.stderr)
 
-    @bzbug('1043830')
+    @bzbug(1044408)
     def test_delete(self):
         """
-        Deletes the report generated for puppet.
+        Delete a puppet report.
         """
 
-        id = generate_id()
-        result = Report().delete({'id': id})
-        self.assertTrue(result.return_code == 0, "Report delete - retcode")
-        sleep_for_seconds(5)  # sleep for about 5 sec.
-        result = Report().list({'search': id})
-        self.assertTrue(len(result.stdout) == 0,
-                        "Report list - does not have deleted id")
-
-    @bzbug('1043830')
-    def test_list(self):
-        """
-        List the report for puppet.
-        """
-
-        id = generate_id()
-        result = Report().list({'search': id})
-        self.assertTrue(len(result.stdout) == 1,
-                        "Report list - stdout contains 'ID'")
-        result = Report().list({'order': id})
-        self.assertTrue(len(result.stdout) == 1,
-                        "Report list - stdout contains 'ID'")
-        result = Report().list({'page': id})
-        self.assertTrue(len(result.stdout) == 1,
-                        "Report list - stdout contains 'ID'")
-        result = Report().list({'per-page': id})
-        self.assertTrue(len(result.stdout) == 1,
-                        "Report list - stdout contains 'ID'")
+        result = ssh.command('puppet agent -t')
+        self.assertEqual(result.return_code, 0)
+        self.assertFalse(result.stderr)
+        result = ssh.command('hammer report list')
+        self.assertEqual(result.return_code, 0)
+        self.assertFalse(result.stderr)
+        output = result.stdout
+        index = [c.strip() for c in output[1].split('|')].index('ID')
+        ids = [line.split('|')[index].strip() for line in output[3:-1]]
+        result = ssh.command('hammer report delete --id ' + ids[0])
+        self.assertEqual(result.return_code, 0)
+        self.assertFalse(result.stderr)
