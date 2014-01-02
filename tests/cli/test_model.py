78141ffa4cb9b301a77cd73d7c5bca2c56723364
Og B. Maciel <omaciel@ogmaciel.com>
2014-01-02 17:04:23 -0500

    Adding default tests to all CLI tests via MetaClass.
    * All CLI tests will inherit some default tests from the base class:
      * test_positive_create
      * test_positive_delete (TBD)
      * test_positive_update (TBD)
      * test_negative_create (TBD)
      * test_negative_delete (TBD)
      * test_negative_update (TBD)

--- tests/cli/test_model.py
+++ tests/cli/test_model.py
@@ -6,24 +6,32 @@ Test class for Model CLI
 """
 
 from basecli import BaseCLI
-from robottelo.cli.factory import make_model
+from ddt import ddt
+from metatest import MetaCLITest
 from robottelo.cli.model import Model
+from robottelo.cli.factory import make_model
 from robottelo.common.helpers import generate_name
 
 
+@ddt
 class TestModel(BaseCLI):
+
+    __metaclass__ = MetaCLITest
+
+    factory = make_model
+    factory_obj = Model
 
     def test_create_model_1(self):
         """Successfully creates a new model"""
 
-        result = make_model()
+        result = self.create()
         model = Model().info({'name': result['name']})
         self.assertEqual(result['name'], model.stdout['name'])
 
     def test_create_model_2(self):
         """Create model with specific vendor class"""
 
-        result = make_model({'vendor-class': generate_name()})
+        result = self.create({'vendor-class': generate_name()})
         # Check that Model was created with proper values
         model = Model().info({'name': result['name']})
         self.assertEqual(result['vendor-class'], model.stdout['vendor-class'])
