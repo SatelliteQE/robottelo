002b129620f77f52e346a8bbe50640940547ac84
Kedar Bidarkar <kbidarka@redhat.com>
2014-01-03 16:45:58 +0530

    Updated resources CRUD with update function

--- tests/ui/test_computeresource.py
+++ tests/ui/test_computeresource.py
@@ -25,6 +25,22 @@ class ComputeResource(BaseUI):
                                               locators["resource.select_name"])
         self.assertIsNotNone(search)
 
+    def test_update_resource(self):
+        "Test to update existing libvirt Compute Resource"
+        name = generate_name(8)
+        newname = generate_name(8)
+        libvirt_url = "qemu+tcp://%s:16509/system"
+        url = (libvirt_url % conf.properties['main.server.hostname'])
+        self.login.login(self.katello_user, self.katello_passwd)
+        self.navigator.go_to_compute_resources()
+        self.compute_resource.create(name, provider_type="Libvirt", url=url)
+        self.navigator.go_to_compute_resources()
+        self.compute_resource.update(name, newname, libvirt_set_passwd=False)
+        self.navigator.go_to_compute_resources()
+        search = self.compute_resource.search(newname,
+                                              locators["resource.select_name"])
+        self.assertIsNotNone(search)
+
     @unittest.skip("Test needs to create other required stuff")
     def test_remove_resource(self):
         "Test to delete a Compute Resource "
