33b3c88d913e74ca62965acef2768b6d5668fd4d
Og B. Maciel <omaciel@ogmaciel.com>
2014-01-08 17:12:20 -0500

    Added positive delete tests to metatest class.

--- robottelo/cli/metatest/template_methods.py
+++ robottelo/cli/metatest/template_methods.py
@@ -11,7 +11,7 @@ def test_positive_create(self, data):
     # Dict keys start with upper case
     _dict_key = self.search_key[0].upper() + self.search_key[1:]
     self.assertEqual(new_obj[self.search_key], result.stdout[_dict_key])
-    self.assertEqual(result.return_code, 0)
+    self.assertTrue(result.return_code == 0, "Failed to create object")
     self.assertTrue(len(result.stderr) == 0)
 
 
@@ -34,7 +34,26 @@ def test_negative_update(self, data):
 
 
 def test_positive_delete(self, data):
-    pass
+    """
+    Successfully deletes an object
+    """
+
+    new_obj = self.factory(data)
+    result = self.factory_obj().info(
+        {self.search_key: new_obj[self.search_key]})
+    self.assertTrue(result.return_code == 0, "Failed to create object")
+
+    # Now delete it...
+    result = self.factory_obj().delete(
+        {self.search_key: new_obj[self.search_key]})
+    self.assertTrue(result.return_code == 0, "Failed to delete object")
+    self.assertTrue(len(result.stderr) == 0)
+    # ... and make sure it does not exist anymore
+    result = self.factory_obj().info(
+        {self.search_key: new_obj[self.search_key]})
+    self.assertFalse(result.return_code == 0, "Return code should not be zero")
+    self.assertTrue(len(result.stderr) > 0, "Should have gotten an error")
+    self.assertEqual(result.stdout, [], "Should not get any output")
 
 
 def test_negative_delete(self, data):
