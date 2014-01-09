1fe96a3c465728abb1b0883979927ccf91841b4e
tkolhar <tazimkolhar@gmail.com>
2014-01-09 19:32:07 +0530

    Fixed the os tests

--- tests/cli/test_os.py
+++ tests/cli/test_os.py
@@ -11,6 +11,9 @@ from tests.cli.basecli import BaseCLI
 
 
 class TestOperatingSystem(BaseCLI):
+    """
+    Test class for Operating System CLI.
+    """
 
     def test_create(self):
         """
@@ -27,8 +30,16 @@ class TestOperatingSystem(BaseCLI):
         """
         Displays list for operating system.
         """
-
         result = OperatingSys().list()
+        self.assertEqual(result.return_code, 0)
+        length = len(result.stdout)
+        result = OperatingSys().create({'name': generate_name(),
+                                        'major': 1, 'minor': 1})
+        self.assertTrue(result.return_code == 0,
+                        "Operating system create - retcode")
+        self.assertEqual(result.return_code, 0)
+        result = OperatingSys().list()
+        self.assertTrue(len(result.stdout) > length)
         self.assertEqual(result.return_code, 0)
 
     def test_info(self):
@@ -47,11 +58,21 @@ class TestOperatingSystem(BaseCLI):
         """
         Displays delete for operating system.
         """
-
+        name = generate_name()
+        report = []
+        result = OperatingSys().create({'name': name,
+                                        'major': 1, 'minor': 1})
+        self.assertTrue(result.return_code == 0,
+                        "Operating system create - retcode")
+        self.assertEqual(result.return_code, 0)
         result = OperatingSys().list()
+        for i in result.stdout:
+            nm = name + " 1.1"
+            if nm == i['Name']:
+                    report.append(i)
+
         # Grab a random report
-        report = random.choice(result.stdout)
-        result = OperatingSys().delete({'id': report['Id']})
+        result = OperatingSys().delete({'id': report[0]['Id']})
         self.assertEqual(result.return_code, 0)
 
     def test_update(self):
@@ -59,10 +80,21 @@ class TestOperatingSystem(BaseCLI):
          Displays update for operating system.
         """
 
-        result = OperatingSys().list()
-        # Grab a random report
-        report = random.choice(result.stdout)
-        result = OperatingSys().update({'id': report['Id'], 'major': 3})
+        name = generate_name()
+        report = []
+        result = OperatingSys().create({'name': name,
+                                        'major': 1, 'minor': 1})
+        self.assertTrue(result.return_code == 0,
+                        "Operating system create - retcode")
         self.assertEqual(result.return_code, 0)
-        result = OperatingSys().info({'id': report['Id']})
+        result = OperatingSys().list()
+        for i in result.stdout:
+            nm = name + " 1.1"
+            if nm == i['Name']:
+                    report.append(i)
+
+        # Grab a random report
+        result = OperatingSys().update({'id': report[0]['Id'], 'major': 3})
+        self.assertEqual(result.return_code, 0)
+        result = OperatingSys().info({'id': report[0]['Id']})
         self.assertEqual(result.return_code, 0)
