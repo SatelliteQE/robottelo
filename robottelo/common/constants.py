265c290da41204ebf29ef6079c26d54b76fced89
Og B. Maciel <omaciel@ogmaciel.com>
2014-01-10 12:10:16 -0500

    Generate a random HTML string for data input.
    Modified generate_string function to include an option that returns a
    very simple HTML string. Right now the string generated is made up of
    a single HTML tag surrounding a random alpha string. It would be nice
    to eventually have a better generator to give us more 'advanced'
    strings such as '<img src="http://example.com" alt="foo" />', but I
    guess this is good for now.

--- robottelo/common/constants.py
+++ robottelo/common/constants.py
@@ -25,3 +25,18 @@ TEMPLATE_TYPES = [
     'PXEGrub',
     'snippet',
 ]
+
+HTML_TAGS = [
+    'A', 'ABBR', 'ACRONYM', 'ADDRESS', 'APPLET', 'AREA', 'B',
+    'BASE', 'BASEFONT', 'BDO', 'BIG', 'BLINK', 'BLOCKQUOTE', 'BODY', 'BR',
+    'BUTTON', 'CAPTION', 'CENTER', 'CITE', 'CODE', 'COL', 'COLGROUP',
+    'DD', 'DEL', 'DFN', 'DIR', 'DIV', 'DL', 'DT',
+    'EM', 'FIELDSET', 'FONT', 'FORM', 'FRAME', 'FRAMESET', 'H1',
+    'H2', 'H3', 'H4', 'H5', 'H6', 'HEAD', 'HR',
+    'HTML', 'I', 'IFRAME', 'IMG', 'INPUT', 'INS', 'ISINDEX',
+    'KBD', 'LABEL', 'LEGEND', 'LI', 'LINK', 'MAP', 'MENU',
+    'META', 'NOFRAMES', 'NOSCRIPT', 'OBJECT', 'OL', 'OPTGROUP', 'OPTION',
+    'P', 'PARAM', 'PRE', 'Q', 'S', 'SAMP', 'SCRIPT',
+    'SELECT', 'SMALL', 'SPAN', 'STRIKE', 'STRONG', 'STYLE', 'SUB',
+    'SUP', 'TABLE', 'TBODY', 'TD', 'TEXTAREA', 'TFOOT', 'TH',
+    'THEAD', 'TITLE', 'TR', 'TT', 'U', 'UL', 'VAR']
