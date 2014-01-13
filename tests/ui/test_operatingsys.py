d31de1d6ed93d7de784229d6611ce33a1eb064c1
sthirugn <sthirugn@redhat.com>
2014-01-13 17:06:23 -0500

    Fixed Org locators issue identified in automation job and removed an unused import in test_operatingsys.py

--- tests/ui/test_operatingsys.py
+++ tests/ui/test_operatingsys.py
@@ -5,8 +5,6 @@
 Test class for Operating System UI
 """
 
-import unittest
-
 from robottelo.ui.locators import locators
 from robottelo.ui.locators import common_locators
 from robottelo.common.helpers import generate_name
