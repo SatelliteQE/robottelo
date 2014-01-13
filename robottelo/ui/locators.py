59bc7f78981a1760e6485334d3e1e94e11800da9
Kedar Bidarkar <kbidarka@redhat.com>
2014-01-14 02:59:27 +0530

    Moved manage_org locator under menu_locator dict

--- robottelo/ui/locators.py
+++ robottelo/ui/locators.py
@@ -131,7 +131,10 @@ menu_locators = {
     # Orgs
     "org.manage_org": (
         By.XPATH,
-        "//div[contains(@style,'static')]/div/ul/li/ul/li/a[@class='manage-menu' and contains(@href, '/organizations')]")}  # @IgnorePep8
+        "//div[contains(@style,'static')]/div/ul/li/ul/li/a[@class='manage-menu' and contains(@href, '/organizations')]"),
+    "org.any_context": (
+        By.XPATH,
+        "//div[contains(@style,'static')]/div/ul/li[@class='dropdown org-switcher menu_tab_dropdown']/a[@class='dropdown-toggle']")}   # @IgnorePep8
 
 tab_locators = {
 
@@ -211,9 +214,6 @@ locators = {
     "login.gravatar": (By.XPATH, "//img[contains(@class, 'gravatar')]"),
 
     # Organizations
-    "org.any_context": (
-        By.XPATH,
-        "//div[contains(@style,'static')]/div/ul/li[@class='dropdown org-switcher menu_tab_dropdown']/a[@class='dropdown-toggle']"),   # @IgnorePep8
     "org.new": (
         By.XPATH,
         "//a[@class='btn btn-success' and contains(@href, '/organizations/new')]"),  # @IgnorePep8
