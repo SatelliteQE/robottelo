50ca1f5c637c171ce043753432425dc8ee8fcb72
Kedar Bidarkar <kbidarka@redhat.com>
2014-01-16 21:54:05 +0530

    Added org support to envs module and few tests

--- tests/ui/test_environment.py
+++ tests/ui/test_environment.py
@@ -5,8 +5,6 @@
 Test class for Environment UI
 """
 
-import unittest
-
 from tests.ui.baseui import BaseUI
 from robottelo.ui.locators import common_locators
 from robottelo.common.helpers import generate_name
@@ -14,23 +12,48 @@ from robottelo.common.helpers import generate_name
 
 class Environment(BaseUI):
 
+    def create_org(self, org_name=None):
+        """Creates Org"""
+        org_name = org_name or generate_name(8, 8)
+        self.navigator.go_to_org()  # go to org page
+        self.org.create(org_name)
+
     def test_create_env(self):
         "create new Environment"
         name = generate_name(6)
+        org_name = generate_name(8, 8)
         self.login.login(self.katello_user, self.katello_passwd)
+        self.create_org(org_name)
         self.navigator.go_to_environments()
-        self.environment.create(name)
-        # TODO: assertion is pending Foreman issue #3826
-        #search = self.environment.search(name, locators["env.env_name"])
-        #self.assertIsNotNone(search)
+        self.environment.create(name, [org_name])
+        search = self.environment.search(name)
+        self.assertIsNotNone(search)
 
-    @unittest.skip("http://projects.theforeman.org/issues/3826")
+    def test_update_env(self):
+        "Updates an existing Environment"
+        name = generate_name(6)
+        new_name = generate_name(6)
+        org_name = generate_name(8, 8)
+        new_org = generate_name(8, 8)
+        self.login.login(self.katello_user, self.katello_passwd)
+        self.create_org(org_name)
+        self.navigator.go_to_environments()
+        self.environment.create(name, [org_name])
+        self.create_org(new_org)
+        self.navigator.go_to_environments()
+        self.environment.update(name, [org_name], [new_org],
+                                new_name=new_name)
+        search = self.environment.search(new_name)
+        self.assertIsNotNone(search)
+
     def test_remove_env(self):
         "Delete an Environment "
         name = generate_name(6)
+        org_name = generate_name(8, 8)
         self.login.login(self.katello_user, self.katello_passwd)
+        self.create_org(org_name)
         self.navigator.go_to_environments()
-        self.environment.create(name)
+        self.environment.create(name, [org_name])
         self.environment.delete(name, really=True)
         notif = self.user.wait_until_element(common_locators["notif.success"])
         self.assertTrue(notif)
