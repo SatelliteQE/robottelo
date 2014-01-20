e9f211ad7ed4bc990d3764c7353f3f5ed5f92ca9
Elyézer Mendes Rezende <elyezermr@gmail.com>
2014-01-20 18:23:48 -0200

    Add redminebug decorator to create proxy test

--- tests/cli/test_proxy.py
+++ tests/cli/test_proxy.py
@@ -5,16 +5,15 @@
 Test class for Smart proxy CLI
 """
 
-import unittest
-
-from basecli import BaseCLI
+from tests.cli.basecli import BaseCLI
 from robottelo.cli.factory import make_proxy
 from robottelo.cli.proxy import Proxy
+from robottelo.common.decorators import redminebug
 
 
 class TestProxy(BaseCLI):
 
-    @unittest.skip("skipped due to http://projects.theforeman.org/issues/3875")
+    @redminebug('3875')
     def test_create_proxy(self):
         """Create a new proxy"""
 
