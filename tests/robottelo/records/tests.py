ac09ceb1387b13ccae8d7db568ea46ca42a3cc91
Elyézer Mendes Rezende <elyezermr@gmail.com>
2014-01-29 14:12:56 -0200

    Add records engine

---
+++ tests/robottelo/records/tests.py
@@ -0,0 +1,46 @@
+import unittest
+
+from .records import SampleRecord
+
+
+class RecordsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.record_class = SampleRecord
+
+    def test_has_meta_attr(self):
+        """Record has the _meta attr"""
+        self.assertTrue(hasattr(self.record_class, '_meta'))
+
+    def test_attr_has_random_value(self):
+        """Record instance generates a random value for a field"""
+        instance = self.record_class()
+        self.assertTrue(
+            instance.name.startswith(self.record_class.__name__))
+
+    def test_can_set_attr(self):
+        """Can set a field attr value"""
+        instance = self.record_class()
+        instance.name = 'New Name'
+        self.assertTrue(instance.name == 'New Name')
+
+    def test_can_set_attr_on_constructor(self):
+        """Can set a field attr value on instantiation"""
+        instance = self.record_class(name='New Name')
+        self.assertTrue(instance.name == 'New Name')
+
+    def test_field_default_value(self):
+        """Field default value is used if value is not passed on constructor"""
+        instance = self.record_class()
+        self.assertEqual(instance.field_with_default, 'mydefault')
+        instance = self.record_class(field_with_default='othervalue')
+        self.assertEqual(instance.field_with_default, 'othervalue')
+
+    def test_field_default_callable(self):
+        """Field default value as callable is resolved"""
+        instance = self.record_class()
+        self.assertEqual(instance.callable_default, 'defaultfromcallable')
+
+    def test_record_post_init_hook(self):
+        """Post init is executed"""
+        instance = self.record_class()
+        self.assertTrue(hasattr(instance, 'post_init_var'))
