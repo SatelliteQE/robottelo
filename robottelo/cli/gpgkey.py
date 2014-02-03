2b06d534f75fcfe5b7da3a740666a7f59033ddf4
Og B. Maciel <omaciel@ogmaciel.com>
2014-02-03 15:59:01 -0500

    Adding support for GPG keys via hammer cli.
    Added support for GPG keys via Hammer CLI, including a factory method.
    The following pieces of imformation are important to take into
    consideration when writing the tests:
    
    * Most, if not all, subcommands that are derived from Katello will
      require ``organization-id`` as an argument for some of its actions.
      For instance, ``hammer gpg list`` will *not* give you a list of all
      available gpg keys in the system, but only those keys for the provided
      ``organization-id``.
    * Searching for Katello subcommands follows *ElasticSearch* syntax. So,
      instead of:
    
    ```bash
    $ hammer gpg list --search name='foo'
    ```
    
    You have to use:
    
    ```bash
    $ hammer gpg list --search name:'foo'
    ```
    
    * The following issues have been found and filed:
      * http://projects.theforeman.org/issues/4272
      * http://projects.theforeman.org/issues/4271
      * http://projects.theforeman.org/issues/4263
      * http://projects.theforeman.org/issues/4262

---
+++ robottelo/cli/gpgkey.py
@@ -0,0 +1,71 @@
+# -*- encoding: utf-8 -*-
+# vim: ts=4 sw=4 expandtab ai
+
+"""
+Usage:
+    hammer gpg [OPTIONS] SUBCOMMAND [ARG] ...
+
+Parameters:
+    SUBCOMMAND                    subcommand
+    [ARG] ...                     subcommand arguments
+
+Subcommands:
+    list                          List GPG Keys
+    info                          Show a GPG key
+    create                        Create a GPG Key
+    update                        Update a GPG Key
+    delete                        Destroy a GPG Key
+"""
+
+from robottelo.cli.base import Base
+
+
+class GPGKey(Base):
+    """
+    Manipulates Foreman's GPG Keys.
+    """
+
+    def __init__(self):
+        """
+        Sets the base command for class.
+        """
+        Base.__init__(self)
+        self.command_base = "gpg"
+
+    def exists(self, organization_id, tuple_search=None):
+        """
+        Search for a GPG Key.
+        """
+
+        # Katello subcommands require the organization-id
+        options = {}
+
+        if tuple_search:
+            search_criteria = "%s:\"%s\"" % (tuple_search[0], tuple_search[1])
+            options["search"] = search_criteria
+        # Katello subcommands require extra arguments such as organization-id
+
+        result = self.list(organization_id, options)
+
+        if result.stdout:
+            result.stdout = result.stdout[0]
+
+        return result
+
+    def list(self, organization_id,  options=None):
+        """
+        Lists available GPG Keys.
+        """
+        self.command_sub = "list"
+
+        if options is None:
+            options = {}
+            options['per-page'] = 10000
+
+        # Katello subcommands require the organization-id
+        options = {"organization-id": organization_id}
+
+        result = self.execute(self._construct_command(options),
+                              expect_csv=True)
+
+        return result
