a52642ba52e9ff0a27e2cc64a9ce2fed5d8065ee
Blair Morrison <blairmorrison@gmail.com>
2014-02-03 15:41:01 -0500

    PEP8 clean-up

--- robottelo/ui/base.py
+++ robottelo/ui/base.py
@@ -10,7 +10,6 @@ from robottelo.ui.locators import locators, common_locators
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
