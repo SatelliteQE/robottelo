469b87d9274e9646726b75dfdd117c98d99b3585
sthirugn <sthirugn@redhat.com>
2014-02-04 15:52:53 -0500

    1. Renamed current test_user.py to test_user_new.py 2. Created new test_user.py for new User test case stubs in the new docstring format 3. Once the test stubs are created, the tests in test_user_new.py will be moved to test_user.py. Then test_user_new.py will be deleted

--- tests/ui/test_user.py
+++ tests/ui/test_user.py
@@ -2,91 +2,32 @@
 # vim: ts=4 sw=4 expandtab ai
 
 """
-Test class for User UI
+Test class for Users UI
 """
 
+from robottelo.common.constants import NOT_IMPLEMENTED
 from tests.ui.baseui import BaseUI
-from robottelo.ui.locators import common_locators
-from robottelo.common.helpers import generate_name
-from robottelo.common.helpers import generate_email_address
 
 
 class User(BaseUI):
     """
-    Implements User tests from UI
+    Implements Users tests in UI
+
+    [1] Positive Name variations - Alpha, Numeric, Alphanumeric, Symbols,
+    Latin1, Multibyte, Max length,  Min length, Max_db_size, html, css,
+    javascript, url, shell commands, sql, spaces in name
+    [2] Negative Name Variations -  Blank, Greater than Max Length,
+    Lesser than Min Length, Greater than Max DB size
     """
 
-    def create_user(self, name=None, password=None,
-                    email=None, search_key=None):
+    def test_positive_create_user_1(self):
         """
-        Function to create a new User
+        @Feature: User - Positive Create
+        @Test: Create User for all variations of Username
+        @Steps:
+        1. Create User for all valid Username variation
+        in [1] using valid First Name, Surname, Email Address, Language, authorized by
+        @Assert: Activation key is created
+        @Status: Manual
         """
-
-        name = name or generate_name(8)
-        password = password or generate_name(8)
-        email = email or generate_email_address()
-        self.navigator.go_to_users()
-        self.user.create(name, email, password, password)
-        self.assertIsNotNone(self.user.search(name, search_key))
-
-    def test_create_user(self):
-        """
-        Create a new User
-        """
-
-        name = generate_name(6)
-        password = generate_name(8)
-        email = generate_email_address()
-        search_key = "login"
-        self.login.login(self.katello_user, self.katello_passwd)
-        self.create_user(name, password, email, search_key)
-
-    def test_delete_user(self):
-        """
-        Create and Delete a User
-        """
-
-        name = generate_name(6)
-        password = generate_name(8)
-        email = generate_email_address()
-        search_key = "login"
-        self.login.login(self.katello_user, self.katello_passwd)  # login
-        self.create_user(name, password, email, search_key)
-        self.user.delete(name, search_key, really=True)
-        self.assertTrue(self.user.wait_until_element(common_locators
-                                                     ["notif.success"]))
-
-    def test_update_password(self):
-        """
-        Creates a User and updates the password
-        """
-
-        name = generate_name(6)
-        password = generate_name(8)
-        new_password = generate_name(8)
-        email = generate_email_address()
-        search_key = "login"
-        self.login.login(self.katello_user, self.katello_passwd)  # login
-        self.create_user(name, password, email, search_key)
-        self.user.update(search_key, name, None, None, new_password)
-        self.login.logout()
-        self.login.login(name, new_password)
-        self.assertTrue(self.login.is_logged())
-
-    def test_update_role(self):
-        """
-        Creates a User and updates the password
-        """
-
-        name = generate_name(6)
-        password = generate_name(8)
-        email = generate_email_address()
-        role = generate_name(6)
-        search_key = "login"
-        self.login.login(self.katello_user, self.katello_passwd)  # login
-        self.navigator.go_to_roles()
-        self.role.create(role)
-        self.assertIsNotNone(self, self.role.search(role))
-        self.create_user(name, password, email, search_key)
-        self.user.update(search_key, name, new_roles=[role])
-        #TODO assert newly added role/permissions for user
+        self.fail(NOT_IMPLEMENTED)
