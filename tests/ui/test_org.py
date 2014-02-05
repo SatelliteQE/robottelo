b2b545d8f7e289e005541c55667ffcdb69c11739
Kedar Bidarkar <kbidarka@redhat.com>
2014-02-05 18:48:22 +0530

    Added org-selection support
    performs org selection
    
    javascript does the clicking for dynamic select org locator
    
    select-org:Fixed some pep8 issues
    
    Fixed the menu_click & go_to_select_org funcs in navigator module
    
    select-org: Fixes for travis
    
    Added exception if org slection fails

--- tests/ui/test_org.py
+++ tests/ui/test_org.py
@@ -4,7 +4,6 @@
 """
 Test class for Organization UI
 """
-
 from robottelo.common.helpers import generate_name
 from tests.ui.baseui import BaseUI
 
@@ -26,6 +25,8 @@ class Org(BaseUI):
         org_name = generate_name(8, 8)
         self.login.login(self.katello_user, self.katello_passwd)
         self.create_org(org_name)
+        select_org = self.navigator.go_to_select_org(org_name)
+        self.assertIsNotNone(select_org)
         self.assertIsNotNone(
             self.org.search(org_name))
 
