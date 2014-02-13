174133ffef7c17e763c753921f2538bf47cc82ca
sghai <sghai@redhat.com>
2014-02-13 15:50:41 +0530

    positive update activation keys tests and fn

--- robottelo/common/constants.py
+++ robottelo/common/constants.py
@@ -85,4 +85,6 @@ SNIPPET_URL = 'https://gist.github.com/sghai/8434467/raw'
 
 INSTALL_MEDIUM_URL = "http://mirror.fakeos.org/%s/$major.$minor/os/$arch"
 
+ENVIRONMENT = "Library"
+
 NOT_IMPLEMENTED = 'Test not implemented'
