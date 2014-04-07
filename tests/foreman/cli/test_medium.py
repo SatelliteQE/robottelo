534a3806107ec62f9afdbe28af27ad4ce16df72a
tkolhar <tazimkolhar@gmail.com>
2014-04-07 10:43:24 +0530

    Added ddt create/delete tests for Medium

--- tests/foreman/cli/test_medium.py
+++ tests/foreman/cli/test_medium.py
@@ -6,11 +6,21 @@ Test class for Medium  CLI
 """
 
 import random
-
+from ddt import data
+from ddt import ddt
+from tests.foreman.cli.basecli import BaseCLI
+from robottelo.common.constants import NOT_IMPLEMENTED
+from robottelo.cli.factory import make_user
+from robottelo.common.helpers import generate_string
+from robottelo.common.decorators import bzbug, redminebug
 from robottelo.cli.medium import Medium
 from robottelo.cli.factory import make_medium
 from robottelo.common.helpers import generate_name
-from tests.foreman.cli.basecli import MetaCLI
+import sys
+if sys.hexversion >= 0x2070000:
+    import unittest
+else:
+    import unittest2 as unittest
 
 URL = "http://mirror.fakeos.org/%s/$major.$minor/os/$arch"
 OSES = [
@@ -24,7 +34,8 @@ OSES = [
 ]
 
 
-class TestMedium(MetaCLI):
+@ddt
+class TestMedium(BaseCLI):
 
     factory = make_medium
     factory_obj = Medium
@@ -55,21 +66,64 @@ class TestMedium(MetaCLI):
         name = generate_name(6)
         self._create_medium(name)
 
-    def test_delete_medium_1(self):
+    @data({'name': generate_string("latin1", 10)},
+          {'name': generate_string("utf8", 10)},
+          {'name': generate_string("alpha", 10)},
+          {'name': generate_string("alphanumeric", 10)},
+          {'name': generate_string("numeric", 10)},
+          {'name': generate_string("html", 10)})
+    def test_create_medium_2(self, test_data):
+        """
+        @Feature: Medium - Positive Create
+        @Test: Check if Medium can be created
+        @Assert: Medium is created
+        """
+
+        new_obj = make_medium(test_data)
+
+        # Can we find the new object?
+        result = Medium.info({'id': new_obj['id']})
+
+        self.assertEqual(result.return_code, 0, "Failed to create object")
+        self.assertEqual(len(result.stderr), 0,
+                         "There should not be an exception here")
+        self.assertGreater(
+            len(result.stdout), 0, "Failed to fetch medium")
+        self.assertEqual(new_obj['name'],
+                         result.stdout['name'])
+
+    @data({'name': generate_string("latin1", 10)},
+          {'name': generate_string("utf8", 10)},
+          {'name': generate_string("alpha", 10)},
+          {'name': generate_string("alphanumeric", 10)},
+          {'name': generate_string("numeric", 10)},
+          {'name': generate_string("html", 10)})
+    def test_delete_medium_1(self, test_data):
         """
         @Feature: Medium - Positive Delete
         @Test: Check if Medium can be deleted
         @Assert: Medium is deleted
         """
 
-        name = generate_name(6)
-        self._create_medium(name)
+        new_obj = make_medium(test_data)
 
-        medium = Medium().exists(('name', name)).stdout
+        # Can we find the new object?
+        result = Medium.info({'id': new_obj['id']})
 
-        args = {
-            'id': medium['id'],
-        }
+        self.assertEqual(result.return_code, 0)
+        self.assertEqual(len(result.stderr), 0)
+        self.assertEqual(new_obj['name'], result.stdout['name'])
 
-        Medium().delete(args)
-        self.assertFalse(Medium().exists(('name', name)).stdout)
+        return_value = Medium.delete({'id': new_obj['id']})
+        self.assertEqual(return_value.return_code, 0, "Deletion failed")
+        self.assertEqual(
+            len(return_value.stderr), 0, "There should not be an error here")
+
+        # Can we find the object?
+        result = Medium.info({'id': new_obj['id']})
+        self.assertNotEqual(
+            result.return_code, 0, "Medium should be deleted")
+        self.assertGreater(len(result.stderr), 0,
+                           "There should not be an exception here")
+        self.assertEqual(
+            len(result.stdout), 0, "Output should be blank.")
