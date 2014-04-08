7f6eae6865917afc74705ebccf344cbfba86f063
tkolhar <tazimkolhar@gmail.com>
2014-04-08 18:49:17 +0530

    Removed the unittest skip

--- tests/foreman/cli/test_roles.py
+++ tests/foreman/cli/test_roles.py
@@ -8,17 +8,11 @@ Test class for Roles CLI
 from tests.foreman.cli.basecli import BaseCLI
 from robottelo.common.constants import NOT_IMPLEMENTED
 from robottelo.common.decorators import (bzbug)
-import sys
-if sys.hexversion >= 0x2070000:
-    import unittest
-else:
-    import unittest2 as unittest
 
 
 class TestRole(BaseCLI):
 
     @bzbug('1046206')
-    @unittest.skip(NOT_IMPLEMENTED)
     def test_positive_create_role_1(self):
         """
            @test: Create new roles and assign to the custom user
@@ -30,7 +24,6 @@ class TestRole(BaseCLI):
         pass
 
     @bzbug('1046208')
-    @unittest.skip(NOT_IMPLEMENTED)
     def test_create_role_permission_1(self):
         """
            @test: Create new roles Use different set of permission
