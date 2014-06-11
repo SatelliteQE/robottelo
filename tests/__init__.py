0dfd160dab741024a35a10c96f90217abb9b50ad
Jeremy Audet <ichimonji10@gmail.com>
2014-06-11 11:57:08 -0400

    Update module-level documentation
    Add high-level documentation explaining the layout of the codebase.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,12 @@
 # -*- encoding: utf-8 -*-
 # vim: ts=4 sw=4 expandtab ai
+"""
+This module contains tests both for Satellite and Robottelo itself.
+
+Tests for Robottelo are in the ``robottelo`` module. Those tests are
+inward-facing: they help ensure that this codebase is functioning correctly.
+
+Tests for Satellite are in the ``satellite`` module. Those tests are
+outward-facing: they help ensure that a Satellite deployment is functioning
+correctly.
+"""
