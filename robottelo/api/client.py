6ffb69f7e41a4bb009bed5079d3592f8220f4719
Jeremy Audet <ichimonji10@gmail.com>
2014-06-24 12:42:51 -0400

    Add wrappers for more requests functions
    Add wrappers for the following `requests` functions:
    
    * head
    * get
    * post
    * put
    * patch
    * delete
    
    Take logging code and place it in a separate function. Make the logging function
    capable of accepting more inputs and make its output nicer to read.

--- robottelo/api/client.py
+++ robottelo/api/client.py
@@ -4,6 +4,26 @@ You can read up on Requests here: http://docs.python-requests.org/en/latest/
 Details on the wrapped methods are here:
 http://docs.python-requests.org/en/latest/api/#main-interface
 
+The various ``_call_requests_*`` functions in this module are extremely simple
+wrapper functions. They do not alter the arguments passed to them in any way,
+nor do they do anything else such as logging. They exist soley to ease unit
+testing: each one can be overridden in a unit test for mocking purposes.
+
+The following methods are also simple wrappers for the corresponding methods in
+the ``requests`` module:
+
+* request
+* head
+* get
+* post
+* put
+* patch
+* delete
+
+These methods do not functionally alter the behaviour of their wrapped methods.
+However, they do log out information about the request being made before it is
+sent, and they log out information about the response received.
+
 """
 from urllib import urlencode
 import logging
@@ -19,8 +39,8 @@ def _curl_arg_user(kwargs):
     ``kwargs['auth']`` is used to construct the equivalent curl option, if
     present.
 
-    :param dict kwargs: Arguments, such as one might pass to the ``request``
-        method.
+    :param dict kwargs: Keyword arguments, such as one might pass to the
+        ``request`` method.
     :return: Either ``'--user <user:password> '`` or ``''``.
     :rtype: str
 
@@ -36,8 +56,8 @@ def _curl_arg_insecure(kwargs):
     Return the curl option for disabling SSL verification if ``kwargs``
     contains an equivalent option. Return no curl option otherwise.
 
-    :param dict kwargs: Arguments, such as one might pass to the ``request``
-        method.
+    :param dict kwargs: Keyword arguments, such as one might pass to the
+        ``request`` method.
     :return: Either ``'--insecure '`` or ``''``.
     :rtype: str
 
@@ -53,8 +73,8 @@ def _curl_arg_data(kwargs):
     Return the curl ``--data <data>`` option, and use ``kwargs`` as ``<data>``.
     Ignore the ``'auth'`` and ``'verify'`` keys when assembling ``<data>``.
 
-    :param dict kwargs: Arguments, such as one might pass to the ``request``
-        method.
+    :param dict kwargs: Keyword arguments, such as one might pass to the
+        ``request`` method.
     :return: The curl ``--data <data>`` option.
     :rtype: str
 
@@ -65,40 +85,23 @@ def _curl_arg_data(kwargs):
     return urlencode(trimmed_kwargs)
 
 
-def _call_requests_request(method, url, **kwargs):
-    """Call ``requests.request``. Pass all arguments to that function.
+def _log_request(method, url, kwargs, data=None):
+    """Log out information about the arguments given.
 
-    This function is a stupid-simple wrapper around the ``requests.request``
-    function. It does not alter the behaviour of that function in any way. This
-    function exists soley to make unit testing easier: this function can be
-    overridden to mock ``requests.request``.
+    The arguments provided to this function correspond to the arguments that
+    one can pass to ``requests.request``.
 
-    """
-    return requests.request(method, url, **kwargs)
-
-
-def request(method, url, **kwargs):
-    """A wrapper for ``requests.request``.
-
-    This wrapper does not change the functional behaviour of
-    ``requests.request``. Instead, it logs out information about the the
-    request and response.
-
-    For detailed information about the behaviour and arguments of
-    ``requests,request``, see:
-    http://docs.python-requests.org/en/latest/api/#requests.request.
-
-    :param str method: method for the new Request object.
-    :param str url: URL for the new Request object.
-    :param \**kwargs: Other, optional arguments for the new Request object.
-    :return: A ``Response`` object.
-    :rtype: Response
+    :return: Nothing is returned.
+    :rtype: None
 
     """
-    # provide info about call
     logger.info(
-        'Making HTTP {0} request to {1} with the following options: '
-        '{2}'.format(method, url, kwargs)
+        'Making HTTP {0} request to {1} with {2} and {3}.'.format(
+            method,
+            url,
+            'options {0}'.format(kwargs) if len(kwargs) > 0 else 'no options',
+            'data {0}'.format(data) if data is not None else 'no data',
+        )
     )
     logger.info('Equivalent curl command: curl -X {0} {1}{2}{3} {4}'.format(
         method,
@@ -108,10 +111,110 @@ def request(method, url, **kwargs):
         url
     ))
 
-    # provide info about response
-    response = _call_requests_request(method, url, **kwargs)
+
+def _log_response(response):
+    """Log out information about a ``Request`` object.
+
+    After calling ``requests.request`` or one of its convenience methods, the
+    object returned can be passed to this method. If done, information about
+    the object returned is logged.
+
+    :return: Nothing is returned.
+    :rtype: None
+
+    """
     logger.debug('Received HTTP {0} response: {1}'.format(
         response.status_code,
         response.content,
     ))
+
+
+def _call_requests_request(method, url, **kwargs):
+    """Call ``requests.request``."""
+    return requests.request(method, url, **kwargs)
+
+
+def _call_requests_head(url, **kwargs):
+    """Call ``requests.head``."""
+    return requests.head(url, **kwargs)
+
+
+def _call_requests_get(url, **kwargs):
+    """Call ``requests.get``."""
+    return requests.get(url, **kwargs)
+
+
+def _call_requests_post(url, data=None, **kwargs):
+    """Call ``requests.post``."""
+    return requests.post(url, data, **kwargs)
+
+
+def _call_requests_put(url, data=None, **kwargs):
+    """Call ``requests.put``."""
+    return requests.put(url, data, **kwargs)
+
+
+def _call_requests_patch(url, data=None, **kwargs):
+    """Call ``requests.patch``."""
+    return requests.patch(url, data, **kwargs)
+
+
+def _call_requests_delete(url, **kwargs):
+    """Call ``requests.delete``."""
+    return requests.delete(url, **kwargs)
+
+
+def request(method, url, **kwargs):
+    """A wrapper for ``requests.request``."""
+    _log_request(method, url, kwargs)
+    response = _call_requests_request(method, url, **kwargs)
+    _log_response(response)
+    return response
+
+
+def head(url, **kwargs):
+    """A wrapper for ``requests.head``."""
+    _log_request('HEAD', url, kwargs)
+    response = _call_requests_head(url, **kwargs)
+    _log_response(response)
+    return response
+
+
+def get(url, **kwargs):
+    """A wrapper for ``requests.get``."""
+    _log_request('GET', url, kwargs)
+    response = _call_requests_get(url, **kwargs)
+    _log_response(response)
+    return response
+
+
+def post(url, data=None, **kwargs):
+    """A wrapper for ``requests.post``."""
+    _log_request('POST', url, kwargs)
+    response = _call_requests_post(url, data, **kwargs)
+    _log_response(response)
+    return response
+
+
+def put(url, data=None, **kwargs):
+    """A wrapper for ``requests.put``. Sends a PUT request."""
+    _log_request('PUT', url, kwargs)
+    response = _call_requests_put(url, data, **kwargs)
+    _log_response(response)
+    return response
+
+
+def patch(url, data=None, **kwargs):
+    """A wrapper for ``requests.patch``. Sends a PATCH request."""
+    _log_request('PATCH', url, kwargs)
+    response = _call_requests_patch(url, data, **kwargs)
+    _log_response(response)
+    return response
+
+
+def delete(url, **kwargs):
+    """A wrapper for ``requests.delete``. Sends a DELETE request."""
+    _log_request('DELETE', url, kwargs)
+    response = _call_requests_delete(url, **kwargs)
+    _log_response(response)
     return response
