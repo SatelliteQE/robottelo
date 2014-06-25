7cf4e4a4dcc277b8bf370cdf322bc96267b33405
Jeremy Audet <ichimonji10@gmail.com>
2014-06-25 12:16:43 -0400

    Rename module robottelo.models to robottelo.orm
    This renaming has been done for the sake of clarity. Module robottelo.orm
    contains components for mapping Python classes to Satellite entities, and module
    robottelo.entities contains complete mappings of Python classes to Satellite
    entities.
    
    Also:
    
    * Fix imports in module robottelo.entities to accomodate the rename
    * Flesh out docstrings in module robottelo.entities
    
    Related to #916

--- robottelo/entities.py
+++ robottelo/entities.py
@@ -1,11 +1,15 @@
-"""This module define all entities from Satellite
+"""This module defines all entities which Satellite exposes.
 
-Each entity is mapped with its required and optional fields.
+Each class in this module corresponds to a certain type of Satellite entity.
+For example, the ``Host`` class corresponds to the "Host" Satellite entity.
+Similarly, each class attribute corresponds to a Satellite entity attribute.
+For example, the ``Host.name`` class attribute corresponds to the "name"
+attribute of a "Host" entity.
 
 """
-from robottelo import models
+from robottelo import orm
 
 
-class Host(models.Entity):
-    """Entity that represents a Host on a Satellite system"""
-    name = models.StringField(required=True)
+class Host(orm.Entity):
+    """Entity that represents a Host on a Satellite system."""
+    name = orm.StringField(required=True)
