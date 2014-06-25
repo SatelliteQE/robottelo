fb9cc3eb00f59277d2abc3c21eb740a4a8ead9be
Elyézer Mendes Rezende <elyezermr@gmail.com>
2014-06-25 18:03:56 -0300

    Implement orm.OneToManyField

--- tests/robottelo/test_orm.py
+++ tests/robottelo/test_orm.py
@@ -1,12 +1,18 @@
+"""Tests for robottelo.orm module"""
 import unittest
 
 from robottelo import orm
 
 
 class SampleEntity(orm.Entity):
-    """Entity class to be used on entities tests"""
-    name = orm.StringField(required=True)
-    cost = orm.IntegerField()
+    """Sample entity to be used in the tests"""
+    name = orm.StringField()
+    value = orm.IntegerField()
+
+
+class ManyRelatedEntity(orm.Entity):
+    """An entity with a OneToManyField"""
+    entities = orm.OneToManyField(SampleEntity)
 
 
 class EntityTestCase(unittest.TestCase):
@@ -16,7 +22,35 @@ class EntityTestCase(unittest.TestCase):
         fields = entity.get_fields()
 
         self.assertIn('name', fields)
-        self.assertIn('cost', fields)
+        self.assertIn('value', fields)
 
         self.assertIsInstance(fields['name'], orm.StringField)
-        self.assertIsInstance(fields['cost'], orm.IntegerField)
+        self.assertIsInstance(fields['value'], orm.IntegerField)
+
+
+class OneToManyFieldTestCase(unittest.TestCase):
+    """Tests for the OneToManyField"""
+
+    def test_value_is_single_model_instance(self):
+        """Test a single entity value"""
+        entity = SampleEntity(name='aname')
+        other = ManyRelatedEntity(entities=entity)
+
+        self.assertIsInstance(other.entities, list)
+        self.assertEqual(other.entities[0].name, 'aname')
+
+    def test_value_is_dictionary(self):
+        """Test a single dictionary value"""
+        entity = {'name': 'aname'}
+        other = ManyRelatedEntity(entities=entity)
+
+        self.assertIsInstance(other.entities, list)
+        self.assertEqual(other.entities[0].name, 'aname')
+
+    def test_value_is_list_of_dictionary(self):
+        """Test a list of dictionaries value"""
+        entity = [{'name': 'aname'}]
+        other = ManyRelatedEntity(entities=entity)
+
+        self.assertIsInstance(other.entities, list)
+        self.assertEqual(other.entities[0].name, 'aname')
