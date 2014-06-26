74e1af87d1c92adc15556aa9fb9030c0772367cc
Jeremy Audet <ichimonji10@gmail.com>
2014-06-26 10:34:15 -0400

    Create some simple tests for path api/v2/models
    Implement a few tests which check whether it's possible to GET or POST to the
    path. Check for things like whether an HTTP 200 is generated when a GET request
    is issued, that sane sets of data are returned, and that an HTTP 401 is
    generated when an unauthorized HTTP request is made.

---
+++ tests/foreman/api/test_model_v2.py
@@ -0,0 +1,63 @@
+"""Unit tests for the "model" resource.
+
+Each ``TestCase`` subclass tests a single URL. A full list of URLs comprising
+the "host" resource can be found here:
+http://theforeman.org/api/apidoc/v2/models.html.
+
+"""
+from robottelo.api import client
+from robottelo.common.helpers import get_server_url, get_server_credentials
+from unittest import TestCase
+from urlparse import urljoin
+# (too many public methods) pylint: disable=R0904
+
+
+class ApiModelsTestCase(TestCase):
+    """Tests for the ``api/v2/models/`` path."""
+    def setUp(self):  # pylint: disable=C0103
+        """Set ``self.path``."""
+        self.path = urljoin(get_server_url(), 'api/v2/models')
+
+    def test_get(self):
+        """GET ``self.path``."""
+        response = client.get(
+            self.path,
+            auth=get_server_credentials(),
+            verify=False
+        )
+
+        # Run sanity checks.
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('application/json', response.headers['content-type'])
+
+        # Are the correct categories of data returned?
+        data = response.json()
+        data_keys = data.keys()
+        self.assertIn(u'total', data_keys)
+        self.assertIn(u'subtotal', data_keys)
+        self.assertIn(u'page', data_keys)
+        self.assertIn(u'per_page', data_keys)
+        self.assertIn(u'search', data_keys)
+        self.assertIn(u'sort', data_keys)
+        self.assertIn(u'results', data_keys)
+
+    def test_get_unauthorized(self):
+        """GET ``self.path`` and do not provide credentials."""
+        response = client.get(self.path, verify=False)
+        self.assertEqual(response.status_code, 401)
+
+    def test_post(self):
+        """POST ``self.path``."""
+        # FIXME: use a factory to populate the POST request arguments
+        response = client.post(
+            self.path,
+            {'model[name]': 'Z62RNEc9J4kbUXOHJiYE'},
+            auth=get_server_credentials(),
+            verify=False,
+        )
+        self.assertEqual(response.status_code, 200, response.json()['error'])
+
+    def test_post_unauthorized(self):
+        """POST ``self.path`` and do not provide credentials."""
+        response = client.post(self.path, verify=False)
+        self.assertEqual(response.status_code, 401)
