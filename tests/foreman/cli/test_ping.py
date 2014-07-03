aedae75647180f150ce26801cdeb869e5bdb7270
Elyézer Mendes Rezende <elyezermr@gmail.com>
2014-07-03 08:08:43 -0300

    Add CLI hammer ping test

---
+++ tests/foreman/cli/test_ping.py
@@ -0,0 +1,42 @@
+from itertools import izip
+from robottelo.common import ssh
+from robottelo.common.decorators import skip_if_bz_bug_open
+from robottelo.test import CLITestCase
+
+
+class PingTestCase(CLITestCase):
+    """Tests related to the hammer ping command"""
+
+    @skip_if_bz_bug_open(1094826)
+    def test_hammer_ping(self):
+        """
+        @test: hammer ping return code
+        @feature: hammer ping
+        @steps:
+        1. Execute hammer ping
+        2. Check its return code, should be 0 if all services are ok else != 0
+        @bz: 1094826
+        """
+        result = ssh.command('hammer ping')
+        self.assertEqual(len(result.stderr), 0)
+
+        status_count = 0
+        ok_count = 0
+
+        # iterate over the lines grouping every 3 lines
+        # example [1, 2, 3, 4, 5, 6] will return [(1, 2, 3), (4, 5, 6)]
+        # only the status line is relevant for this test
+        for _, status, _ in izip(*[iter(result.stdout)]*3):
+            status_count += 1
+
+            if status.split(':')[1].strip().lower() == 'ok':
+                ok_count += 1
+
+        if status_count == ok_count:
+            self.assertEqual(
+                result.return_code, 0,
+                'Return code should be 0 if all services are ok')
+        else:
+            self.assertNotEqual(
+                result.return_code, 0,
+                'Return code should not be 0 if any service is not ok')
