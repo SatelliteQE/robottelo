78094ee6b0b28c5736d9d871ec7a7dcd7963fcfe
Elyézer Mendes Rezende <elyezermr@gmail.com>
2014-07-08 12:37:22 -0300

    Improve generate_entities script
    Make generate_entities print meta information like api_path
    Fields description does not get the "description:" prefix anymore
    Prints only options that is not False, assuming that False is default
    for boolean options.

--- scripts/generate_entities.py
+++ scripts/generate_entities.py
@@ -53,8 +53,7 @@ class EntityField(object):
             description = textwrap.fill(
                 self.description.strip(),
                 width=80,
-                initial_indent='{0}# description: '.format(prefix),
-                subsequent_indent='{0}# '.format(prefix),
+                initial_indent='{0}# '.format(prefix),
             )
             output.write('{0}\n'.format(TAG_RE.sub('', description)))
         if self.validator:
@@ -68,6 +67,7 @@ class EntityField(object):
         options = ', '.join([
             '{0}={1}'.format(key, value)
             for key, value in self.options.items()
+            if value is not False  # as False is default for boolean options
         ])
         output.write('{prefix}{0} = {1}({2})\n'.format(
             self.name, self.type, options, prefix=prefix))
@@ -84,18 +84,36 @@ class EntityClass(object):
     which define a entity class for a resource.
 
     """
-    def __init__(self, name, fields, docstring=None):
+    def __init__(self, name, fields, docstring=None, meta=None):
         self.name = _get_entity_class_name(name)
         self.fields = fields
         self.docstring = docstring
+        self.meta = meta
 
     def __str__(self):
         output = StringIO()
         output.write('class {0}(orm.Entity):\n'.format(self.name))
         if self.docstring is not None:
             output.write('"""{0}"""'.format(self.docstring))
+
         for field in self.fields:
             output.write(field.formatted_field(4))
+
+        if self.meta is not None:
+            output.write('\n    class Meta(object):\n')
+            for meta_field, meta_value in self.meta.items():
+                if isinstance(meta_value, list):
+                    output.write(
+                        '{prefix}{0} = (\n'.format(meta_field, prefix=' ' * 8))
+                    for value in meta_value:
+                        output.write(
+                            '{prefix}{0}\n'.format(value, prefix=' ' * 12))
+                    output.write(
+                        '{prefix})\n'.format(meta_field, prefix=' ' * 8))
+                else:
+                    output.write('{prefix}{0} = {1}\n'.format(
+                        meta_field, meta_value, prefix=' ' * 8))
+
         class_str = output.getvalue()
         output.close()
         return class_str
@@ -159,6 +177,23 @@ def _get_fields(params, fields=None):
     return fields
 
 
+def _get_meta(resource):
+    """Returns a ``dict`` that maps resource's meta fields and its values
+
+    If no meta information is found, returns ``None``.
+
+    """
+    if resource.get('apis') is not None:
+        meta = {}
+        meta['api_path'] = [
+            "'{0[api_url]}',  # {0[short_description]}".format(api)
+            for api in resource['apis']
+        ]
+        return meta
+    else:
+        return None
+
+
 def main():
     """Inpects the API create info and output the generated entities classes"""
     create_info = inspect.get_resource_create_info()
@@ -168,7 +203,12 @@ def main():
     for resource_name in resource_names:
         resource = create_info[resource_name]
         entities_classes.append(str(
-            EntityClass(resource_name, _get_fields(resource['params']))))
+            EntityClass(
+                resource_name,
+                _get_fields(resource['params']),
+                meta=_get_meta(resource),
+            )
+        ))
 
     print '\n\n'.join(entities_classes),
 
