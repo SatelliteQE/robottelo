dba113d25a8ac0ec7dbbd6a736869653e2588188
Kedar Bidarkar <kbidarka@redhat.com>
2014-07-22 21:15:08 +0530

    foreman-env UI: Added many new boundary tests and converted to factory

--- tests/foreman/ui/test_environment.py
+++ tests/foreman/ui/test_environment.py
@@ -5,72 +5,137 @@
 Test class for Environment UI
 """
 
+from ddt import ddt
+from nose.plugins.attrib import attr
 from robottelo.common.decorators import skip_if_bz_bug_open
-from robottelo.common.helpers import generate_string
+from robottelo.common.decorators import data
+from robottelo.common.helpers import generate_string, generate_strings_list
 from robottelo.test import UITestCase
+from robottelo.ui.factory import make_org, make_loc, make_env
 from robottelo.ui.locators import common_locators
+from robottelo.ui.session import Session
 
 
+@ddt
 class Environment(UITestCase):
     """
-    Implements all Environment tests
+    Implements environment tests in UI
     """
+    org_name = None
+    loc_name = None
 
-    def create_org(self, org_name=None):
-        """Creates Org"""
-        org_name = org_name or generate_string("alpha", 8)
-        self.navigator.go_to_org()  # go to org page
-        self.org.create(org_name)
+    def setUp(self):
+        super(Environment, self).setUp()
+        # Make sure to use the Class' org_name instance
+        if (Environment.org_name is None and
+           Environment.loc_name is None):
+            Environment.org_name = generate_string("alpha", 8)
+            Environment.loc_name = generate_string("alpha", 8)
+            with Session(self.browser) as session:
+                make_org(session, org_name=Environment.org_name)
+                make_loc(session, name=Environment.loc_name)
 
     @skip_if_bz_bug_open('1079999')
-    def test_create_env(self):
+    @attr('ui', 'environment', 'implemented')
+    @data(*generate_strings_list(len1=8))
+    def test_create_env_positive_1(self, name):
         """
-        @Feature: Environment - Positive Create
         @Test: Create new environment
+        @Feature: Environment - Positive Create
         @Assert: Environment is created
         """
-        name = generate_string("alpha", 8)
         org_name = generate_string("alpha", 8)
-        self.login.login(self.katello_user, self.katello_passwd)
-        self.create_org(org_name)
-        self.navigator.go_to_environments()
-        self.environment.create(name, [org_name])
-        search = self.environment.search(name)
-        self.assertIsNotNone(search)
+        with Session(self.browser) as session:
+            make_env(session, name=name, orgs=[org_name])
+            search = self.environment.search(name)
+            self.assertIsNotNone(search)
 
-    def test_update_env(self):
+    @attr('ui', 'environment', 'implemented')
+    @data(*generate_strings_list(len1=255))
+    def test_create_env_positive_2(self, name):
         """
-        @Feature: Environment - Positive Update
+        @Test: Create new environment with 255 chars
+        @Feature: Environment - Positive Create
+        @Assert: Environment is created
+        """
+        org_name = generate_string("alpha", 8)
+        with Session(self.browser) as session:
+            make_env(session, name=name, orgs=[org_name])
+            search = self.environment.search(name)
+            self.assertIsNotNone(search)
+
+    @attr('ui', 'environment', 'implemented')
+    @data(*generate_strings_list(len1=256))
+    def test_create_env_negative_1(self, name):
+        """
+        @Test: Create new environment with 256 chars
+        @Feature: Environment - Negative Create
+        @Assert: Environment is not created
+        """
+        org_name = generate_string("alpha", 8)
+        with Session(self.browser) as session:
+            make_env(session, name=name, orgs=[org_name])
+            search = self.environment.search(name)
+            self.assertIsNone(search)
+
+    def test_create_env_negative_2(self):
+        """
+        @Test: Create new environment with blank name
+        @Feature: Environment - Negative Create
+        @Assert: Environment is not created
+        """
+        name = ""
+        org_name = generate_string("alpha", 8)
+        with Session(self.browser) as session:
+            make_env(session, name=name, orgs=[org_name])
+            error = session.nav.wait_until_element(
+                common_locators["name_haserror"])
+            self.assertIsNotNone(error)
+
+    def test_create_env_negative_3(self):
+        """
+        @Test: Create new environment with whitespace name
+        @Feature: Environment - Negative Create
+        @Assert: Environment is not created
+        """
+        name = "   "
+        org_name = generate_string("alpha", 8)
+        with Session(self.browser) as session:
+            make_env(session, name=name, orgs=[org_name])
+            error = session.nav.wait_until_element(
+                common_locators["name_haserror"])
+            self.assertIsNotNone(error)
+
+    @attr('ui', 'environment', 'implemented')
+    @data(*generate_strings_list(len1=8))
+    def test_update_env(self, new_name):
+        """
         @Test: Update an environment and associated OS
+        @Feature: Environment - Positive Update
         @Assert: Environment is updated
         """
         name = generate_string("alpha", 6)
-        new_name = generate_string("alpha", 6)
         org_name = generate_string("alpha", 8)
         new_org = generate_string("alpha", 8)
-        self.login.login(self.katello_user, self.katello_passwd)
-        self.create_org(org_name)
-        self.navigator.go_to_environments()
-        self.environment.create(name, [org_name])
-        self.create_org(new_org)
-        self.navigator.go_to_environments()
-        self.environment.update(name, [org_name], [new_org],
-                                new_name=new_name)
-        search = self.environment.search(new_name)
-        self.assertIsNotNone(search)
+        with Session(self.browser) as session:
+            make_env(session, name=name, orgs=[org_name])
+            self.environment.update(name, [org_name], [new_org],
+                                    new_name=new_name)
+            search = self.environment.search(new_name)
+            self.assertIsNotNone(search)
 
-    def test_remove_env(self):
+    @attr('ui', 'environment', 'implemented')
+    @data(*generate_strings_list(len1=8))
+    def test_remove_env(self, name):
         """
-        @Feature: Environment - Positive Delete
         @Test: Delete an environment
+        @Feature: Environment - Positive Delete
         @Assert: Environment is deleted
         """
-        name = generate_string("alpha", 6)
         org_name = generate_string("alpha", 8)
-        self.login.login(self.katello_user, self.katello_passwd)
-        self.create_org(org_name)
-        self.navigator.go_to_environments()
-        self.environment.create(name, [org_name])
-        self.environment.delete(name, really=True)
-        notif = self.user.wait_until_element(common_locators["notif.success"])
-        self.assertTrue(notif)
+        with Session(self.browser) as session:
+            make_env(session, name=name, orgs=[org_name])
+            self.environment.delete(name, really=True)
+            notif = session.nav.wait_until_element(
+                common_locators["notif.success"])
+            self.assertTrue(notif)
