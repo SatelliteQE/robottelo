a28705d8031645de97eb43eeb5e2a3bf07aa08c1
Tkolhar <tazimkolhar@gmail.com>
2014-07-23 20:17:02 +0530

    Fixed the test for partition-table

--- tests/foreman/cli/test_partitiontable.py
+++ tests/foreman/cli/test_partitiontable.py
@@ -15,6 +15,17 @@ class TestPartitionTable(MetaCLITestCase):
 
     factory = make_partition_table
     factory_obj = PartitionTable
+    file = ""
+
+    def setUp(self):
+        """Set up file"""
+        self.file = tempfile.NamedTemporaryFile(delete=True)
+        self.file.write("This is a test partition table file \n")
+        return self.file.name
+
+    def tearDown(self):
+        """Remove the file"""
+        self.file.close()
 
     def test_dump_ptable_1(self):
         """
@@ -57,3 +68,74 @@ class TestPartitionTable(MetaCLITestCase):
         PartitionTable().delete(args)
         self.assertFalse(
             PartitionTable().exists(tuple_search=('name', name)).stdout)
+
+    def test_create_ptable(self):
+        """
+        @Feature: Partition Table - Create
+        @Test: Check if Partition Table can be created
+        @Assert: Partition Table is created
+        """
+
+        content = "Fake ptable"
+        name = generate_name(6)
+
+        file_name = self.setUp()
+        ssh.upload_file(file_name, remote_file=file_name)
+
+        args = {
+            'name': name,
+            'os-family': 'Redhat',
+            'file': file_name,
+            'content': content
+        }
+
+        try:
+            make_partition_table(args)
+        except Exception as e:
+                self.fail(e)
+
+        self.assertTrue(
+            PartitionTable().exists(tuple_search=('name', name)).stdout)
+        self.tearDown()
+
+    def test_update_ptable(self):
+        """
+        @Feature: Partition Table - Update
+        @Test: Check if Partition Table can be updated
+        @Assert: Partition Table is updated
+        """
+
+        content = "Fake ptable"
+        name = generate_name(6)
+        new_name = generate_name(6)
+
+        file_name = self.setUp()
+        ssh.upload_file(file_name, remote_file=file_name)
+
+        args = {
+            'name': name,
+            'os-family': 'Redhat',
+            'file': file_name,
+            'content': content
+        }
+
+        try:
+            make_partition_table(args)
+        except Exception as e:
+                self.fail(e)
+
+        self.assertTrue(
+            PartitionTable().exists(tuple_search=('name', name)).stdout)
+
+        args = {
+            'name': name,
+            'os-family': 'Redhat',
+            'file': file_name,
+            'new-name': new_name
+        }
+
+        PartitionTable().update(args)
+        self.assertFalse(
+            PartitionTable().exists(tuple_search=('new-name',
+                                                  new_name)).stdout)
+        self.tearDown()
