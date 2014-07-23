faf08262ae14e762c83f5d3d1c5387771d00e72b
sghai <sghai@redhat.com>
2014-07-23 15:07:55 +0530

    Boundary tests for arch

--- tests/foreman/ui/test_architecture.py
+++ tests/foreman/ui/test_architecture.py
@@ -4,63 +4,136 @@
 """
 Test class for Architecture UI
 """
-
-from robottelo.common.helpers import generate_string
+from ddt import ddt
+from robottelo.common.decorators import data
+from robottelo.common.helpers import generate_string, valid_names_list
 from robottelo.test import UITestCase
+from robottelo.ui.factory import (make_org, make_loc,
+                                  make_os, make_arch)
+from robottelo.ui.locators import common_locators
+from robottelo.ui.session import Session
 
 
+@ddt
 class Architecture(UITestCase):
     """
     Implements Architecture tests from UI
     """
 
-    def create_arch(self, arch_name, os_names=None):
-        """
-        Function navigates and creates new architecture from UI
-        """
-        arch_name = arch_name or generate_string("alpha", 10)
-        os_names = os_names or [generate_string("alpha", 10)]
-        self.navigator.go_to_architectures()  # go to architecture page
-        self.architecture.create(arch_name, os_names)
-        self.assertIsNotNone(self.architecture.search(arch_name))
+    org_name = None
+    loc_name = None
 
-    def test_create_arch(self):
+    def setUp(self):
+        super(Architecture, self).setUp()
+        #  Make sure to use the Class' org_name instance
+        if (Architecture.org_name is None and Architecture.loc_name is None):
+            Architecture.org_name = generate_string("alpha", 8)
+            Architecture.loc_name = generate_string("alpha", 8)
+            with Session(self.browser) as session:
+                make_org(session, org_name=Architecture.org_name)
+                make_loc(session, name=Architecture.loc_name)
+
+    def test_positive_create_arch_1(self):
         """
-        @Feature: Architecture - Create
-        @Test: Create a new Architecture
+        @Test: Create a new Architecture with OS
+        @Feature: Architecture - Positive Create
         @Assert: Architecture is created
         """
         name = generate_string("alpha", 4)
         os_name = generate_string("alpha", 6)
         major_version = generate_string('numeric', 1)
-        self.login.login(self.katello_user, self.katello_passwd)  # login
-        self.navigator.go_to_operating_systems()  # go to operating system page
-        self.operatingsys.create(os_name, major_version)
-        self.assertIsNotNone(self.operatingsys.search(os_name))
-        self.create_arch(name, [os_name])
+        with Session(self.browser) as session:
+            make_os(session, name=os_name,
+                    major_version=major_version)
+            self.assertIsNotNone(self.operatingsys.search(os_name))
+            make_arch(session, name=name, os_names=[os_name])
+            self.assertIsNotNone(self.architecture.search(name))
+
+    @data(*valid_names_list())
+    def test_positive_create_arch_2(self, name):
+        """
+        @Test: Create a new Architecture with different data
+        @Feature: Architecture - Positive Create
+        @Assert: Architecture is created
+        """
+        with Session(self.browser) as session:
+            make_arch(session, name=name)
+            self.assertIsNotNone(self.architecture.search(name))
+
+    def test_negative_create_arch_1(self):
+        """
+        @Test: Create a new Architecture with 256 characters in name
+        @Feature: Architecture - Negative Create
+        @Assert: Architecture is not created
+        """
+        name = generate_string("alpha", 256)
+        with Session(self.browser) as session:
+            make_arch(session, name=name)
+            self.assertIsNotNone(self.architecture.wait_until_element
+                                 (common_locators["alert.error"]))
+            self.assertIsNone(self.architecture.search(name))
+
+    def test_negative_create_arch_2(self):
+        """
+        @Test: Create a new Architecture with whitespace in name
+        @Feature: Architecture - Negative Create
+        @Assert: Architecture is not created
+        """
+        name = " "
+        with Session(self.browser) as session:
+            make_arch(session, name=name)
+            self.assertIsNotNone(self.architecture.wait_until_element
+                                 (common_locators["name_haserror"]))
+
+    def test_negative_create_arch_3(self):
+        """
+        @Test: Create a new Architecture with blank name
+        @Feature: Architecture - Negative Create
+        @Assert: Architecture is not created
+        """
+        name = ""
+        with Session(self.browser) as session:
+            make_arch(session, name=name)
+            self.assertIsNotNone(self.architecture.wait_until_element
+                                 (common_locators["name_haserror"]))
+
+    def test_negative_create_arch_4(self):
+        """
+        @Test: Create a new Architecture with same name
+        @Feature: Architecture - Negative Create
+        @Assert: Architecture is not created
+        """
+        name = generate_string("alpha", 4)
+        with Session(self.browser) as session:
+            make_arch(session, name=name)
+            self.assertIsNotNone(self.architecture.search(name))
+            make_arch(session, name=name)
+            self.assertIsNotNone(self.architecture.wait_until_element
+                                 (common_locators["name_haserror"]))
 
     def test_remove_arch(self):
         """
-        @Feature: Architecture - Delete
         @Test: Delete an existing Architecture
+        @Feature: Architecture - Delete
         @Assert: Architecture is deleted
         """
 
         name = generate_string("alpha", 4)
         os_name = generate_string("alpha", 6)
         major_version = generate_string('numeric', 1)
-        self.login.login(self.katello_user, self.katello_passwd)  # login
-        self.navigator.go_to_operating_systems()  # go to operating system page
-        self.operatingsys.create(os_name, major_version)
-        self.assertIsNotNone(self.operatingsys.search(os_name))
-        self.create_arch(name, [os_name])
-        self.architecture.delete(name, True)
-        self.assertIsNone(self.architecture.search(name))
+        with Session(self.browser) as session:
+            make_os(session, name=os_name,
+                    major_version=major_version)
+            self.assertIsNotNone(self.operatingsys.search(os_name))
+            make_arch(session, name=name, os_names=[os_name])
+            self.assertIsNotNone(self.architecture.search(name))
+            self.architecture.delete(name, True)
+            self.assertIsNone(self.architecture.search(name))
 
     def test_update_arch(self):
         """
-        @Feature: Architecture - Update
         @Test: Update Architecture with new name and OS
+        @Feature: Architecture - Update
         @Assert: Architecture is updated
         """
 
@@ -68,11 +141,12 @@ class Architecture(UITestCase):
         new_name = generate_string("alpha", 4)
         os_name = generate_string("alpha", 6)
         major_version = generate_string('numeric', 1)
-        self.login.login(self.katello_user, self.katello_passwd)  # login
-        self.navigator.go_to_operating_systems()  # go to operating system page
-        self.operatingsys.create(os_name, major_version)
-        self.assertIsNotNone(self.operatingsys.search(os_name))
-        self.create_arch(old_name)
-        self.architecture.update(old_name, new_name,
-                                 new_os_names=[os_name])
-        self.assertIsNotNone(self.architecture.search(new_name))
+        with Session(self.browser) as session:
+            make_os(session, name=os_name,
+                    major_version=major_version)
+            self.assertIsNotNone(self.operatingsys.search(os_name))
+            make_arch(session, name=old_name)
+            self.assertIsNotNone(self.architecture.search(old_name))
+            self.architecture.update(old_name, new_name,
+                                     new_os_names=[os_name])
+            self.assertIsNotNone(self.architecture.search(new_name))
