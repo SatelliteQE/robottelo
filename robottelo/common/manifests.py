dd4a0a4d013957ef718645e3367ef8db99a51197
Adam Saleh <asaleh@redhat.com>
2014-07-30 19:44:07 +0200

    added docstrings

--- robottelo/common/manifests.py
+++ robottelo/common/manifests.py
@@ -17,20 +17,38 @@ from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 
 
-def sign(signature_file, file_to_sign):
-    k = open(signature_file).read()
-    key = RSA.importKey(k)
-    signature = PKCS1_v1_5.new(key)
-    data = open(file_to_sign, "rb").read()
-    digest = SHA256.new(data)
-    return signature.sign(digest)
+def sign(key_file, file_to_sign):
+    """
+    Reads the rsa key file and then proceeds to create signed sha256
+    hash of the data in the file according to PKCS1 1.5 standard.
+
+    Returns binary signature data.
+    """
+    with open(key_file).read() as k:
+        key = RSA.importKey(k)
+        signature = PKCS1_v1_5.new(key)
+        with open(file_to_sign, "rb").read() as data:
+            digest = SHA256.new(data)
+            return signature.sign(digest)
 
 
-def edit_in_zip(zipfname, file_edit_functions):
+def edit_in_zip(zip_file, file_edit_functions):
+    """
+    Reads every file in the zip, and for every path in file_edit_functions
+    dictionary it applies the relevant function on the contents of the file
+    and saves it with new content.
+
+    For example if you had zipfile /tmp/test.zip containing files small.txt,
+    filler.txt and large.txt and you'd want small.txt contain 1 and large.txt
+    contain 1000, you would call
+
+    edit_in_zip("/tmp/test.zip", {'small.txt': lambda x: "1", 'large.txt': lambda x: "1000"}
+
+    """
     tempdir = tempfile.mkdtemp()
     try:
         tempname = os.path.join(tempdir, 'new.zip')
-        with zipfile.ZipFile(zipfname, 'r') as zipread:
+        with zipfile.ZipFile(zip_file, 'r') as zipread:
             with zipfile.ZipFile(tempname, 'w') as zipwrite:
                 for item in zipread.infolist():
                     if item.filename not in file_edit_functions:
@@ -47,22 +65,31 @@ def edit_in_zip(zipfname, file_edit_functions):
 
 
 def edit_consumer(data):
+    """
+    Takes the string-data of the consumer file and updates with new uuid
+    """
     content_dict = json.loads(data)
     content_dict['uuid'] = str(uuid.uuid1())
     return json.dumps(content_dict)
 
 
-def clone(key, oldpath, newpath):
-    shutil.copy(oldpath, newpath)
+def clone(key_path, old_path, new_path):
+    """
+    Taking a manifest on oldpath, changes the uuid in consumer.json and resigns
+    it with an RSA key. When accompanying certificate is installed on the
+    candlepin server, it allows us to quickly create uploadable
+    copies of the original manifest.
+    """
+    shutil.copy(old_path, new_path)
     tempdir = tempfile.mkdtemp()
-    with zipfile.ZipFile(newpath) as oldzip:
+    with zipfile.ZipFile(new_path) as oldzip:
         oldzip.extractall(tempdir)
         edit_in_zip(tempdir+"/consumer_export.zip",
                     {"/export/consumer.json": edit_consumer})
-        signature = sign(key, tempdir+"/consumer_export.zip")
+        signature = sign(key_path, tempdir+"/consumer_export.zip")
         with open(tempdir+"/signature", "wb") as sign_file:
             sign_file.write(signature)
-    with zipfile.ZipFile(newpath, "w") as oldzip:
+    with zipfile.ZipFile(new_path, "w") as oldzip:
         oldzip.write(tempdir+"/consumer_export.zip", "/consumer_export.zip")
         oldzip.write(tempdir+"/signature", "/signature")
 
