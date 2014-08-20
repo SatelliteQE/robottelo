3964548ddbdd2c3335aa0d17c12983ac92d62e22
Jeremy Audet <ichimonji10@gmail.com>
2014-08-20 13:25:42 -0400

    Slightly clean up and flesh out an API test

--- tests/foreman/api/test_role_v2.py
+++ tests/foreman/api/test_role_v2.py
@@ -161,21 +161,22 @@ class RoleTestCase(TestCase):
             role_attrs['name'],
         )
 
+    # FIXME: The docstring for this test is wrong.
     @ddt.data(
         orm.StringField(str_type=('alphanumeric',)).get_value()
     )
     def test_positive_create_filter(self, role_name):
-        """
-        @Test: Create a role and delete it
-        @Feature: Role
-        @Assert: Role deletion should succeed
-        """
+        """@Test: Create a role and delete it
 
+        @Feature: Role
+
+        @Assert: Role deletion should succeed
+
+        """
         try:
             role_attrs = entities.Role(name=role_name).create()
         except factory.FactoryError as err:
             self.fail(err)  # fail instead of error
-
         path = entities.Role(id=role_attrs['id']).path()
 
         # GET the role and verify it's name.
@@ -185,21 +186,22 @@ class RoleTestCase(TestCase):
             verify=False,
         ).json()
         self.assertEqual(response['name'], role_name)
-        
-        # Get permission_ids from selected resource type
+
+        # Get permissions that have a resource_type of ConfigTemplate.
         permissions = client.get(
             entities.Permission().path(),
             auth=get_server_credentials(),
-            verify=False, params={'resource_type': 'ConfigTemplate'}
-        ).json()
-        status_code = httplib.OK
-        self.assertEqual(
-            response.status_code,
-            status_code,
-            status_code_error(path, status_code, response),
-        )
-        get_permission_id = permissions['results'][0]
-        filter_attr = entities.Filter(
+            verify=False,
+            data={'resource_type': 'ConfigTemplate'},
+        ).json()['results']
+
+        # FIXME: Create a filter (that has... what properties?) and read it.
+        filter_attrs = entities.Filter(
             role=role_attrs['id'],
-            permission=[get_permission_id['id']]
+            permission=[permission['id'] for permission in permissions]
         ).create()
+        client.get(
+            entities.Filter(id=filter_attrs['id']).path(),
+            auth=get_server_credentials(),
+            verify=False,
+        ).json()
