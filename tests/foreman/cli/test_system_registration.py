0b641d4fc3c7ed2f96ca0ac3a2074352af500615
Elyézer Mendes Rezende <elyezermr@gmail.com>
2014-08-22 17:59:10 -0300

    Add system registration stubs for CLI and UI

---
+++ tests/foreman/cli/test_system_registration.py
@@ -0,0 +1,110 @@
+"""Test module for System Registration CLI"""
+from robottelo.test import CLITestCase
+from robottelo.common.decorators import stubbed
+
+
+class SystemRegistrationTestCase(CLITestCase):
+    """Tests for System Registration CLI"""
+
+    @stubbed
+    def test_rpm_install(self):
+        """@test: rpm can be retrieved
+
+        @feature: system registration
+
+        @assert: rpm required for modifying rhsm.conf can be installed from sat
+        host
+
+        @assert: rhsm.conf has been updated appropriately.
+
+        @status: Manual
+
+        """
+
+    @stubbed
+    def test_register_no_activation_key(self):
+        """@test: register system to sat without activation key
+
+        @feature: system registration
+
+        @assert: system successfully registered to appropriate org
+
+        @status: Manual
+
+        """
+
+    @stubbed
+    def test_register_with_activation_key(self):
+        """@test: register system with activation key
+
+        @feature: system registration
+
+        @assert: system successfully registered to appropriate org and
+        subscribed to content as listed in key
+
+        @status: Manual
+
+        """
+
+    @stubbed
+    def test_cannot_register_twice_negative(self):
+        """@test: Attempt to register a system twice to an instance
+
+        @feature: system registration
+
+        @assert: system cannot be registered twice via RHSM
+
+        @status: Manual
+
+        """
+
+    @stubbed
+    def test_registered_systems_pull_content(self):
+        # variations:  RH rpms/errata; custom content repos
+        """@test: assure RH RPM content can be installed
+
+        @feature: system registration
+
+        @assert: RH RPM/errata content can be retrieved/installed via sat
+
+        @status: Manual
+
+        """
+
+    @stubbed
+    def test_registered_system_can_be_listed_cli(self):
+        """@test: perform a system list for a given org
+
+        @feature: system registration
+
+        @assert: newly registered system appears in system list
+
+        @status: Manual
+
+        """
+
+    @stubbed
+    def test_system_deregister_cli(self):
+        """@test: deregister/delete system via RHSM
+
+        @feature: system registration
+
+        @assert: after deregistering, performing a system list via CLI
+        assures system no longer appears in CLI
+
+        @status: Manual
+
+        """
+
+    @stubbed
+    def test_deregistered_system_cannot_pull_content(self):
+        """@test: try and retrieve content (via yum, etc.) after system has
+        been removed from sat
+
+        @feature: system registration
+
+        @assert: system can no longer retrieve content from satellite
+
+        @status: Manual
+
+        """
---
+++ tests/foreman/ui/test_system_registration.py
@@ -0,0 +1,82 @@
+"""Test module for System Registration UI"""
+from robottelo.test import UITestCase
+from robottelo.common.decorators import stubbed
+
+
+class SystemRegistrationTestCase(UITestCase):
+    """Tests for System Registration UI"""
+
+    @stubbed
+    def test_registered_system_get_pushed_content(self):
+        # variations: content types - RH rpms/errata; custom content rpms;
+        # puppet modules
+        """@test: assure content types can be pushed down to client via UI
+
+        @feature: system registration
+
+        @assert: content is installed on client system
+
+        @status: Manual
+
+        """
+
+    @stubbed
+    def test_registered_system_can_be_listed_ui(self):
+        """@test: perform a system list for a given org
+
+        @feature: system registration
+
+        @assert: newly registered system can be found in Systems UI
+
+        @status: Manual
+
+        """
+
+    @stubbed
+    def test_system_deregister_ui(self):
+        """@test: delete system via Systems UI
+
+        @feature: system registration
+
+        @assert: after deleting, system no longer appears in system UI.
+
+        @status: Manual
+
+        """
+
+    @stubbed
+    def test_compliance_green(self):
+        """@test: system with appropriate entitlements for subscriptions
+
+        @feature: system registration
+
+        @assert: compliance status is green in UI
+
+        @status: Manual
+
+        """
+
+    @stubbed
+    def test_compliance_red(self):
+        """@test: system without appropriate entitlements for subscriptions
+
+        @feature: system registration
+
+        @assert: compliance status is red in UI
+
+        @status: Manual
+
+        """
+
+    @stubbed
+    def test_compliance_yellow(self):
+        """@test: system with some, but not all, appropriate entitlements for
+        subscriptions
+
+        @feature: system registration
+
+        @assert: compliance status is yellow in UI
+
+        @status: Manual
+
+        """
