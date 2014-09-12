72b589cb40d9b96172ea48f43258c081b2c3c8a6
Kedar Bidarkar <kbidarka@redhat.com>
2014-09-12 19:13:08 +0530

    Manifests-UI: Enabling the subscriptions and sync related tests.
    Manifests can now be uploaded via the UI using the `clone` function
    from `robottelo.common.manifests` module.
    
    The `clone` function now uses by default, the `key_file` path  and
    the `manifest_template` path which is specified in the,
    robottelo.properties file.
    
    Also tests related to enabling of RedHat Repos and Syncing these
    Repos have also been fixed..
    
    We are using RedHat Manifests, cloning them and just changing the
    UUID in consumer.json file, so as to enable it to import in
    different Organizations.

--- tests/foreman/ui/test_subscription.py
+++ tests/foreman/ui/test_subscription.py
@@ -2,9 +2,9 @@
 
 from ddt import ddt
 from nose.plugins.attrib import attr
-from robottelo.common.decorators import skipRemote, stubbed
+from robottelo.common.decorators import skipRemote
 from robottelo.common.helpers import generate_string
-# from robottelo.common.manifests import manifest  # FIXME: does not exist
+from robottelo.common.manifests import clone
 from robottelo.common.ssh import upload_file
 from robottelo.test import UITestCase
 from robottelo.ui.factory import make_org
@@ -13,20 +13,19 @@ from robottelo.ui.session import Session
 
 
 @ddt
-class Subscription(UITestCase):
+class SubscriptionTestCase(UITestCase):
     """Implements subscriptions/manifests tests in UI"""
 
     org_name = None
 
     def setUp(self):
-        super(Subscription, self).setUp()
+        super(SubscriptionTestCase, self).setUp()
         # Make sure to use the Class' org_name instance
-        if Subscription.org_name is None:
-            Subscription.org_name = generate_string("alpha", 8)
+        if SubscriptionTestCase.org_name is None:
+            SubscriptionTestCase.org_name = generate_string("alpha", 8)
             with Session(self.browser) as session:
-                make_org(session, org_name=Subscription.org_name)
+                make_org(session, org_name=SubscriptionTestCase.org_name)
 
-    @stubbed('Need to implement a new manifest api')
     @skipRemote
     @attr('ui', 'subs', 'implemented')
     def test_positive_upload_1(self):
@@ -39,23 +38,16 @@ class Subscription(UITestCase):
         """
 
         alert_loc = common_locators['alert.success']
-        # FIXME: remove directives after manifest implemented
-        mdetails = manifest.fetch_manifest()  # flake8:noqa pylint:disable=E0602
-        path = mdetails['path']
-        try:
-            # upload_file function should take care of uploading to sauce labs.
-            upload_file(mdetails['path'], remote_file=mdetails['path'])
-            with Session(self.browser) as session:
-                session.nav.go_to_select_org(self.org_name)
-                session.nav.go_to_red_hat_subscriptions()
-                self.subscriptions.upload(path)
-                success_ele = self.subscriptions.wait_until_element(alert_loc)
-                self.assertTrue(success_ele)
-        finally:
-            # FIXME: remove directives after manifest implemented
-            manifest.delete_distributor(ds_uuid=mdetails['uuid']) # flake8:noqa pylint:disable=E0602
+        path = clone()
+        # upload_file function should take care of uploading to sauce labs.
+        upload_file(path, remote_file=path)
+        with Session(self.browser) as session:
+            session.nav.go_to_select_org(self.org_name)
+            session.nav.go_to_red_hat_subscriptions()
+            self.subscriptions.upload(path)
+            success_ele = self.subscriptions.wait_until_element(alert_loc)
+            self.assertTrue(success_ele)
 
-    @stubbed('Need to implement a new manifest api')
     @skipRemote
     @attr('ui', 'subs', 'implemented')
     def test_positive_delete_1(self):
@@ -68,19 +60,13 @@ class Subscription(UITestCase):
         """
 
         alert_loc = common_locators['alert.success']
-        # FIXME: remove directives after manifest implemented
-        mdetails = manifest.fetch_manifest()  # flake8:noqa pylint:disable=E0602
-        path = mdetails['path']
-        try:
-            # upload_file function should take care of uploading to sauce labs.
-            upload_file(mdetails['path'], remote_file=mdetails['path'])
-            with Session(self.browser) as session:
-                session.nav.go_to_select_org(self.org_name)
-                session.nav.go_to_red_hat_subscriptions()
-                self.subscriptions.upload(path)
-                self.subscriptions.delete()
-                success_ele = self.subscriptions.wait_until_element(alert_loc)
-                self.assertTrue(success_ele)
-        finally:
-            # FIXME: remove directives after manifest implemented
-            manifest.delete_distributor(ds_uuid=mdetails['uuid']) # flake8:noqa pylint:disable=E0602
+        path = clone()
+        # upload_file function should take care of uploading to sauce labs.
+        upload_file(path, remote_file=path)
+        with Session(self.browser) as session:
+            session.nav.go_to_select_org(self.org_name)
+            session.nav.go_to_red_hat_subscriptions()
+            self.subscriptions.upload(path)
+            self.subscriptions.delete()
+            success_ele = self.subscriptions.wait_until_element(alert_loc)
+            self.assertTrue(success_ele)
