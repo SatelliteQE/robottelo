243cb8f744405bf38a6da9f3b3b1006d7f6a7928
tkolhar <tazimkolhar@gmail.com>
2014-12-11 16:25:25 +0530

    Fixed the failing architecture tests in jenkins - update and delete

--- tests/foreman/cli/test_architecture.py
+++ tests/foreman/cli/test_architecture.py
@@ -3,8 +3,10 @@
 
 """Test class for Architecture CLI"""
 
+from fauxfactory import gen_string
 from robottelo.cli.architecture import Architecture
-from robottelo.cli.factory import make_architecture
+from robottelo.cli.factory import make_architecture, CLIFactoryError
+from robottelo.common.decorators import data
 from robottelo.common.decorators import run_only_on
 from robottelo.test import MetaCLITestCase
 
@@ -15,3 +17,64 @@ class TestArchitecture(MetaCLITestCase):
 
     factory = make_architecture
     factory_obj = Architecture
+
+    @data(
+        ' ',
+        gen_string('alpha', 300),
+        gen_string('numeric', 300),
+        gen_string('alphanumeric', 300),
+        gen_string('utf8', 300),
+        gen_string('latin1', 300),
+        gen_string('html', 300))
+    def test_negative_update(self, data):
+        """@test: Create architecture then fail to update
+        its name
+
+        @feature: Architecture
+
+        @assert: architecture name is not updated
+
+        """
+        try:
+            new_obj = make_architecture()
+        except CLIFactoryError as err:
+            self.fail(err)
+
+        # Update the architecture name
+        result = Architecture.update({
+            'id': new_obj['id'],
+            'new-name': data,
+        })
+        self.assertNotEqual(result.return_code, 0)
+        self.assertGreater(len(result.stderr), 0)
+
+    @data(
+        gen_string("latin1", 10),
+        gen_string("utf8", 10),
+        gen_string("alpha", 10),
+        gen_string("alphanumeric", 10),
+        gen_string("numeric", 10),
+        gen_string("html", 10))
+    def test_positive_delete(self, data):
+        """@test: Create architecture with valid values then delete it
+        by ID
+
+        @feature: Architecture
+
+        @assert: architecture is deleted
+
+        """
+        try:
+            new_obj = make_architecture({'name': data})
+        except CLIFactoryError as err:
+            self.fail(err)
+
+        return_value = Architecture.delete({'id': new_obj['id']})
+        self.assertEqual(return_value.return_code, 0)
+        self.assertEqual(len(return_value.stderr), 0)
+
+        # Can we find the object?
+        result = Architecture.info({'id': new_obj['id']})
+        self.assertNotEqual(result.return_code, 0)
+        self.assertGreater(len(result.stderr), 0)
+        self.assertEqual(len(result.stdout), 0)
