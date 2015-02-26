4f19cdcbdb6eb3951ab093b86384b04ab217d705
Corey Welton <cwelton@redhat.com>
2015-02-26 09:15:04 -0500

    A capsule installer test that assures new 'capsule-installer' is used, and that installation uses only Capsule repo
    A capsule installer test that assures new 'capsule-installer' is used, and that installation uses only Capsule repo
    
    two more tests for isolated capsule
    
    minor formatting
    
    Additional formatting fixes to old commits

--- tests/foreman/cli/test_capsule_installer.py
+++ tests/foreman/cli/test_capsule_installer.py
@@ -4,6 +4,61 @@ from robottelo.test import CLITestCase
 
 
 class TestCapsuleInstaller(CLITestCase):
+
+    def capsule_installer_basic_test(self):
+        """@Test: perform a basic install of capsule.
+
+        @Feature: Capsule Installer
+
+        @Steps:
+
+        1. Assure your target capsule has ONLY the Capsule repo enabled. In
+           other words, the Satellite repo itself is not enabled by default.
+        2. attempt to perform a basic, functional install the capsule using
+           `capsule-installer`.
+
+        @Assert: product is installed
+
+        @Status: Manual
+
+        """
+
+    def capsule_installer_qpid(self):
+        """@Test: assure the --qpid-router flag can be used in
+        capsule-installer to enable katello-agent functionality via
+        remote clients
+
+        @Feature: capsule Installer
+
+        @Steps:
+
+        1. Install capsule-installer with the '--qpid-router=true` flag
+
+        @Assert: Capsule installs correctly and qpid functionality is
+        enabled.
+
+        @Status: Manual
+
+        """
+
+    def capsule_installer_reverse_proxy(self):
+        """@Test:  assure the --reverse-proxy flag can be used in
+        capsule-installer to enable katello-agent functionality via
+        remote clients
+
+        @Feature: capsule Installer
+
+        @Steps:
+
+        1. Install using the '--reverse-proxy=true' flag
+
+        @Assert: Capsule installs correctly and functionality is
+        enabled.
+
+        @Status: Manual
+
+        """
+
     def capsule_installer_parent_reverse_proxy_neg(self):
         """@Test: invalid (non-boolean) parameters cannot be passed to flag
 
@@ -11,7 +66,7 @@ class TestCapsuleInstaller(CLITestCase):
 
         @Steps:
         1. attempt to provide a variety of invalid parameters
-        to installer (strings, numerics, whitespace, etc.)
+           to installer (strings, numerics, whitespace, etc.)
 
         @Assert: user is told that such parameters are invalid and install
         aborts.
@@ -28,8 +83,10 @@ class TestCapsuleInstaller(CLITestCase):
         @Setup: na
 
         @Steps:
+
         1. attempt to provide a variety of invalid parameters to
-        --parent-reverse-proxy-port flag (strings, numerics, whitespace, etc.)
+           --parent-reverse-proxy-port flag (strings, numerics, whitespace,
+           etc.)
 
         @Assert: user told parameters are invalid; install aborts.
 
@@ -45,6 +102,7 @@ class TestCapsuleInstaller(CLITestCase):
         @Setup: note that this requires an accompanying, valid port value
 
         @Steps:
+
         1. Attempt to provide a value of "true" to --parent-reverse-proxy
 
         @Assert: Install commences/completes with proxy installed correctly.
@@ -63,6 +121,7 @@ class TestCapsuleInstaller(CLITestCase):
         proxy parameter
 
         @Steps:
+
         1. Attempt to provide a valid proxy port # to flag
 
         @Assert: Install commences and completes with proxy installed
