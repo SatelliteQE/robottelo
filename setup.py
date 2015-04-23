ccef109da69475db99648192bbb6932654df6375
Elyézer Mendes Rezende <elyezermr@gmail.com>
2015-04-23 11:48:17 -0300

    Add setup.py file
    The setup file allow to install Robottelo in the current virtual
    environment and will easy the use of scripts. Also as soon as Robottelo
    evolve as a 100% solid framework we can publish it on PyPI with minimal
    adjustments.

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+REQUIRES = [
+    'ddt',
+    'fauxfactory',
+    'nailgun==0.10.0',
+    'paramiko',
+    'python-bugzilla',
+    'requests',
+    'selenium',
+]
+
+with open('README.rst', 'r') as f:
+    README = f.read()
+
+setup(
+    name='robottelo',
+    version='0.1.0',
+    description='Robottelo is a test suite which exercises The Foreman.',
+    long_description=README,
+    author=u'Satellite QE Team',
+    url='https://github.com/SatelliteQE/robottelo',
+    packages=find_packages(exclude=['tests*']),
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    install_requires=REQUIRES,
+    license='GNU GPL v3.0',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux'
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
