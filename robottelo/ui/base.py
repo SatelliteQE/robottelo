b7665ecf6879e9c2422e1c7d973d29a99832b3ef
Elyézer Mendes Rezende <elyezermr@gmail.com>
2015-07-08 10:04:59 -0300

    Add click helper method for UI
    The click method finds the element and then click on it. If the element
    is not found then an exception is raised. This is intended to reduce the
    amount of UI code needed to click on elements.
    
    Refactor ui.base and ui.activationkey to use the new method to
    illustrate how the click can save lines of code and provide better
    logging if an element can not be found.
    
    Bonus: Change double to single quote and some other code format
    adjustments.

--- robottelo/ui/base.py
+++ robottelo/ui/base.py
@@ -28,16 +28,12 @@ class UIPageSubmitionFailed(Exception):
 
 
 class Base(object):
-    """
-    Base class for UI
-    """
+    """Base class for UI"""
 
     logger = LOGGER
 
     def __init__(self, browser):
-        """
-        Sets up the browser object.
-        """
+        """Sets up the browser object."""
         self.browser = browser
 
     def find_element(self, locator):
@@ -70,17 +66,17 @@ class Base(object):
                       katello=None, timeout=None):
         """Uses the search box to locate an element from a list of elements."""
 
-        search_key = search_key or "name"
+        search_key = search_key or 'name'
         element = None
 
         if katello:
-            searchbox = self.wait_until_element(common_locators["kt_search"])
+            searchbox = self.wait_until_element(common_locators['kt_search'])
             search_button = self.wait_until_element(
-                common_locators["kt_search_button"])
+                common_locators['kt_search_button'])
         else:
-            searchbox = self.wait_until_element(common_locators["search"])
+            searchbox = self.wait_until_element(common_locators['search'])
             search_button = self.wait_until_element(
-                common_locators["search_button"])
+                common_locators['search_button'])
 
         # Do not proceed if searchbox is not found
         if searchbox is None:
@@ -122,40 +118,28 @@ class Base(object):
             alert.dismiss()
 
     def select_deselect_entity(self, filter_key, loc, entity_list):
-        """
-        Function to select and deselect entity like OS, Partition Table, Arch
-        from selection list or by selecting relevant checkbox.
+        """Function to select and deselect entity like OS, Partition Table,
+        Arch from selection list or by selecting relevant checkbox.
+
         """
         for entity in entity_list:
-            strategy, value = common_locators["filter"]
             # Scroll to top
             self.browser.execute_script('window.scroll(0, 0)')
+            strategy, value = common_locators['filter']
             txt_field = self.wait_until_element((strategy, value % filter_key))
             if txt_field:
                 txt_field.clear()
                 txt_field.send_keys(entity)
                 strategy, value = loc
-                select_element = self.wait_until_element((strategy,
-                                                          value % entity))
-                if select_element:
-                    select_element.click()
-                else:
-                    raise UINoSuchElementError(
-                        "Couldn't find element from selection list")
+                self.click((strategy, value % entity))
             else:
-                strategy1, value1 = common_locators["entity_checkbox"]
-                checkbox_element = self.wait_until_element((strategy1,
-                                                            value1 % entity))
-                if checkbox_element:
-                    checkbox_element.click()
-                else:
-                    raise UINoSuchElementError(
-                        "Couldn't find element from checkbox list.")
+                strategy, value = common_locators['entity_checkbox']
+                self.click((strategy, value % entity))
 
     def configure_entity(self, entity_list, filter_key, tab_locator=None,
                          new_entity_list=None, entity_select=True):
-        """
-        Configures entities like orgs, OS, ptable, Archs, Users, Usergroups.
+        """Configures entities like orgs, OS, ptable, Archs, Users, Usergroups.
+
         """
         if entity_list is None:
             entity_list = []
@@ -163,43 +147,33 @@ class Base(object):
             new_entity_list = []
         if entity_list:
             if tab_locator:
-                self.wait_until_element(tab_locator).click()
+                self.click(tab_locator)
             if entity_select:
-                entity_locator = common_locators["entity_select"]
+                entity_locator = common_locators['entity_select']
             else:
-                entity_locator = common_locators["entity_deselect"]
-            self.select_deselect_entity(filter_key,
-                                        entity_locator, entity_list)
+                entity_locator = common_locators['entity_deselect']
+            self.select_deselect_entity(
+                filter_key, entity_locator, entity_list)
         if new_entity_list:
             if tab_locator:
-                self.wait_until_element(tab_locator).click()
-            entity_locator = common_locators["entity_select"]
-            self.select_deselect_entity(filter_key,
-                                        entity_locator, new_entity_list)
+                self.click(tab_locator)
+            entity_locator = common_locators['entity_select']
+            self.select_deselect_entity(
+                filter_key, entity_locator, new_entity_list)
 
     def delete_entity(self, name, really, name_locator, del_locator,
                       drop_locator=None, search_key=None):
         """Delete an added entity, handles both with and without dropdown."""
         searched = self.search_entity(
             name, name_locator, search_key=search_key)
-        if searched:
-            if drop_locator:
-                strategy = drop_locator[0]
-                value = drop_locator[1]
-                dropdown = self.wait_until_element((strategy, value % name))
-                dropdown.click()
-            strategy1 = del_locator[0]
-            value1 = del_locator[1]
-            element = self.wait_until_element((strategy1, value1 % name))
-            if element is None:
-                raise UINoSuchElementError(
-                    u'Could not select the entity "{0}" for deletion.'
-                    .format(name)
-                )
-            element.click()
-            self.handle_alert(really)
-        else:
+        if not searched:
             raise UIError('Could not search the entity "{0}"'.format(name))
+        if drop_locator:
+            strategy, value = drop_locator
+            self.click((strategy, value % name))
+        strategy, value = del_locator
+        self.click((strategy, value % name))
+        self.handle_alert(really)
 
     def wait_until_element_exists(
             self, locator, timeout=12, poll_frequency=0.5):
@@ -256,8 +230,8 @@ class Base(object):
             return element
         except TimeoutException as err:
             self.logger.debug(
-                "%s: Timed out waiting for element '%s' to display or to be "
-                "clickable.",
+                '%s: Timed out waiting for element "%s" to display or to be '
+                'clickable.',
                 type(err).__name__,
                 locator[1]
             )
@@ -272,7 +246,7 @@ class Base(object):
         angular_active = False
 
         try:
-            jquery_active = driver.execute_script("return jQuery.active") > 0
+            jquery_active = driver.execute_script('return jQuery.active') > 0
         except WebDriverException:
             pass
 
@@ -290,14 +264,14 @@ class Base(object):
         WebDriverWait(
             self.browser, timeout, poll_frequency
         ).until(
-            self.ajax_complete, "Timeout waiting for page to load"
+            self.ajax_complete, 'Timeout waiting for page to load'
         )
 
     def scroll_page(self):
         """
         Scrolls page up
         """
-        self.browser.execute_script("scroll(350, 0);")
+        self.browser.execute_script('scroll(350, 0);')
 
     def scroll_right_pane(self):
         """
@@ -327,40 +301,31 @@ class Base(object):
         Function to set parameters for different
         entities like OS and Domain
         """
-        self.wait_until_element(common_locators["parameter_tab"]).click()
-        self.wait_until_element(common_locators["add_parameter"]).click()
-        if self.wait_until_element(common_locators["parameter_name"]):
-            pname = self.find_element(common_locators["parameter_name"])
+        self.click(common_locators['parameter_tab'])
+        self.click(common_locators['add_parameter'])
+        if self.wait_until_element(common_locators['parameter_name']):
+            pname = self.find_element(common_locators['parameter_name'])
             pname.send_keys(param_name)
-        if self.wait_until_element(common_locators["parameter_value"]):
-            pvalue = self.find_element(common_locators["parameter_value"])
+        if self.wait_until_element(common_locators['parameter_value']):
+            pvalue = self.find_element(common_locators['parameter_value'])
             pvalue.send_keys(param_value)
-        self.find_element(common_locators["submit"]).click()
-        self.wait_for_ajax()
+        self.click(common_locators['submit'])
 
     def remove_parameter(self, param_name):
+        """Function to remove parameters for different entities like OS and
+        Domain.
+
         """
-        Function to remove parameters for different
-        entities like OS and Domain
-        """
-        self.wait_until_element(common_locators["parameter_tab"]).click()
-        strategy = common_locators["parameter_remove"][0]
-        value = common_locators["parameter_remove"][1]
-        remove_element = self.wait_until_element((strategy,
-                                                  value % param_name))
-        if remove_element:
-            remove_element.click()
-        self.find_element(common_locators["submit"]).click()
+        self.click(common_locators['parameter_tab'])
+        strategy, value = common_locators['parameter_remove']
+        self.click((strategy, value % param_name))
+        self.click(common_locators['submit'])
 
     def edit_entity(self, edit_loc, edit_text_loc, entity_value, save_loc):
-        """
-        Function to edit the selected entity's  text and save it
-        """
-
-        self.wait_until_element(edit_loc).click()
+        """Function to edit the selected entity's  text and save it."""
+        self.click(edit_loc)
         self.text_field_update(edit_text_loc, entity_value)
-        self.wait_until_element(save_loc).click()
-        self.wait_for_ajax()
+        self.click(save_loc)
 
     def auto_complete_search(self, go_to_page, entity_locator, partial_name,
                              name, search_key):
@@ -377,24 +342,15 @@ class Base(object):
         """
         go_to_page()
         strategy1, value1 = entity_locator
-        searchbox = self.wait_until_element(common_locators["search"])
+        searchbox = self.wait_until_element(common_locators['search'])
         if searchbox is None:
-            raise UINoSuchElementError("Search box not found.")
+            raise UINoSuchElementError('Search box not found.')
         searchbox.clear()
         searchbox.send_keys(search_key + " = " + partial_name)
         self.wait_for_ajax()
-        strategy, value = common_locators["auto_search"]
-        element = self.wait_until_element((strategy, value % name))
-        if element is None:
-            raise UINoSuchElementError(
-                "Entity not found via auto search completion.")
-        element.click()
-        self.wait_for_ajax()
-        search_button = self.wait_until_element(
-            common_locators['search_button'])
-        if search_button is None:
-            raise UINoSuchElementError("Search button not found.")
-        search_button.click()
+        strategy, value = common_locators['auto_search']
+        self.click((strategy, value % name))
+        self.click(common_locators['search_button'])
         entity_elem = self.wait_until_element((strategy1, value1 % name))
         return entity_elem
 
@@ -414,14 +370,14 @@ class Base(object):
 
         """
         go_to_page()
-        strategy, value = common_locators['all_values']
         searched = self.search_entity(entity_name, entity_locator)
         if searched is None:
-            raise UINoSuchElementError("Entity not found via search.")
+            raise UINoSuchElementError('Entity not found via search.')
         searched.click()
-        self.wait_until_element(tab_locator).click()
-        selected = self.find_element((strategy,
-                                      value % context)).is_selected()
+        self.click(tab_locator)
+        strategy, value = common_locators['all_values']
+        selected = self.find_element(
+            (strategy, value % context)).is_selected()
         return selected
 
     def submit_and_validate(self, locator, validation=True):
@@ -433,10 +389,9 @@ class Base(object):
             set to False for the negative tests.
 
         """
-        self.wait_until_element(locator).click()
-        self.wait_for_ajax()
+        self.click(locator)
         if self.wait_until_element(locator) and validation:
-            raise UIPageSubmitionFailed("Page submission failed.")
+            raise UIPageSubmitionFailed('Page submission failed.')
 
     def is_element_enabled(self, locator):
         """Check whether UI element is enabled or disabled
@@ -463,3 +418,24 @@ class Base(object):
             return False
         self.wait_for_ajax()
         return element.is_displayed()
+
+    def click(self, locator, wait_for_ajax=True, timeout=30):
+        """Locate the element described by the ``locator`` and click on it.
+
+        :param locator: The locator that decribes the element.
+        :param wait_for_ajax: Flag that indicates if should wait for AJAX after
+            clicking on the element
+        :param timeout: The amount of time that wait_fox_ajax should wait. This
+            will have effect if ``wait_fox_ajax`` parameter is ``True``.
+        :raise: UINoSuchElementError if the element could not be found.
+
+        """
+        element = self.wait_until_element(locator)
+        if element is None:
+            raise UINoSuchElementError(
+                '{}: element with locator {} not found while trying to click.'
+                .format(type(self).__name__, locator)
+            )
+        element.click()
+        if wait_for_ajax:
+            self.wait_for_ajax(timeout)
