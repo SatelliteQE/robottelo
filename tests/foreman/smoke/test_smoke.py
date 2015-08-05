2da9131523504a362f397b814e40af7fe5d73abc
Elyézer Mendes Rezende <elyezermr@gmail.com>
2015-08-05 09:48:27 -0300

    Improve smoke tests
    Add tests for checking for the processes status and for ensuring that
    there is no error entry on the installer logs.
    
    Closes #2645

--- tests/foreman/smoke/test_smoke.py
+++ tests/foreman/smoke/test_smoke.py
@@ -1,7 +1,10 @@
 """Smoke tests to check installation health"""
 import re
 
+from itertools import izip
 from robottelo.common import ssh
+from robottelo.common.helpers import get_host_info, get_server_credentials
+from robottelo.log import LogFile
 from robottelo.test import TestCase
 
 
@@ -39,3 +42,91 @@ class SELinuxTestCase(TestCase):
             rpm_version = rpm_version[:-2]
 
         self.assertEqual(rpm_version.replace('-', '.'), semodule_version)
+
+    def test_installer_check_services(self):
+        """@Test: Services services start correctly
+
+        @Feature: Installer
+
+        @Assert: All services {'elasticsearch', 'foreman-proxy',
+        'foreman-tasks', 'httpd', 'mongod', 'postgresql', 'pulp_celerybeat',
+        'pulp_resource_manager', 'pulp_workers', 'qdrouterd', 'qpidd',
+        'tomcat'} are started
+
+        """
+        services = (
+            'elasticsearch',
+            'foreman-proxy',
+            'foreman-tasks',
+            'httpd',
+            'mongod',
+            'postgresql',
+            'pulp_celerybeat',
+            'pulp_resource_manager',
+            'pulp_workers',
+            'qdrouterd',
+            'qpidd',
+            'tomcat',
+        )
+
+        # check `services` status using service command
+        if get_host_info()[1] >= 7:
+            status_format = 'systemctl status {0}'
+        else:
+            status_format = 'service {0} status'
+
+        for service in services:
+            result = ssh.command(status_format.format(service))
+            self.assertEqual(result.return_code, 0)
+            self.assertEqual(len(result.stderr), 0)
+
+        # check status reported by hammer ping command
+        result = ssh.command(u'hammer -u {0[0]} -p {0[1]} ping'.format(
+            get_server_credentials()
+        ))
+
+        # iterate over the lines grouping every 3 lines
+        # example [1, 2, 3, 4, 5, 6] will return [(1, 2, 3), (4, 5, 6)]
+        for service, status, server_response in izip(
+                *[iter(result.stdout)] * 3):
+            service = service.replace(':', '').strip()
+            status = status.split(':')[1].strip().lower()
+            server_response = server_response.split(':', 1)[1].strip()
+            self.assertEqual(
+                status, 'ok',
+                '{0} responded with {1}'.format(service, server_response)
+            )
+
+    def test_installer_logfile_check(self):
+        """@Test: Look for ERROR or FATAL references in logfiles
+
+        @Feature: Installer
+
+        @Steps::
+
+        1. search all relevant logfiles for ERROR/FATAL
+
+        @Assert: No ERROR/FATAL notifcations occur in {katello-jobs, tomcat6,
+        foreman, pulp, passenger-analytics, httpd, foreman_proxy,
+        elasticsearch, postgresql, mongod} logfiles.
+
+        """
+        logfiles = (
+            {
+                'path': '/var/log/candlepin/error.log',
+                'pattern': r'ERROR'
+            },
+            {
+                'path': '/var/log/katello-installer/katello-installer.log',
+                'pattern': r'\[\s*(ERROR|FATAL)'
+            },
+        )
+
+        for logfile in logfiles:
+            try:
+                log = LogFile(logfile['path'], logfile['pattern'])
+            except IOError:
+                self.fail(
+                    'Could not find {0} file on server'.format(logfile['path'])
+                )
+            self.assertEqual(len(log.filter()), 0)
