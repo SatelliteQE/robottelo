52a9e88a53c4f119ce9b428fd8d00ae45b8777f2
sthirugn <sthirugn@redhat.com>
2015-08-08 12:07:13 -0400

    Bug fix for passing None to Release Version

--- tests/foreman/api/test_repository.py
+++ tests/foreman/api/test_repository.py
@@ -249,7 +249,7 @@ class RepositorySyncTestCase(APITestCase):
             product=PRDS['rhel'],
             repo=REPOS['rhst7']['name'],
             reposet=REPOSET['rhst7'],
-            releasever='None',
+            releasever=None,
         )
         entities.Repository(id=repo_id).sync()
 
