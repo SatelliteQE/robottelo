efda760c4c9b388155a5eb54f2f3c68e4b84e2a4
oshtaier <alexshtayer@yahoo.com>
2015-08-13 16:19:52 +0300

    Adding more changes according to our new click method implementation mentioned in #2562 issue.

--- robottelo/ui/medium.py
+++ robottelo/ui/medium.py
@@ -2,7 +2,7 @@
 """Implements Medium UI."""
 
 from robottelo.ui.base import Base, UIError
-from robottelo.ui.locators import locators, common_locators
+from robottelo.ui.locators import common_locators, locators
 from robottelo.ui.navigator import Navigator
 from selenium.webdriver.support.select import Select
 
@@ -14,20 +14,19 @@ class Medium(Base):
         """Configures Installation media's OS family."""
         if os_family:
             Select(
-                self.find_element(locators["medium.os_family"])
+                self.find_element(locators['medium.os_family'])
             ).select_by_visible_text(os_family)
 
     def create(self, name, path, os_family=None):
         """Creates new Installation media."""
-        self.wait_until_element(locators["medium.new"]).click()
+        self.click(locators['medium.new'])
 
-        if self.wait_until_element(locators["medium.name"]):
-            self.find_element(locators["medium.name"]).send_keys(name)
-            if self.wait_until_element(locators["medium.path"]):
-                self.find_element(locators["medium.path"]).send_keys(path)
+        if self.wait_until_element(locators['medium.name']):
+            self.find_element(locators['medium.name']).send_keys(name)
+            if self.wait_until_element(locators['medium.path']):
+                self.find_element(locators['medium.path']).send_keys(path)
                 self._configure_medium(os_family)
-                self.find_element(common_locators["submit"]).click()
-                self.wait_for_ajax()
+                self.click(common_locators['submit'])
             else:
                 raise UIError(
                     'Could not create new installation media without path'
@@ -42,18 +41,18 @@ class Medium(Base):
         Navigator(self.browser).go_to_installation_media()
         self.wait_for_ajax()
         if len(name) <= 30:
-            element = self.search_entity(name, locators["medium.medium_name"])
+            element = self.search_entity(name, locators['medium.medium_name'])
         else:
             element = self.search_entity(
-                name, common_locators["select_filtered_entity"])
+                name, common_locators['select_filtered_entity'])
         return element
 
-    def delete(self, name, really):
+    def delete(self, name, really=True):
         """Delete Installation media."""
         self.delete_entity(
             name,
             really,
-            locators["medium.medium_name"],
+            locators['medium.medium_name'],
             locators['medium.delete']
         )
 
@@ -63,14 +62,13 @@ class Medium(Base):
 
         if element:
             element.click()
-            if self.wait_until_element(locators["medium.name"]):
-                self.field_update("medium.name", new_name)
+            if self.wait_until_element(locators['medium.name']):
+                self.field_update('medium.name', new_name)
             if new_path:
-                if self.wait_until_element(locators["medium.path"]):
-                    self.field_update("medium.path", new_path)
+                if self.wait_until_element(locators['medium.path']):
+                    self.field_update('medium.path', new_path)
             self._configure_medium(os_family)
-            self.find_element(common_locators["submit"]).click()
-            self.wait_for_ajax()
+            self.click(common_locators['submit'])
         else:
             raise UIError(
                 'Could not update the installation media "{0}"'
--- tests/foreman/ui/test_architecture.py
+++ tests/foreman/ui/test_architecture.py
@@ -4,7 +4,7 @@ from ddt import ddt
 from fauxfactory import gen_string
 from nailgun import entities
 from robottelo.common.decorators import data, run_only_on, skip_if_bug_open
-from robottelo.common.helpers import generate_strings_list
+from robottelo.common.helpers import generate_strings_list, invalid_values_list
 from robottelo.test import UITestCase
 from robottelo.ui.factory import make_arch
 from robottelo.ui.locators import common_locators
@@ -24,7 +24,7 @@ class Architecture(UITestCase):
            u'os_name': gen_string('utf8')},
           {u'name': gen_string('alphanumeric'),
            u'os_name': gen_string('alphanumeric')})
-    def test_positive_create_arch_1(self, test_data):
+    def test_positive_create_arch_with_os(self, test_data):
         """@Test: Create a new Architecture with OS
 
         @Feature: Architecture - Positive Create
@@ -32,14 +32,14 @@ class Architecture(UITestCase):
         @Assert: Architecture is created
 
         """
-        entities.OperatingSystem(name=test_data['os_name']).create_json()
+        entities.OperatingSystem(name=test_data['os_name']).create()
         with Session(self.browser) as session:
             make_arch(session, name=test_data['name'],
                       os_names=[test_data['os_name']])
             self.assertIsNotNone(self.architecture.search(test_data['name']))
 
-    @data(*generate_strings_list(len1=8))
-    def test_positive_create_arch_2(self, name):
+    @data(*generate_strings_list())
+    def test_positive_create_arch_with_different_names(self, name):
         """@Test: Create a new Architecture with different data
 
         @Feature: Architecture - Positive Create
@@ -51,9 +51,10 @@ class Architecture(UITestCase):
             make_arch(session, name=name)
             self.assertIsNotNone(self.architecture.search(name))
 
-    @data(*generate_strings_list(len1=256))
-    def test_negative_create_arch_1(self, name):
-        """@Test: Create a new Architecture with 256 characters in name
+    @data(*invalid_values_list())
+    def test_negative_create_arch(self, name):
+        """@Test: Try to create architecture and use whitespace, blank, tab
+        symbol or too long string of different types as its name value
 
         @Feature: Architecture - Negative Create
 
@@ -63,24 +64,10 @@ class Architecture(UITestCase):
         with Session(self.browser) as session:
             make_arch(session, name=name)
             self.assertIsNotNone(self.architecture.wait_until_element
-                                 (common_locators["name_haserror"]))
+                                 (common_locators['name_haserror']))
 
-    @data("", "  ")
-    def test_negative_create_arch_2(self, name):
-        """@Test: Create a new Architecture with whitespace in name
-
-        @Feature: Architecture - Negative Create
-
-        @Assert: Architecture is not created
-
-        """
-        with Session(self.browser) as session:
-            make_arch(session, name=name)
-            self.assertIsNotNone(self.architecture.wait_until_element
-                                 (common_locators["name_haserror"]))
-
-    @data(*generate_strings_list(len1=6))
-    def test_negative_create_arch_4(self, name):
+    @data(*generate_strings_list())
+    def test_negative_create_arch_with_same_name(self, name):
         """@Test: Create a new Architecture with same name
 
         @Feature: Architecture - Negative Create
@@ -93,18 +80,11 @@ class Architecture(UITestCase):
             self.assertIsNotNone(self.architecture.search(name))
             make_arch(session, name=name)
             self.assertIsNotNone(self.architecture.wait_until_element
-                                 (common_locators["name_haserror"]))
+                                 (common_locators['name_haserror']))
 
     @skip_if_bug_open('bugzilla', 1123388)
-    @data({u'name': gen_string('alpha'),
-           u'os_name': gen_string('alpha')},
-          {u'name': gen_string('html'),
-           u'os_name': gen_string('html')},
-          {u'name': gen_string('utf8'),
-           u'os_name': gen_string('utf8')},
-          {u'name': gen_string('alphanumeric'),
-           u'os_name': gen_string('alphanumeric')})
-    def test_remove_arch(self, test_data):
+    @data(*generate_strings_list())
+    def test_remove_architecture(self, name):
         """@Test: Delete an existing Architecture
 
         @Feature: Architecture - Delete
@@ -114,28 +94,17 @@ class Architecture(UITestCase):
         @BZ: 1131815
 
         """
-        entities.OperatingSystem(name=test_data['os_name']).create_json()
+        os_name = gen_string('alpha')
+        entities.OperatingSystem(name=os_name).create()
         with Session(self.browser) as session:
-            make_arch(session, name=test_data['name'],
-                      os_names=[test_data['os_name']])
-            self.assertIsNotNone(self.architecture.search(test_data['name']))
-            self.architecture.delete(test_data['name'], True)
-            self.assertIsNone(self.architecture.search(test_data['name']))
+            make_arch(session, name=name, os_names=[os_name])
+            self.assertIsNotNone(self.architecture.search(name))
+            self.architecture.delete(name)
+            self.assertIsNone(self.architecture.search(name))
 
     @skip_if_bug_open('bugzilla', 1123388)
-    @data({u'old_name': gen_string('alpha'),
-           u'new_name': gen_string('alpha'),
-           u'os_name': gen_string('alpha')},
-          {u'old_name': gen_string('html'),
-           u'new_name': gen_string('html'),
-           u'os_name': gen_string('html')},
-          {u'old_name': gen_string('utf8'),
-           u'new_name': gen_string('utf8'),
-           u'os_name': gen_string('utf8')},
-          {u'old_name': gen_string('alphanumeric'),
-           u'new_name': gen_string('alphanumeric'),
-           u'os_name': gen_string('alphanumeric')})
-    def test_update_arch(self, test_data):
+    @data(*generate_strings_list())
+    def test_update_arch(self, name):
         """@Test: Update Architecture with new name and OS
 
         @Feature: Architecture - Update
@@ -143,13 +112,11 @@ class Architecture(UITestCase):
         @Assert: Architecture is updated
 
         """
-        entities.OperatingSystem(name=test_data['os_name']).create_json()
+        old_name = gen_string('alpha')
+        os_name = gen_string('alpha')
+        entities.OperatingSystem(name=os_name).create()
         with Session(self.browser) as session:
-            make_arch(session, name=test_data['old_name'])
-            self.assertIsNotNone(self.architecture.search
-                                 (test_data['old_name']))
-            self.architecture.update(test_data['old_name'],
-                                     test_data['new_name'],
-                                     new_os_names=[test_data['os_name']])
-            self.assertIsNotNone(self.architecture.search
-                                 (test_data['new_name']))
+            make_arch(session, name=old_name)
+            self.assertIsNotNone(self.architecture.search(old_name))
+            self.architecture.update(old_name, name, new_os_names=[os_name])
+            self.assertIsNotNone(self.architecture.search(name))
