7528d9750180d8b36ab68d5a73d698d3b7f42681
kbidarkar <kbidarka@redhat.com>
2015-09-11 19:06:48 +0530

    Added oscap End_to_End testcase to foreman.longrun
    *) Runs rhel6 and rhel7 oscap scans on respective vm's and uploads
       the reports to satellite6.

--- robottelo/ui/oscappolicy.py
+++ robottelo/ui/oscappolicy.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 """Implements Open Scap Policy for UI."""
 from robottelo.ui.base import Base, UIError
-from robottelo.common.constants import FILTER
+from robottelo.constants import FILTER
 from robottelo.ui.locators import locators, common_locators
 from robottelo.ui.navigator import Navigator
 
