d06b5ef9f5fc513bf4c4fe61929e4dda7f1d4847
Anisha Narang <anarang@redhat.com>
2015-10-01 21:11:29 +0530

    refactoring the code to perform test on different versions of VM rhel67/rhel71 alternately

--- robottelo/ui/rhai.py
+++ robottelo/ui/rhai.py
@@ -19,23 +19,33 @@ class RHAI(Base):
         ).text
         return result
 
-    def register_client_to_rhai(self, activation_key, org):
-        self.vm = VirtualMachine(distro='rhel67')
+    def register_client_to_rhai(self, activation_key, org, rhel_distro):
+        self.vm = VirtualMachine(distro=rhel_distro)
         self.vm.create()
         # Download and Install ketello-ca rpm
         self.vm.install_katello_cert()
         self.vm.register_contenthost(activation_key, org)
 
-        # Red Hat Access Insights requires RHEL 6/7 repo and it not
+        # Red Hat Access Insights requires RHEL 6/7 repo and it is not
         # possible to sync the repo during the tests, Adding repo file.
-        self.vm.configure_rhel_repo(conf.properties['clients.rhel6_repo'])
-
+        if rhel_distro == 'rhel67':
+            rhel_repo = conf.properties['clients.rhel6_repo']
+        if rhel_distro == 'rhel71':
+            rhel_repo = conf.properties['clients.rhel7_repo']
         try:
-            insights_repo = conf.properties['insights.insights_el6repo']
-            self.vm.run('wget -O /etc/yum.repos.d/insights.repo {0}'.format(
-                insights_repo))
+            if rhel_distro == 'rhel67':
+                insights_repo = conf.properties['insights.insights_el6repo']
+            if rhel_distro == 'rhel71':
+                insights_repo = conf.properties['insights.insights_el7repo']
         except KeyError:
             pass
+
+        self.logger.info('RHEL repo {0}'.format(rhel_repo))
+        self.logger.info('Insights client repo {0}'.format(insights_repo))
+        self.vm.configure_rhel_repo(rhel_repo)
+
+        self.vm.run('wget -O /etc/yum.repos.d/insights.repo {0}'.
+                    format(insights_repo))
 
         # Install redhat-access-insights package
         package_name = 'redhat-access-insights'
@@ -46,7 +56,10 @@ class RHAI(Base):
             )
 
         # Verify if package is installed by query it
-        result = self.vm.run('rpm -q {0}'.format(package_name))
+        result = self.vm.run('rpm -qi {0}'.format(package_name))
+        self.logger.info('Insights client rpm version: {0}'.format(
+            result.stdout))
+
         if result.return_code != 0:
             raise AccessInsightsError(
                 'Unable to install redhat-access-insights rpm'
