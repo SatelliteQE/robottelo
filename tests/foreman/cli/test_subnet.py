dc3504dfa664515a4587b7fd72115de2be73451e
sthirugn <sthirugn@redhat.com>
2015-10-02 16:26:08 -0400

    ddt removal in cli subnet tests

--- tests/foreman/cli/test_subnet.py
+++ tests/foreman/cli/test_subnet.py
@@ -4,33 +4,54 @@
 import random
 import re
 
-from ddt import ddt
-from fauxfactory import gen_string, gen_integer, gen_ipaddr
+from fauxfactory import gen_integer, gen_ipaddr
 from robottelo.cli.base import CLIReturnCodeError
 from robottelo.cli.factory import make_domain, make_subnet, CLIFactoryError
 from robottelo.cli.subnet import Subnet
 from robottelo.constants import SUBNET_IPAM_TYPES
-from robottelo.decorators import (
-    data,
-    run_only_on,
-    skip_if_bug_open,
-)
+from robottelo.decorators import run_only_on, skip_if_bug_open
+from robottelo.helpers import valid_data_list
 from robottelo.test import CLITestCase
 
 
+def valid_addr_pools():
+    """Returns a tuple of valid address pools"""
+    return(
+        [gen_integer(min_value=1, max_value=255),
+         gen_integer(min_value=1, max_value=255)],
+        [gen_integer(min_value=1, max_value=255)] * 2,
+        [1, 255],
+    )
+
+
+def invalid_addr_pools():
+    """Returns a tuple of invalid address pools"""
+    return(
+        {u'from': gen_integer(min_value=1, max_value=255)},
+        {u'to': gen_integer(min_value=1, max_value=255)},
+        {u'from': gen_integer(min_value=128, max_value=255),
+         u'to': gen_integer(min_value=1, max_value=127)},
+        {u'from': 256, u'to': 257},
+    )
+
+
+def invalid_missing_attributes():
+    """Returns a tuple of invalid missing attributes"""
+    return(
+        {u'name': ''},
+        {u'network': '256.0.0.0'},
+        {u'network': ''},
+        {u'mask': '256.0.0.0'},
+        {u'mask': ''},
+        {u'mask': '255.0.255.0'}
+    )
+
+
 @run_only_on('sat')
-@ddt
 class TestSubnet(CLITestCase):
     """Subnet CLI tests."""
 
-    @data(
-        gen_string(str_type='alpha'),
-        gen_string(str_type='alphanumeric'),
-        gen_string(str_type='numeric'),
-        gen_string(str_type='latin1'),
-        gen_string(str_type='utf8'),
-    )
-    def test_positive_create_1(self, test_data):
+    def test_positive_create_1(self):
         """@Test: Check if Subnet can be created with random names
 
         @Feature: Subnet - Create
@@ -38,16 +59,12 @@ class TestSubnet(CLITestCase):
         @Assert: Subnet is created and has random name
 
         """
-        subnet = make_subnet({'name': test_data})
-        self.assertEqual(subnet['name'], test_data)
+        for name in valid_data_list():
+            with self.subTest(name):
+                subnet = make_subnet({'name': name})
+                self.assertEqual(subnet['name'], name)
 
-    @data(
-        [gen_integer(min_value=1, max_value=255),
-         gen_integer(min_value=1, max_value=255)],
-        [gen_integer(min_value=1, max_value=255)] * 2,
-        [1, 255],
-    )
-    def test_positive_create_2(self, pool):
+    def test_positive_create_2(self):
         """@Test: Create subnet with valid address pool
 
         @Feature: Subnet positive create
@@ -55,19 +72,22 @@ class TestSubnet(CLITestCase):
         @Assert: Subnet is created and address pool is set
 
         """
-        pool.sort()
-        mask = '255.255.255.0'
-        network = gen_ipaddr()  # generate pool range from network address
-        from_ip = re.sub(r'\d+$', str(pool[0]), network)
-        to_ip = re.sub(r'\d+$', str(pool[1]), network)
-        subnet = make_subnet({
-            u'from': from_ip,
-            u'mask': mask,
-            u'network': network,
-            u'to': to_ip,
-        })
-        self.assertEqual(subnet['from'], from_ip)
-        self.assertEqual(subnet['to'], to_ip)
+        for pool in valid_addr_pools():
+            with self.subTest(pool):
+                pool.sort()
+                mask = '255.255.255.0'
+                # generate pool range from network address
+                network = gen_ipaddr()
+                from_ip = re.sub(r'\d+$', str(pool[0]), network)
+                to_ip = re.sub(r'\d+$', str(pool[1]), network)
+                subnet = make_subnet({
+                    u'from': from_ip,
+                    u'mask': mask,
+                    u'network': network,
+                    u'to': to_ip,
+                })
+                self.assertEqual(subnet['from'], from_ip)
+                self.assertEqual(subnet['to'], to_ip)
 
     def test_create_subnet_with_domain(self):
         """@Test: Check if subnet with domain can be created
@@ -112,12 +132,7 @@ class TestSubnet(CLITestCase):
         self.assertIn(gateway, subnet['gateway'])
 
     @skip_if_bug_open('bugzilla', 1213437)
-    @data(
-        SUBNET_IPAM_TYPES['dhcp'],
-        SUBNET_IPAM_TYPES['internal'],
-        SUBNET_IPAM_TYPES['none'],
-    )
-    def test_create_subnet_with_ipam(self, ipam_type):
+    def test_create_subnet_with_ipam(self):
         """@Test: Check if subnet with different ipam types can be created
 
         @Feature: Subnet - Positive create
@@ -127,18 +142,14 @@ class TestSubnet(CLITestCase):
         @BZ: 1213437
 
         """
-        subnet = make_subnet({'ipam': ipam_type})
-        self.assertIn(ipam_type, subnet['ipam'])
+        for ipam_type in (SUBNET_IPAM_TYPES['dhcp'],
+                          SUBNET_IPAM_TYPES['internal'],
+                          SUBNET_IPAM_TYPES['none']):
+            with self.subTest(ipam_type):
+                subnet = make_subnet({'ipam': ipam_type})
+                self.assertIn(ipam_type, subnet['ipam'])
 
-    @data(
-        {u'name': ''},
-        {u'network': '256.0.0.0'},
-        {u'network': ''},
-        {u'mask': '256.0.0.0'},
-        {u'mask': ''},
-        {u'mask': '255.0.255.0'}
-    )
-    def test_negative_create_1(self, options):
+    def test_negative_create_1(self):
         """@Test: Create subnet with invalid or missing required attributes
 
         @Feature: Subnet create
@@ -146,17 +157,12 @@ class TestSubnet(CLITestCase):
         @Assert: Subnet is not created
 
         """
-        with self.assertRaises(CLIFactoryError):
-            make_subnet(options)
+        for options in invalid_missing_attributes():
+            with self.subTest(options):
+                with self.assertRaises(CLIFactoryError):
+                    make_subnet(options)
 
-    @data(
-        {u'from': gen_integer(min_value=1, max_value=255)},
-        {u'to': gen_integer(min_value=1, max_value=255)},
-        {u'from': gen_integer(min_value=128, max_value=255),
-         u'to': gen_integer(min_value=1, max_value=127)},
-        {u'from': 256, u'to': 257},
-    )
-    def test_negative_create_2(self, pool):
+    def test_negative_create_2(self):
         """@Test: Create subnet with invalid address pool range
 
         @Feature: Create subnet negative
@@ -167,11 +173,13 @@ class TestSubnet(CLITestCase):
         mask = '255.255.255.0'
         network = gen_ipaddr()
         opts = {u'mask': mask, u'network': network}
-        # generate pool range from network address
-        for key, val in pool.iteritems():
-            opts[key] = re.sub(r'\d+$', str(val), network)
-        with self.assertRaises(CLIFactoryError):
-            make_subnet(opts)
+        for pool in invalid_addr_pools():
+            with self.subTest(pool):
+                # generate pool range from network address
+                for key, val in pool.iteritems():
+                    opts[key] = re.sub(r'\d+$', str(val), network)
+                with self.assertRaises(CLIFactoryError):
+                    make_subnet(opts)
 
     def test_list(self):
         """@Test: Check if Subnet can be listed
@@ -189,14 +197,7 @@ class TestSubnet(CLITestCase):
         subnets_after = Subnet.list()
         self.assertGreater(len(subnets_after), len(subnets_before))
 
-    @data(
-        gen_string(str_type='alpha'),
-        gen_string(str_type='alphanumeric'),
-        gen_string(str_type='numeric'),
-        gen_string(str_type='latin1'),
-        gen_string(str_type='utf8'),
-    )
-    def test_positive_update_1(self, test_name):
+    def test_positive_update_1(self):
         """@Test: Check if Subnet name can be updated
 
         @Feature: Subnet - Update
@@ -205,12 +206,11 @@ class TestSubnet(CLITestCase):
 
         """
         new_subnet = make_subnet()
-        # Update the name
-        Subnet.update({'id': new_subnet['id'], 'new-name': test_name})
-        # Fetch it again
-        result = Subnet.info({'id': new_subnet['id']})
-        self.assertEqual(result['name'], test_name)
-        self.assertNotEqual(result['name'], new_subnet['name'])
+        for new_name in valid_data_list():
+            with self.subTest(new_name):
+                Subnet.update({'id': new_subnet['id'], 'new-name': new_name})
+                result = Subnet.info({'id': new_subnet['id']})
+                self.assertEqual(result['name'], new_name)
 
     def test_positive_update_2(self):
         """@Test: Check if Subnet network and mask can be updated
@@ -238,13 +238,7 @@ class TestSubnet(CLITestCase):
         self.assertEqual(subnet['network'], new_network)
         self.assertEqual(subnet['mask'], new_mask)
 
-    @data(
-        [gen_integer(min_value=1, max_value=255),
-         gen_integer(min_value=1, max_value=255)],
-        [gen_integer(min_value=1, max_value=255)] * 2,
-        [1, 255],
-    )
-    def test_positive_update_3(self, pool):
+    def test_positive_update_3(self):
         """@Test: Check if Subnet address pool can be updated
 
         @Feature: Subnet - Update
@@ -252,30 +246,23 @@ class TestSubnet(CLITestCase):
         @Assert: Subnet address pool is updated
 
         """
-        pool.sort()
         subnet = make_subnet({u'mask': '255.255.255.0'})
-        # generate pool range from network address
-        ip_from = re.sub(r'\d+$', str(pool[0]), subnet['network'])
-        ip_to = re.sub(r'\d+$', str(pool[1]), subnet['network'])
-        Subnet.update({
-            u'from': ip_from,
-            u'id': subnet['id'],
-            u'to': ip_to,
-        })
-        # check - subnet is updated
-        subnet = Subnet.info({u'id': subnet['id']})
-        self.assertEqual(subnet['from'], ip_from)
-        self.assertEqual(subnet['to'], ip_to)
+        for pool in valid_addr_pools():
+            with self.subTest(pool):
+                pool.sort()
+                # generate pool range from network address
+                ip_from = re.sub(r'\d+$', str(pool[0]), subnet['network'])
+                ip_to = re.sub(r'\d+$', str(pool[1]), subnet['network'])
+                Subnet.update({
+                    u'from': ip_from,
+                    u'id': subnet['id'],
+                    u'to': ip_to,
+                })
+                subnet = Subnet.info({u'id': subnet['id']})
+                self.assertEqual(subnet['from'], ip_from)
+                self.assertEqual(subnet['to'], ip_to)
 
-    @data(
-        {u'name': ''},
-        {u'network': '256.0.0.0'},
-        {u'network': ''},
-        {u'mask': '256.0.0.0'},
-        {u'mask': ''},
-        {u'mask': '255.0.255.0'}
-    )
-    def test_negative_update_1(self, options):
+    def test_negative_update_1(self):
         """@Test: Update subnet with invalid or missing required attributes
 
         @Feature: Subnet - Update
@@ -284,22 +271,17 @@ class TestSubnet(CLITestCase):
 
         """
         subnet = make_subnet()
-        options['id'] = subnet['id']
-        with self.assertRaises(CLIReturnCodeError):
-            Subnet.update(options)
-        # check - subnet is not updated
-        result = Subnet.info({u'id': subnet['id']})
-        for key in options.keys():
-            self.assertEqual(subnet[key], result[key])
+        for options in invalid_missing_attributes():
+            with self.subTest(options):
+                options['id'] = subnet['id']
+                with self.assertRaises(CLIReturnCodeError):
+                    Subnet.update(options)
+                    # check - subnet is not updated
+                    result = Subnet.info({u'id': subnet['id']})
+                    for key in options.keys():
+                        self.assertEqual(subnet[key], result[key])
 
-    @data(
-        {u'from': gen_integer(min_value=1, max_value=255)},
-        {u'to': gen_integer(min_value=1, max_value=255)},
-        {u'from': gen_integer(min_value=128, max_value=255),
-         u'to': gen_integer(min_value=1, max_value=127)},
-        {u'from': 256, u'to': 257},
-    )
-    def test_negative_update_2(self, options):
+    def test_negative_update_2(self):
         """@Test: Update subnet with invalid address pool
 
         @Feature: Subnet - Update
@@ -308,25 +290,20 @@ class TestSubnet(CLITestCase):
 
         """
         subnet = make_subnet()
-        opts = {u'id': subnet['id']}
-        # generate pool range from network address
-        for key, val in options.iteritems():
-            opts[key] = re.sub(r'\d+$', str(val), subnet['network'])
-        with self.assertRaises(CLIReturnCodeError):
-            Subnet.update(opts)
-        # check - subnet is not updated
-        result = Subnet.info({u'id': subnet['id']})
-        for key in options.keys():
-            self.assertEqual(result[key], subnet[key])
+        for options in invalid_addr_pools():
+            with self.subTest(options):
+                opts = {u'id': subnet['id']}
+                # generate pool range from network address
+                for key, val in options.iteritems():
+                    opts[key] = re.sub(r'\d+$', str(val), subnet['network'])
+                with self.assertRaises(CLIReturnCodeError):
+                    Subnet.update(opts)
+                # check - subnet is not updated
+                result = Subnet.info({u'id': subnet['id']})
+                for key in options.keys():
+                    self.assertEqual(result[key], subnet[key])
 
-    @data(
-        gen_string(str_type='alpha'),
-        gen_string(str_type='alphanumeric'),
-        gen_string(str_type='numeric'),
-        gen_string(str_type='latin1'),
-        gen_string(str_type='utf8'),
-    )
-    def test_positive_delete_1(self, test_name):
+    def test_positive_delete_1(self):
         """@Test: Check if Subnet can be deleted
 
         @Feature: Subnet - Delete
@@ -334,9 +311,9 @@ class TestSubnet(CLITestCase):
         @Assert: Subnet is deleted
 
         """
-        subnet = make_subnet({'name': test_name})
-        # Delete it
-        Subnet.delete({'id': subnet['id']})
-        # Fetch it again
-        with self.assertRaises(CLIReturnCodeError):
-            Subnet.info({'id': subnet['id']})
+        for name in valid_data_list():
+            with self.subTest(name):
+                subnet = make_subnet({'name': name})
+                Subnet.delete({'id': subnet['id']})
+                with self.assertRaises(CLIReturnCodeError):
+                    Subnet.info({'id': subnet['id']})
