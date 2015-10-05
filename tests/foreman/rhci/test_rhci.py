9782010a5f036589fefcda1cdd9367862f8860de
sthirugn <sthirugn@redhat.com>
2015-10-05 16:33:09 -0400

    ddt removal in rhci tests

--- tests/foreman/rhci/test_rhci.py
+++ tests/foreman/rhci/test_rhci.py
@@ -1,13 +1,25 @@
+# pylint:disable=too-many-public-methods
 """Unit tests for the ``fusor/api/v21/deployments`` paths."""
-from ddt import data, ddt
 from fauxfactory import gen_string
 from nailgun import entities
 from requests.exceptions import HTTPError
 from robottelo.test import APITestCase
-# pylint:disable=too-many-public-methods
 
 
-@ddt
+def valid_name_update_tests():
+    """Returns a tuple of valid names for update tests."""
+    return(
+        {'name': gen_string(str_type='alpha'),
+         'new-name': gen_string(str_type='alpha')},
+        {'name': gen_string(str_type='alphanumeric'),
+         'new-name': gen_string(str_type='alphanumeric')},
+        {'name': gen_string(str_type='cjk'),
+         'new-name': gen_string(str_type='cjk')},
+        {'name': gen_string(str_type='latin1'),
+         'new-name': gen_string(str_type='latin1')},
+    )
+
+
 class RHCIDeploymentTestCase(APITestCase):
     """Tests for the RHCI deployment API."""
 
@@ -30,13 +42,7 @@ class RHCIDeploymentTestCase(APITestCase):
         for deployment in cls.to_delete:
             deployment.delete()
 
-    @data(
-        gen_string(str_type='alpha'),
-        gen_string(str_type='alphanumeric'),
-        gen_string(str_type='cjk'),
-        gen_string(str_type='latin1'),
-    )
-    def test_positive_create_deployment(self, data):
+    def test_positive_create_deployment(self):
         """@Test: Create a simple deployment.
 
         @Assert: An RHCI deployment is created with a random name.
@@ -44,31 +50,27 @@ class RHCIDeploymentTestCase(APITestCase):
         @Feature: RHCI Deployment
 
         """
-        deployment = entities.RHCIDeployment(
-            name=data,
-            organization=self.org,
-            lifecycle_environment=self.lc_env,
-            deploy_rhev=True,
-            rhev_engine_host=self.rhev_engine_host,
-            rhev_storage_type='NFS',
-            rhev_engine_admin_password='changeme',
-        ).create()
-        self.assertEquals(deployment.name, data)
-        self.assertEqual(deployment.organization.id, self.org.id)
-        self.assertEqual(deployment.lifecycle_environment.id, self.lc_env.id)
-        self.to_delete.append(deployment)
+        for name in (gen_string(str_type='alpha'),
+                     gen_string(str_type='alphanumeric'),
+                     gen_string(str_type='cjk'),
+                     gen_string(str_type='latin1')):
+            with self.subTest(name):
+                deployment = entities.RHCIDeployment(
+                    name=name,
+                    organization=self.org,
+                    lifecycle_environment=self.lc_env,
+                    deploy_rhev=True,
+                    rhev_engine_host=self.rhev_engine_host,
+                    rhev_storage_type='NFS',
+                    rhev_engine_admin_password='changeme',
+                ).create()
+                self.assertEquals(deployment.name, name)
+                self.assertEqual(deployment.organization.id, self.org.id)
+                self.assertEqual(deployment.lifecycle_environment.id,
+                                 self.lc_env.id)
+                self.to_delete.append(deployment)
 
-    @data(
-        {'name': gen_string(str_type='alpha'),
-         'new-name': gen_string(str_type='alpha')},
-        {'name': gen_string(str_type='alphanumeric'),
-         'new-name': gen_string(str_type='alphanumeric')},
-        {'name': gen_string(str_type='cjk'),
-         'new-name': gen_string(str_type='cjk')},
-        {'name': gen_string(str_type='latin1'),
-         'new-name': gen_string(str_type='latin1')},
-    )
-    def test_positive_update_name(self, data):
+    def test_positive_update_name(self):
         """@Test: Update a deployment's name.
 
         @Assert: An RHCI deployment is updated with a random name.
@@ -76,27 +78,26 @@ class RHCIDeploymentTestCase(APITestCase):
         @Feature: RHCI Deployment
 
         """
-        deployment = entities.RHCIDeployment(
-            name=data['name'],
-            organization=self.org,
-            lifecycle_environment=self.lc_env,
-            deploy_rhev=True,
-            rhev_engine_host=self.rhev_engine_host,
-            rhev_storage_type='NFS',
-            rhev_engine_admin_password='changeme',
-        ).create()
-        self.assertEquals(deployment.name, data['name'])
+        for data in valid_name_update_tests():
+            with self.subTest(data):
+                deployment = entities.RHCIDeployment(
+                    name=data['name'],
+                    organization=self.org,
+                    lifecycle_environment=self.lc_env,
+                    deploy_rhev=True,
+                    rhev_engine_host=self.rhev_engine_host,
+                    rhev_storage_type='NFS',
+                    rhev_engine_admin_password='changeme',
+                ).create()
+                self.assertEquals(deployment.name, data['name'])
 
-        # perform the update
-        deployment.name = data['new-name']
-        deployment = deployment.update(['name'])
-        self.assertEqual(deployment.name, data['new-name'])
-        self.to_delete.append(deployment)
+                # perform the update
+                deployment.name = data['new-name']
+                deployment = deployment.update(['name'])
+                self.assertEqual(deployment.name, data['new-name'])
+                self.to_delete.append(deployment)
 
-    @data(
-        gen_string(str_type='alpha'),
-    )
-    def test_positive_delete_deployment(self, data):
+    def test_positive_delete_deployment(self):
         """@Test: Create and delete a simple deployment.
 
         @Assert: An RHCI deployment is deleted.
@@ -104,6 +105,7 @@ class RHCIDeploymentTestCase(APITestCase):
         @Feature: RHCI Deployment
 
         """
+        data = gen_string(str_type='alpha')
         deployment = entities.RHCIDeployment(
             name=data,
             organization=self.org,
