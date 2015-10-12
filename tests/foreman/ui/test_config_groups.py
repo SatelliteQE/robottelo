ccd7d817b8555dc4e66c56e25d4fe2cf7ae16ccc
sthirugn <sthirugn@redhat.com>
2015-10-12 13:11:34 -0400

    ddt removal in ui config groups tests

--- tests/foreman/ui/test_config_groups.py
+++ tests/foreman/ui/test_config_groups.py
@@ -1,8 +1,7 @@
 """Test class for Config Groups UI"""
 
-from ddt import ddt
 from fauxfactory import gen_string
-from robottelo.decorators import data, run_only_on
+from robottelo.decorators import run_only_on
 from robottelo.helpers import (
     generate_strings_list,
     invalid_values_list,
@@ -15,12 +14,10 @@ from robottelo.ui.session import Session
 
 
 @run_only_on('sat')
-@ddt
 class ConfigGroups(UITestCase):
     """Implements Config Groups tests in UI."""
 
-    @data(*valid_data_list())
-    def test_create_positive(self, name):
+    def test_create_positive(self):
         """@Test: Create new Config-Group
 
         @Feature: Config-Groups - Positive Create
@@ -29,12 +26,13 @@ class ConfigGroups(UITestCase):
 
         """
         with Session(self.browser) as session:
-            make_config_groups(session, name=name)
-            search = self.configgroups.search(name)
-            self.assertIsNotNone(search)
+            for name in valid_data_list():
+                with self.subTest(name):
+                    make_config_groups(session, name=name)
+                    search = self.configgroups.search(name)
+                    self.assertIsNotNone(search)
 
-    @data(*invalid_values_list())
-    def test_create_negative(self, name):
+    def test_create_negative(self):
         """@Test: Try to create config group and use whitespace, blank, tab
         symbol or too long string of different types as its name value
 
@@ -44,15 +42,16 @@ class ConfigGroups(UITestCase):
 
         """
         with Session(self.browser) as session:
-            make_config_groups(session, name=name)
-            error = session.nav.wait_until_element(
-                common_locators['name_haserror'])
-            self.assertIsNotNone(error)
-            search = self.configgroups.search(name)
-            self.assertIsNone(search)
+            for name in invalid_values_list('ui'):
+                with self.subTest(name):
+                    make_config_groups(session, name=name)
+                    error = session.nav.wait_until_element(
+                        common_locators['name_haserror'])
+                    self.assertIsNotNone(error)
+                    search = self.configgroups.search(name)
+                    self.assertIsNone(search)
 
-    @data(*generate_strings_list())
-    def test_update_positive(self, new_name):
+    def test_update_positive(self):
         """@Test: Update selected config-group
 
         @Feature: Config-Groups - Positive Update
@@ -62,15 +61,16 @@ class ConfigGroups(UITestCase):
         """
         name = gen_string('alpha')
         with Session(self.browser) as session:
-            make_config_groups(session, name=name)
-            search = self.configgroups.search(name)
-            self.assertIsNotNone(search)
-            self.configgroups.update(name, new_name)
-            search = self.configgroups.search(new_name)
-            self.assertIsNotNone(search)
+            for new_name in generate_strings_list():
+                with self.subTest(new_name):
+                    make_config_groups(session, name=name)
+                    search = self.configgroups.search(name)
+                    self.assertIsNotNone(search)
+                    self.configgroups.update(name, new_name)
+                    search = self.configgroups.search(new_name)
+                    self.assertIsNotNone(search)
 
-    @data(*generate_strings_list(len1=8))
-    def test_delete_positive(self, name):
+    def test_delete_positive(self):
         """@Test: Delete selected config-groups
 
         @Feature: Config-Groups - Positive delete
@@ -79,5 +79,7 @@ class ConfigGroups(UITestCase):
 
         """
         with Session(self.browser) as session:
-            make_config_groups(session, name=name)
-            self.configgroups.delete(name)
+            for name in generate_strings_list(len1=8):
+                with self.subTest(name):
+                    make_config_groups(session, name=name)
+                    self.configgroups.delete(name)
