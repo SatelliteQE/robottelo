85ce7ebc46ecaeb9e85894fb83008ffdb7d81642
sthirugn <sthirugn@redhat.com>
2015-10-21 13:19:52 -0400

    ddt removal in ui hostgroup tests

--- tests/foreman/ui/test_hostgroup.py
+++ tests/foreman/ui/test_hostgroup.py
@@ -1,23 +1,21 @@
 # -*- encoding: utf-8 -*-
+# pylint: disable=invalid-name
 """Test class for Host Group UI"""
 
-from ddt import ddt, data
 from fauxfactory import gen_string
 from robottelo.decorators import run_only_on
-from robottelo.helpers import generate_strings_list
+from robottelo.helpers import generate_strings_list, invalid_values_list
 from robottelo.test import UITestCase
 from robottelo.ui.factory import make_hostgroup
 from robottelo.ui.locators import common_locators
 from robottelo.ui.session import Session
 
 
-@ddt
 class Hostgroup(UITestCase):
     """Implements HostGroup tests from UI"""
 
-    @data(*generate_strings_list(len1=4))
     @run_only_on('sat')
-    def test_create_hostgroup(self, name):
+    def test_positive_create_hostgroup(self):
         """@Test: Create new hostgroup
 
         @Feature: Hostgroup - Positive Create
@@ -26,44 +24,14 @@ class Hostgroup(UITestCase):
 
         """
         with Session(self.browser) as session:
-            make_hostgroup(session, name=name)
-            self.assertIsNotNone(self.hostgroup.search(name))
+            for name in generate_strings_list(len1=4):
+                with self.subTest(name):
+                    make_hostgroup(session, name=name)
+                    self.assertIsNotNone(self.hostgroup.search(name))
 
-    @data(*generate_strings_list(len1=4))
     @run_only_on('sat')
-    def test_delete_hostgroup(self, name):
-        """@Test: Delete a hostgroup
-
-        @Feature: Hostgroup - Positive Delete
-
-        @Assert: Hostgroup is deleted
-
-        """
-        with Session(self.browser) as session:
-            make_hostgroup(session, name=name)
-            self.hostgroup.delete(name)
-
-    @data(*generate_strings_list(len1=4))
-    @run_only_on('sat')
-    def test_update_hostgroup(self, new_name):
-        """@Test: Update hostgroup with a new name
-
-        @Feature: Hostgroup - Positive Update
-
-        @Assert: Hostgroup is updated
-
-        """
-        name = gen_string('alpha')
-        with Session(self.browser) as session:
-            make_hostgroup(session, name=name)
-            self.assertIsNotNone(self.hostgroup.search(name))
-            self.hostgroup.update(name, new_name=new_name)
-            self.assertIsNotNone(self.hostgroup.search(new_name))
-
-    @data(*generate_strings_list(len1=256))
-    @run_only_on('sat')
-    def test_negative_create_hostgroup_with_too_long_name(self, name):
-        """@Test: Create new hostgroup with 256 chars in name
+    def test_negative_create_hostgroup(self):
+        """@Test: Create new hostgroup with invalid names
 
         @Feature: Hostgroup - Negative Create
 
@@ -71,9 +39,11 @@ class Hostgroup(UITestCase):
 
         """
         with Session(self.browser) as session:
-            make_hostgroup(session, name=name)
-            self.assertIsNotNone(self.hostgroup.wait_until_element
-                                 (common_locators['name_haserror']))
+            for name in invalid_values_list(interface='ui'):
+                with self.subTest(name):
+                    make_hostgroup(session, name=name)
+                    self.assertIsNotNone(self.hostgroup.wait_until_element
+                                         (common_locators['name_haserror']))
 
     @run_only_on('sat')
     def test_negative_create_hostgroup_with_same_name(self):
@@ -92,17 +62,36 @@ class Hostgroup(UITestCase):
             self.assertIsNotNone(self.hostgroup.wait_until_element
                                  (common_locators['name_haserror']))
 
-    @data('', '  ')
     @run_only_on('sat')
-    def test_negative_create_hostgroup_with_blank_name(self, name):
-        """@Test: Create new hostgroup with whitespaces in name
+    def test_delete_hostgroup(self):
+        """@Test: Delete a hostgroup
 
-        @Feature: Hostgroup - Negative Create
+        @Feature: Hostgroup - Positive Delete
 
-        @Assert: Hostgroup is not created
+        @Assert: Hostgroup is deleted
 
         """
         with Session(self.browser) as session:
+            for name in generate_strings_list(len1=4):
+                with self.subTest(name):
+                    make_hostgroup(session, name=name)
+                    self.hostgroup.delete(name)
+
+    @run_only_on('sat')
+    def test_update_hostgroup(self):
+        """@Test: Update hostgroup with a new name
+
+        @Feature: Hostgroup - Positive Update
+
+        @Assert: Hostgroup is updated
+
+        """
+        name = gen_string('alpha')
+        with Session(self.browser) as session:
             make_hostgroup(session, name=name)
-            self.assertIsNotNone(self.hostgroup.wait_until_element
-                                 (common_locators['name_haserror']))
+            self.assertIsNotNone(self.hostgroup.search(name))
+            for new_name in generate_strings_list(len1=4):
+                with self.subTest(new_name):
+                    self.hostgroup.update(name, new_name=new_name)
+                    self.assertIsNotNone(self.hostgroup.search(new_name))
+                    name = new_name  # for next iteration
