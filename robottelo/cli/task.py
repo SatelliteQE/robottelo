1beb51aa8d7c4eb40c1d50009fb68e10ae43d748
Lukas Pramuk <lpramuk@redhat.com>
2015-10-27 17:28:21 +0100

    CLI hammer task
    + added hammer task subcommand

---
+++ robottelo/cli/task.py
@@ -0,0 +1,55 @@
+# -*- encoding: utf-8 -*-
+"""
+Usage::
+
+    hammer task [OPTIONS] SUBCOMMAND [ARG] ...
+
+Parameters::
+
+    SUBCOMMAND                    subcommand
+    [ARG] ...                     subcommand arguments
+
+Subcommands::
+
+    list                          List tasks
+    progress                      Show the progress of the task
+    resume                        Resume all tasks paused in error state
+"""
+
+from robottelo.cli.base import Base
+
+
+class Task(Base):
+    """
+    Manipulates Foreman's task.
+    """
+    command_base = 'task'
+
+    @classmethod
+    def progress(cls, options=None):
+        """Shows a task progress
+
+        Usage::
+            hammer task progress [OPTIONS]
+
+        Options::
+            --id ID                       UUID of the task
+            --name NAME                   Name to search by
+        """
+        cls.command_sub = 'progress'
+        return cls.execute(cls._construct_command(options))
+
+    @classmethod
+    def resume(cls, options=None):
+        """Resumes a task
+
+        Usage:
+            hammer task resume [OPTIONS]
+
+        Options:
+            --search SEARCH               Resume tasks matching search string
+            --task-ids TASK_IDS           Comma separated list of values.
+            --tasks TASK_NAMES            Comma separated list of values.
+        """
+        cls.command_sub = 'resume'
+        return cls.execute(cls._construct_command(options))
