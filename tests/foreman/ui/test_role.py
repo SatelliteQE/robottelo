7717f44e07b580d6f6587e40649218a4c8d1752d
sthirugn <sthirugn@redhat.com>
2015-10-27 18:44:23 -0400

    ddt removal in ui role tests

--- tests/foreman/ui/test_role.py
+++ tests/foreman/ui/test_role.py
@@ -1,22 +1,20 @@
 # -*- encoding: utf-8 -*-
+# pylint: disable=invalid-name
 """Test class for Roles UI"""
 
-from ddt import ddt, data
 from fauxfactory import gen_string
 from nailgun import entities
-from robottelo.helpers import generate_strings_list, invalid_names_list
+from robottelo.helpers import generate_strings_list, invalid_values_list
 from robottelo.test import UITestCase
 from robottelo.ui.factory import make_role
 from robottelo.ui.locators import common_locators
 from robottelo.ui.session import Session
 
 
-@ddt
 class Role(UITestCase):
     """Implements Roles tests from UI"""
 
-    @data(*generate_strings_list(len1=10))
-    def test_create_role_basic(self, name):
+    def test_create_role_basic(self):
         """@Test: Create new role
 
         @Feature: Role - Positive Create
@@ -25,26 +23,13 @@ class Role(UITestCase):
 
         """
         with Session(self.browser) as session:
-            make_role(session, name=name)
-            self.assertIsNotNone(self.role.search(name))
+            for name in generate_strings_list(len1=10):
+                with self.subTest(name):
+                    make_role(session, name=name)
+                    self.assertIsNotNone(self.role.search(name))
 
-    @data('', ' ')
-    def test_negative_create_role_with_blank_name(self, name):
-        """@Test: Create new role with blank and whitespace in name
-
-        @Feature: Role - Negative Create
-
-        @Assert: Role is not created
-
-        """
-        with Session(self.browser) as session:
-            make_role(session, name=name)
-            self.assertIsNotNone(session.nav.wait_until_element(
-                common_locators['name_haserror']))
-
-    @data(*invalid_names_list())
-    def test_negative_create_role_with_too_long_names(self, name):
-        """@Test: Create new role with 256 characters in name
+    def test_negative_create_role_invalid_names(self):
+        """@Test: Create new role with invalid names
 
         @Feature: Role - Negative Create
 
@@ -52,12 +37,13 @@ class Role(UITestCase):
 
         """
         with Session(self.browser) as session:
-            make_role(session, name=name)
-            self.assertIsNotNone(session.nav.wait_until_element(
-                common_locators['name_haserror']))
+            for name in invalid_values_list(interface='ui'):
+                with self.subTest(name):
+                    make_role(session, name=name)
+                    self.assertIsNotNone(session.nav.wait_until_element(
+                        common_locators['name_haserror']))
 
-    @data(*generate_strings_list(len1=10))
-    def test_remove_role(self, name):
+    def test_remove_role(self):
         """@Test: Delete an existing role
 
         @Feature: Role - Positive Delete
@@ -66,11 +52,12 @@ class Role(UITestCase):
 
         """
         with Session(self.browser) as session:
-            make_role(session, name=name)
-            self.role.delete(name)
+            for name in generate_strings_list(len1=10):
+                with self.subTest(name):
+                    make_role(session, name=name)
+                    self.role.delete(name)
 
-    @data(*generate_strings_list(len1=10))
-    def test_update_role_name(self, new_name):
+    def test_update_role_name(self):
         """@Test: Update role name
 
         @Feature: Role - Positive Update
@@ -82,8 +69,11 @@ class Role(UITestCase):
         with Session(self.browser) as session:
             make_role(session, name=name)
             self.assertIsNotNone(self.role.search(name))
-            self.role.update(name, new_name)
-            self.assertIsNotNone(self.role.search(new_name))
+            for new_name in generate_strings_list(len1=10):
+                with self.subTest(new_name):
+                    self.role.update(name, new_name)
+                    self.assertIsNotNone(self.role.search(new_name))
+                    name = new_name  # for next iteration
 
     def test_update_role_permission(self):
         """@Test: Update role permissions
