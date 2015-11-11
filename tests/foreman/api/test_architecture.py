0b1d982bf129cdd4e863449f5db9e90c86a552d2
Og B. Maciel <omaciel@ogmaciel.com>
2015-11-11 17:14:53 -0500

    Added 'CRUD' tests to Architecture via API.
    Added 5 new basic 'CRUD' tests for Architecture via the API.

--- tests/foreman/api/test_architecture.py
+++ tests/foreman/api/test_architecture.py
@@ -1,7 +1,9 @@
 """Unit tests for the ``architectures`` paths."""
 from fauxfactory import gen_utf8
 from nailgun import client, entities
+from requests.exceptions import HTTPError
 from robottelo.config import settings
+from robottelo.datafactory import invalid_names_list, valid_data_list
 from robottelo.decorators import skip_if_bug_open
 from robottelo.test import APITestCase
 
@@ -17,7 +19,6 @@ class ArchitectureTestCase(APITestCase):
         organization.
 
         @Feature: Architecture
-
         """
         name = gen_utf8()
         os_id = entities.OperatingSystem().create_json()['id']
@@ -44,7 +45,6 @@ class ArchitectureTestCase(APITestCase):
         read back from the server.
 
         @Feature: Architecture
-
         """
         operating_sys = entities.OperatingSystem().create()
         arch = entities.Architecture(operatingsystem=[operating_sys]).create()
@@ -52,3 +52,76 @@ class ArchitectureTestCase(APITestCase):
             {operating_sys.id},
             {os.id for os in arch.operatingsystem},
         )
+
+    def test_positive_create_name(self):
+        """@Test: Create an architecture providing the initial name.
+
+        @Assert: Architecture is created and contains provided name.
+
+        @Feature: Architecture
+        """
+        for name in valid_data_list():
+            with self.subTest(name):
+                arch = entities.Architecture(name=name).create()
+                self.assertEqual(name, arch.name)
+
+    def test_negative_create_name(self):
+        """@Test: Create architecture providing an invalid initial name.
+        set.
+
+        @Assert: Architecture is not created
+
+        @Feature: Architecture
+        """
+        for name in invalid_names_list():
+            with self.subTest(name):
+                with self.assertRaises(HTTPError):
+                    entities.Architecture(name=name).create()
+
+    def test_positive_update_name(self):
+        """@Test: Create architecture then update its name to another
+        valid name.
+
+        @Assert: Architecture is created, and its name can be updated.
+
+        @Feature: Architecture
+
+        """
+        arch = entities.Architecture().create()
+
+        for new_name in valid_data_list():
+            with self.subTest(new_name):
+                entities.Architecture(
+                    id=arch.id, name=new_name).update(['name'])
+                updated = entities.Architecture(id=arch.id).read()
+                self.assertEqual(new_name, updated.name)
+
+    def test_negative_update_name(self):
+        """@Test: Create architecture then update its name to an invalid name.
+
+        @Assert: Architecture is created, and its name is not updated.
+
+        @Feature: Architecture
+        """
+        arch = entities.Architecture().create()
+        for new_name in invalid_names_list():
+            with self.subTest(new_name):
+                with self.assertRaises(HTTPError):
+                    entities.Architecture(
+                        id=arch.id, name=new_name).update(['name'])
+                arch = entities.Architecture(id=arch.id).read()
+                self.assertNotEqual(arch.name, new_name)
+
+    def test_positive_delete(self):
+        """@Test: Create architecture and then delete it.
+
+        @Assert: architecture is successfully deleted.
+
+        @Feature: Architecture
+        """
+        for name in valid_data_list():
+            with self.subTest(name):
+                arch = entities.Architecture(name=name).create()
+                arch.delete()
+                with self.assertRaises(HTTPError):
+                    entities.Architecture(id=arch.id).read()
