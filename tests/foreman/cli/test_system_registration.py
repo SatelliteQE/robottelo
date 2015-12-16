fcc39a460b5e4df6aad983d0d7e4ae188ec9f319
sthirugn <sthirugn@redhat.com>
2015-12-16 17:14:12 -0500

    Rename cli content host tests as per issue #3083

--- tests/foreman/cli/test_system_registration.py
+++ tests/foreman/cli/test_system_registration.py
@@ -7,96 +7,58 @@ class SystemRegistrationTestCase(CLITestCase):
     """Tests for System Registration CLI"""
 
     @stubbed()
-    def test_rpm_install(self):
-        """@test: rpm can be retrieved
+    def test_positive_register_with_no_ak(self):
+        """@test: Register Content host to satellite without activation key
 
-        @feature: system registration
+        @feature: Content host
 
-        @assert: rpm required for modifying rhsm.conf can be installed from sat
-        host
-
-        @assert: rhsm.conf has been updated appropriately.
+        @assert: Content host successfully registered to appropriate org
 
         @status: Manual
         """
 
     @stubbed()
-    def test_register_no_ak(self):
-        """@test: register system to sat without activation key
+    def test_negative_register_twice(self):
+        """@test: Attempt to register a Content host twice to Satellite
 
-        @feature: system registration
+        @feature: Content host
 
-        @assert: system successfully registered to appropriate org
-
-        @status: Manual
-        """
-
-    @stubbed()
-    def test_register_with_ak(self):
-        """@test: register system with activation key
-
-        @feature: system registration
-
-        @assert: system successfully registered to appropriate org and
-        subscribed to content as listed in key
+        @assert: Content host cannot be registered twice
 
         @status: Manual
         """
 
     @stubbed()
-    def test_cannot_register_twice_negative(self):
-        """@test: Attempt to register a system twice to an instance
+    def test_positive_list(self):
+        """@test: List Content hosts for a given org
 
-        @feature: system registration
+        @feature: Content host
 
-        @assert: system cannot be registered twice via RHSM
-
-        @status: Manual
-        """
-
-    @stubbed()
-    def test_registered_systems_pull_content(self):
-        # variations:  RH rpms/errata; custom content repos
-        """@test: assure RH RPM content can be installed
-
-        @feature: system registration
-
-        @assert: RH RPM/errata content can be retrieved/installed via sat
+        @assert: Content hosts are listed for the given org
 
         @status: Manual
         """
 
     @stubbed()
-    def test_registered_system_can_be_listed_cli(self):
-        """@test: perform a system list for a given org
+    def test_positive_unregister(self):
+        """@test: Unregister Content host
 
-        @feature: system registration
+        @feature: Content host
 
-        @assert: newly registered system appears in system list
-
-        @status: Manual
-        """
-
-    @stubbed()
-    def test_system_deregister_cli(self):
-        """@test: deregister/delete system via RHSM
-
-        @feature: system registration
-
-        @assert: after deregistering, performing a system list via CLI
-        assures system no longer appears in CLI
+        @assert: After unregistering, Content hosts list for the org does not
+        show the Content host
 
         @status: Manual
         """
 
     @stubbed()
-    def test_deregistered_system_cannot_pull_content(self):
-        """@test: try and retrieve content (via yum, etc.) after system has
-        been removed from sat
+    def test_negative_unregistered_chost_pull_content(self):
+        """@test: Attempt to retrieve content after Content host has been
+        unregistered from Satellite
 
-        @feature: system registration
+        @feature: Content host
 
-        @assert: system can no longer retrieve content from satellite
+        @assert: Content host can no longer retrieve content from satellite
 
         @status: Manual
         """
