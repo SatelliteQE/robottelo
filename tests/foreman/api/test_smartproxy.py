a94831a27733a89bb75f73518420817269d1ca49
Andrii Balakhtar <balakhtara@gmail.com>
2015-12-18 21:11:53 +0200

    Implement API smartproxy stubs

--- tests/foreman/api/test_smartproxy.py
+++ tests/foreman/api/test_smartproxy.py
@@ -1,11 +1,8 @@
 """Tests for the ``smart_proxies`` paths."""
 from nailgun import entities
 from robottelo.decorators import (
-    run_only_on,
     skip_if_bug_open,
-    stubbed,
     tier1,
-    tier2,
 )
 from robottelo.test import APITestCase
 from robottelo.api.utils import one_to_many_names
@@ -63,109 +60,3 @@ class SmartProxyMissingAttrTestCase(APITestCase):
             1,
             'None of {0} are in {1}'.format(names, self.smart_proxy_attrs),
         )
-
-
-class SmartProxyTestCaseStub(APITestCase):
-    """Incomplete tests for smart proxies.
-
-    When implemented, each of these tests should probably be data-driven. A
-    decorator of this form might be used::
-
-        @data(
-            medium name is alpha,
-            medium name is alpha_numeric,
-            medium name is html,
-            medium name is latin1,
-            medium name is numeric,
-            medium name is utf-8,
-        )
-
-    """
-
-    @run_only_on('sat')
-    @stubbed()
-    @tier2
-    def test_positive_add_by_name_org_name(self):
-        """
-        @feature: Organizations
-        @test: Add a smart proxy by using organization name and smartproxy name
-        @assert: smartproxy is added
-        @status: manual
-        """
-
-    @run_only_on('sat')
-    @stubbed()
-    @tier2
-    def test_positive_add_by_name_org_id(self):
-        """
-        @feature: Organizations
-        @test: Add a smart proxy by using organization ID and smartproxy name
-        @assert: smartproxy is added
-        @status: manual
-        """
-
-    @run_only_on('sat')
-    @stubbed()
-    @tier2
-    def test_positive_add_by_id_org_name(self):
-        """
-        @feature: Organizations
-        @test: Add a smart proxy by using organization name and smartproxy ID
-        @assert: smartproxy is added
-        @status: manual
-        """
-
-    @run_only_on('sat')
-    @stubbed()
-    @tier2
-    def test_positive_add_by_id_org_id(self):
-        """
-        @feature: Organizations
-        @test: Add a smart proxy by using organization ID and smartproxy ID
-        @assert: smartproxy is added
-        @status: manual
-        """
-
-    @run_only_on('sat')
-    @stubbed()
-    @tier2
-    def test_positive_remove_by_name_org_name(self):
-        """
-        @feature: Organizations
-        @test: Remove smartproxy by using organization name and smartproxy name
-        @assert: smartproxy is added then removed
-        @status: manual
-        """
-
-    @run_only_on('sat')
-    @stubbed()
-    @tier2
-    def test_positive_remove_by_name_org_id(self):
-        """
-        @feature: Organizations
-        @test: Remove smartproxy by using organization ID and smartproxy name
-        @assert: smartproxy is added then removed
-        @status: manual
-        """
-
-    @run_only_on('sat')
-    @stubbed()
-    @tier2
-    def test_positive_remove_by_id_org_name(self):
-        """
-        @feature: Organizations
-        @test: Remove smartproxy by using organization name and smartproxy ID
-        @assert: smartproxy is added then removed
-        @status: manual
-        """
-
-    @run_only_on('sat')
-    @stubbed()
-    @tier2
-    def test_positive_remove_by_id_org_id(self):
-        """
-        @feature: Organizations
-        @test: Remove smartproxy by using organization ID and smartproxy ID
-        @assert: smartproxy is added then removed
-        @status: manual
-        """
