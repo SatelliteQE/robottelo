dbf7823561ec0e1c1d52d58bbbb6be6f91e9ec35
oshtaier <alexshtayer@yahoo.com>
2016-03-14 17:46:56 +0200

    Fixes Subnet entity for 6.2
    Closes #3338

--- robottelo/ui/subnet.py
+++ robottelo/ui/subnet.py
@@ -22,47 +22,41 @@ class Subnet(Base):
                           subnet_gateway=None, subnet_primarydns=None,
                           subnet_secondarydns=None):
         """Configures the subnet."""
-        domain_tablocator = tab_locators['subnet.tab_domain']
         if subnet_network:
-            if self.wait_until_element(locators['subnet.network']):
-                self.text_field_update(locators['subnet.network'],
-                                       subnet_network)
+            self.text_field_update(locators['subnet.network'], subnet_network)
         if subnet_mask:
-            if self.wait_until_element(locators['subnet.mask']):
-                self.text_field_update(locators['subnet.mask'],
-                                       subnet_mask)
+            self.text_field_update(locators['subnet.mask'], subnet_mask)
         if subnet_gateway:
-            if self.wait_until_element(locators['subnet.gateway']):
-                self.text_field_update(locators['subnet.gateway'],
-                                       subnet_gateway)
+            self.text_field_update(locators['subnet.gateway'], subnet_gateway)
         if subnet_primarydns:
-            if self.wait_until_element(locators['subnet.primarydns']):
-                self.text_field_update(locators['subnet.primarydns'],
-                                       subnet_primarydns)
+            self.text_field_update(
+                locators['subnet.primarydns'], subnet_primarydns)
         if subnet_secondarydns:
-            if self.wait_until_element(locators['subnet.secondarydns']):
-                self.text_field_update(locators['subnet.secondarydns'],
-                                       subnet_secondarydns)
+            self.text_field_update(
+                locators['subnet.secondarydns'], subnet_secondarydns)
         if domains:
-            self.configure_entity(domains, FILTER['sub_domain'],
-                                  tab_locator=domain_tablocator)
+            self.configure_entity(
+                domains,
+                FILTER['sub_domain'],
+                tab_locator=tab_locators['subnet.tab_domain']
+            )
 
-    def create(self, orgs, subnet_name=None,
-               subnet_network=None, subnet_mask=None, subnet_gateway=None,
-               subnet_primarydns=None, subnet_secondarydns=None,
-               domains=None, org_select=True):
+    def create(self, orgs, subnet_name=None, subnet_network=None,
+               subnet_mask=None, subnet_gateway=None, subnet_primarydns=None,
+               subnet_secondarydns=None, domains=None, org_select=True):
         """Create Subnet from UI"""
         self.click(locators['subnet.new'])
-
-        if self.wait_until_element(locators['subnet.name']):
-            self.find_element(locators['subnet.name']).send_keys(subnet_name)
+        self.wait_until_element(locators['subnet.name']).send_keys(subnet_name)
         self._configure_subnet(subnet_network, subnet_mask, domains,
                                subnet_gateway, subnet_primarydns,
                                subnet_secondarydns)
         if orgs:
-            self.configure_entity(orgs, FILTER['subnet_org'],
-                                  tab_locator=tab_locators['tab_org'],
-                                  entity_select=org_select)
+            self.configure_entity(
+                orgs,
+                FILTER['subnet_org'],
+                tab_locator=tab_locators['tab_org'],
+                entity_select=org_select
+            )
         self.click(common_locators['submit'])
 
     def delete(self, subnet_name, really=True):
@@ -77,20 +71,16 @@ class Subnet(Base):
         """Search Subnet name, network and mask to validate results."""
         result = None
         subnet_object = self.search(subnet_name)
-        if subnet_object:
-            subnet_object.click()
-            if self.wait_until_element(locators['subnet.name']):
-                result = dict([('name', None), ('network', None),
-                               ('mask', None)])
-                result['name'] = self.find_element(
-                    locators['subnet.name']
-                ).get_attribute('value')
-                result['network'] = self.find_element(
-                    locators['subnet.network']
-                ).get_attribute('value')
-                result['mask'] = self.find_element(
-                    locators['subnet.mask']
-                ).get_attribute('value')
+        self.click(subnet_object)
+        if self.wait_until_element(locators['subnet.name']):
+            result = dict(
+                [('name', None), ('network', None), ('mask', None)])
+            result['name'] = self.find_element(
+                locators['subnet.name']).get_attribute('value')
+            result['network'] = self.find_element(
+                locators['subnet.network']).get_attribute('value')
+            result['mask'] = self.find_element(
+                locators['subnet.mask']).get_attribute('value')
         return result
 
     def update(self, subnet_name, orgs=None, new_orgs=None, org_select=False,
@@ -98,18 +88,16 @@ class Subnet(Base):
                new_subnet_mask=None):
         """Update subnet name, network and mask from UI."""
         subnet_object = self.search(subnet_name)
-
-        if subnet_object:
-            subnet_object.click()
-            if new_subnet_name:
-                if self.wait_until_element(locators['subnet.name']):
-                    self.field_update('subnet.name', new_subnet_name)
-            self._configure_subnet(new_subnet_network, new_subnet_mask)
-            self.configure_entity(
-                orgs,
-                FILTER['subnet_org'],
-                tab_locator=tab_locators['tab_org'],
-                new_entity_list=new_orgs,
-                entity_select=org_select,
-            )
-            self.click(common_locators['submit'])
+        self.click(subnet_object)
+        if new_subnet_name:
+            if self.wait_until_element(locators['subnet.name']):
+                self.field_update('subnet.name', new_subnet_name)
+        self._configure_subnet(new_subnet_network, new_subnet_mask)
+        self.configure_entity(
+            orgs,
+            FILTER['subnet_org'],
+            tab_locator=tab_locators['tab_org'],
+            new_entity_list=new_orgs,
+            entity_select=org_select,
+        )
+        self.click(common_locators['submit'])
