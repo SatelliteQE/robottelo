396ea5e87828fb4736786887052d2066aefbec0d
Andrii Balakhtar <balakhtara@gmail.com>
2016-03-23 17:37:50 +0200

    Implement API bookmarks stubs

--- tests/foreman/api/test_bookmarks.py
+++ tests/foreman/api/test_bookmarks.py
@@ -1,7 +1,11 @@
 """Test classes for Bookmark tests"""
 # -*- encoding: utf-8 -*-
+from fauxfactory import gen_string
+from nailgun import entities
+from requests.exceptions import HTTPError
 from robottelo.constants import BOOKMARK_ENTITIES
-from robottelo.decorators import stubbed, tier1
+from robottelo.datafactory import invalid_values_list, valid_data_list
+from robottelo.decorators import skip_if_bug_open, tier1
 from robottelo.test import APITestCase
 
 
@@ -9,7 +13,6 @@ class BookmarkTestCase(APITestCase):
     """Test for common Bookmark operations via API"""
 
     # CREATE TESTS
-    @stubbed()
     @tier1
     def test_positive_create_with_name(self):
         """Create a bookmark
@@ -24,14 +27,18 @@ class BookmarkTestCase(APITestCase):
 
         @Assert: No errors, Bookmark is listed, controller matches the
         controller the bookmark was created for
-
-        @Status: Manual
         """
         for entity in BOOKMARK_ENTITIES:
             with self.subTest(entity['controller']):
-                pass
+                for name in valid_data_list():
+                    with self.subTest(name):
+                        bm = entities.Bookmark(
+                            controller=entity['controller'],
+                            name=name,
+                        ).create()
+                        self.assertEqual(bm.controller, entity['controller'])
+                        self.assertEqual(bm.name, name)
 
-    @stubbed()
     @tier1
     def test_positive_create_with_query(self):
         """Create a bookmark
@@ -46,14 +53,18 @@ class BookmarkTestCase(APITestCase):
 
         @Assert: No errors, Bookmark is listed, controller matches the
         controller the bookmark was created for
-
-        @Status: Manual
         """
         for entity in BOOKMARK_ENTITIES:
             with self.subTest(entity['controller']):
-                pass
+                for query in valid_data_list():
+                    with self.subTest(query):
+                        bm = entities.Bookmark(
+                            controller=entity['controller'],
+                            query=query,
+                        ).create()
+                        self.assertEqual(bm.controller, entity['controller'])
+                        self.assertEqual(bm.query, query)
 
-    @stubbed()
     @tier1
     def test_positive_create_public(self):
         """Create a public bookmark
@@ -67,35 +78,44 @@ class BookmarkTestCase(APITestCase):
 
         @Assert: No errors, Bookmark is listed, controller matches the entity
         the bookmark was created for and is displayed as public
-
-        @Status: Manual
         """
         for entity in BOOKMARK_ENTITIES:
             with self.subTest(entity['controller']):
-                pass
+                for public in (True, False):
+                    with self.subTest(public):
+                        bm = entities.Bookmark(
+                            controller=entity['controller'],
+                            public=public,
+                        ).create()
+                        self.assertEqual(bm.controller, entity['controller'])
+                        self.assertEqual(bm.public, public)
 
-    @stubbed()
     @tier1
-    def test_negative_create_empty_name(self):
-        """Create a bookmark with empty name
+    def test_negative_create_with_invalid_name(self):
+        """Create a bookmark with invalid name
 
         @Feature: Scoped Search Bookmark Create
 
         @Steps:
 
-        1. Create a bookmark with providing an empty name
+        1. Attempt to create a bookmark with providing an invalid name
         2. List the bookmarks
 
-        @Assert: Error returned - name cannot be empty, Bookmark is not created
-        (not listed)
-
-        @Status: Manual
+        @Assert: Error returned, Bookmark is not created (not listed)
         """
         for entity in BOOKMARK_ENTITIES:
             with self.subTest(entity['controller']):
-                pass
+                for name in invalid_values_list():
+                    with self.subTest(name):
+                        with self.assertRaises(HTTPError):
+                            entities.Bookmark(
+                                controller=entity['controller'],
+                                name=name,
+                            ).create()
+                        result = entities.Bookmark().search(
+                            query={'search': u'name="{0}"'.format(name)})
+                        self.assertEqual(len(result), 0)
 
-    @stubbed()
     @tier1
     def test_negative_create_empty_query(self):
         """Create a bookmark with empty query
@@ -109,14 +129,20 @@ class BookmarkTestCase(APITestCase):
 
         @Assert: Error notification - search query cannot be empty, Bookmark is
         not created (not listed)
-
-        @Status: Manual
         """
         for entity in BOOKMARK_ENTITIES:
             with self.subTest(entity['controller']):
-                pass
+                name = gen_string('alpha')
+                with self.assertRaises(HTTPError):
+                    entities.Bookmark(
+                        controller=entity['controller'],
+                        name=name,
+                        query='',
+                    ).create()
+                result = entities.Bookmark().search(
+                    query={'search': u'name="{0}"'.format(name)})
+                self.assertEqual(len(result), 0)
 
-    @stubbed()
     @tier1
     def test_negative_create_same_name(self):
         """Create bookmarks with the same names
@@ -137,14 +163,24 @@ class BookmarkTestCase(APITestCase):
 
         @Assert: Error notification - name already taken, Bookmark is not
         created (not listed)
-
-        @Status: Manual
         """
         for entity in BOOKMARK_ENTITIES:
             with self.subTest(entity['controller']):
-                pass
+                name = gen_string('alphanumeric')
+                entities.Bookmark(
+                    controller=entity['controller'],
+                    name=name,
+                ).create()
+                with self.assertRaises(HTTPError):
+                    entities.Bookmark(
+                        controller=entity['controller'],
+                        name=name,
+                    ).create()
+                result = entities.Bookmark().search(
+                    query={'search': u'name="{0}"'.format(name)})
+                self.assertEqual(len(result), 1)
 
-    @stubbed()
+    @skip_if_bug_open('bugzilla', 1302725)
     @tier1
     def test_negative_create_null_public(self):
         """Create a bookmark omitting the public parameter
@@ -160,14 +196,22 @@ class BookmarkTestCase(APITestCase):
         @Assert: Error notification – public is required, Bookmark is not
         created (not listed)
 
-        @Status: Manual
+        @BZ: 1302725
         """
         for entity in BOOKMARK_ENTITIES:
             with self.subTest(entity['controller']):
-                pass
+                name = gen_string('alphanumeric')
+                with self.assertRaises(HTTPError):
+                    entities.Bookmark(
+                        controller=entity['controller'],
+                        name=name,
+                        public=None,
+                    ).create()
+                result = entities.Bookmark().search(
+                    query={'search': u'name="{0}"'.format(name)})
+                self.assertEqual(len(result), 0)
 
     # UPDATE TESTS
-    @stubbed()
     @tier1
     def test_positive_update_name(self):
         """Update a bookmark
@@ -183,16 +227,19 @@ class BookmarkTestCase(APITestCase):
         1. Update the previously created bookmark with another random name
 
         @Assert: The new bookmark name is listed
-
-        @Status: Manual
         """
         for entity in BOOKMARK_ENTITIES:
             with self.subTest(entity['controller']):
-                pass
+                bm = entities.Bookmark(
+                    controller=entity['controller']).create()
+                for new_name in valid_data_list():
+                    with self.subTest(new_name):
+                        bm.name = new_name
+                        bm = bm.update(['name'])
+                        self.assertEqual(bm.name, new_name)
 
-    @stubbed()
     @tier1
-    def test_negative_update_name(self):
+    def test_negative_update_same_name(self):
         """Update a bookmark with name already taken
 
         @Feature: Scoped Search Bookmark Update
@@ -207,17 +254,26 @@ class BookmarkTestCase(APITestCase):
         the Setup with the name of the second (or first) Bookmark
 
         @Assert: Error - name already taken, bookmark not updated
-
-        @Status: Manual
         """
         for entity in BOOKMARK_ENTITIES:
             with self.subTest(entity['controller']):
-                pass
+                name = gen_string('alphanumeric')
+                entities.Bookmark(
+                    controller=entity['controller'],
+                    name=name,
+                ).create()
+                bm = entities.Bookmark(
+                    controller=entity['controller'],
+                ).create()
+                bm.name = name
+                with self.assertRaises(HTTPError):
+                    bm.update(['name'])
+                bm = bm.read()
+                self.assertNotEqual(bm.name, name)
 
-    @stubbed()
     @tier1
-    def test_negative_update_empty_name(self):
-        """Update a bookmark with an empty name
+    def test_negative_update_invalid_name(self):
+        """Update a bookmark with an invalid name
 
         @Feature: Scoped Search Bookmark Update
 
@@ -227,18 +283,24 @@ class BookmarkTestCase(APITestCase):
 
         @Steps:
 
-        1. Update the name of the previously created bookmarks to an empty
+        1. Update the name of the previously created bookmarks to an invalid
         value
 
-        @Assert: Error - name cannot be empty, bookmark not updated
-
-        @Status: Manual
+        @Assert: Error - bookmark not updated
         """
         for entity in BOOKMARK_ENTITIES:
             with self.subTest(entity['controller']):
-                pass
+                bm = entities.Bookmark(
+                    controller=entity['controller'],
+                ).create()
+                for new_name in invalid_values_list():
+                    with self.subTest(new_name):
+                        bm.name = new_name
+                        with self.assertRaises(HTTPError):
+                            bm.update(['name'])
+                        bm = bm.read()
+                        self.assertNotEqual(bm.name, new_name)
 
-    @stubbed()
     @tier1
     def test_positive_update_query(self):
         """Update a bookmark query
@@ -254,14 +316,18 @@ class BookmarkTestCase(APITestCase):
         1. Update the query of the previously created bookmark
 
         @Assert: The updated query submitted
-
-        @Status: Manual
         """
         for entity in BOOKMARK_ENTITIES:
             with self.subTest(entity['controller']):
-                pass
+                bm = entities.Bookmark(
+                    controller=entity['controller'],
+                ).create()
+                for new_query in valid_data_list():
+                    with self.subTest(new_query):
+                        bm.query = new_query
+                        bm = bm.update(['query'])
+                        self.assertEqual(bm.query, new_query)
 
-    @stubbed()
     @tier1
     def test_negative_update_empty_query(self):
         """Update a bookmark with an empty query
@@ -277,59 +343,45 @@ class BookmarkTestCase(APITestCase):
         1. Update the query of the pre-created bookmark with an empty value
 
         @Assert: Error - search query cannot be empty, bookmark not updated
-
-        @Status: Manual
         """
         for entity in BOOKMARK_ENTITIES:
             with self.subTest(entity['controller']):
-                pass
+                bm = entities.Bookmark(
+                    controller=entity['controller'],
+                ).create()
+                bm.query = ''
+                with self.assertRaises(HTTPError):
+                    bm.update(['query'])
+                bm = bm.read()
+                self.assertNotEqual(bm.query, '')
 
-    @stubbed()
     @tier1
     def test_positive_update_public(self):
-        """Update a bookmark public state to private
+        """Update a bookmark public state to private and vice versa
 
         @Feature: Scoped Search Bookmark Update
 
         @Setup:
 
         1. Create a bookmark with a random name and random query with public
-        attribute set to True
+        attribute set to True/False
 
         @Steps:
 
-        1. Update the bookmarks 'public' attribute to False
-        3. List the bookmarks
+        1. Update the bookmarks 'public' attribute
+        2. List the bookmarks
 
-        @Assert: Bookmark is now listed as private
-
-        @Status: Manual
+        @Assert: Bookmark is updated with new public state
         """
         for entity in BOOKMARK_ENTITIES:
             with self.subTest(entity['controller']):
-                pass
-
-    @stubbed()
-    @tier1
-    def test_positive_update_private(self):
-        """Update a bookmark public state to public
-
-        @Feature: Scoped Search Bookmark Update
-
-        @Setup:
-
-        1. Create a bookmark with a random name and random query with public
-        attribute set to False
-
-        @Steps:
-
-        1. Update the bookmarks 'public' attribute to True
-        3. List the bookmarks
-
-        @Assert: Bookmark is now listed as public
-
-        @Status: Manual
-        """
-        for entity in BOOKMARK_ENTITIES:
-            with self.subTest(entity['controller']):
-                pass
+                for public in (True, False):
+                    with self.subTest(public):
+                        bm = entities.Bookmark(
+                            controller=entity['controller'],
+                            public=not public,
+                        ).create()
+                        self.assertNotEqual(bm.public, public)
+                        bm.public = public
+                        bm = bm.update(['public'])
+                        self.assertEqual(bm.public, public)
