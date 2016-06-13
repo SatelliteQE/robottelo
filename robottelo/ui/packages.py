f494acc17b0673cb0bd661516fcd79f92db0a8c0
oshtaier <alexshtayer@yahoo.com>
2016-06-13 15:50:20 +0300

    Add more coverage for packages UI

--- robottelo/ui/packages.py
+++ robottelo/ui/packages.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 """Implements Packages UI"""
 
-from robottelo.ui.base import Base
+from robottelo.ui.base import Base, UIError
 from robottelo.ui.locators import locators
 from robottelo.ui.navigator import Navigator
 
@@ -18,3 +18,28 @@ class Package(Base):
     def _search_locator(self):
         """Specify locator for Package entity search procedure"""
         return locators['package.rpm_name']
+
+    def check_package_details(self, name, parameter_list=None):
+        """Check whether package detail section contains expected values or
+        raise exception otherwise.
+        All values should be passed in absolute correspondence to UI. For
+        example, we have 'Description' or 'Checksum Type' fields, so next
+        parameter list should be passed::
+            [
+                ['Description', 'Expected description'],
+                ['Checksum Type', 'sha256'],
+            ]
+        """
+        self.click(self.search(name))
+        for parameter_name, parameter_value in parameter_list:
+            param_locator = '.'.join((
+                'package',
+                (parameter_name.lower()).replace(' ', '_')
+            ))
+            actual_text = self.wait_until_element(locators[param_locator]).text
+            if actual_text != parameter_value:
+                raise UIError(
+                    'Actual text for "{0}" parameter is "{1}", but it is'
+                    ' expected to have "{2}"'.format(
+                        parameter_name, actual_text, parameter_value)
+                )
--- tests/foreman/ui/test_packages.py
+++ tests/foreman/ui/test_packages.py
@@ -3,7 +3,7 @@
 
 from fauxfactory import gen_string
 from nailgun import entities
-from robottelo.constants import FAKE_0_YUM_REPO
+from robottelo.constants import FAKE_0_YUM_REPO, FAKE_3_YUM_REPO
 from robottelo.decorators import tier2
 from robottelo.test import UITestCase
 from robottelo.ui.session import Session
@@ -11,6 +11,26 @@ from robottelo.ui.session import Session
 
 class PackagesTestCase(UITestCase):
     """Implement tests for packages via UI"""
+
+    @classmethod
+    def setUpClass(cls):
+        super(PackagesTestCase, cls).setUpClass()
+        cls.organization = entities.Organization().create()
+        product = entities.Product(organization=cls.organization).create()
+        cls.yum_repo = entities.Repository(
+            name=gen_string('alpha'),
+            product=product,
+            content_type='yum',
+            url=FAKE_0_YUM_REPO,
+        ).create()
+        cls.yum_repo2 = entities.Repository(
+            name=gen_string('alpha'),
+            product=product,
+            content_type='yum',
+            url=FAKE_3_YUM_REPO,
+        ).create()
+        cls.yum_repo.sync()
+        cls.yum_repo2.sync()
 
     @tier2
     def test_positive_search_in_repo(self):
@@ -22,21 +42,63 @@ class PackagesTestCase(UITestCase):
         @Assert: Content search functionality works as intended and expected
         packages are present inside of repository
         """
-        org = entities.Organization().create()
-        product = entities.Product(
-            name=gen_string('alpha'),
-            organization=org,
-        ).create()
-        yum_repo = entities.Repository(
-            name=gen_string('alpha'),
-            product=product,
-            content_type='yum',
-            url=FAKE_0_YUM_REPO,
-        ).create()
-        yum_repo.sync()
-
         with Session(self.browser) as session:
-            session.nav.go_to_select_org(org.name)
-            self.package.select_repo(yum_repo.name)
+            session.nav.go_to_select_org(self.organization.name)
+            self.package.select_repo(self.yum_repo.name)
             self.assertIsNotNone(self.package.search('bear'))
             self.assertIsNotNone(self.package.search('cheetah'))
+
+    @tier2
+    def test_positive_search_in_multiple_repos(self):
+        """Create product with two different yum repositories assigned to it.
+        Search for packages inside of these repositories. Make sure that unique
+        packages present in corresponding repos.
+
+        @Feature: Packages
+
+        @Assert: Content search functionality works as intended and expected
+        packages are present inside of repositories
+        """
+        with Session(self.browser) as session:
+            session.nav.go_to_select_org(self.organization.name)
+            self.assertIsNotNone(self.package.search('tiger'))
+            self.assertIsNotNone(self.package.search('Lizard'))
+            self.package.select_repo(self.yum_repo.name)
+            self.assertIsNotNone(self.package.search('tiger'))
+            self.assertIsNone(self.package.search('Lizard'))
+            self.package.select_repo(self.yum_repo2.name)
+            self.assertIsNotNone(self.package.search('Lizard'))
+            self.assertIsNone(self.package.search('tiger'))
+
+    @tier2
+    def test_positive_check_package_details(self):
+        """Create product with yum repository assigned to it. Search for
+        package inside of it and then open it. Check all the details about that
+        package
+
+        @Feature: Packages
+
+        @Assert: Package is present inside of repository and has all expected
+        values in details section
+        """
+        with Session(self.browser) as session:
+            session.nav.go_to_select_org(self.organization.name)
+            self.package.select_repo(self.yum_repo.name)
+            self.package.check_package_details(
+                'gorilla',
+                [
+                    ['Description', 'A dummy package of gorilla'],
+                    ['Summary', 'A dummy package of gorilla'],
+                    ['Group', 'Internet/Applications'],
+                    ['License', 'GPLv2'],
+                    ['Url', 'http://tstrachota.fedorapeople.org'],
+                    ['Size', '2.39 KB (2452 Bytes)'],
+                    ['Filename', 'gorilla-0.62-1.noarch.rpm'],
+                    ['Checksum', 'ffd511be32adbf91fa0b3f54f23cd1c02add50578344'
+                                 'ff8de44cea4f4ab5aa37'],
+                    ['Checksum Type', 'sha256'],
+                    ['Source RPM', 'gorilla-0.62-1.src.rpm'],
+                    ['Build Host', 'smqe-ws15'],
+                    ['Build Time', '1331831364'],
+                ]
+            )
