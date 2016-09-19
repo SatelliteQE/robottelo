21adf1a1ab5d688dcc4a76b207faff1ceaec6e28
Lukas Pramuk <lpramuk@redhat.com>
2016-09-19 16:58:33 +0200

    [stub] Puppet scenario tests
    - API, CLI, UI variants of puppet test scenario
    - Puppet upgrade tests for Satellite/Capsule

---
+++ tests/foreman/api/test_puppet.py
@@ -0,0 +1,118 @@
+"""End to end test for Puppet funcionality
+
+@Requirement: Puppet
+
+@CaseAutomation: Automated
+
+@CaseLevel: Acceptance
+
+@CaseComponent: API
+
+@TestType: Functional
+
+@CaseImportance: High
+
+@Upstream: No
+"""
+
+from robottelo.config import settings
+from robottelo.decorators import (
+    run_in_one_thread,
+    run_only_on,
+    skip_if_not_set,
+    stubbed,
+    tier3,
+)
+from robottelo.test import APITestCase
+
+
+@run_in_one_thread
+@skip_if_not_set('clients')
+class PuppetTestCase(APITestCase):
+    """Implements Puppet test scenario"""
+
+    @classmethod
+    def setUpClass(cls):
+        super(PuppetTestCase, cls).setUpClass()
+        cls.sat6_hostname = settings.server.hostname
+
+    @run_only_on('sat')
+    @stubbed
+    @tier3
+    def test_positive_puppet_scenario(self):
+        """Tests extensive all-in-one puppet scenario
+
+        @id: 29635f43-e701-4539-844c-275545cdf9c4
+
+        @Steps:
+
+        1. Create an organization and upload a cloned manifest for it.
+        2. Enable respective Satellite Tools repos and sync them.
+        3. Create a product and a LFE
+        4. Create a puppet repos within the product
+        5. Upload motd puppet module into the repo
+        6. Upload parameterizable puppet module and create smart params for it
+        7. Create a CV and add Tools repo and puppet module(s)
+        8. Publish and promote CV to the LFE
+        9. Create AK with the product and enable Satellite Tools in it
+        10. Create a libvirt compute resource
+        11. Create a sane subnet and sane domain to be used by libvirt
+        12. Create a hostgroup associated with all created entities
+            (especially Puppet Classes has added puppet modules)
+        13. Provision a host using the hostgroup on libvirt resource
+        14. Assert that puppet agent can run on the host
+        15. Assert that the puppet modules get installed by provisioning
+        16. Run facter on host and assert that was successful
+
+        @Assert: multiple asserts along the code
+
+        @caseautomation: notautomated
+
+        @CaseLevel: System
+        """
+
+
+@run_in_one_thread
+@skip_if_not_set('clients')
+class PuppetCapsuleTestCase(APITestCase):
+    """Implements Puppet test scenario with standalone capsule"""
+
+    @classmethod
+    def setUpClass(cls):
+        super(PuppetCapsuleTestCase, cls).setUpClass()
+        cls.sat6_hostname = settings.server.hostname
+
+    @run_only_on('sat')
+    @stubbed
+    @tier3
+    def test_positive_puppet_capsule_scenario(self):
+        """Tests extensive all-in-one puppet scenario via Capsule
+
+        @id: faffb182-93dc-4406-805c-3b2f10891854
+
+        @Steps:
+
+        1. Create an organization and upload a cloned manifest for it.
+        2. Enable respective Satellite Tools repos and sync them.
+        3. Create a product and a LFE
+        4. Create a puppet repos within the product
+        5. Upload motd puppet module into the repo
+        6. Upload parameterizable puppet module and create smart params for it
+        7. Create a CV and add Tools repo and puppet module(s)
+        8. Publish and promote CV to the LFE
+        9. Create AK with the product and enable Satellite Tools in it
+        10. Create a libvirt compute resource
+        11. Create a sane subnet and sane domain to be used by libvirt
+        12. Create a hostgroup associated with all created entities
+            (especially Puppet Classes has added puppet modules)
+        13. Provision a host using the hostgroup on libvirt resource
+        14. Assert that puppet agent can run on the host
+        15. Assert that the puppet modules get installed by provisioning
+        16. Run facter on host and assert that was successful
+
+        @Assert: multiple asserts along the code
+
+        @caseautomation: notautomated
+
+        @CaseLevel: System
+        """
---
+++ tests/foreman/longrun/test_puppet_upgrade.py
@@ -0,0 +1,88 @@
+"""Test for upgrading Puppet to Puppet4
+
+@Requirement: Puppet
+
+@CaseAutomation: Automated
+
+@CaseLevel: Acceptance
+
+@CaseComponent: CLI
+
+@TestType: Functional
+
+@CaseImportance: High
+
+@Upstream: No
+"""
+
+from robottelo.config import settings
+from robottelo.decorators import (
+    run_in_one_thread,
+    run_only_on,
+    skip_if_not_set,
+    stubbed,
+    tier4,
+)
+from robottelo.test import CLITestCase
+
+
+@run_in_one_thread
+@skip_if_not_set('clients')
+class PuppetUpgradeTestCase(CLITestCase):
+    """Implements Puppet test scenario"""
+
+    @classmethod
+    def setUpClass(cls):
+        super(PuppetUpgradeTestCase, cls).setUpClass()
+        cls.sat6_hostname = settings.server.hostname
+
+    @run_only_on('sat')
+    @stubbed
+    @tier4
+    def test_positive_puppet_upgrade(self):
+        """Upgrade Satellite/client puppet versions
+
+        @id: fd311168-afda-49b6-ac5f-533c4fd411b5
+
+        @Steps:
+
+        1. register client (p3)
+        2. prepare puppet module
+        3. upgrade Satellite from p3 to p4
+        4. apply puppet module to p3 client
+        5. upgrade client from p3 to p4
+        6. apply puppet module to the client
+        7. register another client (p4)
+        8. apply puppet module to the client
+
+        @Assert: multiple asserts along the code that motd module was applied
+
+        @caseautomation: notautomated
+
+        @CaseLevel: System
+        """
+
+    @run_only_on('sat')
+    @stubbed
+    @tier4
+    def test_positive_puppet_capsule_upgrade(self):
+        """Upgrade standalone Capsule/client puppet versions
+        @id: 7e8e9047-d012-4fc5-9e6e-f11c1b05df5d
+
+        @Steps:
+
+        1. register client (p3)
+        2. prepare puppet module
+        3. upgrade Capsule from p3 to p4
+        4. apply puppet module to p3 client
+        5. upgrade client from p3 to p4
+        6. apply puppet module to the client
+        7. register another client (p4)
+        8. apply puppet module to the client
+
+        @Assert: multiple asserts along the code that motd module was applied
+
+        @caseautomation: notautomated
+
+        @CaseLevel: System
+        """
