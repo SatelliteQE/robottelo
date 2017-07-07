beb32b93c06b91758b11f141ade7f441da200721
Andrii Balakhtar <balakhtara@gmail.com>
2017-07-07 13:44:03 +0300

    Implemented test for BZ1426672

--- robottelo/cli/role.py
+++ robottelo/cli/role.py
@@ -10,9 +10,11 @@ Parameters::
 
 Subcommands::
 
+    clone                         Clone a role
     create                        Create an role.
     delete                        Delete an role.
     filters                       List all filters.
+    info                          Show a role
     list                          List all roles.
     update                        Update an role.
 """
@@ -26,6 +28,20 @@ class Role(Base):
 
     @classmethod
     def filters(cls, options=None):
+        """List all filters"""
         cls.command_sub = 'filters'
         return cls.execute(
             cls._construct_command(options), output_format='json')
+
+    @classmethod
+    def clone(cls, options):
+        """Clone a role"""
+        cls.command_sub = 'clone'
+        result = cls.execute(
+            cls._construct_command(options), output_format='csv')
+        # Fetch new role
+        if len(result) > 0 and 'id' in result[0]:
+            new_role = cls.info({'id': result[0]['id']})
+            if len(new_role) > 0:
+                result = new_role
+        return result
