e06d5b4c36e86fbf285fb5b5791af4efb7054ed8
Chris <dcbrown06@gmail.com>
2017-07-20 15:24:56 -0400

    Added cli object and tests for Sat6.3 realm feature

---
+++ robottelo/cli/realm.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+"""
+Usage:
+    hammer realm [OPTIONS] SUBCOMMAND [ARG] ...
+
+Parameters:
+ SUBCOMMAND                    subcommand
+ [ARG] ...                     subcommand arguments
+
+Subcommands:
+ create                        Create a realm
+ delete                        Delete a realm
+ info                          Show a realm
+ list                          List of realms
+ update                        Update a realm
+
+Options:
+ -h, --help                    print help
+"""
+
+from robottelo.cli.base import Base
+
+
+class Realm(Base):
+    """Manipulates Realm subcommand introduced in Satellite 6.3"""
+
+    command_base = 'realm'
