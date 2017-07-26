5b62b98f000e01cd4a4eeb34efd97b02d6c4cf5a
Sanket Jagtap <san7ketjagtap@gmail.com>
2017-07-26 15:26:35 +0530

    Added support for oscap CLI (#4978)

---
+++ robottelo/cli/scapcontent.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+"""
+Usage::
+
+    scap-content [OPTIONS] SUBCOMMAND [ARG] ...
+
+Parameters::
+
+     SUBCOMMAND                    subcommand
+     [ARG] ...                     subcommand arguments
+
+Subcommands::
+
+     create                        Create SCAP content
+     delete                        Deletes an SCAP content
+     info                          Show an SCAP content
+     list                          List SCAP contents
+     update                        Update an SCAP content
+"""
+
+from robottelo.cli.base import Base
+
+
+class Scapcontent(Base):
+    """
+    Manipulates Satellite's scap-content.
+    """
+
+    command_base = 'scap-content'
