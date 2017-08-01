9ce86c43cb3eb6f86b7040065c58ef5ac40958b0
Bruno Rocha <rochacbruno@gmail.com>
2017-08-01 15:06:23 -0300

    Cleaning up all performance tests related code

--- robottelo/performance/__init__.py
+++
@@ -1,9 +0,0 @@
-"""This package contains helper code used by tests.foreman.performance.
-
-This module is subservient to tests.foreman.performance, and exists soley for
-the sake of helping that module get its work done. For example,
-tests.foreman.performance.test_candlepin_concurrent_delete relies upon
-perf_stat to generate csv files. More generally: code in test calls code
-in this common module, but not the other way around.
-
-"""
