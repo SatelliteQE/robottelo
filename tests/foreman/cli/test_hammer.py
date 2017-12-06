f8216a7d747fc215daa303074aad8400f93258f2
Andrii Balakhtar <balakhtara@gmail.com>
2017-12-06 12:38:51 +0200

    Dramatically speed up hammer's test_positive_all_options by using full-help
    Closes #5380
    
    Since `hammer full-help` command was introduced there's no need to call `--help` of every single hammer command recursively, as `full-help` prints all that info by single request, it's just a matter of parsing the output correctly.
    Previous implementation usually took from 30 to 60 minutes to execute locally (25-30 mins on jenkins due to reduced latency), the new one takes 8-10 secs (!). That's 225-450x faster.

--- tests/foreman/cli/test_hammer.py
+++ tests/foreman/cli/test_hammer.py
@@ -15,6 +15,7 @@
 :Upstream: No
 """
 import json
+import re
 
 from robottelo import ssh
 from robottelo.cli import hammer
@@ -84,66 +85,69 @@ class HammerCommandsTestCase(CLITestCase):
         super(HammerCommandsTestCase, self).__init__(*args, **kwargs)
         self.differences = {}
 
-    def _traverse_command_tree(self, command):
-        """Recursively walk through the hammer commands tree and assert that
-        the expected options are present.
+    def _traverse_command_tree(self):
+        """Walk through the hammer commands tree and assert that the expected
+        options are present.
 
         """
-        output = hammer.parse_help(
-            ssh.command('{0} --help'.format(command)).stdout
-        )
-        command_options = set([option['name'] for option in output['options']])
-        command_subcommands = set(
-            [subcommand['name'] for subcommand in output['subcommands']]
-        )
-        if 'discovery_rule' in command and bz_bug_is_open(1219610):
-            # Adjust the discovery_rule subcommand name. The expected data is
-            # already with the naming convetion name
-            expected = _fetch_command_info(
-                command.replace('discovery_rule', 'discovery-rule'))
-        else:
-            expected = _fetch_command_info(command)
-        expected_options = set()
-        expected_subcommands = set()
-
-        if expected is not None:
-            expected_options = set(
-                [option['name'] for option in expected['options']]
+        raw_output = ssh.command(
+            'hammer full-help', output_format='plain').stdout
+        commands = re.split('.*\n(?=hammer.*\n^[-]+)', raw_output, flags=re.M)
+        commands.pop(0)  # remove "Hammer CLI help" line
+        for raw_command in commands:
+            raw_command = raw_command.splitlines()
+            command = raw_command.pop(0).replace(' >', '')
+            output = hammer.parse_help(raw_command)
+            command_options = set([
+                option['name'] for option in output['options']])
+            command_subcommands = set(
+                [subcommand['name'] for subcommand in output['subcommands']]
             )
-            expected_subcommands = set(
-                [subcommand['name'] for subcommand in expected['subcommands']]
-            )
+            if 'discovery_rule' in command and bz_bug_is_open(1219610):
+                # Adjust the discovery_rule subcommand name. The expected data
+                # is already with the naming convetion name
+                expected = _fetch_command_info(
+                    command.replace('discovery_rule', 'discovery-rule'))
+            else:
+                expected = _fetch_command_info(command)
+            expected_options = set()
+            expected_subcommands = set()
 
-        if command == 'hammer' and bz_bug_is_open(1219610):
-            # Adjust the discovery_rule subcommand name
-            command_subcommands.discard('discovery_rule')
-            command_subcommands.add('discovery-rule')
-
-        added_options = tuple(command_options - expected_options)
-        removed_options = tuple(expected_options - command_options)
-        added_subcommands = tuple(command_subcommands - expected_subcommands)
-        removed_subcommands = tuple(expected_subcommands - command_subcommands)
-
-        if (added_options or added_subcommands or removed_options or
-                removed_subcommands):
-            diff = {
-                'added_command': expected is None,
-            }
-            if added_options:
-                diff['added_options'] = added_options
-            if removed_options:
-                diff['removed_options'] = removed_options
-            if added_subcommands:
-                diff['added_subcommands'] = added_subcommands
-            if removed_subcommands:
-                diff['removed_subcommands'] = removed_subcommands
-            self.differences[command] = diff
-
-        if len(output['subcommands']) > 0:
-            for subcommand in output['subcommands']:
-                self._traverse_command_tree(
-                    '{0} {1}'.format(command, subcommand['name'])
+            if expected is not None:
+                expected_options = set(
+                    [option['name'] for option in expected['options']]
                 )
+                expected_subcommands = set([
+                    subcommand['name']
+                    for subcommand in expected['subcommands']
+                ])
+
+            if command == 'hammer' and bz_bug_is_open(1219610):
+                # Adjust the discovery_rule subcommand name
+                command_subcommands.discard('discovery_rule')
+                command_subcommands.add('discovery-rule')
+
+            added_options = tuple(command_options - expected_options)
+            removed_options = tuple(expected_options - command_options)
+            added_subcommands = tuple(
+                command_subcommands - expected_subcommands)
+            removed_subcommands = tuple(
+                expected_subcommands - command_subcommands)
+
+            if (added_options or added_subcommands or removed_options or
+                    removed_subcommands):
+                diff = {
+                    'added_command': expected is None,
+                }
+                if added_options:
+                    diff['added_options'] = added_options
+                if removed_options:
+                    diff['removed_options'] = removed_options
+                if added_subcommands:
+                    diff['added_subcommands'] = added_subcommands
+                if removed_subcommands:
+                    diff['removed_subcommands'] = removed_subcommands
+                self.differences[command] = diff
 
     @tier1
     @upgrade
@@ -157,7 +161,7 @@ class HammerCommandsTestCase(CLITestCase):
         :CaseImportance: Critical
         """
         self.maxDiff = None
-        self._traverse_command_tree('hammer')
+        self._traverse_command_tree()
         if self.differences:
             self.fail(
                 '\n' + _format_commands_diff(self.differences)
