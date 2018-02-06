4c1303a8b72bc07fc1cd1edbabef7bbff6358553
oshtaier <alexshtayer@yahoo.com>
2018-02-06 22:26:59 +0200

    one more os test

--- tests/foreman/ui_airgun/test_os.py
+++ tests/foreman/ui_airgun/test_os.py
@@ -1,3 +1,5 @@
+from nailgun import entities
+
 from robottelo.datafactory import gen_string, valid_data_list
 from robottelo.decorators import parametrize
 
@@ -8,4 +10,18 @@ def test_positive_create(session, name):
     with session:
         session.os.create_operating_system({
             'name': name, 'major': major_version})
+        assert session.os.search(name) == '{} {}'.format(name, major_version)
+
+
+def test_positive_create_with_ptable(session):
+    name = gen_string('alpha')
+    major_version = gen_string('numeric', 2)
+    ptable = entities.PartitionTable().create()
+    with session:
+        session.os.create_operating_system({
+            'name': name,
+            'major': major_version,
+            'ptable.operation': 'Add',
+            'ptable.values': [ptable.name],
+        })
         assert session.os.search(name) == '{} {}'.format(name, major_version)
