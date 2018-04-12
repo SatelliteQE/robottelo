8465cac66652be1c0c15a27d4b6ee58e372fcae3
oshtaier <alexshtayer@yahoo.com>
2018-04-12 15:00:36 +0300

    Address comments

--- tests/foreman/ui_airgun/test_lifecycleenvironment.py
+++ tests/foreman/ui_airgun/test_lifecycleenvironment.py
@@ -26,6 +26,21 @@ def module_org():
     return entities.Organization().create()
 
 
+def test_positive_edit(session):
+    lce_path_name = gen_string('alpha')
+    new_name = gen_string('alpha')
+    with session:
+        session.lifecycleenvironment.create(
+            values={'name': lce_path_name}
+        )
+        session.lifecycleenvironment.update(
+            values={'details.name': new_name},
+            entity_name=lce_path_name,
+        )
+        lce_values = session.lifecycleenvironment.read()
+        assert new_name in lce_values['lce']
+
+
 @tier2
 def test_positive_create_chain(session):
     """Create Content Environment in a chain
@@ -39,13 +54,13 @@ def test_positive_create_chain(session):
     lce_path_name = gen_string('alpha')
     lce_name = gen_string('alpha')
     with session:
-        session.lce.create_environment_path(
+        session.lifecycleenvironment.create(
             values={'name': lce_path_name}
         )
-        session.lce.create_environment(
-            entity_name=lce_path_name,
-            values={'name': lce_name}
+        session.lifecycleenvironment.create(
+            values={'name': lce_name},
+            prior_entity_name=lce_path_name,
         )
-        lce_values = session.lce.read()
-        assert lce_name in lce_values['LCE']
-        assert lce_path_name in lce_values['LCE'][lce_name]
+        lce_values = session.lifecycleenvironment.read()
+        assert lce_name in lce_values['lce']
+        assert lce_path_name in lce_values['lce'][lce_name]
