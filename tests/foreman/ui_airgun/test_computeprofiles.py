42058b882d72598ccd34d2f53c3600e79ff22087
Tomas Strych <tstrych@redhat.com>
2018-04-27 12:20:40 +0200

    adding compute_profile test, please give me the feedback

---
+++ tests/foreman/ui_airgun/test_computeprofiles.py
@@ -0,0 +1,28 @@
+from nailgun import entities
+from robottelo.datafactory import gen_string
+
+
+def test_positive_create(session):
+    name = gen_string('alpha')
+    with session:
+        session.computeprofile.create({
+            'name': name,
+            })
+        assert session.computeprofile.search(name) == name
+
+
+def test_positive_rename(session):
+    name = gen_string('alpha')
+    entities.ComputeProfile(name=name).create()
+    new_name = gen_string('alpha')
+    with session:
+        session.computeprofile.rename(name, {'name': new_name, })
+        assert session.computeprofile.search(new_name) == new_name
+
+
+def test_positive_delete(session):
+    name = gen_string('alpha')
+    entities.ComputeProfile(name=name).create()
+    with session:
+        session.computeprofile.delete(name)
+        assert session.computeprofile.search(name) is None
