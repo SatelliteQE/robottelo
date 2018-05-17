d22f9c13891aace4d753ce1d47d0f878076454ce
Tomas Strych <tstrych@redhat.com>
2018-05-17 13:50:20 +0200

    change of templates

--- tests/foreman/ui_airgun/test_partitiontable.py
+++ tests/foreman/ui_airgun/test_partitiontable.py
@@ -12,9 +12,18 @@ def test_positive_create(session):
             'name': name,
             'default': True,
             'snippet': True,
-            'audit_comment': audit_comment
-        }, template)
+            'audit_comment': audit_comment,
+            'template': template
+        }, )
         assert session.partitiontable.search(name) == name
+        session.partitiontable.lock(name)
+        try:
+            session.partitiontable.delete(name)
+        except ValueError:
+            assert session.partitiontable.search(name) == name
+        session.partitiontable.unlock(name)
+        session.partitiontable.delete(name)
+        assert session.partitiontable.search(name) is None
 
 
 def test_positive_create_os_family(session):
@@ -24,8 +33,9 @@ def test_positive_create_os_family(session):
         session.partitiontable.create({
             'name': name,
             'snippet': False,
-            'os_family': 'Debian'
-        }, template, )
+            'os_family': 'Debian',
+            'template': template,
+        }, )
         assert session.partitiontable.search(name) == name
 
 
@@ -38,8 +48,9 @@ def test_positive_create_loc_org(session):
         session.partitiontable.create({
             'name': name,
             'locations.assigned': [locations.name],
-            'organizations.assigned': [organizations.name]
-        }, template)
+            'organizations.assigned': [organizations.name],
+            'template': template
+        })
         pt = session.partitiontable.read(name)
         assert locations.name in pt['locations']['assigned']
         assert organizations.name in pt['organizations']['assigned']
@@ -49,25 +60,18 @@ def test_positive_clone(session):
     name = gen_string('alpha')
     new_name = gen_string('alpha')
     template = gen_string('alpha')
+    template_new = gen_string('alpha')
     audit_comment = gen_string('alpha')
     with session:
-        session.partitiontable.create({'name': name, }, template)
+        session.partitiontable.create({'name': name, 'template': template})
         session.partitiontable.clone({
             'name': new_name,
             'default': True,
             'snippet': False,
             'os_family': 'Red Hat',
-            'audit_comment': audit_comment
-        }, template, name)
+            'audit_comment': audit_comment,
+            'template': template_new
+        }, name)
         pt = session.partitiontable.read(new_name)
         assert pt['os_family'] == 'Red Hat'
         assert pt['name'] == new_name
-
-
-def test_positive_delete(session):
-    name = gen_string('alpha')
-    template = gen_string('alpha')
-    with session:
-        session.partitiontable.create({'name': name, }, template)
-        session.partitiontable.delete(name)
-        assert session.partitiontable.search(name) is None
