ff62bc7495e142e463acdae96c305dc466fa9102
Tomas Strych <tstrych@redhat.com>
2018-06-05 17:49:33 +0200

    correcting the id's

--- tests/foreman/ui_airgun/test_partitiontable.py
+++ tests/foreman/ui_airgun/test_partitiontable.py
@@ -31,7 +31,7 @@ def test_positive_create_default_for_organization(session):
     """Create new partition table with enabled 'default' option. Check
     that newly created organization has that partition table assigned to it
 
-    :id: 91c64054-cd0c-4d4b-888b-17d42e2n8527
+    :id: 91c64054-cd0c-4d4b-888b-17d42e298527
 
     :expectedresults: New partition table is created and is present in the
         list of selected partition tables for any new organization
@@ -57,7 +57,7 @@ def test_positive_create_custom_organization(session):
     """Create new partition table with disabled 'default' option. Check
     that newly created organization does not contain that partition table.
 
-    :id: 69e6z3f0f-af1f-4aa2-8987-3e3b9a16be37
+    :id: 69e6df0f-af1f-4aa2-8987-3e3b9a16be37
 
     :expectedresults: New partition table is created and is not present in
         the list of selected partition tables for any new organization
@@ -83,7 +83,7 @@ def test_positive_create_default_for_location(session):
     """Create new partition table with enabled 'default' option. Check
     that newly created location has that partition table assigned to it
 
-    :id: 8df82e7c-2f33-4f0d-93f6-1f78ea4d750d
+    :id: 8dfaae7c-2f33-4f0d-93f6-1f78ea4d750d
 
     :expectedresults: New partition table is created and is present in the
         list of selected partition tables for any new location
@@ -109,7 +109,7 @@ def test_positive_create_custom_location(session):
     """Create new partition table with disabled 'default' option. Check
     that newly created location does not contain that partition table.
 
-    :id: 094d4583-76sb-48d4-a89a-23b90741fd6f
+    :id: 094d4583-763b-48d4-a89a-23b90741fd6f
 
     :expectedresults: New partition table is created and is not present in
         the list of selected partition tables for any new location
@@ -132,8 +132,6 @@ def test_positive_create_custom_location(session):
 def test_positive_delete(session):
     """Delete a partition table
 
-    :id: 405ed98a-4207-4bf8-899e-df7a7791850e
-
     :customerscenario: true
 
     :expectedresults: Partition table is deleted
@@ -153,8 +151,6 @@ def test_positive_delete(session):
 
 def test_positive_update(session):
     """Update partition table with its name, layout and OS family
-
-    :id: 63203508-7c73-4ce0-853e-64564y67bec3
 
     :customerscenario: true
 
@@ -180,41 +176,9 @@ def test_positive_update(session):
         assert pt['name'] == new_name
 
 
-def test_negative_update_name(session):
-    """Update invalid name in partition table
-
-    :id: 704e8336-e14a-4d1a-b9db-2f81c87f6evc
-
-    :expectedresults: Partition table is not updated.  Appropriate error
-        shown.
-
-    :CaseImportance: Critical
-    """
-    name_1 = gen_string('alpha')
-    name_2 = gen_string('alpha')
-    with session:
-        session.partitiontable.create({
-            'name': name_1,
-            'template': PARTITION_SCRIPT_DATA_FILE,
-        })
-        session.partitiontable.create({
-            'name': name_2,
-            'template': PARTITION_SCRIPT_DATA_FILE,
-        })
-        # update name 2 with existing name 1
-        session.partitiontable.update({
-            'name': name_1,
-            'template': PARTITION_SCRIPT_DATA_FILE,
-        }, name_2)
-        # name doesn't change
-        assert session.partitiontable.search(name_2)[0]['Name'] == name_2
-
-
 def test_positive_delete_with_lock_and_unlock(session):
     """Lock and unlock the partition table,
     to prevent partition table from deletion.
-
-    :id: 704e8336-e14a-4d1a-b9db-2f81c8ax68cc
 
     :expectedresults: Partition table is locked, cannot be deleted,
         unlocked and then successfully deleted.
@@ -243,8 +207,6 @@ def test_positive_delete_with_lock_and_unlock(session):
 def test_positive_clone(session):
     """Clone the partition table from existing partition table
     and change parameters in cloned partition table
-
-    :id: 7067855d-fd5g-987h-8713-sd78kb9845wf
 
     :expectedresults: Partition table is cloned.
 
