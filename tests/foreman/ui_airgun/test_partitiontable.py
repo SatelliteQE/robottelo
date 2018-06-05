867e3854353f5a28983f95a4b0f751dd9adfb151
Tomas Strych <tstrych@redhat.com>
2018-06-05 11:44:36 +0200

    fixing tags, deleting tier1 test, fixing tags

--- tests/foreman/ui_airgun/test_partitiontable.py
+++ tests/foreman/ui_airgun/test_partitiontable.py
@@ -15,117 +15,14 @@
 
 :Upstream: No
 """
-
 from fauxfactory import gen_string
-from nailgun import entities
 from pytest import raises
 
-from robottelo.constants import (
-    PARTITION_SCRIPT_DATA_FILE,
-    DEFAULT_LOC,
-    DEFAULT_ORG,
-)
-
-from robottelo.decorators import (
-    run_only_on,
-    tier1,
-    tier2,
-    upgrade
-)
+from robottelo.constants import PARTITION_SCRIPT_DATA_FILE
+from robottelo.decorators import run_only_on, tier2
 from robottelo.helpers import get_data_file
 
 PARTITION_SCRIPT_DATA_FILE = get_data_file(PARTITION_SCRIPT_DATA_FILE)
-
-
-@run_only_on('sat')
-@tier1
-def test_positive_create_with_one_character_name(session):
-    """Create a Partition table with 1 character in name
-
-    :id: 2b8ee84f-34d4-464f-8fcb-4xd9647e43f0
-
-    :expectedresults: Partition table is created
-
-    :BZ: 1229384
-
-    :CaseImportance: Critical
-    """
-    name = gen_string('alpha', 1)
-    with session:
-        session.partitiontable.create({
-            'name': name,
-            'template': PARTITION_SCRIPT_DATA_FILE,
-            'os_family_selection': {'os_family': 'Red Hat'},
-             })
-        assert session.partitiontable.search(
-            'name == ' + name)[0]['Name'] == name
-
-
-@run_only_on('sat')
-@tier1
-def test_positive_create_with_name(session):
-    """Create a new partition table
-
-    :id: 2dd8e34d-5a39-49d0-9bde-dd1cdf2db2ad
-
-    :customerscenario: true
-
-    :expectedresults: Partition table is created
-
-    :CaseImportance: Critical
-    """
-    name = gen_string('alpha')
-    with session:
-        session.partitiontable.create({
-            'name': name,
-            'template': PARTITION_SCRIPT_DATA_FILE,
-            'os_family_selection': {'os_family': 'Red Hat'},
-        })
-        assert session.partitiontable.search(name)[0]['Name'] == name
-
-
-@run_only_on('sat')
-@tier1
-def test_positive_create_with_snippet(session):
-    """Create a new partition table with enabled snippet option
-
-    :id: 37bb748a-g3d1-4d88-954f-71634168072a
-
-    :expectedresults: Partition table is created
-
-    :CaseImportance: Critical
-    """
-    name = gen_string('alpha')
-    with session:
-        session.partitiontable.create({
-            'name': name,
-            'snippet': True,
-            'template': PARTITION_SCRIPT_DATA_FILE
-        }, )
-        assert session.partitiontable.search(name)[0]['Name'] == name
-
-
-@run_only_on('sat')
-@tier1
-def test_positive_create_with_audit_comment(session):
-    """Create a new partition table with some text inputted into audit
-    comment section
-
-    :id: f17e16ff-b07f-44ec-af24-b9af460c35aa
-
-    :expectedresults: Partition table is created
-
-    :CaseImportance: Critical
-    """
-    name = gen_string('alpha')
-    audit_comment = gen_string('alpha')
-    with session:
-        session.partitiontable.create({
-            'name': name,
-            'audit_comment': audit_comment,
-            'template': PARTITION_SCRIPT_DATA_FILE
-        }, )
-        assert session.partitiontable.search(name)[0]['Name'] == name
 
 
 @run_only_on('sat')
@@ -142,15 +39,15 @@ def test_positive_create_default_for_organization(session):
     :CaseLevel: Integration
     """
     name = gen_string('alpha')
-    org_name = DEFAULT_ORG
+    org_name = gen_string('alpha')
     with session:
         session.partitiontable.create({
             'name': name,
             'default': True,
-            'organizations.assigned': [org_name],
             'template': PARTITION_SCRIPT_DATA_FILE
         })
-        session.organization.select(DEFAULT_ORG)
+        session.organization.create({'name': org_name})
+        session.organization.select(org_name)
         assert session.partitiontable.search(name)[0]['Name'] == name
 
 
@@ -168,16 +65,16 @@ def test_positive_create_custom_organization(session):
     :CaseLevel: Integration
     """
     name = gen_string('alpha')
-    organizations = entities.Organization().create()
+    org_name = gen_string('alpha')
     with session:
         session.partitiontable.create({
             'name': name,
             'default': False,
-            'organizations.assigned': [organizations.name],
             'template': PARTITION_SCRIPT_DATA_FILE
         })
-        session.organization.select(org_name=organizations.name)
-        assert session.partitiontable.search(name)[0]['Name'] == name
+        session.organization.create({'name': org_name})
+        session.organization.select(org_name)
+        assert not session.partitiontable.search(name)
 
 
 @run_only_on('sat')
@@ -194,15 +91,15 @@ def test_positive_create_default_for_location(session):
     :CaseLevel: Integration
     """
     name = gen_string('alpha')
-    loc_name = DEFAULT_LOC
+    loc_name = gen_string('alpha')
     with session:
         session.partitiontable.create({
             'name': name,
             'default': True,
-            'locations.assigned': [loc_name],
             'template': PARTITION_SCRIPT_DATA_FILE
         })
-        session.location.select(DEFAULT_LOC)
+        session.location.create({'name': loc_name})
+        session.location.select(loc_name)
         assert session.partitiontable.search(name)[0]['Name'] == name
 
 
@@ -220,21 +117,18 @@ def test_positive_create_custom_location(session):
     :CaseLevel: Integration
     """
     name = gen_string('alpha')
-    locations = entities.Location().create()
+    loc_name = gen_string('alpha')
     with session:
         session.partitiontable.create({
             'name': name,
             'default': False,
-            'locations.assigned': [locations.name],
             'template': PARTITION_SCRIPT_DATA_FILE
         })
-        session.location.select(loc_name=locations.name)
-        assert session.partitiontable.search(name)[0]['Name'] == name
+        session.location.create({'name': loc_name})
+        session.location.select(loc_name)
+        assert not session.partitiontable.search(name)
 
 
-@run_only_on('sat')
-@tier1
-@upgrade
 def test_positive_delete(session):
     """Delete a partition table
 
@@ -257,8 +151,6 @@ def test_positive_delete(session):
         assert not session.partitiontable.search(name)
 
 
-@run_only_on('sat')
-@tier1
 def test_positive_update(session):
     """Update partition table with its name, layout and OS family
 
@@ -277,7 +169,6 @@ def test_positive_update(session):
             'name': old_name,
             'os_family_selection': {'os_family': 'Debian'},
             'template': PARTITION_SCRIPT_DATA_FILE,
-
         })
         session.partitiontable.update({
             'name': new_name,
@@ -289,8 +180,6 @@ def test_positive_update(session):
         assert pt['name'] == new_name
 
 
-@run_only_on('sat')
-@tier1
 def test_negative_update_name(session):
     """Update invalid name in partition table
 
@@ -325,13 +214,13 @@ def test_positive_delete_with_lock_and_unlock(session):
     """Lock and unlock the partition table,
     to prevent partition table from deletion.
 
-        :id: 704e8336-e14a-4d1a-b9db-2f81c8ax68cc
+    :id: 704e8336-e14a-4d1a-b9db-2f81c8ax68cc
 
-        :expectedresults: Partition table is locked, cannot be deleted,
+    :expectedresults: Partition table is locked, cannot be deleted,
         unlocked and then successfully deleted.
 
-        :CaseImportance: High
-        """
+    :CaseImportance: Critical
+    """
     name = gen_string('alpha')
     audit_comment = gen_string('alpha')
     with session:
@@ -355,15 +244,16 @@ def test_positive_clone(session):
     """Clone the partition table from existing partition table
     and change parameters in cloned partition table
 
-        :id: 7067855d-fd5g-987h-8713-sd78kb9845wf
-        :expectedresults: Partition table is cloned.
+    :id: 7067855d-fd5g-987h-8713-sd78kb9845wf
 
-        :CaseImportance: Critical
-        """
+    :expectedresults: Partition table is cloned.
+
+    :CaseImportance: Critical
+    """
     name = gen_string('alpha')
     new_name = gen_string('alpha')
     audit_comment = gen_string('alpha')
-    os_fm_sl = {'os_family': 'Red Hat'}
+    os_family_selection = {'os_family': 'Red Hat'}
     with session:
         session.partitiontable.create({
             'name': name,
@@ -373,9 +263,10 @@ def test_positive_clone(session):
             'name': new_name,
             'default': True,
             'snippet': False,
-            'os_family_selection': os_fm_sl,
+            'os_family_selection': os_family_selection,
             'audit_comment': audit_comment,
         }, name)
         pt = session.partitiontable.read(new_name)
-        assert pt['os_family_selection']['os_family'] == os_fm_sl['os_family']
+        assert pt['os_family_selection']['os_family'] == \
+            os_family_selection['os_family']
         assert pt['name'] == new_name
