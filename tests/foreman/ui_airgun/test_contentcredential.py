d64ea5d5a3272b5e3aab61dabda5d04b8d7c6d08
oshtaier <alexshtayer@yahoo.com>
2018-06-12 16:57:33 +0300

    Port more gpg key tests

--- tests/foreman/ui_airgun/test_contentcredential.py
+++ tests/foreman/ui_airgun/test_contentcredential.py
@@ -58,22 +58,18 @@ class TestGPGKeyProductAssociate(object):
 
         :CaseLevel: Integration
         """
-        name = gen_string('alpha')
+        prod_name = gen_string('alpha')
         gpg_key = entities.GPGKey(
             content=self.key_content,
-            name=name,
-            organization=self.organization,
-        ).create()
-        # Creates new product and associate GPGKey with it
-        product = entities.Product(
-            gpg_key=gpg_key,
             organization=self.organization,
         ).create()
         with session:
             session.organization.select(org_name=self.organization.name)
-            values = session.contentcredential.read(name)
+            session.product.create(
+                {'name': prod_name, 'gpg_key': gpg_key.name})
+            values = session.contentcredential.read(gpg_key.name)
             assert len(values['products']['table']) == 1
-            assert values['products']['table'][0]['Name'] == product.name
+            assert values['products']['table'][0]['Name'] == prod_name
             assert values['products']['table'][0]['Used as'] == 'GPG Key'
 
     @tier2
@@ -504,3 +500,245 @@ class TestGPGKeyProductAssociate(object):
             assert len(values['products']['table']) == 0
             assert len(values['repositories']['table']) == 1
             assert values['repositories']['table'][0]['Name'] == repo1.name
+
+    @tier2
+    def test_positive_delete_key_for_empty_product(self, session):
+        """Create gpg key with valid name and valid gpg key then
+        associate it with empty (no repos) custom product then delete it
+
+        :id: b9766403-61b2-4a88-a744-a25d53d577fb
+
+        :expectedresults: gpg key is associated with product during creation
+            but removed from product after deletion
+
+        :CaseLevel: Integration
+        """
+        gpg_key = entities.GPGKey(
+            content=self.key_content,
+            organization=self.organization,
+        ).create()
+        # Creates new product and associate GPGKey with it
+        product = entities.Product(
+            gpg_key=gpg_key,
+            name=gen_string('alpha'),
+            organization=self.organization,
+        ).create()
+        with session:
+            session.organization.select(org_name=self.organization.name)
+            # Assert that GPGKey is associated with product
+            gpg_values = session.contentcredential.read(gpg_key.name)
+            assert len(gpg_values['products']['table']) == 1
+            assert gpg_values['products']['table'][0]['Name'] == product.name
+            product_values = session.product.read(product.name)
+            assert product_values['details']['gpg_key'] == gpg_key.name
+            session.contentcredential.delete(gpg_key.name)
+            # Assert GPGKey isn't associated with product
+            product_values = session.product.read(product.name)
+            assert not product_values['details']['gpg_key']
+
+    @tier2
+    def test_positive_delete_key_for_product_with_repo(self, session):
+        """Create gpg key with valid name and valid gpg key then
+        associate it with custom product that has one repository then delete it
+
+        :id: 75057dd2-9083-47a8-bea7-4f073bdb667e
+
+        :expectedresults: gpg key is associated with product as well as with
+            the repository during creation but removed from product after
+            deletion
+
+        :CaseLevel: Integration
+        """
+        gpg_key = entities.GPGKey(
+            content=self.key_content,
+            organization=self.organization,
+        ).create()
+        # Creates new product and associate GPGKey with it
+        product = entities.Product(
+            gpg_key=gpg_key,
+            name=gen_string('alpha'),
+            organization=self.organization,
+        ).create()
+        # Creates new repository without GPGKey
+        repo = entities.Repository(
+            name=gen_string('alpha'),
+            url=FAKE_1_YUM_REPO,
+            product=product,
+        ).create()
+        with session:
+            session.organization.select(org_name=self.organization.name)
+            # Assert that GPGKey is associated with product
+            values = session.contentcredential.read(gpg_key.name)
+            assert len(values['products']['table']) == 1
+            assert values['products']['table'][0]['Name'] == product.name
+            assert len(values['repositories']['table']) == 1
+            assert values['repositories']['table'][0]['Name'] == repo.name
+            repo_values = session.repository.read(product.name, repo.name)
+            assert repo_values['repo_content']['gpg_key'] == gpg_key.name
+            session.contentcredential.delete(gpg_key.name)
+            # Assert GPGKey isn't associated with product and repository
+            product_values = session.product.read(product.name)
+            assert not product_values['details']['gpg_key']
+            repo_values = session.repository.read(product.name, repo.name)
+            assert not repo_values['repo_content']['gpg_key']
+
+    @tier2
+    @upgrade
+    def test_positive_delete_key_for_product_with_repos(self, session):
+        """Create gpg key with valid name and valid gpg key then
+        associate it with custom product that has more than one repository then
+        delete it
+
+        :id: cb5d4efd-863a-4b8e-b1f8-a0771e90ff5e
+
+        :expectedresults: gpg key is associated with product as well as with
+            repositories during creation but removed from product after
+            deletion
+
+        :CaseLevel: Integration
+        """
+        gpg_key = entities.GPGKey(
+            content=self.key_content,
+            organization=self.organization,
+        ).create()
+        # Creates new product and associate GPGKey with it
+        product = entities.Product(
+            gpg_key=gpg_key,
+            name=gen_string('alpha'),
+            organization=self.organization,
+        ).create()
+        # Creates new repository_1 without GPGKey
+        repo1 = entities.Repository(
+            name=gen_string('alpha'),
+            product=product,
+            url=FAKE_1_YUM_REPO,
+        ).create()
+        # Creates new repository_2 without GPGKey
+        repo2 = entities.Repository(
+            name=gen_string('alpha'),
+            product=product,
+            url=FAKE_2_YUM_REPO,
+        ).create()
+        with session:
+            session.organization.select(org_name=self.organization.name)
+            # Assert that GPGKey is associated with product
+            values = session.contentcredential.read(gpg_key.name)
+            assert len(values['products']['table']) == 1
+            assert values['products']['table'][0]['Name'] == product.name
+            assert len(values['repositories']['table']) == 2
+            assert (
+                {repo1.name, repo2.name} ==
+                set([
+                    repo['Name']
+                    for repo
+                    in values['repositories']['table']])
+            )
+            session.contentcredential.delete(gpg_key.name)
+            # Assert GPGKey isn't associated with product and repositories
+            product_values = session.product.read(product.name)
+            assert not product_values['details']['gpg_key']
+            for repo in [repo1, repo2]:
+                repo_values = session.repository.read(product.name, repo.name)
+                assert not repo_values['repo_content']['gpg_key']
+
+    @tier2
+    def test_positive_delete_key_for_repo_from_product_with_repo(
+            self, session):
+        """Create gpg key with valid name and valid gpg key then
+        associate it to repository from custom product that has one repository
+        then delete the key
+
+        :id: 92ba492e-79af-48fe-84cb-763102b42fa7
+
+        :expectedresults: gpg key is associated with single repository during
+            creation but removed from repository after deletion
+
+        :CaseLevel: Integration
+        """
+        gpg_key = entities.GPGKey(
+            content=self.key_content,
+            organization=self.organization,
+        ).create()
+        # Creates new product without selecting GPGkey
+        product = entities.Product(
+            name=gen_string('alpha'),
+            organization=self.organization,
+        ).create()
+        # Creates new repository with GPGKey
+        repo = entities.Repository(
+            name=gen_string('alpha'),
+            url=FAKE_1_YUM_REPO,
+            product=product,
+            gpg_key=gpg_key,
+        ).create()
+        with session:
+            session.organization.select(org_name=self.organization.name)
+            # Assert that GPGKey is associated with product
+            values = session.contentcredential.read(gpg_key.name)
+            assert len(values['products']['table']) == 0
+            assert len(values['repositories']['table']) == 1
+            assert values['repositories']['table'][0]['Name'] == repo.name
+            repo_values = session.repository.read(product.name, repo.name)
+            assert repo_values['repo_content']['gpg_key'] == gpg_key.name
+            session.contentcredential.delete(gpg_key.name)
+            # Assert GPGKey isn't associated with repository
+            repo_values = session.repository.read(product.name, repo.name)
+            assert not repo_values['repo_content']['gpg_key']
+
+    @tier2
+    @upgrade
+    def test_positive_delete_key_for_repo_from_product_with_repos(
+            self, session):
+        """Create gpg key with valid name and valid gpg key then
+        associate it to repository from custom product that has more than
+        one repository then delete the key
+
+        :id: 5f204a44-bf7b-4a9c-9974-b701e0d38860
+
+        :expectedresults: gpg key is associated with single repository but not
+            with product during creation but removed from repository after
+            deletion
+
+        :BZ: 1461804
+
+        :CaseLevel: Integration
+        """
+        # Creates New GPGKey
+        gpg_key = entities.GPGKey(
+            content=self.key_content,
+            organization=self.organization,
+        ).create()
+        # Creates new product without GPGKey association
+        product = entities.Product(
+            name=gen_string('alpha'),
+            organization=self.organization,
+        ).create()
+        # Creates new repository_1 with GPGKey association
+        repo1 = entities.Repository(
+            gpg_key=gpg_key,
+            name=gen_string('alpha'),
+            product=product,
+            url=FAKE_1_YUM_REPO,
+        ).create()
+        repo2 = entities.Repository(
+            name=gen_string('alpha'),
+            product=product,
+            url=FAKE_2_YUM_REPO,
+            # notice that we're not making this repo point to the GPG key
+        ).create()
+        with session:
+            session.organization.select(org_name=self.organization.name)
+            # Assert that GPGKey is associated with product
+            values = session.contentcredential.read(gpg_key.name)
+            assert len(values['products']['table']) == 0
+            assert len(values['repositories']['table']) == 1
+            assert values['repositories']['table'][0]['Name'] == repo1.name
+            repo_values = session.repository.read(product.name, repo1.name)
+            assert repo_values['repo_content']['gpg_key'] == gpg_key.name
+            repo_values = session.repository.read(product.name, repo2.name)
+            assert not repo_values['repo_content']['gpg_key']
+            session.contentcredential.delete(gpg_key.name)
+            # Assert GPGKey isn't associated with repositories
+            for repo in [repo1, repo2]:
+                repo_values = session.repository.read(product.name, repo.name)
+                assert not repo_values['repo_content']['gpg_key']
