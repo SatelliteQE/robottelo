c29cb8df837a6c035f1805bf9ca791f571680452
Jan Hutar <jhutar@redhat.com>
2018-07-12 17:23:05 +0200

    Fixes #6127 - renamed field

--- tests/foreman/cli/test_docker.py
+++ tests/foreman/cli/test_docker.py
@@ -483,7 +483,7 @@ class DockerContentViewTestCase(CLITestCase):
             [
                 repo_['id']
                 for repo_
-                in self.content_view['docker-repositories']
+                in self.content_view['container-image-repositories']
             ],
         )
 
@@ -512,7 +512,7 @@ class DockerContentViewTestCase(CLITestCase):
         content_view = ContentView.info({'id': content_view['id']})
         self.assertIn(
             repo['id'],
-            [repo_['id'] for repo_ in content_view['docker-repositories']],
+            [repo_['id'] for repo_ in content_view['container-image-repositories']],
         )
 
     @tier1
@@ -546,7 +546,7 @@ class DockerContentViewTestCase(CLITestCase):
         content_view = ContentView.info({'id': content_view['id']})
         self.assertEqual(
             set([repo['id'] for repo in repos]),
-            set([repo['id'] for repo in content_view['docker-repositories']]),
+            set([repo['id'] for repo in content_view['container-image-repositories']]),
         )
 
     @tier2
@@ -566,7 +566,7 @@ class DockerContentViewTestCase(CLITestCase):
         Repository.synchronize({'id': repo['id']})
         repo = Repository.info({'id': repo['id']})
         self.assertGreaterEqual(
-            int(repo['content-counts']['docker-manifests']), 1)
+            int(repo['content-counts']['container-image-manifests']), 1)
         content_view = make_content_view({
             'composite': False,
             'organization-id': self.org_id,
@@ -578,7 +578,7 @@ class DockerContentViewTestCase(CLITestCase):
         content_view = ContentView.info({'id': content_view['id']})
         self.assertIn(
             repo['id'],
-            [repo_['id'] for repo_ in content_view['docker-repositories']],
+            [repo_['id'] for repo_ in content_view['container-image-repositories']],
         )
 
     @tier1
