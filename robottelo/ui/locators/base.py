8b7484fad5cec06145e2d8cda9e6132bacc89c9d
Dmitry Misharov <quarckster@gmail.com>
2018-07-24 13:47:21 +0200

    RHAIManage model page (#6108)
    * RHAIManage model page
    
    * Added Red Hat Access Insights Actions and Planner destinations
    
    * Added tests for RHAI Manage page
    
    * Added tests for RHAI Manage page

--- robottelo/ui/locators/base.py
+++ robottelo/ui/locators/base.py
@@ -2608,10 +2608,29 @@ locators = LocatorDict({
     "insights.org_selection_msg": (
         By.ID, "content"),
     "insights.no_systems_element": (
-        By.XPATH, (
-            "//div[@class='text-center']//h4")
-        ),
-
+        By.XPATH,
+        "//div[@class='text-center']//h4"
+    ),
+    "insights.manage.service_status": (
+        By.ID,
+        "rha-insights-enabled"
+    ),
+    "insights.manage.save_status": (
+        By.XPATH,
+        "//input[@value='Save']"
+    ),
+    "insights.manage.connection_status": (
+        By.XPATH,
+        "//label[@for='connectionStatus']/..//p"
+    ),
+    "insights.manage.account_number": (
+        By.XPATH,
+        "//label[@for='account']/..//p"
+    ),
+    "insights.manage.check_connection": (
+        By.XPATH,
+        "//input[@value='Check Connection']"
+    ),
     # OpenScap locators
     # Scap Content
     "oscap.upload_content": (
