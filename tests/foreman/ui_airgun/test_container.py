a091d5883905abd52e93c7a861a25565e1209f74
Silvie Chlupova <sisi.chlupova@gmail.com>
2018-09-14 11:53:19 +0200

    Changed using constants and view names

--- tests/foreman/ui_airgun/test_container.py
+++ tests/foreman/ui_airgun/test_container.py
@@ -1,6 +1,11 @@
 from nailgun import entities
 
-from robottelo.constants import ENVIRONMENT, DEFAULT_CV, DOCKER_REGISTRY_HUB, REPO_TYPE
+from robottelo.constants import (
+    ENVIRONMENT,
+    DEFAULT_CV,
+    DOCKER_REGISTRY_HUB,
+    REPO_TYPE
+)
 from robottelo.datafactory import gen_string
 from robottelo.decorators import fixture
 from robottelo.config import settings
@@ -16,10 +21,9 @@ def module_org():
 
 
 @fixture(scope='module')
-def create_docker(module_org):
+def module_docker_cr(module_org):
     docker_url = settings.docker.external_url
     return entities.DockerComputeResource(
-        name=gen_string('alpha'),
         organization=[module_org],
         url=docker_url,
     ).create()
@@ -32,7 +36,8 @@ def module_prod(module_org):
 
 @tier2
 @upgrade
-def test_positive_create_with_compresource(session, create_docker, module_prod):
+def test_positive_create_with_compresource(session,
+                                           module_docker_cr, module_prod):
     """Create containers for a compute resource
 
     :id: 299fc13d-5c2f-4642-a6de-735c89fdd096
@@ -42,29 +47,25 @@ def test_positive_create_with_compresource(session, create_docker, module_prod):
 
     :CaseLevel: Integration
     """
-    deploy_on = create_docker.name + ' (Docker)'
-    lifecycle_environment = ENVIRONMENT
-    content_view = DEFAULT_CV
+    deploy_on = module_docker_cr.name + ' (Docker)'
     repo_name = gen_string('alpha')
-    container_name = gen_string('alpha')
-    upstream_repository_name = "centos"
     repo = entities.Repository(
         name=repo_name,
         url=DOCKER_REGISTRY_HUB,
         product=module_prod,
         content_type=REPO_TYPE['docker'],
-        docker_upstream_name=upstream_repository_name
+        docker_upstream_name="centos"
     ).create()
     repo.sync()
     with session:
         session.container.create({
-            'ContainerPreliminaryCreateView.compute_resource.deploy_on': deploy_on,
-            'ContainerImageCreateView.content_view.lifecycle_environment': lifecycle_environment,
-            'ContainerImageCreateView.content_view.content_view': content_view,
-            'ContainerImageCreateView.content_view.repository': repo.name,
-            'ContainerImageCreateView.content_view.tag': '7',
-            'ContainerImageCreateView.content_view.capsule': settings.server.hostname,
-            'ContainerConfigurationCreateView.name': container_name,
-            'ContainerConfigurationCreateView.command': 'top',
-            'ContainerEnvironmentCreateView.tty': True,
+            'preliminary.compute_resource.deploy_on': deploy_on,
+            'image.content_view.lifecycle_environment': ENVIRONMENT,
+            'image.content_view.content_view': DEFAULT_CV,
+            'image.content_view.repository': repo.name,
+            'image.content_view.tag': 'centos7.2.1511',
+            'image.content_view.capsule': settings.server.hostname,
+            'configuration.name': gen_string('alpha'),
+            'configuration.command': 'top',
+            'environment.tty': True,
         })
