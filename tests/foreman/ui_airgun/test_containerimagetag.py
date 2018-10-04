2cef3286fdca1189efc987830bd887758d7098eb
tstrych <tstrych@redhat.com>
2018-10-04 10:57:11 +0200

    fixing comments, adding word module to name of module constants

--- tests/foreman/ui_airgun/test_containerimagetag.py
+++ tests/foreman/ui_airgun/test_containerimagetag.py
@@ -1,6 +1,6 @@
 """WebUI tests for the Docker Image Tags feature.
 
-:Requirement: Docker
+:Requirement: Docker Image Tag
 
 :CaseAutomation: Automated
 
@@ -16,52 +16,55 @@
 """
 from nailgun import entities
 
+from robottelo.constants import (
+    ENVIRONMENT,
+    DOCKER_REGISTRY_HUB,
+    DOCKER_UPSTREAM_NAME,
+    REPO_TYPE,
+)
 from robottelo.decorators import fixture, tier2
 
 
 @fixture(scope="module")
-def org():
+def module_org():
     return entities.Organization().create()
 
 
 @fixture(scope="module")
-def product(org):
-    return entities.Product(organization=org).create()
+def module_product(module_org):
+    return entities.Product(organization=module_org).create()
 
 
 @fixture(scope="module")
-def sync_repo(product):
+def module_repository(module_product):
     repo = entities.Repository(
-        content_type='docker',
-        docker_upstream_name='busybox',
-        product=product,
-        url=u'https://registry-1.docker.io',
+        content_type=REPO_TYPE['docker'],
+        docker_upstream_name=DOCKER_UPSTREAM_NAME,
+        product=module_product,
+        url=DOCKER_REGISTRY_HUB,
     ).create()
     repo.sync()
     return repo
 
 
 @tier2
-def test_positive_search(session, org, product, sync_repo):
-    """Search for a docker image
+def test_positive_search(
+        session, module_org, module_product, module_repository):
+    """Search for a docker image tag and reads details of it
 
     :id: 28640396-c44d-4487-9d6d-3d5f2ed599d7
 
-    :expectedresults: The docker tag can be searched and found
+    :expectedresults: The docker image tag can be searched and found,
+        details are read
 
     :CaseLevel: Integration
     """
     with session:
-        session.organization.select(org_name=org.name)
+        session.organization.select(org_name=module_org.name)
         search = session.containerimagetag.search('latest')
-        assert product.name == search[0]['Product Name']
-        assert sync_repo.name == search[0]['Repository Name']
-
-
-def test_positve_read_details(session, org, product, sync_repo):
-    with session:
-        session.organization.select(org_name=org.name)
+        assert module_product.name == search[0]['Product Name']
+        assert module_repository.name == search[0]['Repository Name']
         values = session.containerimagetag.read('latest')
-        assert product.name == values['details']['product']
-        assert sync_repo.name == values['details']['repository']
-        assert values['lce']['table'][0]['Environment'] == 'Library'
+        assert module_product.name == values['details']['product']
+        assert module_repository.name == values['details']['repository']
+        assert values['lce']['table'][0]['Environment'] == ENVIRONMENT
