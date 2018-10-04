302c7f586ca98aa6fdcffba1417608a3e216c774
jyejare <jyejare@redhat.com>
2018-10-04 18:35:49 +0530

    Upgrade Scenarios in Robotello

---
+++ tests/upgrades/test_subnet.py
@@ -0,0 +1,67 @@
+"""Test for Subnet related Upgrade Scenario's
+
+:Requirement: Upgraded Satellite
+
+:CaseAutomation: NotAutomated
+
+:CaseLevel: Acceptance
+
+:CaseComponent: API
+
+:TestType: Functional
+
+:CaseImportance: High
+
+:Upstream: No
+"""
+
+from upgrade_tests import pre_upgrade, post_upgrade
+
+
+class scenario_positive_create_parameters_in_existing_subnet:
+    """Parameters can be created in existing subnet post upgrade
+
+    :id: 319317d5-70f0-40f3-bc33-d8846432dea2
+
+    :steps:
+
+        1. Create subnet with all the details preupgrade
+            satellite version
+        2. Create host with this subnet
+        2. Upgrade Satellite to next/latest satellite version
+        3. Go to the subnet created in preupgrade satellite version
+        4. Attempt to add parameter in subnet
+
+    :expectedresults:
+
+        1. Parameter should be created in existing Subnet post
+            upgrade
+        2. Host ENC should return parameter with its value
+    """
+
+    @pre_upgrade
+    def test_pre_create_parameter_in_existing_subnet(self):
+        """Create parameter in preupgrade version
+
+        :steps:
+
+            1. Create subnet with all the details preupgrade
+                satellite version
+            2. Create host with this subnet
+
+        :expectedresults: The subnet should be created successfully
+        """
+
+    @post_upgrade
+    def test_post_create_parameter_in_existing_subnet(self):
+        """Parameter can be added to existing subnet post upgrade
+
+        :steps:
+
+            1. Postupgrade, Add parameter to the existing subnet
+
+        :expectedresults:
+
+            1. Parameter should be added to the existing subnet
+            2. Host ENC should return parameter with its value
+        """
---
+++ tests/upgrades/test_subscription.py
@@ -0,0 +1,92 @@
+"""Test for subscription related Upgrade Scenario's
+
+:Requirement: Upgraded Satellite
+
+:CaseAutomation: Automated
+
+:CaseLevel: Acceptance
+
+:CaseComponent: CLI
+
+:TestType: Functional
+
+:CaseImportance: High
+
+:Upstream: No
+"""
+from nailgun import entities
+from robottelo.test import APITestCase
+from upgrade_tests import post_upgrade, pre_upgrade
+from upgrade_tests.helpers.scenarios import (
+    delete_manifest,
+    upload_manifest
+)
+import os
+
+
+class Scenario_manifest_refresh(APITestCase):
+    """The test class contains pre-upgrade and post-upgrade scenarios to test
+    manifest refresh before and after upgrade
+
+    Test Steps:
+
+    1. Before Satellite upgrade, upload a manifest.
+    2. Refresh the manifest.
+    3. Upgrade satellite.
+    4. Refresh the manifest.
+    5. Delete the manifest.
+
+    """
+    @classmethod
+    def setUpClass(cls):
+        cls.manifest_url = os.environ.get('MANIFEST_URL')
+        cls.org_name = 'preupgrade_subscription_org'
+
+    @pre_upgrade
+    def test_pre_manifest_scenario_refresh(self):
+        """Pre-upgrade scenario that upload and refresh manifest in satellite
+         which will be refreshed in post upgrade scenario.
+
+
+        :id: preupgrade-29b246aa-2c7f-49f4-870a-7a0075e184b1
+
+        :steps:
+            1. Before Satellite upgrade, upload and refresh manifest.
+
+        :expectedresults: Manifest should upload and refresh successfully.
+         """
+        org = entities.Organization(name=self.org_name).create()
+        upload_manifest(self.manifest_url, org.name)
+        history = entities.Subscription(organization=org).manifest_history(
+            data={'organization_id': org.id})
+        self.assertEqual(
+            "{0} file imported successfully.".format(org.name),
+            history[0]['statusMessage'])
+        sub = entities.Subscription(organization=org)
+        sub.refresh_manifest(data={'organization_id': org.id})
+        self.assertGreater(len(sub.search()), 0)
+
+    @post_upgrade
+    def test_post_manifest_scenario_refresh(self):
+        """Post-upgrade scenario that verifies manifest refreshed successfully
+        and deleted successfully.
+
+        :id: postupgrade-29b246aa-2c7f-49f4-870a-7a0075e184b1
+
+        :steps:
+            1. Refresh manifest
+            2. Delete manifest
+
+        :expectedresults:
+            1. The manifest should refresh and delete successfully.
+         """
+        org = entities.Organization().search(query={
+            'search': 'name={0}'.format(self.org_name)})[0]
+        sub = entities.Subscription(organization=org)
+        sub.refresh_manifest(data={'organization_id': org.id})
+        self.assertGreater(len(sub.search()), 0)
+        delete_manifest(self.org_name)
+        history = entities.Subscription(organization=org).manifest_history(
+            data={'organization_id': org.id})
+        self.assertEqual("Subscriptions deleted by foreman_admin",
+                         history[0]['statusMessage'])
