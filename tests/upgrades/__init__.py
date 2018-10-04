9d6921ef0ba3022eee8ab389cbabcd616f21f523
jyejare <jyejare@redhat.com>
2018-10-04 18:35:49 +0530

    CherryPicked from Master to 6.4 - Upgrade Scenarios in Robotello

---
+++ tests/upgrades/__init__.py
@@ -0,0 +1,11 @@
+from automation_tools.satellite6.hammer import set_hammer_config
+from fabric.api import env
+from robottelo.test import TestCase, settings
+
+# Fabric Config setup
+TestCase.setUpClass()
+env.host_string = settings.server.hostname
+env.user = 'root'
+
+# Hammer Config Setup
+set_hammer_config()
