cb669c95aabfebabf24f17fa81d88516dd536ea4
Omkar Khatavkar <okhatavkar007@gmail.com>
2018-10-30 15:55:08 +0530

    Added cli tests for module stream 6.5 feature

---
+++ robottelo/cli/module_stream.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+"""
+Usage::
+
+    hammer module-stream [OPTIONS] SUBCOMMAND [ARG] ...
+
+Parameters::
+
+    SUBCOMMAND                    subcommand
+    [ARG] ...                     subcommand arguments
+
+Subcommands::
+
+    info                          Show a module-stream
+    list                          List module-streams
+"""
+from robottelo.cli.base import Base
+
+
+class ModuleStream(Base):
+    """
+    Manipulates module-stream command.
+    """
+
+    command_base = 'module-stream'
