1ce9f5bc5196bdb70e91885b10fd47109a667aca
root <root@vijsingh.pnq.csb>
2018-12-13 12:13:43 +0530

    6.3.z [Cherry-Pick] Ported hammer based secnario to Nailgun

--- tests/upgrades/test_client.py
+++ tests/upgrades/test_client.py
@@ -17,12 +17,10 @@
 import os
 import time
 
-from automation_tools.satellite6.hammer import (
-    attach_subscription_to_host_from_satellite
-)
 from fabric.api import execute
 from nailgun import entities
 from robottelo.test import APITestCase
+from robottelo.api.utils import attach_custom_product_subscription
 from upgrade.helpers.docker import docker_execute_command
 from upgrade_tests import post_upgrade, pre_upgrade
 from upgrade_tests.helpers.constants import FAKE_REPO_ZOO3
@@ -294,14 +292,12 @@ class Scenario_upgrade_old_client_and_package_installation(APITestCase):
         rhel7_client = dockerize(
             ak_name=ak.name, distro='rhel7', org_label=self.org.label)
         client_container_id = rhel7_client.values()[0]
-        client_name = rhel7_client.keys()[0]
         product, yum_repo = create_yum_test_repo(
             product_name=self.prod_name, repo_url=FAKE_REPO_ZOO3, org=self.org)
         update_product_subscription_in_ak(
             product=product, yum_repo=yum_repo, ak=ak, org=self.org)
-        time.sleep(30)
-        attach_subscription_to_host_from_satellite(
-            self.org.id, product.name, str(client_name).lower())
+        attach_custom_product_subscription(
+            prod_name=product.name, host_name=str(rhel7_client.keys()[0]).lower())
         # Refresh subscriptions on client
         execute(
             docker_execute_command,
@@ -343,8 +339,6 @@ class Scenario_upgrade_old_client_and_package_installation(APITestCase):
             'content_type': 'package',
             'content': [self.package_name],
         })
-        # Holding on for 20 seconds to let the package install
-        time.sleep(20)
         # Validate if that package is really installed
         installed_package = execute(
             docker_execute_command,
@@ -423,9 +417,8 @@ class Scenario_upgrade_new_client_and_package_installation(APITestCase):
             product_name=self.prod_name, repo_url=FAKE_REPO_ZOO3, org=org)
         update_product_subscription_in_ak(
             product=product, yum_repo=yum_repo, ak=ak, org=org)
-        time.sleep(10)
-        attach_subscription_to_host_from_satellite(
-            org.id, product.name, client_name)
+        attach_custom_product_subscription(
+            prod_name=product.name, host_name=client_name)
         # Refresh subscriptions on client
         execute(
             docker_execute_command,
@@ -452,7 +445,6 @@ class Scenario_upgrade_new_client_and_package_installation(APITestCase):
             'content_type': 'package',
             'content': [self.package_name],
         })
-        time.sleep(20)
         # Validate if that package is really installed
         installed_package = execute(
             docker_execute_command,
