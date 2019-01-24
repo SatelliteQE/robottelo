eb129fcc34bfaa88331ccf73d9acf27ceb0f246b
root <root@vijsingh.pnq.csb>
2019-01-24 17:57:55 +0530

    content-host subscription auto-attach in upgrade

--- tests/upgrades/test_subscription.py
+++ tests/upgrades/test_subscription.py
@@ -14,14 +14,21 @@
 
 :Upstream: No
 """
+import os
+
+from fabric.api import execute
 from nailgun import entities
+from robottelo import manifests
 from robottelo.test import APITestCase
+from upgrade.helpers.docker import docker_execute_command
 from upgrade_tests import post_upgrade, pre_upgrade
 from upgrade_tests.helpers.scenarios import (
+    create_dict,
     delete_manifest,
-    upload_manifest
+    dockerize,
+    get_entity_data,
+    upload_manifest,
 )
-import os
 
 
 class Scenario_manifest_refresh(APITestCase):
@@ -90,3 +97,73 @@ class Scenario_manifest_refresh(APITestCase):
             data={'organization_id': org.id})
         self.assertEqual("Subscriptions deleted by foreman_admin",
                          history[0]['statusMessage'])
+
+
+class Scenario_contenthost_subscription_autoattach_check(APITestCase):
+    """Test subscription auto-attach post migration on a pre-upgrade client registered with Satellite.
+
+        Test Steps:
+
+        1. Before Satellite upgrade.
+        2. Create new Organization.
+        3. Upload a manifest in it.
+        4. Create a AK with 'auto-attach False' and without Subscription add in it.
+        5. Create a content host.
+        6. Upgrade Satellite/Capsule.
+        7. Run subscription auto-attach on content host.
+        8. Check if it is Subscribed.
+    """
+    @classmethod
+    def setUpClass(cls):
+        cls.docker_vm = os.environ.get('DOCKER_VM')
+
+    @pre_upgrade
+    def test_pre_subscription_scenario_autoattach(self):
+        """Create content host and register with Satellite
+
+        :id: preupgrade-940fc78c-ffa6-4d9a-9c4b-efa1b9480a22
+
+        :steps:
+            1. Before Satellite upgrade.
+            2. Create new Organization.
+            3. Upload a manifest in it.
+            4. Create a AK with 'auto-attach False' and without Subscription add in it.
+            5. Create a content host.
+
+        :expectedresults:
+            1. Content host should be created.
+        """
+        org = entities.Organization().create()
+        manifests.upload_manifest_locked(org.id, interface=manifests.INTERFACE_API)
+        act_key = entities.ActivationKey(auto_attach=False, organization=org.id,
+                                         environment=org.library.id).create()
+        rhel7_client = dockerize(
+            ak_name=act_key.name, distro='rhel7', org_label=org.label)
+        client_container_id = list(rhel7_client.values())[0]
+        status = execute(docker_execute_command, client_container_id,
+                         'subscription-manager list|grep Status:|cut -d" " -f10-11',
+                         host=self.docker_vm)[self.docker_vm]
+        self.assertEqual("Not Subscribed", status)
+        global_dict = {
+            self.__class__.__name__: {'client_container_id': client_container_id}
+        }
+        create_dict(global_dict)
+
+    @post_upgrade
+    def test_post_subscription_scenario_autoattach(self):
+        """Run subscription auto-attach on pre-upgrade content host registered
+        with Satellite.
+
+        :id: postupgrade-940fc78c-ffa6-4d9a-9c4b-efa1b9480a22
+
+        :steps:
+            1. Run subscription auto-attach on content host.
+
+        :expectedresults:
+            1. Pre-upgrade content host should get Subscribed.
+         """
+        client_container_id = get_entity_data(self.__class__.__name__)['client_container_id']
+        subscription = execute(docker_execute_command, client_container_id,
+                               'subscription-manager attach --auto',
+                               host=self.docker_vm)[self.docker_vm]
+        self.assertEqual("Subscribed", subscription.split()[-1])
