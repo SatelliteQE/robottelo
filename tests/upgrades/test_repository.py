4d9e60d98ef8b4d006ec0c8ca062e67b1231c83e
Vijay Singh <vijsingh@redhat.com>
2019-02-02 09:53:02 +0530

    Updated the foreman rakes

--- tests/upgrades/test_repository.py
+++ tests/upgrades/test_repository.py
@@ -56,7 +56,7 @@ class Scenario_repository_upstream_authorization_check(APITestCase):
 
         org = entities.Organization().create()
         custom_repo = create_sync_custom_repo(org_id=org.id)
-        rake_repo = 'repo = Katello::RootRepository.find_by_id({0})'.format(custom_repo)
+        rake_repo = 'repo = Katello::Repository.find_by_id({0})'.format(custom_repo)
         rake_username = '; repo.upstream_username = "{0}"'.format(self.upstream_username)
         rake_repo_save = '; repo.save!(validate: false)'
         result = run("echo '{0}{1}{2}'|foreman-rake console".format(rake_repo, rake_username,
