20d3c0e172faac59f56c41c8a429d0c98a8f48ea
Jan Hutař <jhutar@redhat.com>
2019-04-24 09:20:04 +0200

    Properly specify org and loc to both CR and host so it works together (#6848)
    * Properly specify orf and loc to both CR and host so it works together
    
    * Incorporated feedback from PR

--- tests/foreman/cli/test_host.py
+++ tests/foreman/cli/test_host.py
@@ -39,6 +39,7 @@ from robottelo.cli.factory import (
     make_host_collection,
     make_hostgroup,
     make_lifecycle_environment,
+    make_location,
     make_medium,
     make_org,
     make_os,
@@ -115,6 +116,7 @@ class HostCreateTestCase(CLITestCase):
         """
         super(HostCreateTestCase, cls).setUpClass()
         cls.new_org = make_org()
+        cls.new_loc = make_location()
         cls.new_lce = make_lifecycle_environment({
             'organization-id': cls.new_org['id']})
         cls.LIBRARY = LifecycleEnvironment.info({
@@ -1017,9 +1019,14 @@ class HostCreateTestCase(CLITestCase):
         compute_resource = entities.LibvirtComputeResource(
             url='qemu+ssh://root@{0}/system'.format(
                 settings.compute_resources.libvirt_hostname
-            )
+            ),
+            organization=[self.new_org['id']],
+            location=[self.new_loc['id']]
         ).create()
-        host = entities.Host()
+        host = entities.Host(
+            organization=self.new_org['id'],
+            location=self.new_loc['id'],
+        )
         host.create_missing()
         result = make_host({
             u'architecture-id': host.architecture.id,
