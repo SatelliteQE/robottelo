1537bc733fa479c14fc96e715af179ceb23d4769
Omkar Khatavkar <okhatavkar007@gmail.com>
2019-06-26 10:15:04 +0530

    replace entities.DockerComputeResource with entities.LibvirtComputeResource

--- robottelo/constants.py
+++ robottelo/constants.py
@@ -1758,7 +1758,7 @@ BOOKMARK_ENTITIES = [
     {'name': 'SmartProxy', 'controller': 'smart_proxies', 'skip_for_ui': True},
     {
         'name': 'ComputeResource', 'controller': 'compute_resources',
-        'setup': entities.DockerComputeResource, 'skip_for_ui': True
+        'setup': entities.LibvirtComputeResource
     },
     {
         'name': 'ComputeProfile', 'controller': 'compute_profiles',
