557c9e36b4f71cba8fd54cd1a245abe9b47fcdd5
tstrych <tstrych@redhat.com>
2019-08-23 12:02:04 +0200

    openscap security from v6 to v7

--- tests/foreman/ui/test_host.py
+++ tests/foreman/ui/test_host.py
@@ -124,7 +124,7 @@ def scap_content():
     scap_profile_id = [
         profile['id']
         for profile in scap_info['scap-content-profiles']
-        if OSCAP_PROFILE['security6'] in profile['title']
+        if OSCAP_PROFILE['security7'] in profile['title']
     ][0]
     return scap_id, scap_profile_id
 
