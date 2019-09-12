b7ab970c9e0284f53fa8a9870c6553e067744105
Devendra Singh <desingh@desingh.pnq.csb>
2019-09-12 16:33:52 +0530

    Created a common function for vm cleanup

--- tests/upgrades/test_remoteexecution.py
+++ tests/upgrades/test_remoteexecution.py
@@ -17,6 +17,7 @@
 from nailgun import entities
 from robottelo.constants import DEFAULT_LOC, DEFAULT_ORG, DISTRO_RHEL7
 from robottelo.helpers import add_remote_execution_ssh_key
+from robottelo.cleanup import vm_cleanup
 from robottelo.vm import VirtualMachine
 from robottelo.test import APITestCase, settings
 from upgrade_tests import post_upgrade, pre_upgrade
@@ -54,22 +55,8 @@ class Scenario_remoteexecution_external_capsule(APITestCase):
         cls.vm_domain_name = settings.upgrade.vm_domain
         cls.vm_domain = entities.Domain().search(query={'search': 'name="{}"'
                                                  .format(cls.vm_domain_name)})
-        cls.proxy_name = settings.upgrade.rhev_cap_host or settings.upgrade.capsule_hostname
-
-    def _vm_cleanup(self, hostname=None):
-        """ Cleanup the VM from provisioning server
-
-        :param str hostname: The content host hostname
-        """
-        if hostname:
-            vm = VirtualMachine(
-                hostname=hostname,
-                target_image=hostname,
-                provisioning_server=self.libvirt_vm,
-                distro=DISTRO_RHEL7,
-                )
-            vm._created = True
-            vm.destroy()
+        cls.proxy_name = settings.upgrade.rhev_cap_host or settings.upgrade.\
+            capsule_hostname
 
     @pre_upgrade
     def test_pre_scenario_remoteexecution_external_capsule(self):
@@ -125,7 +112,9 @@ class Scenario_remoteexecution_external_capsule(APITestCase):
             create_dict(global_dict)
         except Exception as exp:
             if client._created:
-                self._vm_cleanup(hostname=client.hostname)
+                vm_cleanup(hostname=client.hostname,
+                           provisioning_server=self.libvirt_vm,
+                           distro=DISTRO_RHEL7)
             raise Exception(exp)
 
     @post_upgrade(depend_on=test_pre_scenario_remoteexecution_external_capsule)
@@ -146,7 +135,9 @@ class Scenario_remoteexecution_external_capsule(APITestCase):
             'job_template_id': 89, 'inputs': {'command': "ls"},
             'targeting_type': 'static_query', 'search_query': "name = {0}".format(client_name)})
         self.assertEqual(job['output']['success_count'], 1)
-        self._vm_cleanup(hostname=client_name)
+        vm_cleanup(hostname=client_name,
+                   provisioning_server=self.libvirt_vm,
+                   distro=DISTRO_RHEL7)
 
 
 class Scenario_remoteexecution_satellite(APITestCase):
@@ -181,21 +172,6 @@ class Scenario_remoteexecution_satellite(APITestCase):
         cls.vm_domain = entities.Domain().search(query={'search': 'name="{}"'
                                                  .format(cls.vm_domain_name)})
         cls.proxy_name = settings.server.hostname
-
-    def _vm_cleanup(self, hostname=None):
-        """ Cleanup the VM from provisioning server
-
-        :param str hostname: The content host hostname
-        """
-        if hostname:
-            vm = VirtualMachine(
-                hostname=hostname,
-                target_image=hostname,
-                provisioning_server=self.libvirt_vm,
-                distro=DISTRO_RHEL7,
-                )
-            vm._created = True
-            vm.destroy()
 
     @pre_upgrade
     def test_pre_scenario_remoteexecution_satellite(self):
@@ -250,7 +226,9 @@ class Scenario_remoteexecution_satellite(APITestCase):
             create_dict(global_dict)
         except Exception as exp:
             if client._created:
-                self._vm_cleanup(hostname=client.hostname)
+                vm_cleanup(hostname=client.hostname,
+                           provisioning_server=self.libvirt_vm,
+                           distro=DISTRO_RHEL7)
             raise Exception(exp)
 
     @post_upgrade(depend_on=test_pre_scenario_remoteexecution_satellite)
@@ -269,6 +247,9 @@ class Scenario_remoteexecution_satellite(APITestCase):
         client_name = get_entity_data(self.__class__.__name__)['client_name']
         job = entities.JobInvocation().run(data={
             'job_template_id': 89, 'inputs': {'command': "ls"},
-            'targeting_type': 'static_query', 'search_query': "name = {0}".format(client_name)})
+            'targeting_type': 'static_query', 'search_query': "name = {0}".
+            format(client_name)})
         self.assertEqual(job['output']['success_count'], 1)
-        self._vm_cleanup(hostname=client_name)
+        vm_cleanup(hostname=client_name,
+                   provisioning_server=self.libvirt_vm,
+                   distro=DISTRO_RHEL7)
