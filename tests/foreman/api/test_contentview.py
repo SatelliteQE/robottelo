a304e960b9b6a9bde017c89277a98eef53d796dd
koliveir <koliveir@redhat.com>
2019-09-27 14:47:56 -0400

    Add test to publish content_view with multiple repos
    Add test to publish content_view with multiple RPM repos.

--- tests/foreman/api/test_contentview.py
+++ tests/foreman/api/test_contentview.py
@@ -19,6 +19,7 @@ import random
 
 from fauxfactory import gen_integer, gen_string, gen_utf8
 from nailgun import entities
+from nailgun.entity_mixins import TaskFailedError
 from requests.exceptions import HTTPError
 from robottelo import ssh
 from robottelo.api.utils import enable_rhrepo_and_fetchid, promote
@@ -1224,6 +1225,36 @@ class ContentViewPublishPromoteTestCase(APITestCase):
         # environment and latest - 0
         self.assertEqual(len(content_view.version[0].read().environment), 1)
         self.assertEqual(len(content_view.version[-1].read().environment), 0)
+
+    @tier3
+    def test_positive_publish_multiple_repos(self):
+        """Attempt to publish a content view with multiple YUM repos.
+
+        :id: 5557a33b-7a6f-45f5-9fe4-23a704ed9e21
+
+        :expectedresults: Content view publish should not raise an exception.
+
+        :CaseLevel: Integration
+
+        :CaseComponent: Pulp
+
+        :CaseImportance: Medium
+
+        :BZ: 1651930
+        """
+        org = entities.Organization().create()
+        product = entities.Product(organization=org).create()
+        content_view = entities.ContentView(organization=org).create()
+        for _ in range(10):
+            repo = entities.Repository(product=product).create()
+            repo.sync()
+            content_view.repository.append(repo)
+            content_view = content_view.update(['repository'])
+        content_view = content_view.read()
+        self.assertEqual(len(content_view.repository), 10)
+        with self.assertNotRaises(TaskFailedError):
+            content_view.publish()
+        self.assertEqual(len(content_view.read().version), 1)
 
 
 class ContentViewUpdateTestCase(APITestCase):
