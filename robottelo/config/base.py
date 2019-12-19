6cd572a23d0ee8c18a38ec0b7a38b42588fc0157
Lukas Hellebrandt <lhellebr@redhat.com>
2019-12-19 16:55:46 +0100

    Put added IPA params to the error message

--- robottelo/config/base.py
+++ robottelo/config/base.py
@@ -586,7 +586,8 @@ class LDAPIPASettings(FeatureSettings):
         if not all(vars(self).values()):
             validation_errors.append(
                 'All [ipa] basedn_ipa, grpbasedn_ipa, hostname_ipa,'
-                ' password_ipa, username_ipa, user_ipa options must be provided.'
+                ' password_ipa, username_ipa, user_ipa,'
+                ' otp_user, time_based_secret options must be provided.'
             )
         return validation_errors
 
