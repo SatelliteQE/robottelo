33697221fd64f617a1deeb4cb5c70cef52d93e91
Cole Higgins <chiggins@redhat.com>
2020-02-19 16:38:38 -0500

    fixed UI reporting for Entitlements

--- tests/foreman/ui/test_reporttemplates.py
+++ tests/foreman/ui/test_reporttemplates.py
@@ -15,10 +15,23 @@
 :Upstream: No
 """
 import csv
+import json
 import os
 
+import pytest
+import yaml
+from lxml import etree
 from nailgun import entities
 
+from robottelo import manifests
+from robottelo.api.utils import enable_rhrepo_and_fetchid
+from robottelo.api.utils import promote
+from robottelo.api.utils import upload_manifest
+from robottelo.constants import DEFAULT_SUBSCRIPTION_NAME
+from robottelo.constants import DISTRO_RHEL7
+from robottelo.constants import PRDS
+from robottelo.constants import REPOS
+from robottelo.constants import REPOSET
 from robottelo.datafactory import gen_string
 from robottelo.decorators import fixture
 from robottelo.decorators import stubbed
@@ -26,6 +39,7 @@ from robottelo.decorators import tier2
 from robottelo.decorators import tier3
 from robottelo.decorators import upgrade
 from robottelo.ui.utils import create_fake_host
+from robottelo.vm import VirtualMachine
 
 
 @fixture(scope='module')
@@ -36,6 +50,48 @@ def module_org():
 @fixture(scope='module')
 def module_loc():
     return entities.Location().create()
+
+
+@pytest.fixture(scope='module')
+def content_setup(module_org):
+    with manifests.clone() as manifest:
+        upload_manifest(module_org.id, manifest.content)
+    rh_repo_id = enable_rhrepo_and_fetchid(
+        basearch='x86_64',
+        org_id=module_org.id,
+        product=PRDS['rhel'],
+        repo=REPOS['rhst7']['name'],
+        reposet=REPOSET['rhst7'],
+        releasever=None,
+    )
+    rh_repo = entities.Repository(id=rh_repo_id).read()
+    rh_repo.sync()
+    custom_product = entities.Product(organization=module_org).create()
+    custom_repo = entities.Repository(
+        name=gen_string('alphanumeric').upper(),
+        product=custom_product).create()
+    custom_repo.sync()
+    lce = entities.LifecycleEnvironment(organization=module_org).create()
+    cv = entities.ContentView(
+        organization=module_org,
+        repository=[rh_repo_id, custom_repo.id],
+    ).create()
+    cv.publish()
+    cvv = cv.read().version[0].read()
+    promote(cvv, lce.id)
+    ak = entities.ActivationKey(
+        content_view=cv,
+        organization=module_org,
+        environment=lce,
+        auto_attach=True
+    ).create()
+    subscription = entities.Subscription(organization=module_org).search(
+        query={'search': 'name="{}"'.format(DEFAULT_SUBSCRIPTION_NAME)})[0]
+    ak.add_subscriptions(data={
+        'quantity': 1,
+        'subscription_id': subscription.id,
+    })
+    return module_org, ak
 
 
 @tier3
@@ -349,3 +405,67 @@ def test_negative_nonauthor_of_report_cant_download_it(session):
     :expectedresults: Report can't be downloaded. Error.
     :CaseImportance: High
     """
+
+
+@tier3
+def test_positive_gen_entitlements_reports_multiple_formats(session, content_setup, module_org):
+    """Generate reports using the Entitlements template in html, yaml, json, and csv format.
+
+        :id: b268663d-c213-4e59-8f81-61bec0838b1e
+
+
+        :setup: Installed Satellite with Organization, Activation key,
+                Content View, Content Host, and Subscriptions.
+
+        :steps:
+            1. Monitor -> Report Templates
+            2. Entitlements -> Generate
+            3. Click the dropdown to select output format
+            4. Submit
+
+        :expectedresults: reports are generated containing all the expected information
+                          regarding Entitlements for each output format.
+
+        :CaseImportance: High
+    """
+    with VirtualMachine(distro=DISTRO_RHEL7) as vm:
+        vm.install_katello_ca()
+        module_org, ak = content_setup
+        vm.register_contenthost(module_org.label, ak.name)
+        assert vm.subscribed
+        with session:
+            session.location.select('Default Location')
+            result_json = session.reporttemplate.generate(
+                "Entitlements", values={'output_format': 'JSON'}
+            )
+            with open(result_json) as json_file:
+                data_json = json.load(json_file)
+            assert any(entitlement['Name'] == vm.hostname for entitlement in data_json)
+            for entitlement in data_json:
+                assert any(entitlement['Subscription Name'] == DEFAULT_SUBSCRIPTION_NAME)
+            result_yaml = session.reporttemplate.generate(
+                "Entitlements", values={'output_format': 'YAML'}
+            )
+            with open(result_yaml) as yaml_file:
+                data_yaml = yaml.load(yaml_file, Loader=yaml.FullLoader)
+            assert any(entitlement['Name'] == vm.hostname for entitlement in data_yaml)
+            for entitlement in data_yaml:
+                assert any(entitlement['Subscription Name'] == DEFAULT_SUBSCRIPTION_NAME)
+            result_csv = session.reporttemplate.generate(
+                "Entitlements", values={'output_format': 'CSV'}
+            )
+            with open(result_csv) as csv_file:
+                data_csv = csv.DictReader(csv_file)
+                res = next(data_csv)
+            assert res['Name'] == vm.hostname
+            assert res['Subscription Name'] == DEFAULT_SUBSCRIPTION_NAME
+            result_html = session.reporttemplate.generate(
+                "Entitlements", values={'output_format': 'HTML'}
+            )
+            with open(result_html) as html_file:
+                parser = etree.HTMLParser()
+                tree = etree.parse(html_file, parser)
+                tree_result = etree.tostring(tree.getroot(),
+                                             pretty_print=True, method='html').decode()
+            assert vm.hostname in tree_result
+            assert DEFAULT_SUBSCRIPTION_NAME in tree_result
