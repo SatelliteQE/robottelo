11e8d3c6518edaee61c614d3af6c7aafee47b561
Stephen Wadeley <swadeley@redhat.com>
2020-03-19 20:30:08 +0000

    Add tests create capsule certs
     * With absolute paths
     * With relative paths

--- tests/foreman/sys/test_katello_certs_check.py
+++ tests/foreman/sys/test_katello_certs_check.py
@@ -53,10 +53,16 @@ class KatelloCertsCheckTestCase(TestCase):
         """Get host name, scripts, and create working directory."""
         super(KatelloCertsCheckTestCase, cls).setUpClass()
         _, cls.sat6_hostname = os.path.split(settings.server.hostname)
+        cls.capsule_hostname = 'capsule.example.com'
         cls.key_file_name = '{0}/{0}.key'.format(cls.sat6_hostname)
         cls.cert_file_name = '{0}/{0}.crt'.format(cls.sat6_hostname)
         cls.ca_bundle_file_name = 'cacert.crt'
         cls.SUCCESS_MSG = "Validation succeeded"
+        # Need a subdirectory under ssl-build with same name as Capsule name
+        with get_connection(timeout=100) as connection:
+            connection.run('mkdir ssl-build/{0}'.format(cls.capsule_hostname))
+            # Ignore creation error, but assert directory exists
+            assert connection.run('test -e ssl-build/{0}'.format(cls.capsule_hostname))
         upload_file(
             local_file=get_data_file('generate-ca.sh'), remote_file="generate-ca.sh",
         )
@@ -191,43 +197,93 @@ class KatelloCertsCheckTestCase(TestCase):
                 assert result.return_code == 0, 'Not all services are running'
 
     @destructive
-    @stubbed()
-    def test_positive_update_capsule_certs_using_absolute_path(self):
-        """Update certificates on a currently running internal capsule.
+    def test_positive_generate_capsule_certs_using_absolute_path(self):
+        """Create Capsule certs using absolute paths.
 
         :id: 72024757-be6f-49f0-8b88-c57c83f5e7e9
 
         :steps:
 
-            1. Generate custom certs
-            2. Run katello-certs-check with the required valid arguments
-            3. Run  capsule-certs-generate with custom certs and absolute path
+            1. Use Certificates generated in class setup
+            2. Run capsule-certs-generate with custom certs and absolute path
                for --certs-tar
+            3. Assert the certs tar file was created.
 
-        :expectedresults: Katello-certs should be updated.
+        :expectedresults: Capsule certs are generated.
 
-        :CaseAutomation: notautomated
+        :BZ: 1466688
+
+        :CaseAutomation: automated
         """
+        with get_connection(timeout=300) as connection:
+            connection.run('mkdir -p /root/capsule_cert')
+            connection.run(
+                'cp "{0}" /root/capsule_cert/ca_cert_bundle.pem'.format(self.ca_bundle_file_name)
+            )
+            connection.run(
+                'cp "{0}" /root/capsule_cert/capsule_cert.pem'.format(self.cert_file_name)
+            )
+            connection.run(
+                'cp "{0}" /root/capsule_cert/capsule_cert_key.pem'.format(self.key_file_name)
+            )
+            result = connection.run(
+                'capsule-certs-generate '
+                '--foreman-proxy-fqdn {} '
+                '--certs-tar /root/capsule_cert/capsule_certs_Abs.tar '
+                '--server-cert /root/capsule_cert/capsule_cert.pem '
+                '--server-key /root/capsule_cert/capsule_cert_key.pem '
+                '--server-ca-cert /root/capsule_cert/ca_cert_bundle.pem '
+                '--certs-update-server'.format(self.capsule_hostname),
+                timeout=80,
+            )
+            assert result.return_code == 0, 'Capsule certs generate script failed.'
+            # assert the certs.tar was built
+            assert connection.run('test -e /root/capsule_cert/capsule_certs_Abs.tar')
 
     @destructive
-    @stubbed()
     @upgrade
-    def test_positive_update_capsule_certs_using_relative_path(self):
-        """Update certificates on a currently running internal capsule.
+    def test_positive_generate_capsule_certs_using_relative_path(self):
+        """Create Capsule certs using relative paths.
 
         :id: 50df0b87-d2d3-42fb-86d5-988ebaaa9ba3
 
         :steps:
 
-            1. Generate custom certs
-            2. Run katello-certs-check with the required valid arguments
-            3. Run  capsule-certs-generate with custom certs and relative path
+            1. Use Certificates generated in class setup
+            2. Run capsule-certs-generate with custom certs and relative path
                for --certs-tar
+            3. Assert the certs tar file was created.
 
-        :expectedresults: Katello-certs should be updated.
+        :expectedresults: Capsule certs are generated.
 
-        :CaseAutomation: notautomated
+        :BZ: 1466688
+
+        :CaseAutomation: automated
         """
+        with get_connection(timeout=300) as connection:
+            connection.run('mkdir -p /root/capsule_cert')
+            connection.run(
+                'cp "{0}" /root/capsule_cert/ca_cert_bundle.pem'.format(self.ca_bundle_file_name)
+            )
+            connection.run(
+                'cp "{0}" /root/capsule_cert/capsule_cert.pem'.format(self.cert_file_name)
+            )
+            connection.run(
+                'cp "{0}" /root/capsule_cert/capsule_cert_key.pem'.format(self.key_file_name)
+            )
+            result = connection.run(
+                'capsule-certs-generate '
+                '--foreman-proxy-fqdn {} '
+                '--certs-tar ~/capsule_cert/capsule_certs_Rel.tar '
+                '--server-cert ~/capsule_cert/capsule_cert.pem '
+                '--server-key ~/capsule_cert/capsule_cert_key.pem '
+                '--server-ca-cert ~/capsule_cert/ca_cert_bundle.pem '
+                '--certs-update-server'.format(self.capsule_hostname),
+                timeout=80,
+            )
+            assert result.return_code == 0, 'Capsule certs generate script failed.'
+            # assert the certs.tar was built
+            assert connection.run('test -e root/capsule_cert/capsule_certs_Rel.tar')
 
     @stubbed()
     @tier1
