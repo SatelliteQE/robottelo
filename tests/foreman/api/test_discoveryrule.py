09357641abc97becc8fee77bcde4ec0c7cc6fa6e
Dominik Vagala <d.vagala@gmail.com>
2020-03-30 17:29:37 +0200

    Switch to pytest

--- tests/foreman/api/test_discoveryrule.py
+++ tests/foreman/api/test_discoveryrule.py
@@ -15,6 +15,7 @@
 
 :Upstream: No
 """
+import pytest
 from fauxfactory import gen_choice
 from fauxfactory import gen_integer
 from fauxfactory import gen_string
@@ -48,66 +49,56 @@ class DiscoveryRuleTestCase(APITestCase):
             'model = KVM',
             'Organization = Default_Organization',
         ]
+        # Create discovery rule
         name = gen_choice(valid_data_list())
-
-        with self.subTest(name):
-            # Create discovery rule
-            search = gen_choice(searches)
-            hostname = 'myhost-<%= rand(99999) %>'
-            org = entities.Organization().create()
-            loc = entities.Location().create()
-            hostgroup = entities.HostGroup(organization=[org]).create()
-            discovery_rule = entities.DiscoveryRule(
-                name=name,
-                search_=search,
-                hostname=hostname,
-                organization=[org],
-                location=[loc],
-                hostgroup=hostgroup,
-            ).create()
-            self.assertEqual(name, discovery_rule.name)
-            self.assertEqual(hostname, discovery_rule.hostname)
-            self.assertEqual(search, discovery_rule.search_)
-            self.assertEqual(org.name, discovery_rule.organization[0].read().name)
-            self.assertEqual(loc.name, discovery_rule.location[0].read().name)
-            self.assertEqual(True, discovery_rule.enabled)
-            # Update discovery rule
-            name = gen_choice(valid_data_list())
-            search = 'Location = Default_Location'
-            org = entities.Organization().create()
-            loc = entities.Location().create()
-            hostgroup = entities.HostGroup(organization=[org]).create()
-            max_count = gen_integer(1, 100)
-            enabled = False
-            discovery_rule.name = name
-            discovery_rule.search_ = search
-            discovery_rule.organization = [org]
-            discovery_rule.location = [loc]
-            discovery_rule.hostgroup = hostgroup
-            discovery_rule.max_count = max_count
-            discovery_rule.enabled = enabled
-            discovery_rule = discovery_rule.update(
-                [
-                    'name',
-                    'organization',
-                    'location',
-                    'hostgroup',
-                    'search_',
-                    'max_count',
-                    'enabled',
-                ]
-            )
-            self.assertEqual(name, discovery_rule.name)
-            self.assertEqual(search, discovery_rule.search_)
-            self.assertEqual(org.name, discovery_rule.organization[0].read().name)
-            self.assertEqual(loc.name, discovery_rule.location[0].read().name)
-            self.assertEqual(hostgroup.id, discovery_rule.hostgroup.id)
-            self.assertEqual(max_count, discovery_rule.max_count)
-            self.assertEqual(enabled, discovery_rule.enabled)
-            # Delete discovery rule
-            discovery_rule.delete()
-            with self.assertRaises(HTTPError):
-                discovery_rule.read()
+        search = gen_choice(searches)
+        hostname = 'myhost-<%= rand(99999) %>'
+        org = entities.Organization().create()
+        loc = entities.Location().create()
+        hostgroup = entities.HostGroup(organization=[org]).create()
+        discovery_rule = entities.DiscoveryRule(
+            name=name,
+            search_=search,
+            hostname=hostname,
+            organization=[org],
+            location=[loc],
+            hostgroup=hostgroup,
+        ).create()
+        assert name == discovery_rule.name
+        assert hostname == discovery_rule.hostname
+        assert search == discovery_rule.search_
+        assert org.name == discovery_rule.organization[0].read().name
+        assert loc.name == discovery_rule.location[0].read().name
+        assert discovery_rule.enabled is True
+        # Update discovery rule
+        name = gen_choice(valid_data_list())
+        search = 'Location = Default_Location'
+        org = entities.Organization().create()
+        loc = entities.Location().create()
+        hostgroup = entities.HostGroup(organization=[org]).create()
+        max_count = gen_integer(1, 100)
+        enabled = False
+        discovery_rule.name = name
+        discovery_rule.search_ = search
+        discovery_rule.organization = [org]
+        discovery_rule.location = [loc]
+        discovery_rule.hostgroup = hostgroup
+        discovery_rule.max_count = max_count
+        discovery_rule.enabled = enabled
+        discovery_rule = discovery_rule.update(
+            ['name', 'organization', 'location', 'hostgroup', 'search_', 'max_count', 'enabled']
+        )
+        assert name == discovery_rule.name
+        assert search == discovery_rule.search_
+        assert org.name == discovery_rule.organization[0].read().name
+        assert loc.name == discovery_rule.location[0].read().name
+        assert hostgroup.id == discovery_rule.hostgroup.id
+        assert max_count == discovery_rule.max_count
+        assert enabled == discovery_rule.enabled
+        # Delete discovery rule
+        discovery_rule.delete()
+        with pytest.raises(HTTPError):
+            discovery_rule.read()
 
     @tier1
     def test_negative_create_with_invalid_host_limit_and_priority(self):
@@ -117,7 +108,7 @@ class DiscoveryRuleTestCase(APITestCase):
 
         :expectedresults: Validation error should be raised
         """
-        with self.assertRaises(HTTPError):
+        with pytest.raises(HTTPError):
             entities.DiscoveryRule(max_count=gen_string('alpha')).create()
-        with self.assertRaises(HTTPError):
+        with pytest.raises(HTTPError):
             entities.DiscoveryRule(priority=gen_string('alpha')).create()
