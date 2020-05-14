fac1c1ba789b18f0713764cb29b0d08ede40c693
Lukáš Hellebrandt <lhellebr@redhat.com>
2020-05-14 08:13:27 +0200

    Mark for closed loop: BZ1818076 (#7778)

--- tests/foreman/cli/test_remoteexecution.py
+++ tests/foreman/cli/test_remoteexecution.py
@@ -493,6 +493,8 @@ class TestRemoteExecution:
         :id: 811c7747-bec6-1a2d-8e5c-b5045d3fbc0d
 
         :expectedresults: The job passes, installs Receptor that peers with c.r.c
+
+        :BZ: 1818076
         """
         template_name = 'Configure Cloud Connector'
         invocation = make_job_invocation(
