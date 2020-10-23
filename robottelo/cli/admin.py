a1efcc51893830d70795edc2b182710999b8b3cc
Gaurav Talreja <gtalreja@redhat.com>
2020-10-23 19:06:31 +0530

    Add qe_test_coverage for Hammer BZ-1760773
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>

---
+++ robottelo/cli/admin.py
@@ -0,0 +1,27 @@
+"""
+Usage:
+    hammer admin [OPTIONS] SUBCOMMAND [ARG] ...
+
+Parameters:
+ SUBCOMMAND                    Subcommand
+ [ARG] ...                     Subcommand arguments
+
+Subcommands:
+ logging                       Logging verbosity level setup
+
+Options:
+ -h, --help                    Print help
+"""
+from robottelo.cli.base import Base
+
+
+class Admin(Base):
+    """Administrative server-side tasks"""
+
+    command_base = 'admin'
+
+    @classmethod
+    def logging(cls, options=None):
+        """Logging verbosity level setup"""
+        cls.command_sub = 'logging'
+        return cls.execute(cls._construct_command(options), output_format='csv')
