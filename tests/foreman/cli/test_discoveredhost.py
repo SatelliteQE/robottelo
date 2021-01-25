48339f7208c48b3625af0472c33a12d1a3a56473
rplevka <rplevka@redhat.com>
2021-01-25 18:04:56 +0100

    remove or merge redundant or non needed tests

--- tests/foreman/cli/test_discoveredhost.py
+++ tests/foreman/cli/test_discoveredhost.py
@@ -131,72 +131,6 @@ class DiscoveredTestCase(CLITestCase):
             self.assertIsNotNone(host)
 
     @pytest.mark.tier3
-    def test_positive_pxe_less_with_dhcp_unattended(self):
-        """Discover a host with dhcp via bootable discovery ISO by setting
-        "proxy.type=proxy" in PXE default in unattended mode.
-
-        :id: a23bd065-8385-4876-aa45-e38470be79b8
-
-        :Setup: Provisioning should be configured
-
-        :Steps: Boot a host/VM using modified discovery ISO.
-
-        :expectedresults: Host should be successfully discovered
-
-        :CaseImportance: Critical
-        """
-        with LibvirtGuest(boot_iso=True) as pxe_less_host:
-            hostname = pxe_less_host.guest_name
-            host = self._assertdiscoveredhost(hostname)
-            self.assertIsNotNone(host)
-
-    @pytest.mark.stubbed
-    @pytest.mark.tier3
-    def test_positive_custom_facts_pxeless_discovery(self):
-        """Check if defined custom facts of pxeless host are correctly
-        displayed under host's facts
-
-        :id: 0d39f2cc-654f-41ed-8e31-4d9a37c5b9b1
-
-        :Setup:
-
-            1. Provisioning should be configured
-            2. Host is already discovered
-
-        :Steps: Validate specified custom facts
-
-        :expectedresults: All defined custom facts should be displayed
-            correctly
-
-        :CaseAutomation: NotAutomated
-
-        :CaseImportance: High
-        """
-
-    @pytest.mark.stubbed
-    @pytest.mark.tier3
-    def test_positive_custom_facts_pxe_discovery(self):
-        """Check if defined custom facts of pxe-based discovered host are
-        correctly displayed under host's facts
-
-        :id: 2c65925c-05d9-4f6d-b1b7-1fa1492c95a8
-
-        :Setup:
-
-            1. Provisioning should be configured
-            2. Host is already discovered
-
-        :Steps: Validate specified custom facts
-
-        :expectedresults: All defined custom facts should be displayed
-            correctly
-
-        :CaseAutomation: NotAutomated
-
-        :CaseImportance: High
-        """
-
-    @pytest.mark.tier3
     @pytest.mark.upgrade
     def test_positive_provision_pxeless_bios_syslinux(self):
         """Provision and discover the pxe-less BIOS host from cli using SYSLINUX
@@ -270,120 +204,6 @@ class DiscoveredTestCase(CLITestCase):
             # hosts anymore
             with self.assertRaises(CLIReturnCodeError):
                 DiscoveredHost.info({'id': discovered_host['id']})
-
-    @pytest.mark.stubbed
-    @pytest.mark.tier3
-    def test_positive_provision_pxeless_uefi_grub(self):
-        """Provision and discover the pxe-less UEFI host from cli using GRUB
-        loader
-
-        :id: 0704ec80-dfaf-4f25-ad6b-a4cd3f33a1cb
-
-        :Setup:
-            1. Craft the FDI with remaster the image to have ssh enabled
-            2. Synchronize RHEL6 repo (needed for GRUB)
-
-        :Steps:
-            1. Create a UEFI VM and set it to boot from the FDI
-            2. Run assertion steps #1-2
-            3. Provision the discovered host using UEFI_GRUB PXE loader
-            4. Run assertion steps #3-7
-
-        :expectedresults: Host should be provisioned successfully
-            1. [TBD] Ensure FDI loaded and successfully sent out facts
-
-                1.1 ping vm
-                1.2 ssh to the VM and read the logs (if ssh enabled)
-                1.3 optionally sniff the HTTP traffic coming from the host
-
-            2. Ensure host appeared in Discovered Hosts on satellite
-            3. [TBD] Ensure the kexec was successful (e.g. the kexec request
-               result in production.log)
-            4. [TBD] Ensure anaconda loaded and the installation finished
-            5. [TBD] Ensure the host is provisioned with correct attributes
-            6. Ensure the host is created in Hosts
-            7. Ensure the entry from discovered host list disappeared
-
-        :CaseAutomation: NotAutomated
-
-        :CaseImportance: High
-        """
-
-    @pytest.mark.stubbed
-    @pytest.mark.tier3
-    def test_positive_provision_pxeless_uefi_grub2(self):
-        """Provision and discover the pxe-less UEFI host from cli using GRUB2
-        loader
-
-        :id: 81b0b586-617c-4100-80e6-ce924fb26d88
-
-        :Setup:
-            1. Craft the FDI with remaster the image to have ssh enabled
-            2. Synchronize RHEL7+ repo (needed for GRUB2)
-
-        :Steps:
-            1. Create a UEFI VM and set it to boot from the FDI
-            2. Run assertion steps #1-2
-            3. Provision the discovered host using GRUB2_UEFI PXE loader
-            4. Run assertion steps #3-7
-
-        :expectedresults: Host should be provisioned successfully
-            1. Ensure FDI loaded and successfully sent out facts
-
-                1.1 ping vm
-                1.2 ssh to the VM and read the logs (if ssh enabled)
-                1.3 optionally sniff the HTTP traffic coming from the host
-
-            2. Ensure host appeared in Discovered Hosts on satellite
-            3. Ensure the kexec was successful (e.g. the kexec request
-               result in production.log)
-            4. Ensure anaconda loaded and the installation finished
-            5. Ensure the host is provisioned with correct attributes
-            6. Ensure the host is created in Hosts
-            7. Ensure the entry from discovered host list disappeared
-
-        :CaseAutomation: NotAutomated
-
-        :CaseImportance: High
-        """
-
-    @pytest.mark.stubbed
-    @pytest.mark.tier3
-    def test_positive_provision_pxeless_uefi_grub2_secureboot(self):
-        """Provision and discover the pxe-less UEFI SB host from cli using GRUB2
-        loader
-
-        :id: b9896977-9f4a-4f3d-85d9-660f62e76448
-
-        :Setup:
-            1. Craft the FDI with remaster the image to have ssh enabled
-            2. Synchronize RHEL7+ repo (needed for GRUB2)
-
-        :Steps:
-            1. Create a UEFI VM with secureboot and set it to boot from the FDI
-            2. Run assertion steps #1-2
-            3. Provision the discovered host using GRUB2_UEFI_SB PXE loader
-            4. Run assertion steps #3-7
-
-        :expectedresults: Host should be provisioned successfully
-            1. Ensure FDI loaded and successfully sent out facts
-
-                1.1 ping vm
-                1.2 ssh to the VM and read the logs (if ssh enabled)
-                1.3 optionally sniff the HTTP traffic coming from the host
-
-            2. Ensure host appeared in Discovered Hosts on satellite
-            3. Ensure the kexec was successful (e.g. the kexec request
-               result in production.log)
-            4. Ensure anaconda loaded and the installation finished
-            5. Ensure the host is provisioned with correct attributes
-            6. Ensure the host is created in Hosts
-            7. Ensure the entry from discovered host list disappeared
-
-        :CaseAutomation: NotAutomated
-
-        :CaseImportance: High
-        """
 
     @pytest.mark.tier3
     @pytest.mark.upgrade
@@ -477,55 +297,6 @@ class DiscoveredTestCase(CLITestCase):
 
     @pytest.mark.stubbed
     @pytest.mark.tier3
-    def test_positive_provision_pxe_host_with_uefi_grub(self):
-        """Provision the pxe-based UEFI discovered host from cli using PXEGRUB
-        loader
-
-        :id: fc86e56e-9dc6-48ba-aaa7-8b6611b973c9
-
-        :Setup:
-            1. Create an UEFI VM and set it to boot from a network
-            2. for getting more detailed info from FDI, remaster the image to
-               have ssh enabled
-
-        :steps:
-            1. Build a default PXE template
-            2. Run assertion step #1
-            3. Boot the VM (from NW)
-            4. Run assertion steps #2-4
-            5. Provision the discovered host
-            6. Run assertion steps #5-9
-
-        :expectedresults: Host should be provisioned successfully
-            1. Ensure the tftpboot files are updated
-
-              1.1 Ensure fdi-image files have been placed under tftpboot/boot/
-              1.2 Ensure the 'default' pxelinux config has been placed under
-              tftpboot/pxelinux.cfg/
-              1.3 Ensure the discovery section exists inside pxelinux config,
-              it leads to the FDI kernel and the ONTIMEOUT is set to discovery
-
-            2. Ensure PXE handoff goes as expected (tcpdump -p tftp)
-            3. Ensure FDI loaded and successfully sent out facts
-
-                3.1 ping vm
-                3.2 ssh to the VM and read the logs (if ssh enabled)
-                3.3 optionally sniff the HTTP traffic coming from the host
-
-            4. Ensure host appeared in Discovered Hosts on satellite
-            5. Ensure the tftpboot files are updated for the hosts mac
-            6. Ensure PXE handoff goes as expected (tcpdump -p tftp)
-            7. Optionally ensure anaconda loaded and the installation finished
-            8. Ensure the host is provisioned with correct attributes
-            9. Ensure the entry from discovered host list disappeared
-
-        :CaseImportance: High
-
-        :CaseAutomation: NotAutomated
-        """
-
-    @pytest.mark.stubbed
-    @pytest.mark.tier3
     def test_positive_provision_pxe_host_with_uefi_grub2(self):
         """Provision the pxe-based UEFI discovered host from cli using PXEGRUB2
         loader
@@ -574,79 +345,9 @@ class DiscoveredTestCase(CLITestCase):
         :CaseImportance: High
         """
 
-    @pytest.mark.stubbed
     @pytest.mark.tier3
-    def test_positive_provision_pxe_host_with_uefi_grub2_sb(self):
-        """Provision the pxe-based UEFI Secureboot discovered host from cli
-        using PXEGRUB2 loader
-
-        :id: 285ebbac-934b-45ae-a0ca-0dc450836428
-
-        :Setup:
-            1. Create an UEFI SB VM and set it to boot from a network
-            2. Synchronize RHEL6 kickstart repo (rhel7 kernel too new for GRUB)
-            3. for getting more detailed info from FDI, remaster the image to
-               have ssh enabled
-
-        :steps:
-            1. Build a default PXE template
-            2. Run assertion step #1
-            3. Boot the VM (from NW)
-            4. Run assertion steps #2-4
-            5. Provision the discovered host
-            6. Run assertion steps #5-9
-
-        :expectedresults: Host should be provisioned successfully
-            1. Ensure the tftpboot files are updated
-
-              1.1 Ensure fdi-image files have been placed under tftpboot/boot/
-              1.2 Ensure the 'default' pxelinux config has been placed under
-              tftpboot/pxelinux.cfg/
-              1.3 Ensure the discovery section exists inside pxelinux config,
-              it leads to the FDI kernel and the ONTIMEOUT is set to discovery
-
-            2. Ensure PXE handoff goes as expected (tcpdump -p tftp)
-            3. Ensure FDI loaded and successfully sent out facts
-
-                3.1 ping vm
-                3.2 ssh to the VM and read the logs (if ssh enabled)
-                3.3 optionally sniff the HTTP traffic coming from the host
-
-            4. Ensure host appeared in Discovered Hosts on satellite
-            5. Ensure the tftpboot files are updated for the hosts mac
-            6. Ensure PXE handoff goes as expected (tcpdump -p tftp)
-            7. Optionally ensure anaconda loaded and the installation finished
-            8. Ensure the host is provisioned with correct attributes
-            9. Ensure the entry from discovered host list disappeared
-
-        :CaseAutomation: NotAutomated
-
-        :CaseImportance: High
-        """
-
-    @pytest.mark.tier3
-    def test_positive_delete_pxeless_host(self):
-        """Delete the selected pxe-less discovered host
-
-        :id: 3959abd7-a1c0-418f-a75a-dec06b5ea0e2
-
-        :Setup: Host should already be discovered
-
-        :expectedresults: Selected host should be removed successfully
-
-        :CaseImportance: High
-        """
-        with LibvirtGuest(boot_iso=True) as pxe_less_host:
-            hostname = pxe_less_host.guest_name
-            host = self._assertdiscoveredhost(hostname)
-            self.assertIsNotNone(host)
-        DiscoveredHost.delete({'id': host['id']})
-        with self.assertRaises(CLIReturnCodeError):
-            DiscoveredHost.info({'id': host['id']})
-
-    @pytest.mark.tier3
-    def test_positive_delete_pxe_host(self):
-        """Delete the selected pxe-based discovered host
+    def test_positive_delete(self):
+        """Delete the selected discovered host
 
         :id: c4103de8-145c-4a7d-b837-a1dec97231a2
 
@@ -677,7 +378,7 @@ class DiscoveredTestCase(CLITestCase):
 
         :CaseAutomation: NotAutomated
 
-        :CaseImportance: High
+        :CaseImportance: Medium
         """
 
     @pytest.mark.stubbed
@@ -693,7 +394,7 @@ class DiscoveredTestCase(CLITestCase):
 
         :CaseAutomation: NotAutomated
 
-        :CaseImportance: High
+        :CaseImportance: Medium
         """
 
     @pytest.mark.stubbed
@@ -709,7 +410,7 @@ class DiscoveredTestCase(CLITestCase):
 
         :CaseAutomation: NotAutomated
 
-        :CaseImportance: Medium
+        :CaseImportance: High
         """
 
     @pytest.mark.stubbed
@@ -760,32 +461,17 @@ class DiscoveredTestCase(CLITestCase):
 
     @pytest.mark.stubbed
     @pytest.mark.tier3
-    def test_positive_list_discovered_host(self):
-        """List pxe-based and pxe-less discovered hosts
-
-        :id: 3a827080-3fab-4f64-a830-1b41841aa2df
-
-        :expectedresults: Host should be discovered and listed with names.
-
-        :CaseAutomation: NotAutomated
-
-        :CaseImportance: High
-        """
-
-    @pytest.mark.stubbed
-    @pytest.mark.tier3
     def test_positive_assign_discovery_manager_role(self):
         """Assign 'Discovery_Manager' role to a normal user
 
         :id: f694c361-5fbb-4c3a-b2ff-6dfe6ea14820
 
-        :expectedresults: User should be able to view, provision, edit and
-            destroy one or more discovered host as well view, create_new, edit,
-            execute and delete discovery rules.
+        :expectedresults: User should be able to view, provision, edit and destroy one or more
+            discovered host as well as view, create_new, edit, execute and delete discovery rules
 
         :CaseAutomation: NotAutomated
 
-        :CaseImportance: High
+        :CaseImportance: Medium
         """
 
     @pytest.mark.stubbed
@@ -796,11 +482,11 @@ class DiscoveredTestCase(CLITestCase):
         :id: 873e8411-563d-4bf9-84ce-62e522410cfe
 
         :expectedresults: User should be able to list, provision, and destroy
-            one or more discovered host
+            one or more discovered hosts
 
         :CaseAutomation: NotAutomated
 
-        :CaseImportance: High
+        :CaseImportance: Medium
         """
 
     @pytest.mark.stubbed
@@ -811,68 +497,28 @@ class DiscoveredTestCase(CLITestCase):
 
         :id: 10071eb1-ec2a-4061-b798-480643d8f4ed
 
-        :expectedresults: Host should be discovered with name as
-            'Hostname_prefix + hostname_facts'.
+        :expectedresults: Host should be discovered with name as 'hostname_prefix + hostname_facts'
+
+        :CaseAutomation: NotAutomated
+
+        :CaseImportance: Medium
+        """
+
+    @pytest.mark.stubbed
+    @pytest.mark.tier3
+    def test_positive_list_facts(self):
+        """Check if defined facts of a discovered host are
+        correctly displayed under host's facts
+
+        :id: 2c65925c-05d9-4f6d-b1b7-1fa1492c95a8
+
+        :Setup: 1. Provisioning is configured and Host is already discovered
+
+        :Steps: Validate specified builtin and custom facts
+
+        :expectedresults: All checked facts should be displayed correctly
 
         :CaseAutomation: NotAutomated
 
         :CaseImportance: High
         """
-
-    @pytest.mark.tier3
-    @pytest.mark.upgrade
-    def test_positive_provision_pxe_host_with_parameters(self):
-        """Provision the pxe-based BIOS discovered host with host parameters from cli using
-        SYSLINUX loader
-
-        :id: 4b315fe1-2eba-4e59-bd0a-9d16ce319ce2
-
-        :Setup:
-            1. Create a BIOS VM and set it to boot from a network
-
-        :steps:
-            1. Build a default PXE template
-            2. Configure HostGroup to be used for provisioning the discovered host
-               after discovery
-            3. PXE Boot the VM (from Network)
-            4. Wait for Host to be discovered by Satellite
-            5. Provision the discovered host with host parameters
-
-        :expectedresults:
-            1. Ensure host appeared in Discovered Hosts on satellite
-            2. Ensure the host is provisioned with correct and all the host parameters
-               given during provisioning
-            3. Ensure the discovered host is no more available to provision
-
-        :CaseImportance: High
-
-        :BZ: 1572947
-        """
-        param1_key, param1_value = gen_string('alpha'), gen_string('alphanumeric')
-        param2_key, param2_value = gen_string('alpha'), gen_string('alphanumeric')
-        host_params = [f'{param1_key}={param1_value}, {param2_key}={param2_value}']
-        if not self.configured_env:
-            self.__class__.configured_env = configure_env_for_provision(org=self.org, loc=self.loc)
-        with LibvirtGuest() as pxe_host:
-            hostname = pxe_host.guest_name
-            discovered_host = self._assertdiscoveredhost(hostname)
-            self.assertIsNotNone(discovered_host)
-            DiscoveredHost.provision(
-                {
-                    'name': discovered_host['name'],
-                    'hostgroup': self.configured_env['hostgroup']['name'],
-                    'root-password': gen_string('alphanumeric'),
-                    'parameters': host_params,
-                }
-            )
-            provisioned_host = Host.info(
-                {
-                    'name': '{}.{}'.format(
-                        discovered_host['name'], self.configured_env['domain']['name']
-                    )
-                }
-            )
-            self.assertEqual(provisioned_host['parameters'][str(param1_key).lower()], param1_value)
-            self.assertEqual(provisioned_host['parameters'][str(param2_key).lower()], param2_value)
-            with self.assertRaises(CLIReturnCodeError):
-                DiscoveredHost.info({'id': discovered_host['id']})
