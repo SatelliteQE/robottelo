068fcd7a3b2dc1400c13fb4de6547463115e9d57
MisterJekyll <38880644+akhil-jha@users.noreply.github.com>
2021-02-08 19:17:47 +0530

    parameterize (#8193)

--- tests/foreman/api/test_computeprofile.py
+++ tests/foreman/api/test_computeprofile.py
@@ -19,102 +19,108 @@ from nailgun import entities
 from requests.exceptions import HTTPError
 
 from robottelo.datafactory import invalid_values_list
+from robottelo.datafactory import parametrized
 from robottelo.datafactory import valid_data_list
-from robottelo.test import APITestCase
 
 
-class ComputeProfileTestCase(APITestCase):
-    """Tests for compute profiles."""
+@pytest.mark.parametrize('name', **parametrized(valid_data_list()))
+@pytest.mark.tier1
+def test_positive_create_with_name(name):
+    """Create new Compute Profile using different names
 
-    @pytest.mark.tier1
-    def test_positive_create_with_name(self):
-        """Create new Compute Profile using different names
+    :id: 97d04911-9368-4674-92c7-1e3ff114bc18
 
-        :id: 97d04911-9368-4674-92c7-1e3ff114bc18
+    :expectedresults: Compute Profile is created
 
-        :expectedresults: Compute Profile is created
+    :CaseImportance: Critical
 
-        :CaseImportance: Critical
+    :CaseLevel: Component
 
-        :CaseLevel: Component
-        """
-        for name in valid_data_list():
-            with self.subTest(name):
-                profile = entities.ComputeProfile(name=name).create()
-                self.assertEqual(name, profile.name)
+    :parametrized: yes
+    """
+    profile = entities.ComputeProfile(name=name).create()
+    assert name == profile.name
 
-    @pytest.mark.tier1
-    def test_negative_create(self):
-        """Attempt to create Compute Profile using invalid names only
 
-        :id: 2d34a1fd-70a5-4e59-b2e2-86fbfe8e31ab
+@pytest.mark.parametrize('name', **parametrized(invalid_values_list()))
+@pytest.mark.tier1
+def test_negative_create(name):
+    """Attempt to create Compute Profile using invalid names only
 
-        :expectedresults: Compute Profile is not created
+    :id: 2d34a1fd-70a5-4e59-b2e2-86fbfe8e31ab
 
-        :CaseImportance: Critical
+    :expectedresults: Compute Profile is not created
 
-        :CaseLevel: Component
-        """
-        for name in invalid_values_list():
-            with self.subTest(name):
-                with self.assertRaises(HTTPError):
-                    entities.ComputeProfile(name=name).create()
+    :CaseImportance: Critical
 
-    @pytest.mark.tier1
-    def test_positive_update_name(self):
-        """Update selected Compute Profile entity using proper names
+    :CaseLevel: Component
 
-        :id: c79193d7-2e0f-4ed9-b947-05feeddabfda
+    :parametrized: yes
+    """
+    with pytest.raises(HTTPError):
+        entities.ComputeProfile(name=name).create()
 
-        :expectedresults: Compute Profile is updated.
 
-        :CaseImportance: Critical
+@pytest.mark.parametrize('new_name', **parametrized(valid_data_list()))
+@pytest.mark.tier1
+def test_positive_update_name(new_name):
+    """Update selected Compute Profile entity using proper names
 
-        :CaseLevel: Component
-        """
-        profile = entities.ComputeProfile().create()
-        for new_name in valid_data_list():
-            with self.subTest(new_name):
-                updated_profile = entities.ComputeProfile(id=profile.id, name=new_name).update(
-                    ['name']
-                )
-                self.assertEqual(new_name, updated_profile.name)
+    :id: c79193d7-2e0f-4ed9-b947-05feeddabfda
 
-    @pytest.mark.tier1
-    def test_negative_update_name(self):
-        """Attempt to update Compute Profile entity using invalid names only
+    :expectedresults: Compute Profile is updated.
 
-        :id: 042b40d5-a78b-4e65-b5cb-5b270b800b37
+    :CaseImportance: Critical
 
-        :expectedresults: Compute Profile is not updated.
+    :CaseLevel: Component
 
-        :CaseImportance: Critical
+    :parametrized: yes
+    """
+    profile = entities.ComputeProfile().create()
+    entities.ComputeProfile(id=profile.id, name=new_name).update(['name'])
+    updated_profile = entities.ComputeProfile(id=profile.id).read()
+    assert new_name == updated_profile.name
 
-        :CaseLevel: Component
-        """
-        profile = entities.ComputeProfile().create()
-        for new_name in invalid_values_list():
-            with self.subTest(new_name):
-                with self.assertRaises(HTTPError):
-                    entities.ComputeProfile(id=profile.id, name=new_name).update(['name'])
-                updated_profile = entities.ComputeProfile(id=profile.id).read()
-                self.assertNotEqual(new_name, updated_profile.name)
 
-    @pytest.mark.tier1
-    def test_positive_delete(self):
-        """Delete Compute Profile entity
+@pytest.mark.parametrize('new_name', **parametrized(invalid_values_list()))
+@pytest.mark.tier1
+def test_negative_update_name(new_name):
+    """Attempt to update Compute Profile entity using invalid names only
 
-        :id: 0a620e23-7ba6-4178-af7a-fd1e332f478f
+    :id: 042b40d5-a78b-4e65-b5cb-5b270b800b37
 
-        :expectedresults: Compute Profile is deleted successfully.
+    :expectedresults: Compute Profile is not updated.
 
-        :CaseImportance: Critical
+    :CaseImportance: Critical
 
-        :CaseLevel: Component
-        """
-        for name in valid_data_list():
-            with self.subTest(name):
-                profile = entities.ComputeProfile(name=name).create()
-                profile.delete()
-                with self.assertRaises(HTTPError):
-                    entities.ComputeProfile(id=profile.id).read()
+    :CaseLevel: Component
+
+    :parametrized: yes
+    """
+    profile = entities.ComputeProfile().create()
+    with pytest.raises(HTTPError):
+        entities.ComputeProfile(id=profile.id, name=new_name).update(['name'])
+    updated_profile = entities.ComputeProfile(id=profile.id).read()
+    assert new_name != updated_profile.name
+
+
+@pytest.mark.parametrize('new_name', **parametrized(valid_data_list()))
+@pytest.mark.tier1
+def test_positive_delete(new_name):
+    """Delete Compute Profile entity
+
+    :id: 0a620e23-7ba6-4178-af7a-fd1e332f478f
+
+    :expectedresults: Compute Profile is deleted successfully.
+
+    :CaseImportance: Critical
+
+    :CaseLevel: Component
+
+    :parametrized: yes
+    """
+    for new_name in valid_data_list():
+        profile = entities.ComputeProfile(name=new_name).create()
+        profile.delete()
+        with pytest.raises(HTTPError):
+            entities.ComputeProfile(id=profile.id).read()
