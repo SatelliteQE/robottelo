039bfee8726c7aba59a32af31e8d5077554b78bf
Peter Ondrejka <pondrejk@users.noreply.github.com>
2021-02-17 10:28:44 +0100

    fixed NotAutomated tag in test_positive_update_all_locales
    It actually is automated

--- tests/foreman/cli/test_myaccount.py
+++ tests/foreman/cli/test_myaccount.py
@@ -149,7 +149,7 @@ class MyAccountTestCase(CLITestCase):
 
         :expectedresults: Current User is updated
 
-        :CaseAutomation: NotAutomated
+        :CaseAutomation: Automated
 
         :CaseImportance: Critical
         """
