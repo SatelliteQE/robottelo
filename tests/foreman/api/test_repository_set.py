dc99d35dd0b82c29af31c3284cd493e14eeb8065
Tasos Papaioannou <tpapaioa@redhat.com>
2021-02-18 12:45:04 -0500

    Pytest conversion for test_repository_set

--- tests/foreman/api/test_repository_set.py
+++ tests/foreman/api/test_repository_set.py
@@ -1,8 +1,7 @@
-"""Unit tests for the ``repository_sets`` paths.
+"""Tests for ``repository_sets`` API endpoints.
 
-A full API reference for products can be found here:
-http://www.katello.org/docs/api/apidoc/repository_sets.html
-
+A full API reference can be found here:
+https://theforeman.org/plugins/katello/3.16/api/apidoc/v2/repository_sets.html
 
 :Requirement: Repository Set
 
@@ -25,69 +24,86 @@ from robottelo import manifests
 from robottelo.api.utils import upload_manifest
 from robottelo.constants import PRDS
 from robottelo.constants import REPOSET
-from robottelo.test import APITestCase
+
+pytestmark = [pytest.mark.run_in_one_thread, pytest.mark.tier1]
+
+PRODUCT_NAME = PRDS['rhel']
+REPOSET_NAME = REPOSET['rhva6']
+ARCH = 'x86_64'
+RELEASE = '6Server'
 
 
-@pytest.mark.run_in_one_thread
-class RepositorySetTestCase(APITestCase):
-    """Tests for ``katello/api/v2/products/<product_id>/repository_sets``."""
+@pytest.fixture
+def org():
+    """Create organization."""
+    return entities.Organization().create()
 
-    @pytest.mark.tier1
-    def test_positive_reposet_enable(self):
-        """Enable repo from reposet
 
-        :id: dedcecf7-613a-4e85-a3af-92fb57e2b0a1
+@pytest.fixture
+def manifest_org(org):
+    """Upload manifest to organization."""
+    with manifests.clone() as manifest:
+        upload_manifest(org.id, manifest.content)
+    return org
 
-        :expectedresults: Repository was enabled
 
-        :CaseImportance: Critical
-        """
-        org = entities.Organization().create()
-        with manifests.clone() as manifest:
-            upload_manifest(org.id, manifest.content)
-        product = entities.Product(name=PRDS['rhel'], organization=org).search()[0]
-        reposet = entities.RepositorySet(name=REPOSET['rhva6'], product=product).search()[0]
-        data = {'basearch': 'x86_64', 'releasever': '6Server', 'product_id': product.id}
-        reposet.enable(data=data)
-        repositories = reposet.available_repositories(data=data)['results']
-        self.assertTrue(
-            [
-                repo['enabled']
-                for repo in repositories
-                if (
-                    repo['substitutions']['basearch'] == 'x86_64'
-                    and repo['substitutions']['releasever'] == '6Server'
-                )
-            ][0]
-        )
+@pytest.fixture
+def product(manifest_org):
+    """Find and return the product matching PRODUCT_NAME."""
+    return entities.Product(name=PRODUCT_NAME, organization=manifest_org).search()[0]
 
-    @pytest.mark.tier1
-    @pytest.mark.upgrade
-    def test_positive_reposet_disable(self):
-        """Disable repo from reposet
 
-        :id: 60a102df-099e-4325-8924-2a31e5f738ba
+@pytest.fixture
+def reposet(product):
+    """Find and return the repository set matching REPOSET_NAME and product."""
+    return entities.RepositorySet(name=REPOSET_NAME, product=product).search()[0]
 
-        :expectedresults: Repository was disabled
 
-        :CaseImportance: Critical
-        """
-        org = entities.Organization().create()
-        with manifests.clone() as manifest:
-            upload_manifest(org.id, manifest.content)
-        product = entities.Product(name=PRDS['rhel'], organization=org).search()[0]
-        reposet = entities.RepositorySet(name=REPOSET['rhva6'], product=product).search()[0]
-        data = {'basearch': 'x86_64', 'releasever': '6Server', 'product_id': product.id}
-        reposet.enable(data=data)
-        reposet.disable(data=data)
-        repositories = reposet.available_repositories(data=data)['results']
-        self.assertFalse(
-            [
-                repo['enabled']
-                for repo in repositories
-                if (
-                    repo['substitutions']['basearch'] == 'x86_64'
-                    and repo['substitutions']['releasever'] == '6Server'
-                )
-            ][0]
-        )
+@pytest.fixture
+def params(product):
+    """Return dictionary of arch, release, and product id, for use in enabling and disabling
+    repositories within a repository set.
+    """
+    enable = {'basearch': ARCH, 'releasever': RELEASE, 'product_id': product.id}
+    match = {'substitutions': {'basearch': ARCH, 'releasever': RELEASE}, 'enabled': True}
+
+    return {'enable': enable, 'match': match}
+
+
+def match_repos(repos, match_params):
+    """Return a list of all repos that match every key-value pair in match_params."""
+    return [repo for repo in repos if match_params.items() <= repo.items()]
+
+
+def test_positive_reposet_enable(reposet, params):
+    """Enable repo from reposet
+
+    :id: dedcecf7-613a-4e85-a3af-92fb57e2b0a1
+
+    :expectedresults: Repository was enabled
+
+    :CaseImportance: Critical
+    """
+    reposet.enable(data=params['enable'])
+
+    repos = reposet.available_repositories(data=params['enable'])['results']
+    matching_repos = match_repos(repos, params['match'])
+    assert len(matching_repos) == 1
+
+
+@pytest.mark.upgrade
+def test_positive_reposet_disable(reposet, params):
+    """Disable repo from reposet
+
+    :id: 60a102df-099e-4325-8924-2a31e5f738ba
+
+    :expectedresults: Repository was disabled
+
+    :CaseImportance: Critical
+    """
+    reposet.enable(data=params['enable'])
+    reposet.disable(data=params['enable'])
+
+    repos = reposet.available_repositories(data=params['enable'])['results']
+    matching_repos = match_repos(repos, params['match'])
+    assert len(matching_repos) == 0
