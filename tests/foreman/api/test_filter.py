07f2bb6ea90fbe67620ed4354a69f8c183686a52
Peter Ondrejka <pondrejk@redhat.com>
2021-02-24 10:33:43 +0100

    filter tests moved to pytest

--- tests/foreman/api/test_filter.py
+++ tests/foreman/api/test_filter.py
@@ -24,68 +24,66 @@ import pytest
 from nailgun import entities
 from requests.exceptions import HTTPError
 
-from robottelo.test import APITestCase
+
+@pytest.fixture(scope='module')
+def module_perms():
+    """Search for provisioning template permissions. Set ``cls.ct_perms``."""
+    ct_perms = entities.Permission().search(
+        query={'search': 'resource_type="ProvisioningTemplate"'}
+    )
+    return ct_perms
 
 
-class FilterTestCase(APITestCase):
-    """Tests for ``api/v2/filters``."""
+@pytest.mark.tier1
+def test_positive_create_with_permission(module_perms):
+    """Create a filter and assign it some permissions.
 
-    @classmethod
-    def setUpClass(cls):
-        """Search for provisioning template permissions. Set ``cls.ct_perms``."""
-        super().setUpClass()
-        cls.ct_perms = entities.Permission().search(
-            query={'search': 'resource_type="ProvisioningTemplate"'}
-        )
+    :id: b8631d0a-a71a-41aa-9f9a-d12d62adc496
 
-    @pytest.mark.tier1
-    def test_positive_create_with_permission(self):
-        """Create a filter and assign it some permissions.
+    :expectedresults: The created filter has the assigned permissions.
 
-        :id: b8631d0a-a71a-41aa-9f9a-d12d62adc496
+    :CaseImportance: Critical
+    """
+    # Create a filter and assign all ProvisioningTemplate permissions to it
+    filter_ = entities.Filter(permission=module_perms).create()
+    filter_perms = [perm.id for perm in filter_.permission]
+    perms = [perm.id for perm in module_perms]
+    assert filter_perms == perms
 
-        :expectedresults: The created filter has the assigned permissions.
 
-        :CaseImportance: Critical
-        """
-        # Create a filter and assign all ProvisioningTemplate permissions to it
-        filter_ = entities.Filter(permission=self.ct_perms).create()
-        self.assertListEqual(
-            [perm.id for perm in filter_.permission], [perm.id for perm in self.ct_perms]
-        )
+@pytest.mark.tier1
+def test_positive_delete(module_perms):
+    """Create a filter and delete it afterwards.
 
-    @pytest.mark.tier1
-    def test_positive_delete(self):
-        """Create a filter and delete it afterwards.
+    :id: f0c56fd8-c91d-48c3-ad21-f538313b17eb
 
-        :id: f0c56fd8-c91d-48c3-ad21-f538313b17eb
+    :expectedresults: The deleted filter cannot be fetched.
 
-        :expectedresults: The deleted filter cannot be fetched.
+    :CaseImportance: Critical
+    """
+    filter_ = entities.Filter(permission=module_perms).create()
+    filter_.delete()
+    with pytest.raises(HTTPError):
+        filter_.read()
 
-        :CaseImportance: Critical
-        """
-        filter_ = entities.Filter(permission=self.ct_perms).create()
-        filter_.delete()
-        with self.assertRaises(HTTPError):
-            filter_.read()
 
-    @pytest.mark.tier1
-    def test_positive_delete_role(self):
-        """Create a filter and delete the role it points at.
+@pytest.mark.tier1
+def test_positive_delete_role(module_perms):
+    """Create a filter and delete the role it points at.
 
-        :id: b129642d-926d-486a-84d9-5952b44ac446
+    :id: b129642d-926d-486a-84d9-5952b44ac446
 
-        :expectedresults: The filter cannot be fetched.
+    :expectedresults: The filter cannot be fetched.
 
-        :CaseImportance: Critical
-        """
-        role = entities.Role().create()
-        filter_ = entities.Filter(permission=self.ct_perms, role=role).create()
+    :CaseImportance: Critical
+    """
+    role = entities.Role().create()
+    filter_ = entities.Filter(permission=module_perms, role=role).create()
 
-        # A filter depends on a role. Deleting a role implicitly deletes the
-        # filter pointing at it.
-        role.delete()
-        with self.assertRaises(HTTPError):
-            role.read()
-        with self.assertRaises(HTTPError):
-            filter_.read()
+    # A filter depends on a role. Deleting a role implicitly deletes the
+    # filter pointing at it.
+    role.delete()
+    with pytest.raises(HTTPError):
+        role.read()
+    with pytest.raises(HTTPError):
+        filter_.read()
