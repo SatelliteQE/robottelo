ca7027214d9c6ce2320be2856bc577306f57e1dc
Mike Shriver <mshriver@redhat.com>
2021-03-17 10:59:23 -0400

    Refactor cli/test_ping for pytest

--- tests/foreman/cli/test_ping.py
+++ tests/foreman/cli/test_ping.py
@@ -12,57 +12,47 @@
 
 :TestType: Functional
 
-:CaseImportance: High
+:CaseImportance: Critical
 
 :Upstream: No
 """
 import pytest
 
 from robottelo import ssh
-from robottelo.test import CLITestCase
 
 
-class PingTestCase(CLITestCase):
-    """Tests related to the hammer ping command"""
+@pytest.mark.tier1
+@pytest.mark.upgrade
+def test_positive_ping():
+    """hammer ping return code
 
-    @pytest.mark.tier1
-    @pytest.mark.upgrade
-    def test_positive_ping(self):
-        """hammer ping return code
+    :id: dfa3ab4f-a64f-4a96-8c7f-d940df22b8bf
 
-        :id: dfa3ab4f-a64f-4a96-8c7f-d940df22b8bf
+    :steps:
+        1. Execute hammer ping
+        2. Check its return code, should be 0 if all services are ok else
+           != 0
 
-        :steps:
-            1. Execute hammer ping
-            2. Check its return code, should be 0 if all services are ok else
-               != 0
+    :expectedresults: hammer ping returns a right return code
+    """
+    result = ssh.command('hammer ping')
+    assert result.stderr.decode() == ''
 
-        :expectedresults: hammer ping returns a right return code
+    status_count = 0
+    ok_count = 0
+    # Exclude message from stdout for services candlepin_events and katello_events
+    result.stdout = [line for line in result.stdout if 'message' not in line]
 
-        :CaseImportance: Critical
-        """
-        result = ssh.command(f'hammer -u {self.foreman_user} -p {self.foreman_password} ping')
-        self.assertEqual(len(result.stderr), 0)
+    # iterate over the lines grouping every 3 lines
+    # example [1, 2, 3, 4, 5, 6] will return [(1, 2, 3), (4, 5, 6)]
+    # only the status line is relevant for this test
+    for _, status, _ in zip(*[iter(result.stdout)] * 3):
+        status_count += 1
 
-        status_count = 0
-        ok_count = 0
-        # Exclude message from stdout for services candlepin_events and katello_events
-        result.stdout = [line for line in result.stdout if "message" not in line]
+        if status.split(':')[1].strip().lower() == 'ok':
+            ok_count += 1
 
-        # iterate over the lines grouping every 3 lines
-        # example [1, 2, 3, 4, 5, 6] will return [(1, 2, 3), (4, 5, 6)]
-        # only the status line is relevant for this test
-        for _, status, _ in zip(*[iter(result.stdout)] * 3):
-            status_count += 1
-
-            if status.split(':')[1].strip().lower() == 'ok':
-                ok_count += 1
-
-        if status_count == ok_count:
-            self.assertEqual(
-                result.return_code, 0, 'Return code should be 0 if all services are ok'
-            )
-        else:
-            self.assertNotEqual(
-                result.return_code, 0, 'Return code should not be 0 if any service is not ok'
-            )
+    if status_count == ok_count:
+        assert result.return_code == 0, 'Return code should be 0 if all services are ok'
+    else:
+        assert result.return_code != 0, 'Return code should not be 0 if any service is not ok'
