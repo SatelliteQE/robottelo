30a351003caa9ea468f37e1b2d8bb4480c88a7c4
Mike Shriver <mshriver@redhat.com>
2021-03-17 10:48:25 -0400

    Refactor test_globalparam to pytest, condense tests

--- tests/foreman/cli/test_globalparam.py
+++ tests/foreman/cli/test_globalparam.py
@@ -12,68 +12,42 @@
 
 :TestType: Functional
 
-:CaseImportance: High
+:CaseImportance: Critical
 
 :Upstream: No
 """
+from functools import partial
+
 import pytest
 from fauxfactory import gen_string
 
 from robottelo.cli.globalparam import GlobalParameter
-from robottelo.test import CLITestCase
 
 
-class GlobalParameterTestCase(CLITestCase):
-    """GlobalParameter related CLI tests."""
-
-    @pytest.mark.tier1
-    def test_positive_set(self):
-        """Check if Global Param can be set
-
-        :id: af0d3338-d7a1-41e5-959a-289ebc326c5b
-
-        :expectedresults: Global Param is set
+pytestmark = [pytest.mark.tier1]
 
 
-        :CaseImportance: Critical
-        """
-        name = 'opt-%s' % gen_string('alpha', 10)
-        value = 'val-{} {}'.format(gen_string('alpha', 10), gen_string('alpha', 10))
-        GlobalParameter().set({'name': name, 'value': value})
+@pytest.mark.upgrade
+def test_positive_list_delete_by_name():
+    """Test Global Param List
 
-    @pytest.mark.tier1
-    def test_positive_list_by_name(self):
-        """Test Global Param List
+    :id: 8dd6c4e8-4ec9-4bee-8a04-f5788960973a
 
-        :id: 8dd6c4e8-4ec9-4bee-8a04-f5788960973a
+    :expectedresults: Global Param is set, listed, and deleted by name
+    """
+    alphastring = partial(gen_string, 'alpha', 10)
 
-        :expectedresults: Global Param List is displayed
+    name = f'opt-{alphastring()}'
+    value = f'val-{alphastring()} {alphastring()}'
 
+    # Create
+    GlobalParameter().set({'name': name, 'value': value})
 
-        :CaseImportance: Critical
-        """
-        name = 'opt-%s' % gen_string('alpha', 10)
-        value = 'val-{} {}'.format(gen_string('alpha', 10), gen_string('alpha', 10))
-        GlobalParameter().set({'name': name, 'value': value})
-        result = GlobalParameter().list({'search': name})
-        self.assertEqual(len(result), 1)
-        self.assertEqual(result[0]['value'], value)
+    # List by name
+    result = GlobalParameter().list({'search': name})
+    assert len(result) == 1
+    assert result[0]['value'] == value
 
-    @pytest.mark.tier1
-    @pytest.mark.upgrade
-    def test_positive_delete_by_name(self):
-        """Check if Global Param can be deleted
-
-        :id: 2c44d9c9-2a21-4415-8e89-cfd3d963891b
-
-        :expectedresults: Global Param is deleted
-
-
-        :CaseImportance: Critical
-        """
-        name = 'opt-%s' % gen_string('alpha', 10)
-        value = 'val-{} {}'.format(gen_string('alpha', 10), gen_string('alpha', 10))
-        GlobalParameter().set({'name': name, 'value': value})
-        GlobalParameter().delete({'name': name})
-        result = GlobalParameter().list({'search': name})
-        self.assertEqual(len(result), 0)
+    # Delete
+    GlobalParameter().delete({'name': name})
+    assert len(GlobalParameter().list({'search': name})) == 0
