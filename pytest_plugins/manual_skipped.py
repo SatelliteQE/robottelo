001eaf07920d3737795e88fcac985c475c864c3a
Mike Shriver <mshriver@redhat.com>
2021-03-17 10:17:41 -0400

    Add option to control stubbed case collection (#8395)
    * Add option to control stubbed case collection
    
    Update the manual_skipped module to add a `--stubbed` option
    
    Default test collection to exclude stubbed test cases
    
    Included stubbed cases only when `--stubbed` is passed
    
    * Update pytest_plugins/manual_skipped.py
    
    Co-authored-by: Jacob Callahan <jacob.callahan05@gmail.com>
    
    Co-authored-by: Jacob Callahan <jacob.callahan05@gmail.com>

--- pytest_plugins/manual_skipped.py
+++ pytest_plugins/manual_skipped.py
@@ -9,33 +9,56 @@ LOGGER = logging.getLogger('robottelo')
 
 def pytest_configure(config):
     """Register custom marker for stubbed test cases."""
-    marker = "stubbed: Tests that are not automated yet or manual only."
-    config.addinivalue_line("markers", marker)
+    config.addinivalue_line('markers', 'stubbed: Tests that are not automated yet or manual only.')
 
 
 def pytest_collection_modifyitems(session, config, items):
-    """
-    Collects tests based on pytest option to select tests marked as failed on Report Portal
-    """
-    mark_passed = config.getvalue('mark_manuals_passed')
-    mark_skipped = config.getvalue('mark_manuals_skipped')
-    if mark_passed or (not mark_skipped):
-        return
+    """Remove/Include stubbed tests from collection based on CLI option"""
+    opt_passed = config.getvalue('mark_manuals_passed')
+    opt_skipped = config.getvalue('mark_manuals_skipped')
+    # TODO turn this into a flag or a choice option, this logic is just silly.
+    mark_skipped = opt_skipped and not opt_passed
+    include_stubbed = config.getvalue('include_stubbed')
+    selected = []
+    deselected = []
     for item in items:
-        if item.get_closest_marker(name='stubbed'):
-            item.add_marker(marker=pytest.mark.skip(reason=NOT_IMPLEMENTED))
+        stub_marked = item.get_closest_marker(name='stubbed')
+        # The test case is stubbed, and --include-stubbed was passed, include in collection
+        if stub_marked and include_stubbed:
+            selected.append(item)
+            # enforce skip/pass behavior by marking skip
+            if mark_skipped:
+                item.add_marker(marker=pytest.mark.skip(reason=NOT_IMPLEMENTED))
+            continue
+
+        # The test case is stubbed, but --include-stubbed was NOT passed, deselect the item
+        if stub_marked and not include_stubbed:
+            deselected.append(item)
+            continue
+
+        # Its a non-stubbed item, this hook doesn't apply
+        selected.append(item)
+
+    config.hook.pytest_deselected(items=deselected)
+    items[:] = selected
 
 
 def pytest_addoption(parser):
-    """Add options to pytest to modify manual tests as skipped"""
+    """Add options to pytest to modify manual tests as skipped and control collection"""
     parser.addoption(
-        "--mark-manuals-skipped",
+        '--include-stubbed',
+        action='store_true',
+        default=False,
+        help='Include stubbed tests in collection, by default they are deselected',
+    )
+    parser.addoption(
+        '--mark-manuals-skipped',
         action='store_true',
         default=True,
         help='Mark stubbed tests as skipped tests.',
     )
     parser.addoption(
-        "--mark-manuals-passed",
+        '--mark-manuals-passed',
         action='store_true',
         default=False,
         help='Mark stubbed tests as passed tests.',
