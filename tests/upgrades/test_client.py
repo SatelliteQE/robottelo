a6dd923f6dd21d29b32337ced30a5aaf8c917f35
Mike Shriver <mshriver@redhat.com>
2021-05-10 17:47:31 -0400

    Indent functions under classes in upgrades/test_client
    The classes were removed and added back, indentation was missed

--- tests/upgrades/test_client.py
+++ tests/upgrades/test_client.py
@@ -159,94 +159,94 @@ class TestScenarioUpgradeOldClientAndPackageInstallation:
         4. Check if the package is installed on the pre-upgrade client
     """
 
+    @pre_upgrade
+    def test_pre_scenario_preclient_package_installation(
+        default_org, pre_upgrade_cv, pre_upgrade_repo, module_ak
+    ):
+        """Create product and repo, from which a package will be installed
+        post upgrade. Create a content host and register it.
 
-@pre_upgrade
-def test_pre_scenario_preclient_package_installation(
-    default_org, pre_upgrade_cv, pre_upgrade_repo, module_ak
-):
-    """Create product and repo, from which a package will be installed
-    post upgrade. Create a content host and register it.
+        :id: preupgrade-eedab638-fdc9-41fa-bc81-75dd2790f7be
 
-    :id: preupgrade-eedab638-fdc9-41fa-bc81-75dd2790f7be
+        :setup:
 
-    :setup:
-
-        1. Create and sync repo from which a package can be
-            installed on content host
-        2. Add repo to CV and then to Activation key
-
-
-    :steps:
-
-        1. Create a container as content host and register with Activation key
-
-    :expectedresults:
-
-        1. The "pre-upgrade" content host is created and registered.
-        2. The new repo is enabled on the content host.
-
-    """
-    rhel7_client = dockerize(ak_name=module_ak.name, distro='rhel7', org_label=default_org.label)
-    client_container_id = list(rhel7_client.values())[0]
-    # Wait 60 seconds as script in /tmp takes up to 2 min inc the repo sync time
-    time.sleep(60)
-    # Use yum install again in case script was not yet finished
-    installed_package = execute(
-        docker_execute_command,
-        client_container_id,
-        'yum -y install katello-agent',
-        host=docker_vm,
-    )[docker_vm]
-    # Assert gofer was installed after yum completes
-    installed_package = execute(
-        docker_execute_command,
-        client_container_id,
-        'rpm -q gofer',
-        host=docker_vm,
-    )[docker_vm]
-    assert 'package gofer is not installed' not in installed_package
-    # Run goferd on client as its docker container
-    kwargs = {'async': True, 'host': docker_vm}
-    execute(docker_execute_command, client_container_id, 'goferd -f', **kwargs)
-    # Holding on for 30 seconds while goferd starts
-    time.sleep(30)
-    status = execute(docker_execute_command, client_container_id, 'ps -aux', host=docker_vm)[
-        docker_vm
-    ]
-    assert 'goferd' in status
-    # Save client info to disk for post-upgrade test
-    create_dict({__name__: rhel7_client})
+            1. Create and sync repo from which a package can be
+                installed on content host
+            2. Add repo to CV and then to Activation key
 
 
-@post_upgrade(depend_on=test_pre_scenario_preclient_package_installation)
-def test_post_scenario_preclient_package_installation(default_org):
-    """Post-upgrade install of a package on a client created and registered pre-upgrade.
+        :steps:
 
-    :id: postupgrade-eedab638-fdc9-41fa-bc81-75dd2790f7be
+            1. Create a container as content host and register with Activation key
 
-    :steps: Install package on the pre-upgrade registered client
+        :expectedresults:
 
-    :expectedresults: The package is installed on client
-    """
-    client = get_entity_data(__name__)
-    client_name = str(list(client.keys())[0]).lower()
-    client_id = entities.Host().search(query={'search': f'name={client_name}'})[0].id
-    entities.Host().install_content(
-        data={
-            'organization_id': default_org.id,
-            'included': {'ids': [client_id]},
-            'content_type': 'package',
-            'content': [FAKE_0_CUSTOM_PACKAGE],
-        }
-    )
-    # Verifies that package is really installed
-    installed_package = execute(
-        docker_execute_command,
-        list(client.values())[0],
-        f'rpm -q {FAKE_0_CUSTOM_PACKAGE}',
-        host=docker_vm,
-    )[docker_vm]
-    assert FAKE_0_CUSTOM_PACKAGE in installed_package
+            1. The "pre-upgrade" content host is created and registered.
+            2. The new repo is enabled on the content host.
+
+        """
+        rhel7_client = dockerize(
+            ak_name=module_ak.name, distro='rhel7', org_label=default_org.label
+        )
+        client_container_id = list(rhel7_client.values())[0]
+        # Wait 60 seconds as script in /tmp takes up to 2 min inc the repo sync time
+        time.sleep(60)
+        # Use yum install again in case script was not yet finished
+        installed_package = execute(
+            docker_execute_command,
+            client_container_id,
+            'yum -y install katello-agent',
+            host=docker_vm,
+        )[docker_vm]
+        # Assert gofer was installed after yum completes
+        installed_package = execute(
+            docker_execute_command,
+            client_container_id,
+            'rpm -q gofer',
+            host=docker_vm,
+        )[docker_vm]
+        assert 'package gofer is not installed' not in installed_package
+        # Run goferd on client as its docker container
+        kwargs = {'async': True, 'host': docker_vm}
+        execute(docker_execute_command, client_container_id, 'goferd -f', **kwargs)
+        # Holding on for 30 seconds while goferd starts
+        time.sleep(30)
+        status = execute(docker_execute_command, client_container_id, 'ps -aux', host=docker_vm)[
+            docker_vm
+        ]
+        assert 'goferd' in status
+        # Save client info to disk for post-upgrade test
+        create_dict({__name__: rhel7_client})
+
+    @post_upgrade(depend_on=test_pre_scenario_preclient_package_installation)
+    def test_post_scenario_preclient_package_installation(default_org):
+        """Post-upgrade install of a package on a client created and registered pre-upgrade.
+
+        :id: postupgrade-eedab638-fdc9-41fa-bc81-75dd2790f7be
+
+        :steps: Install package on the pre-upgrade registered client
+
+        :expectedresults: The package is installed on client
+        """
+        client = get_entity_data(__name__)
+        client_name = str(list(client.keys())[0]).lower()
+        client_id = entities.Host().search(query={'search': f'name={client_name}'})[0].id
+        entities.Host().install_content(
+            data={
+                'organization_id': default_org.id,
+                'included': {'ids': [client_id]},
+                'content_type': 'package',
+                'content': [FAKE_0_CUSTOM_PACKAGE],
+            }
+        )
+        # Verifies that package is really installed
+        installed_package = execute(
+            docker_execute_command,
+            list(client.values())[0],
+            f'rpm -q {FAKE_0_CUSTOM_PACKAGE}',
+            host=docker_vm,
+        )[docker_vm]
+        assert FAKE_0_CUSTOM_PACKAGE in installed_package
 
 
 class TestScenarioUpgradeNewClientAndPackageInstallation:
@@ -262,74 +262,75 @@ class TestScenarioUpgradeNewClientAndPackageInstallation:
         4. Check if the package is installed on the post-upgrade client
     """
 
+    @post_upgrade
+    def test_post_scenario_postclient_package_installation(
+        default_org, post_upgrade_repo, module_ak, module_lce
+    ):
+        """Post-upgrade test that creates a client, installs a package on
+        the post-upgrade created client and then verifies the package is installed.
 
-@post_upgrade
-def test_post_scenario_postclient_package_installation(
-    default_org, post_upgrade_repo, module_ak, module_lce
-):
-    """Post-upgrade test that creates a client, installs a package on
-    the post-upgrade created client and then verifies the package is installed.
+        :id: postupgrade-1a881c07-595f-425f-aca9-df2337824a8e
 
-    :id: postupgrade-1a881c07-595f-425f-aca9-df2337824a8e
+        :steps:
 
-    :steps:
+            1. Create a content host with existing client ak
+            2. Create and sync new post-upgrade repo from which a package will be
+                installed on content host
+            3. Add repo to CV and then in Activation key
+            4. Install package on the pre-upgrade client
 
-        1. Create a content host with existing client ak
-        2. Create and sync new post-upgrade repo from which a package will be
-            installed on content host
-        3. Add repo to CV and then in Activation key
-        4. Install package on the pre-upgrade client
+        :expectedresults:
 
-    :expectedresults:
-
-        1. The content host is created
-        2. The new repo and its product has been added to ak using which
-            the content host is created
-        3. The package is installed on post-upgrade client
-    """
-    rhel7_client = dockerize(ak_name=module_ak.name, distro='rhel7', org_label=default_org.label)
-    client_container_id = list(rhel7_client.values())[0]
-    client_name = list(rhel7_client.keys())[0].lower()
-    # Wait 60 seconds as script in /tmp takes up to 2 min inc the repo sync time
-    time.sleep(60)
-    # Use yum install again in case script was not yet finished
-    installed_package = execute(
-        docker_execute_command,
-        client_container_id,
-        'yum -y install katello-agent',
-        host=docker_vm,
-    )[docker_vm]
-    # Assert gofer was installed after yum completes
-    installed_package = execute(
-        docker_execute_command,
-        client_container_id,
-        'rpm -q gofer',
-        host=docker_vm,
-    )[docker_vm]
-    assert 'package gofer is not installed' not in installed_package
-    # Run goferd on client as its docker container
-    kwargs = {'async': True, 'host': docker_vm}
-    execute(docker_execute_command, client_container_id, 'goferd -f', **kwargs)
-    # Holding on for 30 seconds while goferd starts
-    time.sleep(30)
-    status = execute(docker_execute_command, client_container_id, 'ps -aux', host=docker_vm)[
-        docker_vm
-    ]
-    assert 'goferd' in status
-    client_id = entities.Host().search(query={'search': f'name={client_name}'})[0].id
-    entities.Host().install_content(
-        data={
-            'organization_id': default_org.id,
-            'included': {'ids': [client_id]},
-            'content_type': 'package',
-            'content': [FAKE_4_CUSTOM_PACKAGE],
-        }
-    )
-    # Validate if that package is really installed
-    installed_package = execute(
-        docker_execute_command,
-        client_container_id,
-        f'rpm -q {FAKE_4_CUSTOM_PACKAGE}',
-        host=docker_vm,
-    )[docker_vm]
-    assert FAKE_4_CUSTOM_PACKAGE in installed_package
+            1. The content host is created
+            2. The new repo and its product has been added to ak using which
+                the content host is created
+            3. The package is installed on post-upgrade client
+        """
+        rhel7_client = dockerize(
+            ak_name=module_ak.name, distro='rhel7', org_label=default_org.label
+        )
+        client_container_id = list(rhel7_client.values())[0]
+        client_name = list(rhel7_client.keys())[0].lower()
+        # Wait 60 seconds as script in /tmp takes up to 2 min inc the repo sync time
+        time.sleep(60)
+        # Use yum install again in case script was not yet finished
+        installed_package = execute(
+            docker_execute_command,
+            client_container_id,
+            'yum -y install katello-agent',
+            host=docker_vm,
+        )[docker_vm]
+        # Assert gofer was installed after yum completes
+        installed_package = execute(
+            docker_execute_command,
+            client_container_id,
+            'rpm -q gofer',
+            host=docker_vm,
+        )[docker_vm]
+        assert 'package gofer is not installed' not in installed_package
+        # Run goferd on client as its docker container
+        kwargs = {'async': True, 'host': docker_vm}
+        execute(docker_execute_command, client_container_id, 'goferd -f', **kwargs)
+        # Holding on for 30 seconds while goferd starts
+        time.sleep(30)
+        status = execute(docker_execute_command, client_container_id, 'ps -aux', host=docker_vm)[
+            docker_vm
+        ]
+        assert 'goferd' in status
+        client_id = entities.Host().search(query={'search': f'name={client_name}'})[0].id
+        entities.Host().install_content(
+            data={
+                'organization_id': default_org.id,
+                'included': {'ids': [client_id]},
+                'content_type': 'package',
+                'content': [FAKE_4_CUSTOM_PACKAGE],
+            }
+        )
+        # Validate if that package is really installed
+        installed_package = execute(
+            docker_execute_command,
+            client_container_id,
+            f'rpm -q {FAKE_4_CUSTOM_PACKAGE}',
+            host=docker_vm,
+        )[docker_vm]
+        assert FAKE_4_CUSTOM_PACKAGE in installed_package
