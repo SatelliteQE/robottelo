929a792bb626ee878912f2d241ca46043ea6cc37
jyejare <jyejare@redhat.com>
2021-05-25 12:07:36 +0530

    Mikes Suggestions

--- tests/upgrades/scenario_workers.py
+++ tests/upgrades/scenario_workers.py
@@ -1,5 +1,5 @@
 import json
-import os
+from pathlib import Path
 
 import pytest
 from automation_tools.satellite6.hammer import set_hammer_config
@@ -12,47 +12,52 @@ from robottelo.config import settings
 _json_file = 'upgrade_workers.json'
 
 
-def save_worker_id(test_name, worker_id):
+def save_worker_hostname(test_name):
     data = {}
-    if os.path.exists(_json_file):
-        with open(_json_file) as rjson:
-            data = json.load(rjson)
-    with open(_json_file, 'w') as wjson:
-        # Removing the parameter name from test name before save
-        test_name = test_name.split('[')[0] if '[' in test_name else test_name
-        data.update({test_name: worker_id})
-        wjson.seek(0)
-        json.dump(data, wjson)
+    json_file = Path(_json_file)
+    if json_file.exists():
+        data = json.loads(json_file.read_text())
+    # Removing the parameter name from test name before save
+    test_name = test_name.split('[')[0] if '[' in test_name else test_name
+    data.update({test_name: settings.server.hostname})
+    json_file.write_text(json.dumps(data))
 
 
-def get_worker_id(test_name):
+def get_worker_hostname(test_name):
     with open(_json_file) as rjson:
         data = json.load(rjson)
         return data.get(test_name)
 
 
 @pytest.fixture(autouse=True)
-def save_pretest_worker_id(request, worker_id):
+def save_pre_upgrade_worker_hostname(request):
     """Saves the worker id of pre_upgrade test in json"""
     if request.node.get_closest_marker('pre_upgrade'):
-        save_worker_id(request.node.name, worker_id)
+        save_worker_hostname(request.node.name)
 
 
 @pytest.fixture(autouse=True)
-def set_posttest_worker_id(request):
-    """Retrieves worker_id of pre_upgrade and sets the same to run post_upgrade"""
+def set_post_upgrade_hostname(request):
+    """Retrieves worker hostname of pre_upgrade and sets the same to run post_upgrade
+
+    The limitation of this implementation is that the XDIST_BEHAVIOR won't be observed,
+    and tests running under xdist will not be isolated.
+    """
     post_upgrade = request.node.get_closest_marker('post_upgrade')
     if post_upgrade:
         depend_test = post_upgrade.kwargs.get('depend_on')
         if depend_test:
             pre_test_name = depend_test.__name__
-            # settings.configure()
+            settings.configure()
             cache_proxy = robottelo.config.settings_proxy._cache
+            pre_hostname = get_worker_hostname(pre_test_name)
+            if (pre_hostname is None) or (pre_hostname not in settings.server.hostnames):
+                pytest.skip(
+                    'Skipping the post_upgrade test as the pre_upgrade hostname is not found!'
+                )
             # Read the worker id of pre test name
-            worker_int = int(get_worker_id(pre_test_name).replace('gw', ''))
-            cache_proxy['server.hostname'] = settings.server.hostnames[worker_int]
+            cache_proxy['server.hostname'] = env.host_string = pre_hostname
+            env.user = 'root'
             settings.configure_nailgun()
             settings.configure_airgun()
-            env.host_string = settings.server.hostname
-            env.user = 'root'
             set_hammer_config()
