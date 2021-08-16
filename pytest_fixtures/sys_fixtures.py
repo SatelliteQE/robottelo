80115258bbf14ba008fad374dc7493ba0dab2a63
tstrych <tstrych@redhat.com>
2021-08-16 22:29:27 +0200

    install cockpit on default_sat in robottelo (#8812)
    * install cockpit on default_sat in robottelo
    
    * install katello ca and register to dogfood

--- pytest_fixtures/sys_fixtures.py
+++ pytest_fixtures/sys_fixtures.py
@@ -1,6 +1,7 @@
 import pytest
 
 from robottelo.config import settings
+from robottelo.hosts import SatelliteHostError
 
 
 @pytest.fixture
@@ -32,3 +33,37 @@ def proxy_port_range(default_sat):
         port_pool_range = settings.fake_capsules.port_range
         if default_sat.execute(f'semanage port -l | grep {port_pool_range}').status != 0:
             default_sat.execute(f'semanage port -a -t websm_port_t -p tcp {port_pool_range}')
+
+
+@pytest.fixture
+def register_to_dogfood(default_sat):
+    dogfood_canonical_hostname = settings.repos.dogfood_repo_host.partition('//')[2]
+    # get hostname of dogfood machine
+    dig_result = default_sat.execute(f'dig +short {dogfood_canonical_hostname}')
+    # the host name finishes with a dot, so last character is removed
+    dogfood_hostname = dig_result.stdout.split()[0][:-1]
+    default_sat.install_katello_ca(sat_hostname=dogfood_hostname)
+    # satellite version consist from x.y.z, we need only x.y
+    sat_release = '.'.join(default_sat.version.split('.')[:-1])
+    cmd_result = default_sat.register_contenthost(
+        org='Sat6-CI', activation_key=f'satellite-{sat_release}-qa-rhel7'
+    )
+    if cmd_result.status != 0:
+        raise SatelliteHostError(f'Error during registration, command output: {cmd_result.stdout}')
+
+
+@pytest.fixture
+def install_cockpit_plugin(default_sat, register_to_dogfood):
+    cmd_result = default_sat.execute(
+        'foreman-installer --enable-foreman-plugin-remote-execution-cockpit'
+    )
+    if cmd_result.status != 0:
+        raise SatelliteHostError(
+            f'Error during cockpit installation, installation output: {cmd_result.stdout}'
+        )
+    cmd_result = default_sat.execute(
+        f'sshpass -p "{settings.server.ssh_password}" ssh-copy-id \
+        -i ~foreman-proxy/.ssh/id_rsa_foreman_proxy -o StrictHostKeyChecking=no localhost'
+    )
+    if cmd_result.status != 0:
+        raise SatelliteHostError(f'Error during ssh-copy-id, command output: {cmd_result.stdout}')
