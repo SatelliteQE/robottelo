b964e5c268ca674842b85ee3066f57ae34fe1195
jyejare <jyejare@redhat.com>
2021-09-16 20:07:35 +0530

    Content Host fixtures updated to filter tests based on wf

--- pytest_plugins/fixture_markers.py
+++ pytest_plugins/fixture_markers.py
@@ -7,14 +7,14 @@ from robottelo.config import settings
 
 def pytest_generate_tests(metafunc):
     if 'rhel_contenthost' in metafunc.fixturenames:
+        wf = settings.content_host.deploy_workflow
         rhel_parameters = [
-            dict(workflow=settings.content_host.deploy_workflow, rhel_version=ver)
-            for ver in settings.content_host.rhel_versions
+            dict(workflow=wf, rhel_version=ver) for ver in settings.content_host.rhel_versions
         ]
         metafunc.parametrize(
             'rhel_contenthost',
             rhel_parameters,
-            ids=[f'rhel_{r["rhel_version"]}' for r in rhel_parameters],
+            ids=[f'{wf}_rhel_{r["rhel_version"]}' for r in rhel_parameters],
             indirect=True,
         )
 
