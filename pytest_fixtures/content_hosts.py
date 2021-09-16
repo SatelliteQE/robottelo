1cd5d72d1525da2670fe8db829f7ba2e71e0490d
jyejare <jyejare@redhat.com>
2021-09-16 20:07:35 +0530

    Content Host fixtures updated to filter tests based on wf

--- pytest_fixtures/content_hosts.py
+++ pytest_fixtures/content_hosts.py
@@ -13,6 +13,18 @@ from robottelo.hosts import ContentHost
 
 
 @pytest.fixture
+def host_conf(request):
+    """A function-level fixture that returns parameters for VMBroker host deployment"""
+    conf = {}
+    params = [*request.node.callspec.params.values()][0]
+    conf['workflow'] = params.get('workflow', 'deploy_base_rhel')
+    conf['rhel'] = params.get('rhel', settings.content_host.default_rhel_version)
+    conf['memory'] = params.get('memory', settings.broker.clients.get(f"rhel{conf['rhel']}").memory)
+    conf['cores'] = params.get('cores', settings.broker.clients.get(f"rhel{conf['rhel']}").cores)
+    return conf
+
+
+@pytest.fixture
 def rhel_contenthost(request):
     """A function-level fixture that provides a content host object parametrized"""
     # Request should be parametrized through pytest_fixtures.fixture_markers
@@ -26,52 +38,113 @@ def rhel_contenthost(request):
         yield host
 
 
-@pytest.fixture
-def rhel7_contenthost():
-    """A function-level fixture that provides a content host object based on the rhel7 nick"""
-    with VMBroker(nick='rhel7', host_classes={'host': ContentHost}) as host:
+@pytest.fixture(
+    params=[{'workflow': settings.content_host.deploy_workflow, 'rhel': '7'}],
+    ids=[settings.content_host.deploy_workflow],
+)
+def rhel7_contenthost(host_conf):
+    """A function-level fixture that provides a rhel7 content host object"""
+    with VMBroker(
+        workflow=host_conf['workflow'],
+        rhel_version=host_conf['rhel'],
+        memory=host_conf['memory'],
+        cores=host_conf['cores'],
+        host_classes={'host': ContentHost},
+    ) as host:
         yield host
 
 
 @pytest.fixture
 def rhel7_contenthost_fips():
     """A function-level fixture that provides a content host object based on the rhel7_fips nick"""
-    with VMBroker(nick='rhel7_fips', host_classes={'host': ContentHost}) as host:
+    rhel7 = settings.broker.clients.rhel7
+    with VMBroker(
+        workflow='deploy-base-rhel-fips',
+        rhel_version=rhel7.release,
+        memory=rhel7.memory,
+        cores=rhel7.cores,
+        host_classes={'host': ContentHost},
+    ) as host:
         yield host
 
 
-@pytest.fixture(scope="module")
-def rhel7_contenthost_module():
+@pytest.fixture(
+    scope='module',
+    params=[(settings.content_host.deploy_workflow, '7')],
+    ids=[settings.content_host.deploy_workflow],
+)
+def rhel7_contenthost_module(host_conf):
     """A module-level fixture that provides a content host object based on the rhel7 nick"""
-    with VMBroker(nick='rhel7', host_classes={'host': ContentHost}) as host:
+    with VMBroker(
+        workflow=host_conf['workflow'],
+        rhel_version=host_conf['rhel'],
+        memory=host_conf['memory'],
+        cores=host_conf['cores'],
+        host_classes={'host': ContentHost},
+    ) as host:
         yield host
 
 
-@pytest.fixture
-def rhel8_contenthost():
+@pytest.fixture(
+    params=[(settings.content_host.deploy_workflow, '8')],
+    ids=[settings.content_host.deploy_workflow],
+)
+def rhel8_contenthost(host_conf):
     """A fixture that provides a content host object based on the rhel8 nick"""
-    with VMBroker(nick='rhel8', host_classes={'host': ContentHost}) as host:
+    with VMBroker(
+        workflow=host_conf['workflow'],
+        rhel_version=host_conf['rhel'],
+        memory=host_conf['memory'],
+        cores=host_conf['cores'],
+        host_classes={'host': ContentHost},
+    ) as host:
         yield host
 
 
 @pytest.fixture
 def rhel8_contenthost_fips():
     """A function-level fixture that provides a content host object based on the rhel8_fips nick"""
-    with VMBroker(nick='rhel8_fips', host_classes={'host': ContentHost}) as host:
+    rhel8 = settings.broker.clients.rhel8
+    with VMBroker(
+        workflow='deploy-base-rhel-fips',
+        rhel_version=rhel8.release,
+        memory=rhel8.memory,
+        cores=rhel8.cores,
+        host_classes={'host': ContentHost},
+    ) as host:
         yield host
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(
+    scope='module',
+    params=[(settings.content_host.deploy_workflow, '8')],
+    ids=[settings.content_host.deploy_workflow],
+)
 def rhel8_contenthost_module():
     """A module-level fixture that provides a content host object based on the rhel8 nick"""
-    with VMBroker(nick='rhel8', host_classes={'host': ContentHost}) as host:
+    with VMBroker(
+        workflow=host_conf['workflow'],
+        rhel_version=host_conf['rhel'],
+        memory=host_conf['memory'],
+        cores=host_conf['cores'],
+        host_classes={'host': ContentHost},
+    ) as host:
         yield host
 
 
-@pytest.fixture
+@pytest.fixture(
+    params=[(settings.content_host.deploy_workflow, '6')],
+    ids=[settings.content_host.deploy_workflow],
+)
 def rhel6_contenthost():
     """A function-level fixture that provides a content host object based on the rhel6 nick"""
-    with VMBroker(nick='rhel6', host_classes={'host': ContentHost}) as host:
+    with VMBroker(
+        workflow=host_conf['workflow'],
+        rhel_version=host_conf['rhel'],
+        memory=host_conf['memory'],
+        cores=host_conf['cores'],
+        host_classes={'host': ContentHost},
+    ) as host:
         yield host
 
 
@@ -97,10 +170,21 @@ def rhel77_contenthost_class(request):
         yield host
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(
+    scope='module',
+    params=[(settings.content_host.deploy_workflow, '7')],
+    ids=[settings.content_host.deploy_workflow],
+)
 def content_hosts():
     """A module-level fixture that provides two content hosts object based on the rhel7 nick"""
-    with VMBroker(nick='rhel7', host_classes={'host': ContentHost}, _count=2) as hosts:
+    with VMBroker(
+        workflow=host_conf['workflow'],
+        rhel_version=host_conf['rhel'],
+        memory=host_conf['memory'],
+        cores=host_conf['cores'],
+        host_classes={'host': ContentHost},
+        _count=2,
+    ) as hosts:
         hosts[0].set_infrastructure_type('physical')
         yield hosts
 
