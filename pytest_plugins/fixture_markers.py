d4c39c8d929c12faa598d265946876f654783920
jyejare <jyejare@redhat.com>
2021-09-19 23:45:10 +0530

    Content host fixtures ready for dynamic WFs

---
+++ pytest_plugins/fixture_markers.py
@@ -0,0 +1,33 @@
+from inspect import getmembers
+from inspect import isfunction
+
+from pytest_fixtures import content_hosts
+from robottelo.config import settings
+
+
+def pytest_generate_tests(metafunc):
+    if 'rhel_contenthost' in metafunc.fixturenames:
+        rhel_parameters = [
+            dict(workflow=settings.content_host.deploy_workflow, rhel_version=ver)
+            for ver in settings.content_host.rhel_versions
+        ]
+        metafunc.parametrize(
+            'rhel_contenthost',
+            rhel_parameters,
+            ids=[f'rhel_{r["rhel_version"]}' for r in rhel_parameters],
+            indirect=True,
+        )
+
+
+def pytest_configure(config):
+    """Register markers related to testimony tokens"""
+    for marker in ['content_host: Test uses a content host deployed by broker']:
+        config.addinivalue_line("markers", marker)
+
+
+def pytest_collection_modifyitems(session, items, config):
+    content_host_fixture_names = [m[0] for m in getmembers(content_hosts, isfunction)]
+    for item in items:
+        if set(item.fixturenames).intersection(set(content_host_fixture_names)):
+            # TODO check param for indirect version parametrization
+            item.add_marker('content_host')
