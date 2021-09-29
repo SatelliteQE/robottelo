239c011c51fb7f61db9ec152c7b338b355806062
Mike Shriver <mshriver@redhat.com>
2021-09-29 15:21:26 -0400

    Resolve hung channel on ncat (#9000)
    Rework the pattern for using ncat to create tunnels for capsule proxies
    
    create shared fixture.  This fixture can be applied to two  test modules
    using default_url_on_new_port directly, not including that in scope of
    this commit which is just addressing the hung test behavior.

--- pytest_fixtures/smartproxy_fixtures.py
+++ pytest_fixtures/smartproxy_fixtures.py
@@ -1,7 +1,12 @@
 import pytest
+from fauxfactory import gen_string
 from nailgun import entities
 
+from robottelo.cleanup import capsule_cleanup
+from robottelo.cli.proxy import CapsuleTunnelError
 from robottelo.config import settings
+from robottelo.helpers import default_url_on_new_port
+from robottelo.helpers import get_available_capsule_port
 
 
 @pytest.fixture(scope='session')
@@ -15,3 +20,18 @@ def default_smart_proxy():
 @pytest.fixture(scope='session')
 def import_puppet_classes(default_smart_proxy):
     default_smart_proxy.import_puppetclasses(environment='production')
+
+
+@pytest.fixture(scope='module')
+def module_fake_proxy(request, default_sat):
+    """Create a Proxy and register the cleanup function"""
+    args = {'name': gen_string(str_type='alpha')}
+    newport = get_available_capsule_port()
+    try:
+        with default_url_on_new_port(9090, newport) as url:
+            args['url'] = url
+            proxy = default_sat.api.SmartProxy(**args).create()
+            yield proxy
+            capsule_cleanup(proxy.id)
+    except CapsuleTunnelError as err:
+        pytest.fail(f'Failed to create ssh tunnel: {err}')
