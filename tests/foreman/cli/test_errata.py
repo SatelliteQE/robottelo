630a2ee1a3c66c22bdb77757fd8d57bc27a1d569
Akhil Jha <akjha@redhat.com>
2021-10-11 15:48:32 +0530

    Waiting before execution

--- tests/foreman/cli/test_errata.py
+++ tests/foreman/cli/test_errata.py
@@ -20,6 +20,7 @@ from datetime import date
 from datetime import datetime
 from datetime import timedelta
 from operator import itemgetter
+from time import sleep
 
 import pytest
 from broker.broker import VMBroker
@@ -1417,6 +1418,7 @@ def test_update_applicable_package_using_default_content_view(errata_host):
     :CaseImportance: High
     """
     # check that package is applicable
+    sleep(5)
     applicable_packages = Package.list(
         {
             'host-id': errata_host.nailgun_host.id,
@@ -1527,6 +1529,7 @@ def test_install_applicable_package_to_registerd_host(chost):
     :CaseImportance: High
     """
     # Assert that the package is not applicable
+    sleep(5)
     applicable_packages = Package.list(
         {
             'host-id': chost.nailgun_host.id,
