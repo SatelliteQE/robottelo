4e6ae601e800265bfb1894f78e52082f03d0babf
Jacob Callahan <jacob.callahan05@gmail.com>
2021-10-13 11:36:09 -0400

    Allow arbitrary deploy arguments for server and capsule Tis allows users to specify a dictionary of arguments that will be passed to broker during satellite/capsule_factory checkouts

--- pytest_fixtures/broker.py
+++ pytest_fixtures/broker.py
@@ -17,7 +17,8 @@ def default_sat(align_to_satellite):
 @pytest.fixture(scope='session')
 def satellite_factory():
     def factory(retry_limit=3, delay=300, workflow=None, **broker_args):
-        broker_args.update(settings.server.deploy_arguments)
+        if settings.server.deploy_arguments:
+            broker_args.update(settings.server.deploy_arguments)
         vmb = VMBroker(
             host_classes={'host': Satellite},
             workflow=workflow or settings.server.deploy_workflow,
@@ -33,7 +34,8 @@ def satellite_factory():
 @pytest.fixture(scope='session')
 def capsule_factory():
     def factory(retry_limit=3, delay=300, workflow=None, **broker_args):
-        broker_args.update(settings.capsule.deploy_arguments)
+        if settings.capsule.deploy_arguments:
+            broker_args.update(settings.capsule.deploy_arguments)
         vmb = VMBroker(
             host_classes={'host': Capsule},
             workflow=workflow or settings.capsule.deploy_workflow,
@@ -47,47 +49,32 @@ def capsule_factory():
 
 
 @pytest.fixture
-def satellite_latest(default_sat):
-    """A fixture that provides a latest Satellite"""
-    version_args = dict(
-        deploy_sat_version=default_sat.version or settings.server.version.get('release', ''),
-        deploy_snap_version=settings.server.version.get('snap', ''),
-    )
-
-    with VMBroker(
-        host_classes={'host': Satellite}, workflow=settings.server.deploy_workflow, **version_args
-    ) as sat:
-        yield sat
+def satellite_host(satellite_factory):
+    """A fixture that provides a Satellite based on config settings"""
+    new_sat = satellite_factory()
+    yield new_sat
+    VMBroker(hosts=[new_sat]).checkin()
 
 
 @pytest.fixture
-def capsule_latest(default_sat):
-    """A fixture that provides an unconfigured latest Capsule"""
-    version_args = dict(
-        deploy_sat_version=getattr(default_sat, 'version', None)
-        or settings.server.version.get('release', ''),
-        deploy_snap_version=settings.server.version.get('snap', ''),
-    )
-
-    with VMBroker(
-        host_classes={'host': Capsule},
-        workflow=str(settings.capsule.deploy_workflow),
-        **version_args,
-    ) as cap:
-        yield cap
+def capsule_host(capsule_factory):
+    """A fixture that provides a Capsule based on config settings"""
+    new_cap = capsule_factory()
+    yield new_cap
+    VMBroker(hosts=[new_cap]).checkin()
 
 
 @pytest.fixture
-def capsule_configured(capsule_latest, default_sat):
+def capsule_configured(capsule_host, default_sat):
     """Configure the capsule instance with the satellite from settings.server.hostname"""
-    capsule_latest.install_katello_ca(default_sat)
-    capsule_latest.register_contenthost()
-    capsule_latest.capsule_setup(sat_host=default_sat)
-    yield capsule_latest
+    capsule_host.install_katello_ca(default_sat)
+    capsule_host.register_contenthost()
+    capsule_host.capsule_setup(sat_host=default_sat)
+    yield capsule_host
 
 
 @pytest.fixture
-def destructive_sat(satellite_latest):
+def destructive_sat(satellite_host):
     """Destructive tests require changing settings.server.hostname for now"""
-    with satellite_latest as sat:
+    with satellite_host as sat:
         yield sat
