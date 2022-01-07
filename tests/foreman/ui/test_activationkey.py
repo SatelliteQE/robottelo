fc588a2b6b5506700ac7f3bd9877c79921a49229
jyejare <jyejare@redhat.com>
2022-01-07 21:30:06 +0530

    Quick Fix: joining 2 AKs for registeration

--- tests/foreman/ui/test_activationkey.py
+++ tests/foreman/ui/test_activationkey.py
@@ -1008,7 +1008,7 @@ def test_positive_add_multiple_aks_to_system(session, module_org, rhel6_contenth
             assert product_name in subscriptions
         # Create VM
         rhel6_contenthost.install_katello_ca(default_sat)
-        rhel6_contenthost.register_contenthost(module_org.label, ','.join(key_1_name, key_2_name))
+        rhel6_contenthost.register_contenthost(module_org.label, ','.join([key_1_name, key_2_name]))
         assert rhel6_contenthost.subscribed
         # Assert the content-host association with activation keys
         for key_name in [key_1_name, key_2_name]:
