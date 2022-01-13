5a8676298c8180c0a3b83ca23734a2b27aa9ee0c
Gaurav Talreja <gtalreja@redhat.com>
2022-01-13 21:46:59 +0530

    [6.10.z] Initial RemoteDB config and foreman-maintain CLI's
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>

---
+++ robottelo/cli/fm_restore.py
@@ -0,0 +1,29 @@
+"""
+Usage:
+    foreman-maintain restore [OPTIONS] BACKUP_DIR
+
+Parameters:
+    BACKUP_DIR                    Path to backup directory to restore
+
+Options:
+    -y, --assumeyes               Automatically answer yes for all questions
+    -w, --whitelist whitelist     Comma-separated list of labels of steps to be skipped
+    -f, --force                   Force steps that would be skipped as they were already run
+    -i, --incremental             Restore an incremental backup
+    -h, --help                    print help
+"""
+from robottelo.cli.base import Base
+
+
+class Restore(Base):
+    """Manipulates Foreman-maintain's restore command"""
+
+    command_base = 'restore'
+
+    @classmethod
+    def run(cls, backup_dir='/tmp/', timeout='30m', options=None):
+        """Build foreman-maintain restore"""
+        # cls.command_sub = 'No subcommand for restore'
+        cls.command_end = backup_dir
+        options = options or {}
+        return cls.fm_execute(cls._construct_command(options), timeout=timeout)
