931e478496b974699972d53ee9856eaa2435bdc4
Roman Plevka <rplevka@redhat.com>
2022-02-02 17:11:25 +0100

    add rhel_contenthost-specific markers to filter os version parameterization (#9334)

--- pytest_plugins/fixture_markers.py
+++ pytest_plugins/fixture_markers.py
@@ -1,3 +1,4 @@
+import re
 from inspect import getmembers
 from inspect import isfunction
 
@@ -6,16 +7,43 @@ from robottelo.config import settings
 
 def pytest_generate_tests(metafunc):
     if 'rhel_contenthost' in metafunc.fixturenames:
-        rhel_parameters = [
-            dict(workflow=settings.content_host.deploy_workflow, rhel_version=ver)
-            for ver in settings.content_host.rhel_versions
-        ]
-        metafunc.parametrize(
-            'rhel_contenthost',
-            rhel_parameters,
-            ids=[f'rhel{r["rhel_version"]}' for r in rhel_parameters],
-            indirect=True,
-        )
+        function_marks = getattr(metafunc.function, 'pytestmark', [])
+        # process eventual rhel_version_list markers
+        matchers = [i.args for i in function_marks if i.name == 'rhel_ver_list']
+        list_params = []
+        for matcher in matchers:
+            list_params.extend(
+                [
+                    setting_rhel_ver
+                    for setting_rhel_ver in settings.content_host.rhel_versions
+                    if str(setting_rhel_ver) in str(matcher)
+                ]
+            )
+        # process eventual rhel_version_match markers
+        matchers = [i.args for i in function_marks if i.name == 'rhel_ver_match']
+        match_params = []
+        for matcher in matchers:
+            match_params.extend(
+                [
+                    setting_rhel_ver
+                    for setting_rhel_ver in settings.content_host.rhel_versions
+                    if re.fullmatch(str(matcher[0]), str(setting_rhel_ver))
+                ]
+            )
+        rhel_params = []
+        filtered_versions = set(list_params + match_params)
+        for ver, data in settings.content_host.rhel_versions.items():
+            if ver in (filtered_versions or settings.content_host.rhel_versions):
+                # prefer version-specific deploy workflow before using the default one
+                workflow = data.get('deploy_workflow', settings.content_host.deploy_workflow)
+                rhel_params.append(dict(workflow=workflow, rhel_version=ver))
+        if rhel_params:
+            metafunc.parametrize(
+                'rhel_contenthost',
+                rhel_params,
+                ids=[f'rhel{r["rhel_version"]}' for r in rhel_params],
+                indirect=True,
+            )
 
 
 def pytest_configure(config):
