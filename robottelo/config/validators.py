0c2846b2c674a6936bca6a20b1b64537d655d2bd
Mike Shriver <mshriver@redhat.com>
2022-02-07 20:50:48 -0500

    Isolate rhel support, params, and deploy
    The content host yaml was trying to do far too much with the
    increasingly abstracted test generation for rhel versions.
    
    This creates a new supportability.yaml, which is not a template but a
    direct definition of the supported versions of RHEL for the current
    master branch of robottelo. This will change when backported to 6.10,
    and in future master commits.
    
    Remove handling of the workflow selection during test generation. The
    fixture being indirectly parametrized already has a function that deals
    with processing host arguments used for deployment.

--- robottelo/config/validators.py
+++ robottelo/config/validators.py
@@ -5,6 +5,9 @@ from robottelo.constants import VALID_GCE_ZONES
 
 
 VALIDATORS = dict(
+    supportability=[
+        Validator('supportability.content_hosts.rhel.versions', must_exist=True, is_type_of=list)
+    ],
     server=[
         Validator('server.hostname', default=''),
         Validator('server.hostnames', must_exist=True, is_type_of=list),
@@ -29,9 +32,8 @@ VALIDATORS = dict(
         Validator('server.ssh_password', default=None),
     ],
     content_host=[
-        Validator('content_host.rhel_versions', must_exist=True),
         Validator('content_host.default_rhel_version', must_exist=True),
-        Validator('content_host.deploy_workflow', must_exist=True),
+        Validator('content_host.deploy_workflow.default', must_exist=True),
     ],
     subscription=[
         Validator('subscription.rhn_username', must_exist=True),
