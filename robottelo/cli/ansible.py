c0c01bedd0d79e2dbfd0f19474c25df576ee695d
Griffin Sullivan <gsulliva@redhat.com>
2022-02-16 13:16:06 -0500

    adding fam test

--- robottelo/cli/ansible.py
+++ robottelo/cli/ansible.py
@@ -18,8 +18,20 @@ class Ansible(Base):
 
     @classmethod
     def roles_import(cls, options=None):
-        """Import ansible roles"""
+        """DEPRECATED - Import ansible roles"""
         cls.command_sub = 'roles import'
+        return cls.execute(cls._construct_command(options), output_format='csv')
+
+    @classmethod
+    def roles_sync(cls, options=None):
+        """Sync Ansible roles"""
+        cls.command_sub = 'roles sync'
+        return cls.execute(cls._construct_command(options), output_format='csv')
+
+    @classmethod
+    def roles_delete(cls, options=None):
+        """Delete Ansible roles"""
+        cls.command_sub = 'roles delete'
         return cls.execute(cls._construct_command(options), output_format='csv')
 
     @classmethod
