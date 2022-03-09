aebe09ee52bbf11e365502ba840bd3979e94108f
synkd <dsynk@redhat.com>
2022-03-09 15:47:43 -0500

    Actually add docstrings

--- tests/foreman/ui/test_ansible.py
+++ tests/foreman/ui/test_ansible.py
@@ -44,12 +44,23 @@ def test_positive_import_all_roles(default_sat):
         assert available_roles_count == imported_roles_count
         ansible_variables_count = int(session.ansiblevariables.read_total_variables())
         assert ansible_variables_count > 0
-        # delete_role = 'theforeman.foreman_scap_client'
-        # session.ansibleroles.delete(delete_role)
-        # assert not session.ansibleroles.search(delete_role)
 
 
 def test_positive_create_and_delete_variable(default_sat):
+    """Create an Ansible variable with the minimum required values, then delete the variable.
+
+    :id: 7006d7c7-788a-4447-a564-d6b03ec06aaf
+
+    :Steps:
+
+        1. Import Ansible roles if none have been imported yet.
+        2. Create an Ansible variable with only a name and an assigned Ansible role.
+        3. Verify that the Ansible variable has been created.
+        4. Delete the Ansible variable.
+        5. Verify that the Ansible Variable has been deleted.
+
+    :expectedresults: The variable is successfully created and deleted.
+    """
     key = gen_string('alpha')
     role = 'redhat.satellite.activation_keys'
     with Session(hostname=default_sat.hostname) as session:
@@ -68,6 +79,18 @@ def test_positive_create_and_delete_variable(default_sat):
 
 
 def test_positive_create_variable_with_overrides(default_sat):
+    """Create an Ansible variable with all values populated.
+
+    :id: 90acea37-4c2f-42e5-92a6-0c88148f4fb6
+
+    :Steps:
+
+        1. Import Ansible roles if none have been imported yet.
+        2. Create an Anible variable, populating all fields on the creation form.
+        3. Verify that the Ansible variable was created successfully.
+
+    :expectedresults: The variable is successfully created.
+    """
     key = gen_string('alpha')
     role = 'redhat.satellite.activation_keys'
     with Session(hostname=default_sat.hostname) as session:
