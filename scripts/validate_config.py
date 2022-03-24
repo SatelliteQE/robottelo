d977a86b3fe07c4c0034d1e430faea18fcedf643
Silas Pusateri <spusater@redhat.com>
2022-03-24 15:44:15 -0400

    Improve validation script to add more context

--- scripts/validate_config.py
+++ scripts/validate_config.py
@@ -1,17 +1,38 @@
 """Usage: python scripts/validate_config.py"""
+from distutils.log import error
 from dynaconf.validator import ValidationError
 
-from robottelo.config import settings
+try:
+    from robottelo.config import settings
+except Exception as err:
+    print(f"Encountered the following exception, continuing for the sake of validation\n {err}")
 
+    from dynaconf import LazySettings
+    from robottelo.config.validators import VALIDATORS
+    settings = LazySettings(
+        envvar_prefix="ROBOTTELO",
+        core_loaders=["YAML"],
+        settings_file="settings.yaml",
+        preload=["conf/*.yaml"],
+        includes=["settings.local.yaml", ".secrets.yaml", ".secrets_*.yaml"],
+        envless_mode=True,
+        lowercase_read=True,
+    )
+
+    settings.validators.register(**VALIDATORS)
 
 def binary_validation(validators):
     settings.validators.clear()
     settings.validators.extend(validators)
     try:
         settings.validators.validate()
-    except ValidationError as err:
+    except (ValidationError, AttributeError) as err:
         if len(validators) == 1:
-            print(err)
+            if isinstance(err, AttributeError):
+                print(f"validator={validators[0].names}:\n    {err}")
+                #breakpoint();
+            else:
+                print(err)
         else:
             left, right = (
                 validators[: len(validators) // 2],
