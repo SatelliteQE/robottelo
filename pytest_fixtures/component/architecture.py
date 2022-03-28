458b80492a92fca153c78faea68c6313857fd83e
Adarsh Dubey <addubey@redhat.com>
2022-03-28 15:11:32 +0530

    Updated default_arch to use puppet enabled sat

--- pytest_fixtures/component/architecture.py
+++ pytest_fixtures/component/architecture.py
@@ -13,6 +13,16 @@ def default_architecture():
     return arch
 
 
+@pytest.fixture(scope='session')
+def session_puppet_default_architecture(session_puppet_enabled_sat):
+    arch = (
+        session_puppet_enabled_sat.api.Architecture()
+        .search(query={'search': f'name="{DEFAULT_ARCHITECTURE}"'})[0]
+        .read()
+    )
+    return arch
+
+
 @pytest.fixture(scope='module')
 def module_architecture():
     return entities.Architecture().create()
