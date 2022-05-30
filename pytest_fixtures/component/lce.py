b6b8fadecc165c1d68c9baa64b6ae526d111a3b4
vsedmik <vsedmik@redhat.com>
2022-05-30 15:34:08 +0200

    Speed up capsule-content tests
    Introduced changes:
    1) Used module-scoped capsule for all test cases, isolation on the org level.
    2) Removed on-disk artifact checks, content is checked properly another
       way.
    3) Wider use of fixtures rather than in-test entities creation.
    4) Repetitive code moved to function (wait_for_sync).

--- pytest_fixtures/component/lce.py
+++ pytest_fixtures/component/lce.py
@@ -15,11 +15,26 @@ def module_lce(module_org):
     return entities.LifecycleEnvironment(organization=module_org).create()
 
 
+@pytest.fixture(scope='function')
+def function_lce(function_org):
+    return entities.LifecycleEnvironment(organization=function_org).create()
+
+
 @pytest.fixture(scope='module')
 def module_lce_library(module_org):
     """Returns the Library lifecycle environment from chosen organization"""
     return (
         entities.LifecycleEnvironment()
         .search(query={'search': f'name={ENVIRONMENT} and organization_id={module_org.id}'})[0]
+        .read()
+    )
+
+
+@pytest.fixture(scope='function')
+def function_lce_library(function_org):
+    """Returns the Library lifecycle environment from chosen organization"""
+    return (
+        entities.LifecycleEnvironment()
+        .search(query={'search': f'name={ENVIRONMENT} and organization_id={function_org.id}'})[0]
         .read()
     )
--- pytest_fixtures/core/broker.py
+++ pytest_fixtures/core/broker.py
@@ -136,8 +136,23 @@ def capsule_host(capsule_factory):
     VMBroker(hosts=[new_cap]).checkin()
 
 
+@pytest.fixture(scope='module')
+def module_capsule_host(capsule_factory):
+    """A fixture that provides a Capsule based on config settings"""
+    new_cap = capsule_factory()
+    yield new_cap
+    VMBroker(hosts=[new_cap]).checkin()
+
+
 @pytest.fixture
 def capsule_configured(capsule_host, target_sat):
     """Configure the capsule instance with the satellite from settings.server.hostname"""
     capsule_host.capsule_setup(sat_host=target_sat)
     yield capsule_host
+
+
+@pytest.fixture(scope='module')
+def module_capsule_configured(module_capsule_host, module_target_sat):
+    """Configure the capsule instance with the satellite from settings.server.hostname"""
+    module_capsule_host.capsule_setup(sat_host=module_target_sat)
+    yield module_capsule_host
