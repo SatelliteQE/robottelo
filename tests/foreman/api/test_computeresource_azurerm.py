eb3490c3d2a9473daf2578927cb28c8365d59f58
Jitendra Yejare <jyejare@redhat.com>
2022-05-31 00:51:21 +0530

    Removed skip test for bug 1850934 and UI test teardown fix (#9630)

--- tests/foreman/api/test_computeresource_azurerm.py
+++ tests/foreman/api/test_computeresource_azurerm.py
@@ -267,7 +267,6 @@ class TestAzureRMHostProvisioningTestCase:
 
         return azurermclient.get_vm(name=class_host_ft.name.split('.')[0])
 
-    @pytest.mark.skip_if_open("BZ:1850934")
     @pytest.mark.upgrade
     @pytest.mark.tier3
     def test_positive_azurerm_host_provisioned(self, class_host_ft, azureclient_host):
@@ -301,7 +300,6 @@ class TestAzureRMHostProvisioningTestCase:
         assert self.hostname.lower() == azureclient_host.name
         assert self.vm_size == azureclient_host.type
 
-    @pytest.mark.skip_if_open("BZ:1850934")
     @pytest.mark.tier3
     def test_positive_azurerm_host_power_on_off(self, class_host_ft, azureclient_host):
         """Host can be powered on and off
@@ -418,7 +416,6 @@ class TestAzureRMUserDataProvisioning:
 
         return azurermclient.get_vm(name=class_host_ud.name.split('.')[0])
 
-    @pytest.mark.skip_if_open("BZ:1850934")
     @pytest.mark.upgrade
     @pytest.mark.tier3
     def test_positive_azurerm_ud_host_provisioned(self, class_host_ud, azureclient_host):
@@ -454,7 +451,6 @@ class TestAzureRMUserDataProvisioning:
         assert self.hostname.lower() == azureclient_host.name
         assert self.vm_size == azureclient_host.type
 
-    @pytest.mark.skip_if_open("BZ:1850934")
     @pytest.mark.upgrade
     @pytest.mark.tier3
     def test_positive_host_disassociate_associate(self, class_host_ud, module_azurerm_cr):
@@ -573,7 +569,6 @@ class TestAzureRMSharedGalleryFinishTemplateProvisioning:
 
         return azurermclient.get_vm(name=class_host_gallery_ft.name.split('.')[0])
 
-    @pytest.mark.skip_if_open("BZ:1850934")
     @pytest.mark.upgrade
     @pytest.mark.tier3
     def test_positive_azurerm_shared_gallery_host_provisioned(
@@ -700,7 +695,6 @@ class TestAzureRMCustomImageFinishTemplateProvisioning:
 
         return azurermclient.get_vm(name=class_host_custom_ft.name.split('.')[0])
 
-    @pytest.mark.skip_if_open("BZ:1850934")
     @pytest.mark.upgrade
     @pytest.mark.tier3
     def test_positive_azurerm_custom_image_host_provisioned(
