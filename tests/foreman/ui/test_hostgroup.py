d6a31ef1b1e7d0303e62fb928740e6bb40c531ac
Omkar Khatavkar <okhatavkar007@gmail.com>
2022-06-02 18:39:03 +0530

    fixed the puppet tests for hostgroup for Sat-6.11 (#9644)

--- tests/foreman/ui/test_hostgroup.py
+++ tests/foreman/ui/test_hostgroup.py
@@ -20,6 +20,7 @@ import pytest
 from fauxfactory import gen_string
 from nailgun import entities
 
+from robottelo.config import settings
 from robottelo.constants import DEFAULT_CV
 from robottelo.constants import ENVIRONMENT
 
@@ -95,6 +96,80 @@ def test_negative_delete_with_discovery_rule(session, module_org, module_locatio
             session.hostgroup.delete(hostgroup.name)
         assert "Cannot delete record because dependent discovery rules exist" in str(context.value)
         assert session.hostgroup.search(hostgroup.name)[0]['Name'] == hostgroup.name
+
+
+@pytest.mark.tier2
+def test_create_with_config_group(module_puppet_org, module_puppet_loc, session_puppet_enabled_sat):
+    """Create new host group with assigned config group to it
+
+    :id: 05a64d6b-113b-4652-86bf-19bc65b70131
+
+    :CaseImportance: Medium
+
+    :expectedresults: Host group created and contains proper config group
+    """
+    name = gen_string('alpha')
+    environment = session_puppet_enabled_sat.api.Environment(
+        organization=[module_puppet_org], location=[module_puppet_loc]
+    ).create()
+    config_group = session_puppet_enabled_sat.api.ConfigGroup().create()
+    with session_puppet_enabled_sat.ui_session as session:
+        # Create host group with config group
+        session.organization.select(org_name=module_puppet_org.name)
+        session.location.select(loc_name=module_puppet_loc.name)
+        session.hostgroup.create(
+            {
+                'host_group.name': name,
+                'host_group.puppet_environment': environment.name,
+                'puppet_enc.config_groups.assigned': [config_group.name],
+            }
+        )
+        hostgroup_values = session.hostgroup.read(name, widget_names='puppet_enc')
+        assert len(hostgroup_values['puppet_enc']['config_groups']['assigned']) == 1
+        assert hostgroup_values['puppet_enc']['config_groups']['assigned'][0] == config_group.name
+
+
+@pytest.mark.tier2
+@pytest.mark.skipif((not settings.robottelo.REPOS_HOSTING_URL), reason='Missing repos_hosting_url')
+def test_create_with_puppet_class(module_puppet_org, module_puppet_loc, session_puppet_enabled_sat):
+    """Create new host group with assigned puppet class to it
+
+    :id: 166ca6a6-c0f7-4fa0-a3f2-b0d6980cf50d
+
+    :CaseImportance: Medium
+
+    :expectedresults: Host group created and contains proper puppet class
+    """
+    name = gen_string('alpha')
+    pc_name = 'generic_1'
+    env_name = session_puppet_enabled_sat.create_custom_environment(repo=pc_name)
+    env = (
+        session_puppet_enabled_sat.api.Environment()
+        .search(query={'search': f'name={env_name}'})[0]
+        .read()
+    )
+    env = session_puppet_enabled_sat.api.Environment(
+        id=env.id,
+        location=[module_puppet_loc],
+        organization=[module_puppet_org],
+    ).update(['location', 'organization'])
+    env = session_puppet_enabled_sat.api.Environment(
+        id=env.id, location=[module_puppet_loc]
+    ).update(['location'])
+    with session_puppet_enabled_sat.ui_session as session:
+        session.organization.select(org_name=module_puppet_org.name)
+        session.location.select(loc_name=module_puppet_loc.name)
+        # Create host group with puppet class
+        session.hostgroup.create(
+            {
+                'host_group.name': name,
+                'host_group.puppet_environment': env.name,
+                'puppet_enc.classes.assigned': [pc_name],
+            }
+        )
+        hostgroup_values = session.hostgroup.read(name, widget_names='puppet_enc')
+        assert len(hostgroup_values['puppet_enc']['classes']['assigned']) == 1
+        assert hostgroup_values['puppet_enc']['classes']['assigned'][0] == pc_name
 
 
 @pytest.mark.stubbed
