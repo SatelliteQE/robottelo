1954af47acb79462d0d5e6655d206818c897096a
Ondřej Gajdušek <ogajduse@redhat.com>
2022-06-07 20:39:08 +0200

    Refactor test and fixtures

--- tests/foreman/cli/test_provisioning.py
+++ tests/foreman/cli/test_provisioning.py
@@ -17,110 +17,102 @@
 :Upstream: No
 """
 import ipaddress
-import re
 
 import pytest
+from box import Box
 from broker.broker import VMBroker
 from fauxfactory import gen_string
-from box import Box
 
 from robottelo import constants
-from robottelo.config import settings
 from robottelo.api.utils import enable_rhrepo_and_fetchid
+from robottelo.config import settings
+from robottelo.hosts import ContentHost
 
 
-# TODO: parametrize this fixture to give more than one RHEL
+# TODO: parametrize this fixture to provide more than one RHEL
+# TODO: sync at the module level?
 @pytest.fixture(scope="module")
-def provisioning_rhel_content(default_sat, module_manifest_org):
-    sat = default_sat
+def provisioning_rhel_content(module_target_sat, module_manifest_org):
+    sat = module_target_sat
     repo_id = enable_rhrepo_and_fetchid(
         basearch='x86_64',
         org_id=module_manifest_org.id,
         product=constants.PRDS['rhel8'],
         repo=constants.REPOS['rhel8_bos_ks']['name'],
         reposet=constants.REPOSET['rhel8_bos_ks'],
-        releasever='8.4',  # should be parametrizable - from fixture
-
-    ) # TODO: add appstream repo
+        releasever='8.4',  # TODO: should be parametrizable
+    )  # TODO: add appstream repo
     repo = sat.api.Repository(id=repo_id).read()
     repo.sync()
     repo = repo.read()
 
     os = (
         sat.api.OperatingSystem()
-        .search(query={'search': 'family=Redhat and major=8 and minor=4'})[0]  # parametrize fixture
+        .search(query={'search': 'family=Redhat and major=8 and minor=4'})[
+            0
+        ]  # TODO: parametrize fixture
         .read()
     )
-    return Box({"sat": sat, "os": os, "repo": repo})
+    return Box(sat=sat, os=os, repo=repo)
+
 
 @pytest.fixture(scope='module')
 def provisioning_sat(
-    default_sat,
+    module_target_sat,
     provisioning_rhel_content,
     module_manifest_org,
     module_location,
     default_architecture,
     default_partitiontable,
 ):
-    sat = default_sat
+    sat = module_target_sat
     host_root_pass = settings.provisioning.host_root_password
-    pxe_loader = "PXELinux BIOS"  # TODO: this should come from fixture
+    pxe_loader = "PXELinux BIOS"  # TODO: this should be a test parameter
     provisioning_domain_name = f"{gen_string('alpha').lower()}.foo"
-    provisioning_activation_key = f"satellite-{settings.robottelo.satellite_version}-qa-rhel{sat.os_version.major}"
     provisioning_network_vlan_id = settings.provisioning.vlan_id
 
-    provisioning_network = None
-    provisioning_network_addr = None
-    provisioning_gw_ipv4 = None
-
-    # provisioning_network = "10.1.5.88/29"  # to be supplied by broker's output
-    # provisioning_gw_ipv4 = "10.1.5.94"  # to be supplied by broker's output
-    # provisioning_interface = "eth1"  # to be supplied by broker's output
-
-    # provisioning_network = ipaddress.IPv4Network(provisioning_network)  # to be supplied by broker's output
-    # provisioning_network_addr = str(provisioning_network.network_address)  # to be supplied by broker's output
-
-    # provisioning_addr_ipv4 = str(list(provisioning_network.hosts())[0])  # to be supplied by broker's output
-    # provisioning_addr_ipv4 = f"{provisioning_addr_ipv4}/{provisioning_network.prefixlen}"  # to be supplied by broker's output
-
-    # range = subnet host range - GW address - Satellite host address
-    provisioning_dhcp_host_range = list(provisioning_network.hosts())[1:-1]
-    provisioning_network_netmask = str(provisioning_network.netmask)
-    provisioning_upstream_dns = []
-    resolv_conf = sat.execute("grep nameserver /etc/resolv.conf").stdout.splitlines()
-    for line in resolv_conf:
-        match = re.search(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b", line)
-        if match:
-            provisioning_upstream_dns.append(match.group())  # TODO: make a nice log message if no nameserver was found
-    provisioning_upstream_dns.reverse()  # TODO: use reverse record?
-    provisioning_upstream_dns_primary = (
-        provisioning_upstream_dns.pop()
-    )  # There should always be at least one upstream DNS
-    provisioning_upstream_dns_secondary = (
-        provisioning_upstream_dns.pop() if len(provisioning_upstream_dns) else None
-    )
-    
-    # run add-configure-vlan-interface on the Satellite host
-    VMBroker().execute(
-        workflow="add-configure-vlan-iface",
+    broker_data_out = VMBroker().execute(
+        workflow="configure-install-sat-provisioning-rhv",
+        artifacts="last",
         target_vlan_id=provisioning_network_vlan_id,
         target_host=sat.name,
+        provisioning_dns_zone=provisioning_domain_name,
+        sat_version=settings.server.version.release,
     )
 
-    # run configure-provisioning-ifaces-rhv on Satellite host
-    # TODO: JT should accept variables like and do not assume them
-    VMBroker().execute(
-        job_template="configure-provisioning-ifaces-rhv",
-        provisioning_dns_zone=provisioning_domain_name,
-        activation_key=provisioning_activation_key,
-        target_host=sat.name,
+    # temp mock data
+    # broker_data_out = {
+    #     'data_out': {
+    #         'provisioning_addr_ipv4': '10.1.5.57/29',
+    #         'provisioning_gw_ipv4': '10.1.5.62',
+    #         'provisioning_host_range_end': '10.1.5.61',
+    #         'provisioning_host_range_start': '10.1.5.58',
+    #         'provisioning_iface': 'eth1',
+    #         'provisioning_upstream_dns': ['10.5.30.160', '10.11.5.19'],
+    #     }
+    # }
+    broker_data_out = Box(**broker_data_out['data_out'])
+    provisioning_iface = broker_data_out.provisioning_iface  # TODO: is this needed?
+    provisioning_iface
+
+    provisioning_interface = ipaddress.ip_interface(broker_data_out.provisioning_addr_ipv4)
+    provisioning_network = provisioning_interface.network
+    # TODO: investigate DNS setup issue on Satellite,
+    # we might need to set up Sat as the primary DNS server
+    provisioning_upstream_dns_primary = (
+        broker_data_out.provisioning_upstream_dns.pop()
+    )  # There should always be at least one upstream DNS
+    provisioning_upstream_dns_secondary = (
+        broker_data_out.provisioning_upstream_dns.pop()
+        if len(broker_data_out.provisioning_upstream_dns)
+        else None
     )
 
     # use internal Capsule
-    provisioning_capsule = sat.api.SmartProxy().search(
-        query={'search': f'name={sat.hostname}'}
-    )[0].read()
-    
+    provisioning_capsule = (
+        sat.api.SmartProxy().search(query={'search': f'name={sat.hostname}'})[0].read()
+    )
+
     domain = sat.api.Domain(
         location=[module_location],
         organization=[module_manifest_org],
@@ -131,14 +123,15 @@ def provisioning_sat(
     subnet = sat.api.Subnet(
         location=[module_location],
         organization=[module_manifest_org],
-        network=provisioning_network_addr,
-        mask=provisioning_network_netmask,
-        gateway=provisioning_gw_ipv4,
-        from_=str(provisioning_dhcp_host_range[:1][0]),
-        to=str(provisioning_dhcp_host_range[-1:][0]),
+        network=str(provisioning_network.network_address),
+        mask=str(provisioning_network.netmask),
+        gateway=broker_data_out.provisioning_gw_ipv4,
+        from_=broker_data_out.provisioning_host_range_start,
+        to=broker_data_out.provisioning_host_range_end,
         dns_primary=provisioning_upstream_dns_primary,
         dns_secondary=provisioning_upstream_dns_secondary,
-        boot_mode="DHCP",
+        boot_mode='DHCP',
+        ipam='DHCP',
         dhcp=provisioning_capsule.id,
         tftp=provisioning_capsule.id,
         template=provisioning_capsule.id,
@@ -146,7 +139,7 @@ def provisioning_sat(
         httpboot=provisioning_capsule.id,
         discovery=provisioning_capsule.id,
         remote_execution_proxy=[provisioning_capsule.id],
-        domain=[domain.id],  # TODO: add IPAM param
+        domain=[domain.id],
     ).create()
 
     # use default Environment
@@ -182,36 +175,25 @@ def provisioning_sat(
         pxe_loader=pxe_loader,
     ).create()
 
-    return Box({"sat": sat, "hostgroup": hostgroup, "subnet": subnet, "cv": cv, "lce": lce})
+    return Box(sat=sat, hostgroup=hostgroup, subnet=subnet, cv=cv, lce=lce)
 
 
-@pytest.fixture(scope="module")
-def provisioning_contenthost(rhel8_contenthost):
-    provisioning_network_vlan_id = settings.provisioning.vlan_id
-    provisioning_vm_firmware = "bios"
+@pytest.fixture(scope="module")  # TODO: scope="function"?
+def provisioning_contenthost():
+    vlan_id = settings.provisioning.vlan_id
+    vm_firmware = "bios"  # TODO: this should be parametrizable by the test
+    cd_iso = ""  # TODO: this should be parametrizable by the test
 
-    # run add-configure-vlan-interface on the host-to-be-provisioned
-    VMBroker().execute(
-        workflow="add-configure-vlan-iface",
-        target_vlan_id=provisioning_network_vlan_id,
-        target_host=rhel8_contenthost.name,
-    )
+    with VMBroker(
+        workflow="deploy-configure-pxe-provisioning-host-rhv",
+        host_classes={'host': ContentHost},
+        target_vlan_id=vlan_id,
+        target_vm_firmware=vm_firmware,
+        target_vm_cd_iso=cd_iso,
+    ) as host:
+        yield host
 
-    # run configure-pxe-boot-rhv on the host-to-be-provisioned
-    VMBroker().execute(
-        job_template="configure-pxe-boot-rhv",
-        target_host=rhel8_contenthost.name,
-        target_vlan_id=provisioning_network_vlan_id,
-        target_vm_firmware=provisioning_vm_firmware,
-    )
-
-    ######## begin mock ########
-    from robottelo.hosts import ContentHost
-    rhel8_contenthost = ContentHost(hostname="dhcp-2-235.vms.sat.rdu2.redhat.com", username="root", password="dog8code")
-    ######## end mock ########
-
-    contenthost_mac_addr = rhel8_contenthost.execute("cat /sys/class/net/eth1/address").stdout.splitlines()[0]
-    return Box({"mac_addr": contenthost_mac_addr})
+    # TODO: Tell Satellite to access the host by IP address, not FQDN
 
 
 @pytest.mark.stubbed
@@ -256,22 +238,24 @@ def test_rhel_pxe_provisioning_on_rhv(
         2. Host is registered to Satellite and subscription status is 'Success'
         3. Host can install package from Satellite
     """
-    sat = provisioning_sat.sat
-    contenthost_mac_addr = provisioning_contenthost.mac_addr
-    hostgroup = provisioning_sat.hostgroup
-    subnet = provisioning_sat.subnet
-    os = provisioning_rhel_content.os
-    cv = provisioning_sat.cv
-    lce = provisioning_sat.lce
+    bios_firmware = "BIOS"  # TODO: teach the test to use this parameter
+    bios_firmware
+    # TODO: get mac addr from the broker inventory
+    # host_mac_addr = provisioning_contenthost.mac_addr
 
     host = provisioning_sat.api.Host(
-        hostgroup=hostgroup,
+        hostgroup=provisioning_sat.hostgroup,
         organization=module_manifest_org,
         location=module_location,
-        content_facet_attributes={'content_view_id': cv.id, 'lifecycle_environment_id': lce.id},
-        name=gen_string('alpha'),
-        mac=contenthost_mac_addr,
-        operatingsystem=os,
-        subnet=subnet,
-        # ip="10.1.5.91"
+        content_facet_attributes={
+            'content_view_id': provisioning_sat.cv.id,
+            'lifecycle_environment_id': provisioning_sat.lce.id,
+        },
+        name=gen_string('alpha').lower(),
+        # mac=host_mac_addr,
+        operatingsystem=provisioning_rhel_content.os,
+        subnet=provisioning_sat.subnet,
+        # ip="10.1.5.91"  # TODO how to get IP from the Satellite API?
     ).create(create_missing=False)
+
+    host
