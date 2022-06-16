89ffdab6e2f45e3ff8c7aa6799b5d4e2f9eff82e
yanpliu <yanpliu@redhat.com>
2022-06-16 14:57:49 +0800

    update the hypersior and guest json format

--- robottelo/virtwho_utils.py
+++ robottelo/virtwho_utils.py
@@ -373,29 +373,25 @@ def hypervisor_json_create(hypervisors, guests):
     """
     Create a hypervisor guest json data. For example:
     {'hypervisors': [{'hypervisorId': '820b5143-3885-4dba-9358-4ce8c30d934e',
-    'guests': [{'guestId': 'afb91b1f-8438-46f5-bc67-d7ab328ef782', 'state': 1,
+    'guestIds': [{'guestId': 'afb91b1f-8438-46f5-bc67-d7ab328ef782', 'state': 1,
     'attributes': {'active': 1, 'virtWhoType': 'esx'}}]}]}
     :param hypervisors: how many hypervisors will be created
     :param guests: how many guests will be created
     """
+    hypervisors_list = []
     for i in range(hypervisors):
         guest_list = []
         for c in range(guests):
             guest_list.append(
                 {
-                    "guestIds": [
-                        {
-                            "attributes": {"active": 1, "virtWhoType": "esx"},
-                            "state": 1,
-                            "guestId": str(uuid.uuid4()),
-                        }
-                    ],
-                    "hypervisorId": {
-                        "hypervisorId": str(uuid.uuid4()),
-                    },
+                    "guestId": str(uuid.uuid4()),
+                    "state": 1,
+                    "attributes": {"active": 1, "virtWhoType": "esx"},
                 }
             )
-    mapping = {"hypervisors": guest_list}
+        hypervisor = {"hypervisorId": str(uuid.uuid4()), "guestIds": guest_list}
+        hypervisors_list.append(hypervisor)
+    mapping = {"hypervisors": hypervisors_list}
     return mapping
 
 
