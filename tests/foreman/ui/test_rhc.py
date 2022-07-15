a41d8245776e2027de28835365c5100513d24293
Jameer Pathan <jameerpathan111@gmail.com>
2022-07-15 17:02:44 +0200

    add subscribe_rhc_satellite

--- tests/foreman/ui/test_rhc.py
+++ tests/foreman/ui/test_rhc.py
@@ -20,7 +20,6 @@ import pytest
 from wait_for import wait_for
 
 from robottelo import constants
-from robottelo.api.utils import enable_sync_redhat_repo
 from robottelo.config import settings
 from robottelo.constants import DEFAULT_SUBSCRIPTION_NAME
 from robottelo.datafactory import gen_string
@@ -28,27 +27,53 @@ from robottelo.helpers import file_downloader
 from robottelo.logging import logger
 
 
-@pytest.fixture()
-def fixture_enable_rhc_repos(target_sat):
-    """Enable repos required for configuring RHC."""
-    if target_sat.os_version.major == 8:
-        target_sat.enable_repo(constants.REPOS['rhel8_bos']['id'])
-        target_sat.enable_repo(constants.REPOS['rhel8_aps']['id'])
+@pytest.fixture(scope='module')
+def subscribe_rhc_satellite(clean_rhsm, module_target_sat):
+    """subscribe rhc satellite to cdn"""
+    if module_target_sat.os_version.major < 8:
+        release_version = f'{module_target_sat.os_version.major}Server'
     else:
-        target_sat.enable_repo(constants.REPOS['rhscl7']['id'])
-        target_sat.enable_repo(constants.REPOS['rhel7']['id'])
+        release_version = f'{module_target_sat.os_version.major}'
+    module_target_sat.register_contenthost(
+        org=None,
+        lce=None,
+        username=settings.subscription.rhn_username,
+        password=settings.subscription.rhn_password,
+        releasever=release_version,
+    )
+    result = module_target_sat.subscription_manager_attach_pool([settings.subscription.rhn_poolid])[0]
+    if 'Successfully attached a subscription' in result.stdout:
+        # extras is not in RHEL8: https://access.redhat.com/solutions/5331391
+        if module_target_sat.os_version.major < 8:
+            module_target_sat.enable_repo(
+                f'rhel-{module_target_sat.os_version.major}-server-extras-rpms', force=True
+            )
+        yield
+    else:
+        pytest.fail('Failed to attach system to pool. Aborting Test!.')
+
+
+@pytest.fixture()
+def fixture_enable_rhc_repos(module_target_sat):
+    """Enable repos required for configuring RHC."""
+    if module_target_sat.os_version.major == 8:
+        module_target_sat.enable_repo(constants.REPOS['rhel8_bos']['id'])
+        module_target_sat.enable_repo(constants.REPOS['rhel8_aps']['id'])
+    else:
+        module_target_sat.enable_repo(constants.REPOS['rhscl7']['id'])
+        module_target_sat.enable_repo(constants.REPOS['rhel7']['id'])
 
 
 @pytest.fixture(scope='module')
-def module_rhc_org(target_sat):
+def module_rhc_org(module_target_sat):
     """Module level fixture for creating organization"""
-    org = target_sat.api.Organization(
+    org = module_target_sat.api.Organization(
         name=gen_string('alpha')
         if not settings.rh_cloud.organization
         else settings.rh_cloud.organization
     ).create()
     # adding remote_execution_connect_by_ip=Yes at org level
-    target_sat.api.Parameter(
+    module_target_sat.api.Parameter(
         name='remote_execution_connect_by_ip',
         value='Yes',
         organization=org.id,
@@ -57,27 +82,27 @@ def module_rhc_org(target_sat):
 
 
 @pytest.fixture()
-def fixture_setup_rhc_satellite(request, target_sat, module_rhc_org):
+def fixture_setup_rhc_satellite(request, module_target_sat, module_rhc_org):
     """Create Organization and activation key after successful test execution"""
     yield
     if request.node.rep_call.passed:
         manifests_path = file_downloader(
-            file_url=settings.fake_manifest.url['default'], hostname=target_sat.hostname
+            file_url=settings.fake_manifest.url['default'], hostname=module_target_sat.hostname
         )[0]
-        target_sat.cli.Subscription.upload(
+        module_target_sat.cli.Subscription.upload(
             {'file': manifests_path, 'organization-id': module_rhc_org.id}
         )
         # Create Activation key
-        ak = target_sat.api.ActivationKey(
+        ak = module_target_sat.api.ActivationKey(
             name=gen_string('alpha')
             if not settings.rh_cloud.activation_key
             else settings.rh_cloud.activation_key,
             content_view=module_rhc_org.default_content_view,
             organization=module_rhc_org,
-            environment=target_sat.api.LifecycleEnvironment(id=module_rhc_org.library.id),
+            environment=module_target_sat.api.LifecycleEnvironment(id=module_rhc_org.library.id),
             auto_attach=True,
         ).create()
-        subscription = target_sat.api.Subscription(organization=module_rhc_org)
+        subscription = module_target_sat.api.Subscription(organization=module_rhc_org)
         default_subscription = subscription.search(
             query={'search': f'name="{DEFAULT_SUBSCRIPTION_NAME}"'}
         )[0]
@@ -88,8 +113,8 @@ def fixture_setup_rhc_satellite(request, target_sat, module_rhc_org):
 @pytest.mark.tier3
 def test_positive_configure_cloud_connector(
     session,
-    target_sat,
-    subscribe_satellite,
+    module_target_sat,
+    subscribe_rhc_satellite,
     fixture_enable_rhc_repos,
     module_rhc_org,
     fixture_setup_rhc_satellite,
@@ -113,13 +138,13 @@ def test_positive_configure_cloud_connector(
     :BZ: 1818076
     """
     # Copy foreman-proxy user's key to root@localhost user's authorized_keys
-    target_sat.add_rex_key(satellite=target_sat)
+    module_target_sat.add_rex_key(satellite=module_target_sat)
 
     # Set Host parameter source_display_name to something random.
     # To avoid 'name has already been taken' error when run multiple times
     # on a machine with the same hostname.
-    host_id = target_sat.cli.Host.info({'name': target_sat.hostname})['id']
-    target_sat.cli.Host.set_parameter(
+    host_id = module_target_sat.cli.Host.info({'name': module_target_sat.hostname})['id']
+    module_target_sat.cli.Host.set_parameter(
         {'host-id': host_id, 'name': 'source_display_name', 'value': gen_string('alpha')}
     )
 
@@ -135,34 +160,34 @@ def test_positive_configure_cloud_connector(
 
     template_name = 'Configure Cloud Connector'
     invocation_id = (
-        target_sat.api.JobInvocation()
+        module_target_sat.api.JobInvocation()
         .search(query={'search': f'description="{template_name}"'})[0]
         .id
     )
     wait_for(
-        lambda: target_sat.api.JobInvocation(id=invocation_id).read().status_label
+        lambda: module_target_sat.api.JobInvocation(id=invocation_id).read().status_label
         in ["succeeded", "failed"],
         timeout="1500s",
     )
 
-    result = target_sat.cli.JobInvocation.get_output(
-        {'id': invocation_id, 'host': target_sat.hostname}
+    result = module_target_sat.cli.JobInvocation.get_output(
+        {'id': invocation_id, 'host': module_target_sat.hostname}
     )
     logger.debug(f"Invocation output>>\n{result}\n<<End of invocation output")
     # if installation fails, it's often due to missing rhscl repo -> print enabled repos
-    repolist = target_sat.execute('yum repolist')
+    repolist = module_target_sat.execute('yum repolist')
     logger.debug(f"Repolist>>\n{repolist}\n<<End of repolist")
-    assert target_sat.api.JobInvocation(id=invocation_id).read().status == 0
+    assert module_target_sat.api.JobInvocation(id=invocation_id).read().status == 0
     assert "Install yggdrasil-worker-forwarder and rhc" in result
     assert "Restart rhcd" in result
     assert 'Exit status: 0' in result
 
-    result = target_sat.execute('rhc status')
+    result = module_target_sat.execute('rhc status')
     logger.debug(f"rhc status>>\n{result}\n<<End of rhc status")
     assert result.status == 0
     assert "Connected to Red Hat Subscription Management" in result.stdout
     assert "The Red Hat connector daemon is active" in result.stdout
 
-    result = target_sat.execute('journalctl --unit=rhcd')
+    result = module_target_sat.execute('journalctl --unit=rhcd')
     logger.debug(f"journalctl log>>\n{result}\n<<End of log")
     assert "error" not in result.stdout
