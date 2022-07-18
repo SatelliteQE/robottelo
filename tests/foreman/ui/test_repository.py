2276992b4a1bc1f8de7504e99b3a659f59bbae89
Cole Higgins <chiggins@redhat.com>
2022-07-18 04:17:02 -0400

    Test for Verifying Checksum (#9765)
    * Test for Verifying Checksum
    
    * updated test file with session call
    
    * Finished test and airgun functionality

--- tests/foreman/ui/test_repository.py
+++ tests/foreman/ui/test_repository.py
@@ -1192,3 +1192,37 @@ def test_positive_select_org_in_any_context():
     :CaseImportance: High
     """
     pass
+
+
+@pytest.mark.tier2
+@pytest.mark.upgrade
+@pytest.mark.skipif((not settings.robottelo.REPOS_HOSTING_URL), reason='Missing repos_hosting_url')
+def test_positive_sync_repo_and_verify_checksum(session, module_org):
+    """Tests that Verify Content Checksum succeeds when executing from the products page
+
+    :id: 577be1f8-7510-49d2-8b33-600db60bd960
+
+    :customerscenario: true
+
+    :BZ: 1951626
+
+    :Steps:
+        1. Enable and sync repository
+        2. Go to Products -> Select Action -> Verify Content Checksum
+
+    :expectedresults: Verify Content Checksum task succeeds
+    """
+    repo_name = gen_string('alpha')
+    product = entities.Product(organization=module_org).create()
+    with session:
+        session.repository.create(
+            product.name,
+            {
+                'name': repo_name,
+                'repo_type': REPO_TYPE['yum'],
+                'repo_content.upstream_url': settings.repos.yum_1.url,
+            },
+        )
+        session.repository.synchronize(product.name, repo_name)
+        results = session.product.verify_content_checksum([product.name])
+        assert results['task']['result'] == 'success'
