aa2dfcef1e7b9735c070cb241be8ae532522d704
Jameer Pathan <jameerpathan111@gmail.com>
2022-07-25 18:51:33 +0200

    re-order assertions

--- tests/foreman/ui/test_rhc.py
+++ tests/foreman/ui/test_rhc.py
@@ -17,20 +17,14 @@
 :Upstream: No
 """
 import pytest
+from fauxfactory import gen_string
 from wait_for import wait_for
 
 from robottelo import constants
 from robottelo.config import settings
 from robottelo.constants import DEFAULT_SUBSCRIPTION_NAME
-from robottelo.datafactory import gen_string
 from robottelo.helpers import file_downloader
 from robottelo.logging import logger
-
-
-@pytest.fixture(scope='module')
-def subscribe_rhc_satellite(clean_rhsm, module_target_sat):
-    """subscribe rhc satellite to cdn"""
-    module_target_sat.register_to_cdn()
 
 
 @pytest.fixture(scope='module')
@@ -50,9 +44,7 @@ def fixture_enable_rhc_repos(module_target_sat):
 def module_rhc_org(module_target_sat):
     """Module level fixture for creating organization"""
     org = module_target_sat.api.Organization(
-        name=gen_string('alpha')
-        if not settings.rh_cloud.organization
-        else settings.rh_cloud.organization
+        name=settings.rh_cloud.organization or gen_string('alpha')
     ).create()
     # adding remote_execution_connect_by_ip=Yes at org level
     module_target_sat.api.Parameter(
@@ -63,7 +55,7 @@ def module_rhc_org(module_target_sat):
     return org
 
 
-@pytest.fixture()
+@pytest.fixture(scope='module')
 def fixture_setup_rhc_satellite(request, module_target_sat, module_rhc_org):
     """Create Organization and activation key after successful test execution"""
     yield
@@ -76,18 +68,15 @@ def fixture_setup_rhc_satellite(request, module_target_sat, module_rhc_org):
         )
         # Create Activation key
         ak = module_target_sat.api.ActivationKey(
-            name=gen_string('alpha')
-            if not settings.rh_cloud.activation_key
-            else settings.rh_cloud.activation_key,
+            name=settings.rh_cloud.activation_key or gen_string('alpha'),
             content_view=module_rhc_org.default_content_view,
             organization=module_rhc_org,
             environment=module_target_sat.api.LifecycleEnvironment(id=module_rhc_org.library.id),
             auto_attach=True,
         ).create()
-        subscription = module_target_sat.api.Subscription(organization=module_rhc_org)
-        default_subscription = subscription.search(
-            query={'search': f'name="{DEFAULT_SUBSCRIPTION_NAME}"'}
-        )[0]
+        default_subscription = module_target_sat.api.Subscription(
+            organization=module_rhc_org
+        ).search(query={'search': f'name="{DEFAULT_SUBSCRIPTION_NAME}"'})[0]
         ak.add_subscriptions(data={'quantity': 10, 'subscription_id': default_subscription.id})
         logger.debug(f"Activation key: {ak} \n Organization: {module_rhc_org}")
 
@@ -151,24 +140,27 @@ def test_positive_configure_cloud_connector(
         timeout="1500s",
     )
 
-    result = module_target_sat.cli.JobInvocation.get_output(
+    job_output = module_target_sat.cli.JobInvocation.get_output(
         {'id': invocation_id, 'host': module_target_sat.hostname}
     )
-    logger.debug(f"Invocation output>>\n{result}\n<<End of invocation output")
+    logger.debug(f"Invocation output>>\n{job_output}\n<<End of invocation output")
     # if installation fails, it's often due to missing rhscl repo -> print enabled repos
     repolist = module_target_sat.execute('yum repolist')
     logger.debug(f"Repolist>>\n{repolist}\n<<End of repolist")
+    # print rhc status
+    rhc_status = module_target_sat.execute('rhc status')
+    logger.debug(f"rhc status>>\n{rhc_status}\n<<End of rhc status")
+    # print rhcd log
+    rhcd_log = module_target_sat.execute('journalctl --unit=rhcd')
+    logger.debug(f"journalctl log>>\n{rhcd_log}\n<<End of log")
+
     assert module_target_sat.api.JobInvocation(id=invocation_id).read().status == 0
-    assert "Install yggdrasil-worker-forwarder and rhc" in result
-    assert "Restart rhcd" in result
-    assert 'Exit status: 0' in result
+    assert "Install yggdrasil-worker-forwarder and rhc" in job_output
+    assert "Restart rhcd" in job_output
+    assert 'Exit status: 0' in job_output
 
-    result = module_target_sat.execute('rhc status')
-    logger.debug(f"rhc status>>\n{result}\n<<End of rhc status")
-    assert result.status == 0
-    assert "Connected to Red Hat Subscription Management" in result.stdout
-    assert "The Red Hat connector daemon is active" in result.stdout
+    assert rhc_status.status == 0
+    assert "Connected to Red Hat Subscription Management" in rhc_status.stdout
+    assert "The Red Hat connector daemon is active" in rhc_status.stdout
 
-    result = module_target_sat.execute('journalctl --unit=rhcd')
-    logger.debug(f"journalctl log>>\n{result}\n<<End of log")
-    assert "error" not in result.stdout
+    assert "error" not in rhcd_log.stdout
