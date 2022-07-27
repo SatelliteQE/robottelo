25eeec55b4e6c2192b5fe03856934f1cadd5a255
Vladimir Sedmik <vsedmik@redhat.com>
2022-07-27 14:41:11 +0200

    Fix and take over puppet-related report tests

--- tests/foreman/cli/test_report.py
+++ tests/foreman/cli/test_report.py
@@ -6,9 +6,9 @@
 
 :CaseLevel: Component
 
-:CaseComponent: SCAPPlugin
+:CaseComponent: Puppet
 
-:Assignee: jpathan
+:Assignee: vsedmik
 
 :TestType: Functional
 
@@ -24,17 +24,17 @@ from robottelo.cli.base import CLIReturnCodeError
 from robottelo.cli.report import ConfigReport
 
 
-@pytest.fixture(scope='module', autouse=True)
-def run_puppet_agent(module_target_sat):
+@pytest.fixture(scope='module')
+def run_puppet_agent(session_puppet_enabled_sat):
     """Retrieves the client configuration from the puppet master and
     applies it to the local host. This is required to make sure
     that we have reports available.
     """
-    module_target_sat.execute('puppet agent -t')
+    session_puppet_enabled_sat.execute('puppet agent -t')
 
 
 @pytest.mark.tier1
-def test_positive_info():
+def test_positive_info(run_puppet_agent):
     """Test Info for Puppet report
 
     :id: 32646d4b-7101-421a-85e0-777d3c6b71ec
@@ -53,7 +53,7 @@ def test_positive_info():
 
 @pytest.mark.tier1
 @pytest.mark.upgrade
-def test_positive_delete_by_id():
+def test_positive_delete_by_id(run_puppet_agent):
     """Check if Puppet Report can be deleted by its ID
 
     :id: bf918ec9-e2d4-45d0-b913-ab939b5d5e6a
