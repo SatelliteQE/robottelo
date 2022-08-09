c1fca0b72904feb1457070e36a5e7c4d8897f1b8
Jameer Pathan <jameerpathan111@gmail.com>
2022-08-09 16:21:32 +0200

    Add setup for iqe remediation tests. (#9780)
    * RHC pipeline - stage 2
    
    * Add default subscription to ak
    
    * remove rhscl
    
    * update rh_cloud.yaml.template
    
    * add subscribe_rhc_satellite
    
    * Update fixture
    
    * re-order assertions
    
    * search and update host param using api
    
    * address pr comments

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 """Global Configurations for py.test runner"""
+import pytest
 
 pytest_plugins = [
     # Plugins
@@ -55,3 +56,15 @@ pytest_plugins = [
     'pytest_fixtures.component.templatesync',
     'pytest_fixtures.component.user_role',
 ]
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+
+    setattr(item, "rep_" + rep.when, rep)
