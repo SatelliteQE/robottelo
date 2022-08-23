26ff52f42ce8cec0a27e84e31c1f5fb6bfb0b1c3
Peter Ondrejka <pondrejk@users.noreply.github.com>
2022-08-23 13:16:35 +0200

    rex pull-provider tests (#9920)
    * rex pull-provider tests
    
    * Update tests/foreman/cli/test_remoteexecution.py
    
    Co-authored-by: Gaurav Talreja <gauravtalreja1@gmail.com>
    
    * Update tests/foreman/cli/test_remoteexecution.py
    
    Co-authored-by: Gaurav Talreja <gauravtalreja1@gmail.com>
    
    * reviewer feedback
    
    * another round of feedback
    
    Co-authored-by: Gaurav Talreja <gauravtalreja1@gmail.com>

--- robottelo/hosts.py
+++ robottelo/hosts.py
@@ -5,6 +5,7 @@ from functools import cached_property
 from pathlib import Path
 from pathlib import PurePath
 from tempfile import NamedTemporaryFile
+from urllib.parse import urlencode
 from urllib.parse import urljoin
 from urllib.parse import urlunsplit
 
@@ -478,6 +479,113 @@ class ContentHost(Host, ContentHostMixins):
         result = self.execute('yum install cockpit -y')
         if result.status != 0:
             raise ContentHostError('Failed to install the cockpit')
+
+    def register(
+        self,
+        target,
+        org,
+        loc,
+        activation_keys,
+        setup_insights=False,
+        setup_remote_execution=True,
+        setup_remote_execution_pull=False,
+        lifecycle_environment=None,
+        operating_system=None,
+        packages=None,
+        repo=None,
+        repo_gpg_key_url=None,
+        remote_execution_interface=None,
+        update_packages=False,
+        ignore_subman_errors=True,
+        force=True,
+        insecure=True,
+        username=settings.server.admin_username,
+        password=settings.server.admin_password,
+    ):
+        """Registers content host to the Satellite or Capsule server
+        using a global registration template.
+
+        :param target: Satellite or Capusle hostname to register to, required.
+        :param org: Organization to register content host for, required.
+        :param loc: Location to register content host for, required.
+        :param activation_keys: Activation key name to register content host
+            with, required.
+        :param setup_insights: Install and register Insights client, requires OS repo.
+        :param setup_remote_execution: Copy remote execution SSH key.
+        :param setup_remote_execution_pull: Deploy pull provider client on host
+        :param lifecycle_environment: Lifecycle environment.
+        :param operating_system: Operating system.
+        :param packages: A list of packages to install on the host when registered.
+        :param repo: Repository to be added before the registration is performed, supply url.
+        :param repo_gpg_key_url: Public key to verify the package signatures, supply url.
+        :param remote_execution_interface: Identifier of the host interface for remote execution.
+        :param update_packages: Update all packages on the host.
+        :param ignore_subman_errors: Ignore subscription manager errors.
+        :param force: Register the content host even if it's already registered.
+        :param insecure: Don't verify server authenticity.
+        :param username: Satellite admin username
+        :param password: Satellite admin password
+        :return: SSHCommandResult instance filled with the result of the
+            registration.
+        """
+        insights = (
+            # requires OS repo enabled for host
+            f'&setup_insights={str(setup_insights).lower()}'
+            if setup_insights is not None
+            else ''
+        )
+        rex = (
+            f'&setup_remote_execution={str(setup_remote_execution).lower()}'
+            if setup_remote_execution is not None
+            else ''
+        )
+        rex_pull = (
+            # requires Satellite Client repo enabled for host
+            f'&setup_remote_execution_pull={str(setup_remote_execution_pull).lower()}'
+            if setup_remote_execution_pull is not None
+            else ''
+        )
+        lce = (
+            f'&lifecycle_environment_id={lifecycle_environment.id}'
+            if lifecycle_environment is not None
+            else ''
+        )
+        os = f'&operating_system_id={operating_system.id}' if operating_system is not None else ''
+        pkgs = f'&packages={"+".join(packages)}' if packages is not None else ''
+        rex_iface = (
+            f'&remote_execution_interface={remote_execution_interface}'
+            if remote_execution_interface is not None
+            else ''
+        )
+        rp = f'&{urlencode({"repo": repo })}' if repo is not None else ''
+        gpg = (
+            f'&{urlencode({"repo_gpg_key_url": repo_gpg_key_url })}'
+            if repo_gpg_key_url is not None
+            else ''
+        )
+        cmd = (
+            'curl -sS '
+            f'-u {username}:{password} '
+            f'{"--insecure " if insecure else ""}'
+            f"'https://{target.hostname}:9090/register?"
+            f'activation_keys={activation_keys}'
+            f'&organization_id={org.id}'
+            f'&location_id={loc.id}'
+            f'{insights}'
+            f'{rex}'
+            f'&update_packages={"true" if update_packages else "false"}'
+            f'{rex_pull}'
+            f'{rex_iface}'
+            f'{lce}'
+            f'{os}'
+            f'{pkgs}'
+            f'{"&ignore_subman_errors=true" if ignore_subman_errors else ""}'
+            f'{"&force=true" if force else ""}'
+            f'{rp}'
+            f'{gpg}'
+            "' | bash"
+        )
+        return self.execute(cmd)
 
     def register_contenthost(
         self,
@@ -1272,6 +1380,22 @@ class Capsule(ContentHost, CapsuleMixins):
             raise CapsuleHostError(
                 f'A core service is not running at capsule host\n{result.stdout}'
             )
+
+    def enable_mqtt(self):
+        installer_opts = {
+            'foreman-proxy-templates': 'true',
+            'foreman-proxy-registration': 'true',
+            'foreman-proxy-plugin-remote-execution-script-mode': 'pull-mqtt',
+        }
+        enable_mqtt_command = InstallerCommand(
+            installer_opts=installer_opts,
+        )
+        result = self.execute(
+            enable_mqtt_command.get_command(),
+            timeout='20m',
+        )
+        if result.status != 0:
+            raise SatelliteHostError(f'Failed to enable pull provider: {result.stdout}')
 
 
 class Satellite(Capsule, SatelliteMixins):
