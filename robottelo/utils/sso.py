55e42db2aaa4dde23765b3f2256fa991891aca88
Cole Higgins <chiggins@redhat.com>
2022-08-30 15:33:00 -0400

    fixed sat_obj

--- robottelo/utils/sso.py
+++ robottelo/utils/sso.py
@@ -202,7 +202,7 @@ class SSOHost(Host):
                 f"https://{sat_obj.hostname}/users/extlogin",
             ]
         }
-        self.update_client_configuration(client_config)
+        self.update_client_configuration(sat_obj, client_config)
 
 
 sso_host = SSOHost()
