2f3b375c79625bcff7e0d560b2dce28f885ad080
latran <ltran@redhat.com>
2022-09-09 15:35:28 -0400

    Test cases for new cv UI

--- tests/foreman/ui/test_contentview_new.py
+++ tests/foreman/ui/test_contentview_new.py
@@ -20,21 +20,40 @@ Feature details: https://fedorahosted.org/katello/wiki/ContentViews
 :Upstream: No
 """
 import pytest
-from nailgun import entities
 
 from robottelo.datafactory import gen_string
 
 
 @pytest.mark.tier2
-def test_positive_create_cv_and_search_for_name(session):
+def test_positive_create_cv(session):
+    """Able to create cv and search for it
+
+    :id: 1bad50d8-4909-47ef-8524-8c703c75069d
+
+    :CaseLevel: System
+    """
     cv = gen_string('alpha')
     with session:
-        session.contentview_new.create({'name': cv})
+        session.contentview_new.create(dict(name=cv))
         assert session.contentview_new.search(cv)[0]['Name'] == cv
 
 
 @pytest.mark.tier2
-def test_positive_search_for_name(session, module_org):
-    cv = entities.ContentView(organization=module_org).create()
+def test_positive_search_for_cv(session, module_org, target_sat):
+    """Able to search for a cv name
+
+    :id: 4a097af5-66bf-4dce-8f79-25a5f60eb2cb
+
+    :CaseLevel: System
+    """
+    cv = target_sat.api.ContentView(organization=module_org).create()
     with session:
-        assert session.contentview_new.search(cv.name)[0]['Name'] == cv
+        assert session.contentview_new.search(cv.name)[0]['Name'] == cv.name
+
+
+@pytest.mark.tier1
+def test_positive_publish(session, module_org, target_sat):
+    cv = target_sat.api.ContentView(organization=module_org).create()
+    with session:
+        result = session.contentview_new.publish(cv.name)
+        assert result['Version'] == 'Version 1.0'
