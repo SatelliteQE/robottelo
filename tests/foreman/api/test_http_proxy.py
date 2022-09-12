be842ffa7c1f02cd70f605ef56b8038c922918e0
Jameer Pathan <jameerpathan111@gmail.com>
2022-09-12 18:10:18 +0200

    Add test for auto_attach with http proxy configured

--- tests/foreman/api/test_http_proxy.py
+++ tests/foreman/api/test_http_proxy.py
@@ -17,10 +17,19 @@
 :Upstream: No
 """
 import pytest
+from broker import Broker
 
 from robottelo import constants
+from robottelo import manifests
 from robottelo.api.utils import enable_rhrepo_and_fetchid
+from robottelo.api.utils import promote
+from robottelo.api.utils import upload_manifest
 from robottelo.config import settings
+from robottelo.constants import PRDS
+from robottelo.constants import REPOS
+from robottelo.constants import REPOSET
+from robottelo.hosts import ContentHost
+from robottelo.hosts import ContentHostError
 
 
 @pytest.mark.tier2
@@ -125,3 +134,82 @@ def test_positive_end_to_end(setup_http_proxy, module_target_sat, module_manifes
     )
     assert len(yum_repo['output']) >= 1
     assert 'quay/busybox' in yum_repo['output']
+
+
+@pytest.mark.upgrade
+@pytest.mark.run_in_one_thread
+@pytest.mark.parametrize(
+    'setup_http_proxy',
+    [None, True, False],
+    indirect=True,
+    ids=['no_http_proxy', 'auth_http_proxy', 'unauth_http_proxy'],
+)
+@pytest.mark.cli_host_subscription
+@pytest.mark.tier3
+def test_positive_auto_attach(setup_http_proxy, module_target_sat, function_org):
+    """Attempt to auto attach a subscription to content host
+
+    :id: cce888b5-e023-4ee2-bffe-efa9260224ee
+
+    :expectedresults: host successfully subscribed, subscription
+        repository enabled, and repository package installed
+
+    :parametrized: yes
+
+    :CaseLevel: System
+    """
+    with manifests.clone() as manifest:
+        upload_manifest(function_org.id, manifest.content)
+    lce = module_target_sat.api.LifecycleEnvironment(organization=function_org).create()
+    content_view = module_target_sat.api.ContentView(organization=function_org).create()
+    content_view.publish()
+    content_view = content_view.read()
+    module_target_sat.api.LifecycleEnvironment(organization=function_org).create()
+    promote(content_view.version[0], environment_id=lce.id)
+    rh_repo_id = enable_rhrepo_and_fetchid(
+        basearch=constants.DEFAULT_ARCHITECTURE,
+        org_id=function_org.id,
+        product=PRDS['rhel'],
+        repo=REPOS['rhst7']['name'],
+        reposet=REPOSET['rhst7'],
+        releasever=None,
+    )
+    rh_repo = module_target_sat.api.Repository(id=rh_repo_id).read()
+    rh_repo.sync()
+    cv = module_target_sat.api.ContentView(id=content_view.id, repository=[rh_repo]).update(
+        ["repository"]
+    )
+    cv.publish()
+    cv = cv.read()
+    promote(cv.version[-1], environment_id=lce.id)
+    subscription = module_target_sat.api.Subscription(organization=function_org.id).search(
+        query={'search': f'name="{constants.DEFAULT_SUBSCRIPTION_NAME}"'}
+    )
+    assert len(subscription)
+    activation_key = module_target_sat.api.ActivationKey(
+        content_view=cv,
+        organization=function_org,
+        environment=lce,
+        auto_attach=False,
+    ).create()
+    activation_key.add_subscriptions(data={'subscription_id': subscription[0].id})
+
+    with Broker(nick='rhel7', host_classes={'host': ContentHost}) as vm:
+        # Test auto-attach
+        module_target_sat.cli.Host.subscription_register(
+            {
+                'organization-id': function_org.id,
+                'content-view-id': content_view.id,
+                'lifecycle-environment-id': lce.id,
+                'name': vm.hostname,
+            }
+        )
+        host = module_target_sat.cli.Host.info({'name': vm.hostname})
+        vm.register_contenthost(org=function_org.name, activation_key=activation_key.name)
+        module_target_sat.cli.Host.subscription_auto_attach({'host-id': host['id']})
+        vm.enable_repo(REPOS['rhst7']['id'])
+        # ensure that katello agent can be installed
+        try:
+            vm.install_katello_agent()
+        except ContentHostError:
+            pytest.fail('ContentHostError raised unexpectedly!')
