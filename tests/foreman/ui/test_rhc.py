7c27030773411ca9493e8ec6a30be17bd60a96e2
Jameer Pathan <jameerpathan111@gmail.com>
2022-09-16 17:28:08 +0200

    address pr comments

--- tests/foreman/ui/test_rhc.py
+++ tests/foreman/ui/test_rhc.py
@@ -23,7 +23,6 @@ from wait_for import wait_for
 from robottelo import constants
 from robottelo.api.utils import enable_sync_redhat_repo
 from robottelo.config import settings
-from robottelo.constants import DEFAULT_SUBSCRIPTION_NAME
 from robottelo.logging import logger
 
 
@@ -66,23 +65,9 @@ def fixture_setup_rhc_satellite(request, module_target_sat, module_rhc_org):
         module_target_sat.cli.Subscription.upload(
             {'file': manifests_path, 'organization-id': module_rhc_org.id}
         )
-        rh_repo1 = {
-            'name': constants.REPOS['rhel8_aps']['name'],
-            'product': constants.PRDS['rhel8'],
-            'reposet': constants.REPOSET['rhel8_aps'],
-            'basearch': constants.DEFAULT_ARCHITECTURE,
-            'releasever': '8',
-        }
-        rh_repo2 = {
-            'name': constants.REPOS['rhel7']['name'],
-            'product': constants.PRDS['rhel'],
-            'reposet': constants.REPOSET['rhel7'],
-            'basearch': constants.DEFAULT_ARCHITECTURE,
-            'releasever': '7Server',
-        }
         # Enable and sync required repos
-        repo1_id = enable_sync_redhat_repo(rh_repo1, module_rhc_org.id)
-        repo2_id = enable_sync_redhat_repo(rh_repo2, module_rhc_org.id)
+        repo1_id = enable_sync_redhat_repo(constants.REPOS['rhel8_aps'], module_rhc_org.id)
+        repo2_id = enable_sync_redhat_repo(constants.REPOS['rhel7'], module_rhc_org.id)
         # Add repos to Content view
         content_view = module_target_sat.api.ContentView(
             organization=module_rhc_org, repository=[repo1_id, repo2_id]
@@ -98,10 +83,10 @@ def fixture_setup_rhc_satellite(request, module_target_sat, module_rhc_org):
         ).create()
         default_subscription = module_target_sat.api.Subscription(
             organization=module_rhc_org
-        ).search(query={'search': f'name="{DEFAULT_SUBSCRIPTION_NAME}"'})[0]
+        ).search(query={'search': f'name="{constants.DEFAULT_SUBSCRIPTION_NAME}"'})[0]
         ak.add_subscriptions(data={'quantity': 10, 'subscription_id': default_subscription.id})
         logger.debug(
-            f"Activation key: {ak} \n Content view: {content_view} \n Organization: {module_rhc_org}"
+            f"Activation key: {ak} \n CV: {content_view} \n Organization: {module_rhc_org}"
         )
 
 
