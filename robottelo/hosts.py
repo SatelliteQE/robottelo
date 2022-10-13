2438f9fcde38f3875b765f132348a92cecf0c8d1
Jake Callahan <jacob.callahan05@gmail.com>
2022-10-13 16:11:27 -0400

    Companion to Broker #170 This PR should be merged after the above Broker PR has been merged. This update's the version and the name of an attribute we need to keep.

--- robottelo/hosts.py
+++ robottelo/hosts.py
@@ -208,7 +208,7 @@ class ContentHost(Host, ContentHostMixins):
                 self.nailgun_host.delete()
             self.unregister()
         # Strip most unnecessary attributes from our instance for checkin
-        keep_keys = set(self.to_dict()) | {'release', '_prov_inst', '_cont_inst_p'}
+        keep_keys = set(self.to_dict()) | {'release', '_prov_inst', '_cont_inst'}
         self.__dict__ = {k: v for k, v in self.__dict__.items() if k in keep_keys}
         self.__class__ = Host
 
