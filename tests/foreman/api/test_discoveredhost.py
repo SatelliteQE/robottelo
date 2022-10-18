1d45461c148d0883fd699beb307b8f7432577497
Griffin Sullivan <gsulliva@redhat.com>
2022-10-18 11:32:15 -0400

    Removing vlan_networking and LibvirtDiscovery

--- tests/foreman/api/test_discoveredhost.py
+++ tests/foreman/api/test_discoveredhost.py
@@ -65,8 +65,6 @@ def _assert_discovered_host(host, channel=None, user_config=None):
     retrieved back
     Introduced a delay of 300secs by polling every 10 secs to get expected
     host
-
-    DEPRECATED: Will replace all tests using this function.
     """
     # assert that server receives DHCP discover from hosts PXELinux
     for pattern in [
