4b9feb18ff1ec65d1a370c882175ea79f06efa57
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2022-11-08 02:47:30 -0500

    [6.10.z] Companion to Broker #170 (#10244)
    Companion to Broker #170
    This PR should be merged after the above Broker PR has been merged.
    This update's the version and the name of an attribute we need to keep.
    
    Co-authored-by: Jake Callahan <jacob.callahan05@gmail.com>

--- robottelo/hosts.py
+++ robottelo/hosts.py
@@ -200,7 +200,7 @@ class ContentHost(Host):
             self.nailgun_host.delete()
         self.unregister()
         # Strip most unnecessary attributes from our instance for checkin
-        keep_keys = set(self.to_dict()) | {'release', '_prov_inst'}
+        keep_keys = set(self.to_dict()) | {'release', '_prov_inst', '_cont_inst'}
         self.__dict__ = {k: v for k, v in self.__dict__.items() if k in keep_keys}
         self.__class__ = Host
 
