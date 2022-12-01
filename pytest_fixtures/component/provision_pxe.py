66f9fc7b0f85740080d256425206e615c0c67380
Ondřej Gajdušek <ogajduse@redhat.com>
2022-12-01 10:49:53 +0100

    Fix pxeloader parametrization (#10273)
    * Attempt to fix pxeloader parametrization
    
    * Move pxe_loader_map from constants to fixture
    
    Co-authored-by: Gaurav Talreja <gauravtalreja1@gmail.com>
    
    Co-authored-by: Gaurav Talreja <gauravtalreja1@gmail.com>

--- pytest_fixtures/component/provision_pxe.py
+++ pytest_fixtures/component/provision_pxe.py
@@ -24,18 +24,13 @@ def module_provisioning_capsule(module_target_sat, module_location):
     return capsule.update(['location'])
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def module_provisioning_rhel_content(
     request,
     module_provisioning_sat,
-    module_provisioning_capsule,
     module_sca_manifest_org,
     module_lce_library,
     module_default_org_view,
-    module_location,
-    provisioning_host,
-    default_architecture,
-    default_partitiontable,
 ):
     """
     This fixture sets up kickstart repositories for a specific RHEL version
@@ -86,37 +81,7 @@ def module_provisioning_rhel_content(
         environment=module_lce_library,
     ).create()
 
-    host_root_pass = settings.provisioning.host_root_password
-    pxe_loader = 'PXELinux BIOS' if provisioning_host.vm_firmware == 'bios' else 'Grub2 UEFI'
-    hostgroup = sat.api.HostGroup(
-        organization=[module_sca_manifest_org],
-        location=[module_location],
-        architecture=default_architecture,
-        domain=module_provisioning_sat.domain,
-        content_source=module_provisioning_capsule.id,
-        content_view=module_default_org_view,
-        kickstart_repository=ksrepo,
-        lifecycle_environment=module_lce_library,
-        root_pass=host_root_pass,
-        operatingsystem=os,
-        ptable=default_partitiontable,
-        subnet=module_provisioning_sat.subnet,
-        pxe_loader=pxe_loader,
-        group_parameters_attributes=[
-            {
-                'name': 'remote_execution_ssh_keys',
-                'parameter_type': 'string',
-                'value': settings.provisioning.host_ssh_key_pub,
-            },
-            # assign AK in order the hosts to be subscribed
-            {
-                'name': 'kt_activation_keys',
-                'parameter_type': 'string',
-                'value': ak.name,
-            },
-        ],
-    ).create()
-    return Box(hostgroup=hostgroup, os=os, ksrepo=ksrepo, pxe=pxe_loader)
+    return Box(os=os, ak=ak, ksrepo=ksrepo)
 
 
 @pytest.fixture(scope='module')
@@ -200,10 +165,9 @@ def module_ssh_key_file():
 
 
 @pytest.fixture()
-def provisioning_host(module_ssh_key_file, request):
+def provisioning_host(module_ssh_key_file, pxe_loader):
     """Fixture to check out blank VM"""
     vlan_id = settings.provisioning.vlan_id
-    vm_firmware = getattr(request, 'param', 'bios')
     cd_iso = (
         ""  # TODO: Make this an optional fixture parameter (update vm_firmware when adding this)
     )
@@ -211,15 +175,68 @@ def provisioning_host(module_ssh_key_file, request):
         workflow="deploy-configure-pxe-provisioning-host-rhv",
         host_class=ContentHost,
         target_vlan_id=vlan_id,
-        target_vm_firmware=vm_firmware,
+        target_vm_firmware=pxe_loader.vm_firmware,
         target_vm_cd_iso=cd_iso,
         blank=True,
         target_memory='6GiB',
         auth=module_ssh_key_file,
     ) as prov_host:
-        yield Box(prov_host=prov_host, vm_firmware=vm_firmware)
+        yield prov_host
         # Set host as non-blank to run teardown of the host
         prov_host.blank = getattr(prov_host, 'blank', False)
+
+
+@pytest.fixture
+def provisioning_hostgroup(
+    module_provisioning_sat,
+    module_sca_manifest_org,
+    module_location,
+    default_architecture,
+    module_provisioning_rhel_content,
+    module_lce_library,
+    default_partitiontable,
+    module_default_org_view,
+    module_provisioning_capsule,
+    pxe_loader,
+):
+    return module_provisioning_sat.sat.api.HostGroup(
+        organization=[module_sca_manifest_org],
+        location=[module_location],
+        architecture=default_architecture,
+        domain=module_provisioning_sat.domain,
+        content_source=module_provisioning_capsule.id,
+        content_view=module_default_org_view,
+        kickstart_repository=module_provisioning_rhel_content.ksrepo,
+        lifecycle_environment=module_lce_library,
+        root_pass=settings.provisioning.host_root_password,
+        operatingsystem=module_provisioning_rhel_content.os,
+        ptable=default_partitiontable,
+        subnet=module_provisioning_sat.subnet,
+        pxe_loader=pxe_loader.pxe_loader,
+        group_parameters_attributes=[
+            {
+                'name': 'remote_execution_ssh_keys',
+                'parameter_type': 'string',
+                'value': settings.provisioning.host_ssh_key_pub,
+            },
+            # assign AK in order the hosts to be subscribed
+            {
+                'name': 'kt_activation_keys',
+                'parameter_type': 'string',
+                'value': module_provisioning_rhel_content.ak.name,
+            },
+        ],
+    ).create()
+
+
+@pytest.fixture
+def pxe_loader(request):
+    """Map the appropriate PXE loader to VM bootloader"""
+    PXE_LOADER_MAP = {
+        'bios': {'vm_firmware': 'bios', 'pxe_loader': 'PXELinux BIOS'},
+        'uefi': {'vm_firmware': 'uefi', 'pxe_loader': 'Grub2 UEFI'},
+    }
+    return Box(PXE_LOADER_MAP[getattr(request, 'param', 'bios')])
 
 
 @pytest.fixture()
