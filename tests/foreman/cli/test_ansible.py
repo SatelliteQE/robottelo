c7e022a47d44a6b7274cd22d2ffc3d7331c26ec7
Shweta Singh <shwsingh@redhat.com>
2023-01-16 21:18:20 +0530

    persistent ansible changes (#10525)

---
+++ tests/foreman/cli/test_ansible.py
@@ -0,0 +1,18 @@
+"""Test class for Ansible Roles and Variables pages
+
+:Requirement: Ansible
+
+:CaseAutomation: Automated
+
+:CaseLevel: Acceptance
+
+:CaseComponent: Ansible
+
+:Assignee: sbible
+
+:TestType: Functional
+
+:CaseImportance: High
+
+:Upstream: No
+"""
