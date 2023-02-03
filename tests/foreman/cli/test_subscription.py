d18e6bd5ee8355a9c2d13457472e6ce126498b3f
synkd <48261305+synkd@users.noreply.github.com>
2023-02-03 02:28:04 -0500

    Convert CLI tests to manifester (#10209)
    * Resolve merge conflict after #10232 was merged
    
    * Convert CLI content access tests to manifester
    
    * Remove fake_manifest skip marker from CLI content view test
    
    * Convert CLI errata tests to manifester
    
    * Convert CLI logging test to manifester
    
    * Convert CLI REX test to manifester
    
    * Convert CLI report template tests to manifester
    
    * Convert cli repository set tests to manifester
    
    Please note that `test_positive_list_available_repositories` is failing
    locally and in CI with the same error, which is unrelated to manifester.
    
    * Convert rh_cloud pytest fixtures to manifester
    
    This change converts the fixtures in
    pytest_fixtures/component/rh_cloud.py to manifester. This change should
    also convert cli/test_rhcloud_insights.py and
    cli/test_rhcloud_inventory.py to manifester.
    
    * Replace ad-hoc manifester usage with existing fixture
    
    * Convert CLI satellitesync tests to manifester
    
    This commit converts 5 inter-Satellite sync tests to obtain manifests
    via manifester. It also adds a second set of fixtures for adding
    function-scoped manifests. These are intended to be used when a single
    test requires more than one manifest.
    
    * Convert CLI subscription tests to manifester
    
    * Convert CLI syncplan tests to manifester
    
    * Convert CLI VM package installation tests to manifester
    
    * Replace ssh.put with target_sat.put
    
    This commit standardizes the tests changed in this PR on using
    target_sat.put to copy remote files rather than calling Robottelo's ssh
    module.
    
    * Resolve black and flake failures
    
    * Address PRT failures and add extra rhel manifest
    
    This commit resolves several errors from a recent PRT run. It also adds
    fixtures to create a manifest and organization with more RHEL
    entitlements than are provided by the existing `entitlement` manifest
    category, as having insufficient entitlement was a source of error in
    one test module.
    
    * Modify extra entitlement manifest fixture for 6.13
    
    Satellite 6.13 changes the way that organizations and manifests interact
    such that it is now required to explicitly disable simple content access
    either at or after organization creation time in order to use
    entitlement mode. This PR modifies the extra entitlement manifest fixture
    to account for this change.
    
    * Debug errata, sat sync, and activation key tests
    
    * Implement reviewer suggested change for local_ak fixture in report template test module
    
    * Address additional reviewer feedback
    
    * Remove  manifest fixtures
    
    * Address reviewer comments
    
    * Disable SCA before uploading manifest
    
    (cherry picked from commit 68aba5f021b6c8dbe16f9746f2accd27457502a7)

--- tests/foreman/cli/test_subscription.py
+++ tests/foreman/cli/test_subscription.py
@@ -31,39 +31,28 @@ from robottelo.cli.subscription import Subscription
 from robottelo.constants import PRDS
 from robottelo.constants import REPOS
 from robottelo.constants import REPOSET
-from robottelo.utils.manifest import clone
-from robottelo.utils.manifest import original_manifest
 
 pytestmark = [pytest.mark.run_in_one_thread]
 
 
 @pytest.fixture(scope='module')
-def golden_ticket_host_setup(request, module_org, module_target_sat):
-    with clone(name='golden_ticket') as manifest:
-        module_target_sat.upload_manifest(module_org.id, manifest.content)
-    new_product = make_product({'organization-id': module_org.id})
+def golden_ticket_host_setup(request, module_sca_manifest_org):
+    new_product = make_product({'organization-id': module_sca_manifest_org.id})
     new_repo = make_repository({'product-id': new_product['id']})
     Repository.synchronize({'id': new_repo['id']})
     new_ak = make_activation_key(
         {
             'lifecycle-environment': 'Library',
             'content-view': 'Default Organization View',
-            'organization-id': module_org.id,
+            'organization-id': module_sca_manifest_org.id,
             'auto-attach': False,
         }
     )
     return new_ak
 
 
-@pytest.fixture(scope='function')
-def manifest_clone_upload(function_org, target_sat):
-    with clone() as cloned_manifest:
-        target_sat.upload_manifest(function_org.id, cloned_manifest.content)
-        yield
-
-
 @pytest.mark.tier1
-def test_positive_manifest_upload(function_org, manifest_clone_upload):
+def test_positive_manifest_upload(function_entitlement_manifest_org):
     """upload manifest
 
     :id: e5a0e4f8-fed9-4896-87a0-ac33f6baa227
@@ -73,12 +62,12 @@ def test_positive_manifest_upload(function_org, manifest_clone_upload):
     :CaseImportance: Critical
     """
 
-    Subscription.list({'organization-id': function_org.id}, per_page=False)
+    Subscription.list({'organization-id': function_entitlement_manifest_org.id}, per_page=False)
 
 
 @pytest.mark.tier1
 @pytest.mark.upgrade
-def test_positive_manifest_delete(function_org, manifest_clone_upload):
+def test_positive_manifest_delete(function_entitlement_manifest_org):
     """Delete uploaded manifest
 
     :id: 01539c07-00d5-47e2-95eb-c0fd4f39090f
@@ -87,14 +76,14 @@ def test_positive_manifest_delete(function_org, manifest_clone_upload):
 
     :CaseImportance: Critical
     """
-    Subscription.list({'organization-id': function_org.id}, per_page=False)
-    Subscription.delete_manifest({'organization-id': function_org.id})
-    Subscription.list({'organization-id': function_org.id}, per_page=False)
+    Subscription.list({'organization-id': function_entitlement_manifest_org.id}, per_page=False)
+    Subscription.delete_manifest({'organization-id': function_entitlement_manifest_org.id})
+    Subscription.list({'organization-id': function_entitlement_manifest_org.id}, per_page=False)
 
 
 @pytest.mark.tier2
 @pytest.mark.upgrade
-def test_positive_enable_manifest_reposet(function_org, manifest_clone_upload):
+def test_positive_enable_manifest_reposet(function_entitlement_manifest_org):
     """enable repository set
 
     :id: cc0f8f40-5ea6-4fa7-8154-acdc2cb56b45
@@ -106,12 +95,12 @@ def test_positive_enable_manifest_reposet(function_org, manifest_clone_upload):
 
     :CaseImportance: Critical
     """
-    Subscription.list({'organization-id': function_org.id}, per_page=False)
+    Subscription.list({'organization-id': function_entitlement_manifest_org.id}, per_page=False)
     RepositorySet.enable(
         {
             'basearch': 'x86_64',
             'name': REPOSET['rhva6'],
-            'organization-id': function_org.id,
+            'organization-id': function_entitlement_manifest_org.id,
             'product': PRDS['rhel'],
             'releasever': '6Server',
         }
@@ -119,14 +108,14 @@ def test_positive_enable_manifest_reposet(function_org, manifest_clone_upload):
     Repository.synchronize(
         {
             'name': REPOS['rhva6']['name'],
-            'organization-id': function_org.id,
+            'organization-id': function_entitlement_manifest_org.id,
             'product': PRDS['rhel'],
         }
     )
 
 
 @pytest.mark.tier3
-def test_positive_manifest_history(function_org, manifest_clone_upload):
+def test_positive_manifest_history(function_entitlement_manifest_org):
     """upload manifest and check history
 
     :id: 000ab0a0-ec1b-497a-84ff-3969a965b52c
@@ -135,15 +124,15 @@ def test_positive_manifest_history(function_org, manifest_clone_upload):
 
     :CaseImportance: Medium
     """
-    Subscription.list({'organization-id': function_org.id}, per_page=None)
-    history = Subscription.manifest_history({'organization-id': function_org.id})
-    assert f'{function_org.name} file imported successfully.' in ''.join(history)
+    org = function_entitlement_manifest_org
+    Subscription.list({'organization-id': org.id}, per_page=None)
+    history = Subscription.manifest_history({'organization-id': org.id})
+    assert f'{org.name} file imported successfully.' in ''.join(history)
 
 
-@pytest.mark.skip('Skipping due to manifest refresh issues')
 @pytest.mark.tier1
 @pytest.mark.upgrade
-def test_positive_manifest_refresh(function_org, target_sat):
+def test_positive_manifest_refresh(function_entitlement_manifest_org):
     """upload manifest and refresh
 
     :id: 579bbbf7-11cf-4d78-a3b1-16d73bd4ca57
@@ -152,14 +141,13 @@ def test_positive_manifest_refresh(function_org, target_sat):
 
     :CaseImportance: Critical
     """
-    target_sat.upload_manifest(function_org.id, original_manifest().content)
-    Subscription.list({'organization-id': function_org.id}, per_page=False)
-    Subscription.refresh_manifest({'organization-id': function_org.id})
-    Subscription.delete_manifest({'organization-id': function_org.id})
+    Subscription.list({'organization-id': function_entitlement_manifest_org.id}, per_page=False)
+    Subscription.refresh_manifest({'organization-id': function_entitlement_manifest_org.id})
+    Subscription.delete_manifest({'organization-id': function_entitlement_manifest_org.id})
 
 
 @pytest.mark.tier2
-def test_positive_subscription_list(function_org, manifest_clone_upload):
+def test_positive_subscription_list(function_entitlement_manifest_org):
     """Verify that subscription list contains start and end date
 
     :id: 4861bcbc-785a-436d-98ce-14cfef7d6907
@@ -172,13 +160,15 @@ def test_positive_subscription_list(function_org, manifest_clone_upload):
 
     :CaseImportance: Medium
     """
-    subscription_list = Subscription.list({'organization-id': function_org.id}, per_page=False)
+    subscription_list = Subscription.list(
+        {'organization-id': function_entitlement_manifest_org.id}, per_page=False
+    )
     for column in ['start-date', 'end-date']:
         assert column in subscription_list[0].keys()
 
 
 @pytest.mark.tier2
-def test_positive_delete_manifest_as_another_user(target_sat):
+def test_positive_delete_manifest_as_another_user(target_sat, function_entitlement_manifest):
     """Verify that uploaded manifest if visible and deletable
         by a different user than the one who uploaded it
 
@@ -202,10 +192,9 @@ def test_positive_delete_manifest_as_another_user(target_sat):
         admin=True, password=user2_password, organization=[org], default_organization=org
     ).create()
     # use the first admin to upload a manifest
-    with clone() as manifest:
-        target_sat.put(manifest, manifest.filename)
+    target_sat.put(f'{function_entitlement_manifest.path}', f'{function_entitlement_manifest.name}')
     Subscription.with_user(username=user1.login, password=user1_password).upload(
-        {'file': manifest.filename, 'organization-id': org.id}
+        {'file': f'{function_entitlement_manifest.name}', 'organization-id': f'{org.id}'}
     )
     # try to search and delete the manifest with another admin
     Subscription.with_user(username=user2.login, password=user2_password).delete_manifest(
