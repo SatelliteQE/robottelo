660053c0db888608b5a5e785f891d792e21a8b3d
jyejare <jyejare@redhat.com>
2023-02-09 21:49:00 +0530

    Factory Satellite fixtures to ignore in upgrade tests

--- conftest.py
+++ conftest.py
@@ -17,6 +17,7 @@ pytest_plugins = [
     'pytest_plugins.rerun_rp.rerun_rp',
     'pytest_plugins.fspath_plugins',
     'pytest_plugins.fixture_collection',
+    'pytest_plugins.factory_collection',
     # Fixtures
     'pytest_fixtures.core.broker',
     'pytest_fixtures.core.contenthosts',
