c2f7e0e53b710f3970740bd01b77ef667397473c
Gaurav Talreja <gtalreja@redhat.com>
2023-03-06 19:15:16 +0530

    Fix infoblox plugin install tests (#10828)
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>

--- tests/foreman/destructive/test_infoblox.py
+++ tests/foreman/destructive/test_infoblox.py
@@ -16,21 +16,15 @@
 """
 import pytest
 
-from robottelo.hosts import get_sat_rhel_version
 from robottelo.utils.installer import InstallerCommand
 
-if get_sat_rhel_version().base_version == '7':
-    dhcp_isc_package = 'tfm-rubygem-smart_proxy_dhcp_remote_isc'
-    infoblox_package = 'tfm-rubygem-infoblox'
-    infoblox_dhcp_package = 'tfm-rubygem-smart_proxy_dhcp_infoblox'
-    infoblox_dns_package = 'tfm-rubygem-smart_proxy_dns_infoblox'
-else:
-    dhcp_isc_package = 'rubygem-smart_proxy_dhcp_remote_isc'
-    infoblox_package = 'rubygem-infoblox'
-    infoblox_dhcp_package = 'rubygem-smart_proxy_dhcp_infoblox'
-    infoblox_dns_package = 'rubygem-smart_proxy_dns_infoblox'
-
 pytestmark = pytest.mark.destructive
+
+dhcp_isc_package = 'rubygem-smart_proxy_dhcp_remote_isc'
+infoblox_package = 'rubygem-infoblox'
+infoblox_dhcp_package = 'rubygem-smart_proxy_dhcp_infoblox'
+infoblox_dns_package = 'rubygem-smart_proxy_dns_infoblox'
+
 params = [
     (
         'enable-foreman-proxy-plugin-dhcp-remote-isc',
@@ -78,10 +72,7 @@ def test_plugin_installation(target_sat, command_args, command_opts, rpm_command
 
     :BZ: 1994490, 2000237
     """
-    target_sat.download_repofile()
     target_sat.register_to_cdn()
-    installer_obj = InstallerCommand(command_args, **command_opts)
-    command_output = target_sat.execute(installer_obj.get_command())
-    assert 'Success!' in command_output.stdout
-    rpm_result = target_sat.execute(rpm_command)
-    assert rpm_result.status == 0
+    installer = target_sat.install(InstallerCommand(command_args, **command_opts))
+    assert 'Success!' in installer.stdout
+    assert target_sat.execute(rpm_command).status == 0
