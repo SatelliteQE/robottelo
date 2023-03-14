56a5cc242a45c5b8ef0b399d84120bf7d90a6d8d
David Moore <damoore@redhat.com>
2023-03-14 12:55:07 -0400

    assert HTTPError message
    added capfd fixture, for capturing the 500 error's display message

--- tests/foreman/api/test_contentcredentials.py
+++ tests/foreman/api/test_contentcredentials.py
@@ -232,7 +232,7 @@ def test_positive_delete(module_org):
 
 
 @pytest.mark.tier3
-def test_positive_block_delete_key_in_use(module_org, target_sat):
+def test_positive_block_delete_key_in_use(module_org, target_sat, capfd):
     """Create a GPG key with valid content. Create a new product and
         associated repository, assigning the GPG key to both. Attempt to delete the
         GPG key in use.
@@ -259,9 +259,13 @@ def test_positive_block_delete_key_in_use(module_org, target_sat):
     assert product.gpg_key.id == repo.gpg_key.id
 
     # Attempt to delete gpg in use
-    with pytest.raises(HTTPError) as e:
+    with pytest.raises(HTTPError) as err:
         gpg_key.delete()
-    assert '500' in str(e.value)
+
+    # Check for 500 error and display message
+    err_message = capfd.readouterr()[1]
+    assert '500' in str(err.value)
+    assert 'Cannot delete' in str(err_message)
 
     # Assert gpg matches unmodified copy
     assert gpg_key.id == gpg_copy.id
