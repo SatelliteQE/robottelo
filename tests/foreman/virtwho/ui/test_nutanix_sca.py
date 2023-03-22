7a36253922e2474bbaf4a5211b13110310ae2133
yanpliu <yanpliu@redhat.com>
2023-03-22 05:30:35 -0400

    put session create and delete to fixture

--- tests/foreman/virtwho/ui/test_nutanix_sca.py
+++ tests/foreman/virtwho/ui/test_nutanix_sca.py
@@ -42,12 +42,22 @@ def form_data(target_sat, module_sca_manifest_org):
     }
     return form
 
+@pytest.fixture()
+def virtwho_config(form_data, target_sat, session_sca):
+    name = gen_string('alpha')
+    form_data['name'] = name
+    with session_sca:
+        session_sca.virtwho_configure.create(form_data)
+        yield virtwho_config
+        session_sca.virtwho_configure.delete(name)
+        assert not session_sca.virtwho_configure.search(name)
+
 
 class TestVirtwhoConfigforNutanix:
     @pytest.mark.tier2
     @pytest.mark.parametrize('deploy_type', ['id', 'script'])
     def test_positive_deploy_configure_by_id_script(
-        self, module_sca_manifest_org, session_sca, form_data, deploy_type
+        self, module_sca_manifest_org, virtwho_config, session_sca, form_data, deploy_type
     ):
         """Verify configure created and deployed with id.
 
@@ -63,34 +73,28 @@ class TestVirtwhoConfigforNutanix:
 
         :CaseImportance: High
         """
-        name = gen_string('alpha')
-        form_data['name'] = name
-        with session_sca:
-            print(form_data)
-            session_sca.virtwho_configure.create(form_data)
-            values = session_sca.virtwho_configure.read(name)
-            if deploy_type == "id":
-                command = values['deploy']['command']
-                deploy_configure_by_command(
-                    command,
-                    form_data['hypervisor_type'],
-                    debug=True,
-                    org=module_sca_manifest_org.label,
-                )
-            elif deploy_type == "script":
-                script = values['deploy']['script']
-                deploy_configure_by_script(
-                    script,
-                    form_data['hypervisor_type'],
-                    debug=True,
-                    org=module_sca_manifest_org.label,
-                )
-            assert session_sca.virtwho_configure.search(name)[0]['Status'] == 'ok'
-            session_sca.virtwho_configure.delete(name)
-            assert not session_sca.virtwho_configure.search(name)
+        name = form_data['name']
+        values = session_sca.virtwho_configure.read(name)
+        if deploy_type == "id":
+            command = values['deploy']['command']
+            deploy_configure_by_command(
+                command,
+                form_data['hypervisor_type'],
+                debug=True,
+                org=module_sca_manifest_org.label,
+            )
+        elif deploy_type == "script":
+            script = values['deploy']['script']
+            deploy_configure_by_script(
+                script,
+                form_data['hypervisor_type'],
+                debug=True,
+                org=module_sca_manifest_org.label,
+            )
+        assert session_sca.virtwho_configure.search(name)[0]['Status'] == 'ok'
 
     @pytest.mark.tier2
-    def test_positive_hypervisor_id_option(self, module_sca_manifest_org, session_sca, form_data):
+    def test_positive_hypervisor_id_option(self, module_sca_manifest_org, virtwho_config, session_sca, form_data):
         """Verify Hypervisor ID dropdown options.
 
         :id: 8f5771bd-4b74-49a7-93bb-31eb8e467477
@@ -103,27 +107,22 @@ class TestVirtwhoConfigforNutanix:
 
         :CaseImportance: Medium
         """
-        name = gen_string('alpha')
-        form_data['name'] = name
-        with session_sca:
-            session_sca.virtwho_configure.create(form_data)
-            values = session_sca.virtwho_configure.read(name)
-            config_id = get_configure_id(name)
-            command = values['deploy']['command']
-            config_file = get_configure_file(config_id)
-            for value in ['uuid', 'hostname']:
-                session_sca.virtwho_configure.edit(name, {'hypervisor_id': value})
-                results = session_sca.virtwho_configure.read(name)
-                assert results['overview']['hypervisor_id'] == value
-                deploy_configure_by_command(
-                    command,
-                    form_data['hypervisor_type'],
-                    debug=True,
-                    org=module_sca_manifest_org.label,
-                )
-                assert get_configure_option('hypervisor_id', config_file) == value
-            session_sca.virtwho_configure.delete(name)
-            assert not session_sca.virtwho_configure.search(name)
+        name = form_data['name']
+        values = session_sca.virtwho_configure.read(name)
+        config_id = get_configure_id(name)
+        command = values['deploy']['command']
+        config_file = get_configure_file(config_id)
+        for value in ['uuid', 'hostname']:
+            session_sca.virtwho_configure.edit(name, {'hypervisor_id': value})
+            results = session_sca.virtwho_configure.read(name)
+            assert results['overview']['hypervisor_id'] == value
+            deploy_configure_by_command(
+                command,
+                form_data['hypervisor_type'],
+                debug=True,
+                org=module_sca_manifest_org.label,
+            )
+            assert get_configure_option('hypervisor_id', config_file) == value
 
     @pytest.mark.tier2
     @pytest.mark.parametrize('deploy_type', ['id', 'script'])
@@ -215,7 +214,7 @@ class TestVirtwhoConfigforNutanix:
 
     @pytest.mark.tier2
     def test_positive_ahv_internal_debug_option(
-        self, module_sca_manifest_org, session_sca, form_data
+        self, module_sca_manifest_org, virtwho_config, session_sca, form_data
     ):
         """Verify ahv_internal_debug option by hammer virt-who-config"
 
@@ -238,44 +237,41 @@ class TestVirtwhoConfigforNutanix:
 
         :customerscenario: true
         """
-        name = gen_string('alpha')
-        form_data['name'] = name
-        with session_sca:
-            session_sca.virtwho_configure.create(form_data)
-            config_id = get_configure_id(name)
-            values = session_sca.virtwho_configure.read(name)
-            command = values['deploy']['command']
-            config_file = get_configure_file(config_id)
-            deploy_configure_by_command(
-                command, form_data['hypervisor_type'], debug=True, org=module_sca_manifest_org.label
-            )
-            results = session_sca.virtwho_configure.read(name)
-            assert str(results['overview']['ahv_internal_debug']) == 'False'
-            # ahv_internal_debug does not set in virt-who-config-X.conf
-            option = 'ahv_internal_debug'
-            env_error = f"option {option} is not exist or not be enabled in {config_file}"
-            try:
-                get_configure_option("ahv_internal_debug", config_file)
-            except Exception as VirtWhoError:
-                assert env_error == str(VirtWhoError)
-            # check message exist in log file /var/log/rhsm/rhsm.log
-            message = 'Value for "ahv_internal_debug" not set, using default: False'
-            assert check_message_in_rhsm_log(message) == message
+        name = form_data['name']
+        config_id = get_configure_id(name)
+        values = session_sca.virtwho_configure.read(name)
+        command = values['deploy']['command']
+        config_file = get_configure_file(config_id)
+        deploy_configure_by_command(
+            command, form_data['hypervisor_type'], debug=True, org=module_sca_manifest_org.label
+        )
+        results = session_sca.virtwho_configure.read(name)
+        assert str(results['overview']['ahv_internal_debug']) == 'False'
+        # ahv_internal_debug does not set in virt-who-config-X.conf
+        option = 'ahv_internal_debug'
+        env_error = f"option {option} is not exist or not be enabled in {config_file}"
+        try:
+            get_configure_option("ahv_internal_debug", config_file)
+        except Exception as VirtWhoError:
+            assert env_error == str(VirtWhoError)
+        # check message exist in log file /var/log/rhsm/rhsm.log
+        message = 'Value for "ahv_internal_debug" not set, using default: False'
+        assert check_message_in_rhsm_log(message) == message
 
-            # Update ahv_internal_debug option to true
-            session_sca.virtwho_configure.edit(name, {'ahv_internal_debug': True})
-            results = session_sca.virtwho_configure.read(name)
-            command = results['deploy']['command']
-            assert str(results['overview']['ahv_internal_debug']) == 'True'
-            deploy_configure_by_command(
-                command, form_data['hypervisor_type'], debug=True, org=module_sca_manifest_org.label
-            )
-            assert (
-                get_hypervisor_ahv_mapping(form_data['hypervisor_type']) == 'Host UUID found for VM'
-            )
-            # ahv_internal_debug bas been set to true in virt-who-config-X.conf
-            config_file = get_configure_file(config_id)
-            assert get_configure_option("ahv_internal_debug", config_file) == 'true'
-            # check message does not exist in log file /var/log/rhsm/rhsm.log
-            message = 'Value for "ahv_internal_debug" not set, using default: False'
+        # Update ahv_internal_debug option to true
+        session_sca.virtwho_configure.edit(name, {'ahv_internal_debug': True})
+        results = session_sca.virtwho_configure.read(name)
+        command = results['deploy']['command']
+        assert str(results['overview']['ahv_internal_debug']) == 'True'
+        deploy_configure_by_command(
+            command, form_data['hypervisor_type'], debug=True, org=module_sca_manifest_org.label
+        )
+        assert (
+            get_hypervisor_ahv_mapping(form_data['hypervisor_type']) == 'Host UUID found for VM'
+        )
+        # ahv_internal_debug bas been set to true in virt-who-config-X.conf
+        config_file = get_configure_file(config_id)
+        assert get_configure_option("ahv_internal_debug", config_file) == 'true'
+        # check message does not exist in log file /var/log/rhsm/rhsm.log
+        message = 'Value for "ahv_internal_debug" not set, using default: False'
             assert str(check_message_in_rhsm_log(message)) == 'False'
