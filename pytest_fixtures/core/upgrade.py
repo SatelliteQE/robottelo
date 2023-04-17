ce9081d89116cf1e36cd61be489d6e35178543bd
Ondřej Gajdušek <ogajduse@redhat.com>
2023-04-17 14:01:24 +0200

    Remove unnecessary checks, add logging to pre_configured_capsule fixture

--- pytest_fixtures/core/upgrade.py
+++ pytest_fixtures/core/upgrade.py
@@ -2,6 +2,7 @@ import pytest
 from broker import Broker
 
 from robottelo.hosts import Capsule
+from robottelo.logging import logger
 
 
 @pytest.fixture(scope="function")
@@ -18,7 +19,7 @@ def dependent_scenario_name(request):
 
 
 @pytest.fixture(scope="session")
-def pre_configured_capsule(session_target_sat):
+def pre_configured_capsule(worker_id, session_target_sat):
     """
     This fixture returns the Capsule object matching the pre-configured capsule
 
@@ -27,18 +28,19 @@ def pre_configured_capsule(session_target_sat):
     """
     inventory = {h.hostname for h in Broker().from_inventory()}
     capsules = {h.name for h in session_target_sat.api.Capsule().search()}
+    logger.debug(
+        f'Hosts in the inventory: {inventory}. Capsules attached to Satellite: {capsules}.'
+    )
     # do an intersection of both sets to just select those hosts that we have in the capsules
     # from that intersection take the Capsule that is not Satellite and return it
     intersect = inventory.intersection(capsules)
     intersect.discard(session_target_sat.hostname)
+    logger.debug(f'Capsules found: {intersect}')
     assert len(intersect) == 1, "More than one Capsule found in the inventory"
     target_capsule = intersect.pop()
     hosts = Broker(host_class=Capsule).from_inventory(filter=f'hostname={target_capsule}')
     # hosts = Broker(host_class=Capsule).from_inventory(
     #     filter=f'@inv.hostname == "{target_capsule}"'
     # )  # broker 0.3.0 syntax
-    if not hosts:
-        raise Exception(f'No hosts found with hostname {target_capsule}')
-    if len(hosts) > 1:
-        raise Exception(f'Multiple hosts found with hostname {target_capsule}')
+    logger.info(f'xdist worker {worker_id} was assigned pre-configured Capsule {target_capsule}')
     return hosts[0]
