9c03a45f426599a38ad4c712d534f53fc05df5d6
Adarsh Dubey <addubey@redhat.com>
2023-05-19 20:46:03 +0530

    Close loop - Ansible job invocation shows wrong info after remote execution job
    Signed-off-by: Adarsh Dubey <addubey@redhat.com>

--- pytest_fixtures/core/contenthosts.py
+++ pytest_fixtures/core/contenthosts.py
@@ -89,6 +89,13 @@ def rhel6_contenthost(request):
         yield host
 
 
+@pytest.fixture(params=[{'rhel_version': '9'}])
+def rhel9_contenthost(request):
+    """A fixture that provides a rhel9 content host object"""
+    with Broker(**host_conf(request), host_class=ContentHost) as host:
+        yield host
+
+
 @pytest.fixture()
 def content_hosts(request):
     """A function-level fixture that provides two rhel content hosts object"""
