c0b0b7bba8a005f94525b69c5d60d2030f8a9501
synkd <48261305+synkd@users.noreply.github.com>
2023-06-21 07:03:49 -0400

    Convert remaining tests to manifester (#11669)
    * Convert remaining test to manifester
    
    This PR converts to manifester all tests using manifests to that are not
    contained in the API, CLI, or UI namespaces.
    
    * Convert CLI end-to-end test to manifester
    
    * Disable SCA in API end-to-end test
    
    * Convert longrun test to manifester

--- tests/foreman/endtoend/test_cli_endtoend.py
+++ tests/foreman/endtoend/test_cli_endtoend.py
@@ -40,7 +40,6 @@ from robottelo.cli.user import User
 from robottelo.config import setting_is_set
 from robottelo.config import settings
 from robottelo.constants.repos import CUSTOM_RPM_REPO
-from robottelo.utils.manifest import clone
 
 
 @pytest.fixture(scope='module')
@@ -93,7 +92,7 @@ def test_positive_cli_find_admin_user():
 @pytest.mark.e2e
 @pytest.mark.upgrade
 @pytest.mark.skipif((not settings.robottelo.REPOS_HOSTING_URL), reason='Missing repos_hosting_url')
-def test_positive_cli_end_to_end(fake_manifest_is_set, target_sat, rhel7_contenthost):
+def test_positive_cli_end_to_end(function_entitlement_manifest, target_sat, rhel7_contenthost):
     """Perform end to end smoke tests using RH and custom repos.
 
     1. Create a new user with admin permissions
@@ -131,12 +130,13 @@ def test_positive_cli_end_to_end(fake_manifest_is_set, target_sat, rhel7_content
 
     # step 2.1: Create a new organization
     org = _create(user, Org, {'name': gen_alphanumeric()})
+    target_sat.cli.SimpleContentAccess.disable({'organization-id': org['id']})
 
     # step 2.2: Clone and upload manifest
-    if fake_manifest_is_set:
-        with clone() as manifest:
-            target_sat.put(manifest, manifest.filename)
-        Subscription.upload({'file': manifest.filename, 'organization-id': org['id']})
+    target_sat.put(f'{function_entitlement_manifest.path}', f'{function_entitlement_manifest.name}')
+    Subscription.upload(
+        {'file': f'{function_entitlement_manifest.name}', 'organization-id': org['id']}
+    )
 
     # step 2.3: Create a new lifecycle environment
     lifecycle_environment = _create(
@@ -164,24 +164,23 @@ def test_positive_cli_end_to_end(fake_manifest_is_set, target_sat, rhel7_content
     repositories.append(custom_repo)
 
     # step 2.6: Enable a Red Hat repository
-    if fake_manifest_is_set:
-        RepositorySet.enable(
-            {
-                'basearch': 'x86_64',
-                'name': constants.REPOSET['rhst7'],
-                'organization-id': org['id'],
-                'product': constants.PRDS['rhel'],
-                'releasever': None,
-            }
-        )
-        rhel_repo = Repository.info(
-            {
-                'name': constants.REPOS['rhst7']['name'],
-                'organization-id': org['id'],
-                'product': constants.PRDS['rhel'],
-            }
-        )
-        repositories.append(rhel_repo)
+    RepositorySet.enable(
+        {
+            'basearch': 'x86_64',
+            'name': constants.REPOSET['rhst7'],
+            'organization-id': org['id'],
+            'product': constants.PRDS['rhel'],
+            'releasever': None,
+        }
+    )
+    rhel_repo = Repository.info(
+        {
+            'name': constants.REPOS['rhst7']['name'],
+            'organization-id': org['id'],
+            'product': constants.PRDS['rhel'],
+        }
+    )
+    repositories.append(rhel_repo)
 
     # step 2.7: Synchronize these two repositories
     for repo in repositories:
@@ -255,15 +254,14 @@ def test_positive_cli_end_to_end(fake_manifest_is_set, target_sat, rhel7_content
             )
 
     # step 2.13.1: Enable product content
-    if fake_manifest_is_set:
-        ActivationKey.with_user(user['login'], user['password']).content_override(
-            {
-                'content-label': constants.REPOS['rhst7']['id'],
-                'id': activation_key['id'],
-                'organization-id': org['id'],
-                'value': '1',
-            }
-        )
+    ActivationKey.with_user(user['login'], user['password']).content_override(
+        {
+            'content-label': constants.REPOS['rhst7']['id'],
+            'id': activation_key['id'],
+            'organization-id': org['id'],
+            'value': '1',
+        }
+    )
 
     # BONUS: Create a content host and associate it with promoted
     # content view and last lifecycle where it exists
