134cf13beb1eb169adb1e0a5146362e87f8bdae7
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2023-06-21 06:51:08 -0400

    [6.12.z] Add Closed loop BZ#2173199 (#11663)
    * Add Closed loop BZ#2173199 (#11608)
    
    * follow ohsnap repofile redirects
    
    * Add Closed loop BZ#2173199
    
    Signed-off-by: Shubham Ganar <shubhamsg123m@gmail.com>
    
    ---------
    
    Signed-off-by: Shubham Ganar <shubhamsg123m@gmail.com>
    Co-authored-by: Radek Mynar <rmynar@redhat.com>
    (cherry picked from commit 225098def7e70d600cbf68a1aa78930442dd4af7)
    
    * add skip bz
    
    ---------
    
    Co-authored-by: Shubham Ganar <67952129+shubhamsg199@users.noreply.github.com>
    Co-authored-by: Shubham Ganar <shubhamsg123m@gmail.com>

---
+++ robottelo/cli/bootdisk.py
@@ -0,0 +1,41 @@
+"""
+Usage::
+
+    hammer bootdisk [OPTIONS] SUBCOMMAND [ARG] ...
+
+Parameters::
+
+    SUBCOMMAND                    Subcommand
+    [ARG] ...                     Subcommand arguments
+
+Subcommands::
+
+    generic                       Download generic image
+    host                          Download host image
+    subnet                        Download subnet generic image
+"""
+from robottelo.cli.base import Base
+
+
+class Bootdisk(Base):
+    """Manipulates Bootdisk."""
+
+    command_base = 'bootdisk'
+
+    @classmethod
+    def generic(cls, options=None):
+        """Download generic image"""
+        cls.command_sub = 'generic'
+        return cls.execute(cls._construct_command(options), output_format='json')
+
+    @classmethod
+    def host(cls, options=None):
+        """Download host image"""
+        cls.command_sub = 'host'
+        return cls.execute(cls._construct_command(options), output_format='json')
+
+    @classmethod
+    def subnet(cls, options=None):
+        """Download subnet generic image"""
+        cls.command_sub = 'subnet'
+        return cls.execute(cls._construct_command(options), output_format='json')
---
+++ tests/foreman/cli/test_bootdisk.py
@@ -0,0 +1,93 @@
+"""Tests for BootdiskPlugin
+
+:Requirement: Bootdisk
+
+:CaseAutomation: Automated
+
+:CaseLevel: System
+
+:CaseComponent: BootdiskPlugin
+
+:Team: Rocket
+
+:TestType: Functional
+
+:CaseImportance: High
+
+:Upstream: No
+"""
+import pytest
+from fauxfactory import gen_mac
+from fauxfactory import gen_string
+
+from robottelo.config import settings
+from robottelo.constants import HTTPS_MEDIUM_URL
+
+
+@pytest.mark.skip_if_open("BZ:2216211")
+@pytest.mark.parametrize('module_sync_kickstart_content', [7, 8, 9], indirect=True)
+def test_positive_bootdisk_download_https(
+    module_location,
+    module_sync_kickstart_content,
+    module_provisioning_capsule,
+    module_target_sat,
+    module_sca_manifest_org,
+    module_default_org_view,
+    module_lce_library,
+    default_architecture,
+    default_partitiontable,
+):
+    """Verify bootdisk is able to download using https url media .
+
+    :id: ebced3cf-99e8-4ed6-a41d-d53789e90f8e
+
+    :steps:
+        1. Create a host with https url media
+        2. Download the full host bootdisk
+        3. Check if bootdisk is downloaded properly
+
+    :expectedresults: Full host bootdisk is downloaded properly
+
+    :BZ: 2173199, 2216211
+
+    :customerscenario: true
+
+    :parametrized: yes
+    """
+
+    macaddress = gen_mac(multicast=False)
+    capsule = module_target_sat.nailgun_smart_proxy
+    # create medium with https url
+    media = module_target_sat.cli_factory.make_medium(
+        {
+            'name': gen_string('alpha'),
+            'operatingsystem-ids': module_sync_kickstart_content.os.id,
+            'location-ids': module_provisioning_capsule.id,
+            'organization-ids': module_sca_manifest_org.id,
+            'path': HTTPS_MEDIUM_URL,
+            'os-family': 'Redhat',
+        }
+    )
+    host = module_target_sat.cli_factory.make_host(
+        {
+            'organization-id': module_sca_manifest_org.id,
+            'location-id': module_location.id,
+            'name': gen_string('alpha').lower(),
+            'mac': macaddress,
+            'operatingsystem-id': module_sync_kickstart_content.os.id,
+            'medium-id': media['id'],
+            'architecture-id': default_architecture.id,
+            'domain-id': module_sync_kickstart_content.domain.id,
+            'build': 'true',
+            'root-password': settings.provisioning.host_root_password,
+            'partition-table-id': default_partitiontable.id,
+            'content-source-id': capsule.id,
+            'content-view-id': module_default_org_view.id,
+            'lifecycle-environment-id': module_lce_library.id,
+        }
+    )
+    # Check if full-host bootdisk can be downloaded.
+    bootdisk = module_target_sat.cli.Bootdisk.host({'host-id': host['id'], 'full': 'true'})
+    assert 'Successfully downloaded host disk image' in bootdisk['message']
+    module_target_sat.api.Host(id=host.id).delete()
+    module_target_sat.api.Media(id=media['id']).delete()
