f65c95f4f19977093402a722cb2380bd625e1aee
vsedmik <46570670+vsedmik@users.noreply.github.com>
2023-06-23 20:59:27 +0200

    Add test for the Capsule sync deadlock issue (#10424)

--- robottelo/config/validators.py
+++ robottelo/config/validators.py
@@ -66,7 +66,6 @@ VALIDATORS = dict(
         Validator('bugzilla.api_key', must_exist=True),
     ],
     capsule=[
-        Validator('capsule.instance_name', must_exist=True),
         Validator('capsule.version.release', must_exist=True),
         Validator('capsule.version.source', must_exist=True),
         Validator('capsule.deploy_workflow', must_exist=True),
