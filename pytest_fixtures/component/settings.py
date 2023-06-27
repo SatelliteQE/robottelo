fee8388d8faa13bdc218ad0b554c947b8a85140b
Gaurav Talreja <gtalreja@redhat.com>
2023-06-27 13:57:43 +0530

    Remove redundant rhev provisioning test (#11737)
    * Remove redundant rhev provisioning test
    
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>
    
    * Cleanup host in rhevm when deleted from satellite
    
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>
    
    * Fix settings_update indirect parametrization to accept values
    
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>
    (cherry picked from commit 8697d09cd799f9b0bae10d1ec9e8e2cfff764dec)
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>
    
    ---------
    
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>

--- pytest_fixtures/component/settings.py
+++ pytest_fixtures/component/settings.py
@@ -1,19 +1,21 @@
 # Settings Fixtures
 import pytest
-from nailgun import entities
 
 
-@pytest.fixture(scope="function")
-def setting_update(request):
+@pytest.fixture()
+def setting_update(request, target_sat):
     """
     This fixture is used to create an object of the provided settings parameter that we use in
     each test case to update their attributes and once the test case gets completed it helps to
     restore their default value
     """
-    setting_object = entities.Setting().search(query={'search': f'name={request.param}'})[0]
+    key_val = request.param
+    setting, new_value = tuple(key_val.split('=')) if '=' in key_val else (key_val, None)
+    setting_object = target_sat.api.Setting().search(query={'search': f'name={setting}'})[0]
     default_setting_value = setting_object.value
-    if default_setting_value is None:
-        default_setting_value = ''
+    if new_value is not None:
+        setting_object.value = new_value
+        setting_object.update({'value'})
     yield setting_object
     setting_object.value = default_setting_value
     setting_object.update({'value'})
