d9959538feddda7f07c4759e3c8ce14df1eccf48
Cole Higgins <chiggins@redhat.com>
2023-07-06 15:39:23 -0400

    [6.14 RFE] Custom products disabled by default (#11380)
    * applying stash
    
    * added UI test for prt job
    
    * saving point for new host testing
    
    * latest changes
    
    * removed api tests and fixed ui
    
    * updated host call
    
    * updated comments and docstring
    
    * updated test to override status
    
    * final push for review
    
    * updated docstring
    
    * addressing comments 1
    
    * address comments for ak and quotes
    
    * adressing comments and updating tests
    
    * updated case level

---
+++ tests/foreman/cli/test_repositories.py
@@ -0,0 +1,47 @@
+"""Test module for Repositories CLI.
+
+:Requirement: Repository
+
+:CaseAutomation: Automated
+
+:CaseLevel: Component
+
+:CaseComponent: Repositories
+
+:team: Phoenix-content
+
+:TestType: Functional
+
+:CaseImportance: Critical
+
+:Upstream: No
+"""
+import pytest
+
+
+@pytest.mark.rhel_ver_match('[^6]')
+def test_positive_custom_products_disabled_by_default(
+    setup_content,
+    default_location,
+    rhel_contenthost,
+    target_sat,
+):
+    """Verify that custom products should be disabled by default for content hosts
+
+    :id: ba237e11-3b41-49e3-94b3-63e1f404d9e5
+
+    :steps:
+        1. Create custom product and upload repository
+        2. Attach to activation key
+        3. Register Host
+        4. Assert that custom proudcts are disabled by default
+
+    :expectedresults: Custom products should be disabled by default. "Enabled: 0"
+
+    :BZ: 1265120
+    """
+    ak, org, _ = setup_content
+    rhel_contenthost.register(org, default_location, ak.name, target_sat)
+    assert rhel_contenthost.subscribed
+    product_details = rhel_contenthost.run('subscription-manager repos --list')
+    assert 'Enabled:   0' in product_details.stdout
