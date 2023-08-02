4c87973ecb9e6fa6cccbddc07032c3501854e2f7
Gaurav Talreja <gtalreja@redhat.com>
2023-08-02 21:18:25 +0530

    Update scap tests to use deploy_flavor and Global Registration
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>

--- robottelo/constants/__init__.py
+++ robottelo/constants/__init__.py
@@ -1539,8 +1539,6 @@ LDAP_ATTR = {
     'mail': 'mail',
 }
 
-OSCAP_TARGET_CORES = 4
-OSCAP_TARGET_MEMORY = '16GiB'
 OSCAP_PERIOD = {'weekly': 'Weekly', 'monthly': 'Monthly', 'custom': 'Custom'}
 OSCAP_TAILORING_FILE = 'ssg-rhel7-ds-tailoring.xml'
 
