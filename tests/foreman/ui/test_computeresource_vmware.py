771151ad1426ecb602b551439c2e13e02d9d3871
Sam Bible <sbible@redhat.com>
2023-08-22 09:33:59 -0500

    Turn VM on if it's off, and check IP addr isn't none

--- tests/foreman/ui/test_computeresource_vmware.py
+++ tests/foreman/ui/test_computeresource_vmware.py
@@ -593,21 +593,25 @@ def test_positive_virt_card(
             module_vmware_settings['vm_name'],
         )
         host_name = module_vmware_settings['vm_name'] + '.' + domain.name
+        power_status = session.computeresource.vm_status(cr_name, module_vmware_settings['vm_name'])
+        if power_status is False:
+            session.computeresource.vm_poweron(cr_name, module_vmware_settings['vm_name'])
+            session.browser.refresh()
         virt_card = session.host_new.get_virtualization(host_name)['details']
         assert virt_card['datacenter'] == module_vmware_settings['datacenter']
         assert virt_card['cluster'] == module_vmware_settings['cluster']
         assert virt_card['memory'] == '2 GB'
-        assert 'public_ip_address' in virt_card
+        assert virt_card['public_ip_address'] != ''
         assert virt_card['mac_address'] == module_vmware_settings['mac_address']
         assert virt_card['cpus'] == '1'
         assert virt_card['disk_label'] == 'Hard disk 1'
-        assert 'disk_capacity' in virt_card
-        assert 'partition_capacity' in virt_card
+        assert virt_card['disk_capacity'] != ''
+        assert virt_card['partition_capacity'] != ''
         assert virt_card['partition_path'] == '/boot'
-        assert 'partition_allocation' in virt_card
+        assert virt_card['partition_allocation'] != ''
         assert virt_card['cores_per_socket'] == '1'
         assert virt_card['firmware'] == 'bios'
-        assert 'hypervisor' in virt_card
+        assert virt_card['hypervisor'] != ''
         assert virt_card['connection_state'] == 'connected'
         assert virt_card['overall_status'] == 'green'
         assert virt_card['annotation_notes'] == ''
