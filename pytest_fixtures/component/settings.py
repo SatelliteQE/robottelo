d58fb2dac3c31857e1b2ac9bd5fbdacdbc4e9d99
Gaurav Talreja <gtalreja@redhat.com>
2023-08-30 11:35:55 +0530

    Fix setting_update fixture to handle None value (#12414)
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>
    (cherry picked from commit 9decdeab64957407cc1a4c6db6b646c8764f1885)

--- pytest_fixtures/component/settings.py
+++ pytest_fixtures/component/settings.py
@@ -12,7 +12,7 @@ def setting_update(request, target_sat):
     key_val = request.param
     setting, new_value = tuple(key_val.split('=')) if '=' in key_val else (key_val, None)
     setting_object = target_sat.api.Setting().search(query={'search': f'name={setting}'})[0]
-    default_setting_value = setting_object.value
+    default_setting_value = '' if setting_object.value is None else setting_object.value
     if new_value is not None:
         setting_object.value = new_value
         setting_object.update({'value'})
