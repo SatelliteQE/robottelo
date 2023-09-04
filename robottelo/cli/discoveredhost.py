6b6d092235af4210e2c68b6232ae81c89528c94d
Adarsh Dubey <addubey@redhat.com>
2023-09-04 19:19:50 +0530

    Adding coverage for Discovery provisioning via CLI
    Signed-off-by: Adarsh Dubey <addubey@redhat.com>

--- robottelo/cli/discoveredhost.py
+++ robottelo/cli/discoveredhost.py
@@ -31,10 +31,28 @@ class DiscoveredHost(Base):
     def provision(cls, options=None):
         """Manually provision discovered host"""
         cls.command_sub = 'provision'
-        return cls.execute(cls._construct_command(options))
+        return cls.execute(cls._construct_command(options), output_format='csv')
 
     @classmethod
     def facts(cls, options=None):
         """Get all the facts associated with discovered host"""
         cls.command_sub = 'facts'
-        return cls.execute(cls._construct_command(options))
+        return cls.execute(cls._construct_command(options), output_format='csv')
+
+    @classmethod
+    def auto_provision(cls, options=None):
+        """Auto provision discovered host"""
+        cls.command_sub = 'auto-provision'
+        return cls.execute(cls._construct_command(options), output_format='csv')
+
+    @classmethod
+    def reboot(cls, options=None):
+        """Reboot discovered host"""
+        cls.command_sub = 'reboot'
+        return cls.execute(cls._construct_command(options), output_format='csv')
+
+    @classmethod
+    def refresh_facts(cls, options=None):
+        """Refresh facts associated with discovered host"""
+        cls.command_sub = 'refresh-facts'
+        return cls.execute(cls._construct_command(options), output_format='csv')
