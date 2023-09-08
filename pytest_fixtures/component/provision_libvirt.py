7e8d2623def8063a39c524f2e9c717ebf3da13c0
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2023-09-08 04:54:19 -0400

    [6.14.z] Fix libvirt provisioning tests to use new fixtures (#12458)
    Fix libvirt provisioning tests to use new fixtures (#12335)
    
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>
    (cherry picked from commit 3194afa0e302f10c0de551d0455d7b01bf991baa)
    
    Co-authored-by: Gaurav Talreja <gtalreja@redhat.com>

--- pytest_fixtures/component/provision_libvirt.py
+++ pytest_fixtures/component/provision_libvirt.py
@@ -12,3 +12,10 @@ def module_cr_libvirt(module_target_sat, module_org, module_location):
 @pytest.fixture(scope='module')
 def module_libvirt_image(module_target_sat, module_cr_libvirt):
     return module_target_sat.api.Image(compute_resource=module_cr_libvirt).create()
+
+
+@pytest.fixture(scope='module')
+def module_libvirt_provisioning_sat(module_provisioning_sat):
+    # Configure Libvirt CR for provisioning
+    module_provisioning_sat.sat.configure_libvirt_cr()
+    yield module_provisioning_sat
