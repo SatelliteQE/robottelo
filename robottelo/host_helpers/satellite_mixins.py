SatelliteQE/robottelo

commit b2f69d5e886161e98b264dfa0fe64bfb55c4f88a

Debug failover to cloned manifests (#12515)
This PR, in combination with
https://github.com/SatelliteQE/manifester/pull/21, addresses some of the
errors that have been occurring in CI in cases where Manifester times
out when exporting a manifest due to an upstream RHSM issue. These
changes should enable Robottelo to successfully fail over to using
cloned manifests in those cases.

robottelo/host_helpers/satellite_mixins.py
@@ -141,13 +141,13 @@ class ContentInfo:
         :returns: the manifest upload result
 
         """
-        if manifest is None:
+        if manifest.content is None:
             manifest = clone()
         if timeout is None:
             # Set the timeout to 1500 seconds to align with the API timeout.
             timeout = 1500000
         if interface == 'CLI':
-            if isinstance(manifest.content, (bytes, io.BytesIO)):
+            if hasattr(manifest, 'path'):
                 self.put(f'{manifest.path}', f'{manifest.name}')
                 result = self.cli.Subscription.upload(
                     {'file': manifest.name, 'organization-id': org_id}, timeout=timeout

robottelo/hosts.py
@@ -907,7 +907,7 @@ class ContentHost(Host, ContentHostMixins):
         If local_path is a manifest object, write its contents to a temporary file
         then continue with the upload.
         """
-        if 'utils.Manifest' in str(local_path):
+        if 'utils.manifest' in str(local_path):
             with NamedTemporaryFile(dir=robottelo_tmp_dir) as content_file:
                 content_file.write(local_path.content.read())
                 content_file.flush()

commit 839a1b6b99668c778552f02c7b32bf360e1dfc86

Fix regression from cloned manifest failover
This PR adds a check to resolve a regression in `upload_manifest`
introduced by https://github.com/SatelliteQE/robottelo/pull/12515.

robottelo/host_helpers/satellite_mixins.py
@@ -141,8 +141,9 @@ class ContentInfo:
         :returns: the manifest upload result
 
         """
-        if manifest.content is None:
-            manifest = clone()
+        if not isinstance(manifest, (bytes, io.BytesIO)):
+            if manifest.content is None:
+                manifest = clone()
         if timeout is None:
             # Set the timeout to 1500 seconds to align with the API timeout.
             timeout = 1500000