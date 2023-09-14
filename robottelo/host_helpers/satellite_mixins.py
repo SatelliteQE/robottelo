dbf2f47200ab42c9ea6066300b4629257fb39e0c
Ondřej Gajdušek <ogajduse@redhat.com>
2023-09-14 11:14:48 +0200

    Fix UP038 - non-pep604-isinstance

--- robottelo/host_helpers/satellite_mixins.py
+++ robottelo/host_helpers/satellite_mixins.py
@@ -141,7 +141,7 @@ class ContentInfo:
         :returns: the manifest upload result
 
         """
-        if not isinstance(manifest, (bytes, io.BytesIO)):
+        if not isinstance(manifest, bytes | io.BytesIO):
             if manifest.content is None:
                 manifest = clone()
         if timeout is None:
@@ -159,7 +159,7 @@ class ContentInfo:
                     {'file': manifest.filename, 'organization-id': org_id}, timeout=timeout
                 )
         else:
-            if not isinstance(manifest, (bytes, io.BytesIO)):
+            if not isinstance(manifest, bytes | io.BytesIO):
                 manifest = manifest.content
             result = self.api.Subscription().upload(
                 data={'organization_id': org_id}, files={'content': manifest}
