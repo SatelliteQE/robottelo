91494deefc4d5cce2e8dafe156d13acb30eae7da
Ladislav Vasina <lvasina@redhat.com>
2023-10-20 12:57:58 +0200

    Address comments and add class_sca_manifest

--- pytest_fixtures/component/taxonomy.py
+++ pytest_fixtures/component/taxonomy.py
@@ -103,6 +103,13 @@ def module_sca_manifest_org(module_org, module_sca_manifest, module_target_sat):
     return module_org
 
 
+@pytest.fixture(scope='class')
+def class_sca_manifest_org(class_org, class_sca_manifest, class_target_sat):
+    """Creates an organization and uploads an SCA mode manifest generated with manifester"""
+    class_target_sat.upload_manifest(class_org.id, class_sca_manifest.content)
+    return class_org
+
+
 @pytest.fixture(scope='module')
 def module_extra_rhel_entitlement_manifest_org(
     module_target_sat,
@@ -191,6 +198,14 @@ def module_extra_rhel_entitlement_manifest():
 
 @pytest.fixture(scope='module')
 def module_sca_manifest():
+    """Yields a manifest in Simple Content Access mode with subscriptions determined by the
+    `manifest_category.golden_ticket` setting in conf/manifest.yaml."""
+    with Manifester(manifest_category=settings.manifest.golden_ticket) as manifest:
+        yield manifest
+
+
+@pytest.fixture(scope='class')
+def class_sca_manifest():
     """Yields a manifest in Simple Content Access mode with subscriptions determined by the
     `manifest_category.golden_ticket` setting in conf/manifest.yaml."""
     with Manifester(manifest_category=settings.manifest.golden_ticket) as manifest:
