5e60b036da08626b9d0c75b1eae31aa61e82b791
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2023-10-26 05:28:17 -0400

    [6.12.z] [Upgrades] Add upgrade tests for ProvisioningTemplates (#12953)
    [Upgrades] Add upgrade tests for ProvisioningTemplates (#12897)
    
    Add upgrade tests for ProvisioningTemplates
    
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>
    (cherry picked from commit f9cec5efe795129617cfe6ae2f1ff0a2343e01e0)
    
    Co-authored-by: Gaurav Talreja <gtalreja@redhat.com>

--- pytest_fixtures/component/domain.py
+++ pytest_fixtures/component/domain.py
@@ -1,15 +1,15 @@
 # Domain Fixtures
-from nailgun import entities
 import pytest
 
 
 @pytest.fixture(scope='session')
 def default_domain(session_target_sat, default_smart_proxy):
     domain_name = session_target_sat.hostname.partition('.')[-1]
-    dom = entities.Domain().search(query={'search': f'name={domain_name}'})[0]
-    dom.dns = default_smart_proxy
-    dom.update(['dns'])
-    return entities.Domain(id=dom.id).read()
+    dom = session_target_sat.api.Domain().search(query={'search': f'name={domain_name}'})[0]
+    if 'dns' in session_target_sat.get_features():
+        dom.dns = default_smart_proxy
+        dom.update(['dns'])
+    return session_target_sat.api.Domain(id=dom.id).read()
 
 
 @pytest.fixture(scope='module')
