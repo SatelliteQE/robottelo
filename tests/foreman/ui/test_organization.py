f3232e68cc0d4ffce995a20906ee7eec11785463
Cole Higgins <chiggins@redhat.com>
2023-11-06 08:24:52 -0500

    Adding test for sca deprecation

--- tests/foreman/ui/test_organization.py
+++ tests/foreman/ui/test_organization.py
@@ -362,3 +362,24 @@ def test_positive_product_view_organization_switch(session, module_org, module_p
         assert session.product.search(module_product.name)
         session.organization.select(org_name="Default Organization")
         assert not session.product.search(module_product.name) == module_product.name
+
+
+def test_positive_prepare_for_sca_only_deprecation(target_sat):
+    """Verify that Simple Content Access endpoints are depreacated and will be required
+        for all organizations in Satellite 6.16
+
+    :id: 08539596-1bd3-4363-9737-e45f32ee5cbb
+
+    :expectedresults: Attepting to create an Organization with sca set to False, will throw
+        deprecation endpoint message
+    """
+    with target_sat.ui_session() as session:
+        session.organization.create(
+            {
+                'name': gen_string('alpha'),
+                'label': gen_string('alpha'),
+                'simple_content_access': False,
+            }
+        )
+        results = target_sat.execute('tail -100 /var/log/foreman/production.log').stdout
+    assert 'Simple Content Access will be required for all organizations in Katello 4.12' in results
