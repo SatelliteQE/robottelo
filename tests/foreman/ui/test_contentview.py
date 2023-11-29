42f72463932a4df804a63302dc4d33b2a786a486
Samuel Bible <sbible@redhat.com>
2023-11-29 08:28:14 -0600

    Whitespace

--- tests/foreman/ui/test_contentview.py
+++ tests/foreman/ui/test_contentview.py
@@ -19,9 +19,7 @@
 
 from fauxfactory import gen_string
 import pytest
-
 from robottelo.constants import PRDS, REPOS, REPOSET
-
 
 @pytest.mark.tier2
 def test_positive_create_cv(session, target_sat):
@@ -43,7 +41,6 @@ def test_positive_create_cv(session, target_sat):
     with target_sat.ui_session() as session:
         session.contentview_new.create(dict(name=cv))
         assert session.contentview_new.search(cv)[0]['Name'] == cv
-
 
 @pytest.mark.tier2
 def test_different_org_publish(
@@ -92,7 +89,6 @@ def test_different_org_publish(
         response = session.contentview_new.publish(cv.name)
         assert response[0]['Version'] == 'Version 2.0'
 
-        
 def test_no_blank_page_on_language_switch(session, target_sat, module_org):
     """Able to view the new CV UI when the language is set to something other
     than English
@@ -118,6 +114,7 @@ def test_no_blank_page_on_language_switch(session, target_sat, module_org):
         password=user_password,
         admin=True,
     ).create()
+    
     cv = target_sat.api.ContentView(organization=module_org).create()
     cv.publish()
     with target_sat.ui_session(user=user.login, password=user_password) as session:
