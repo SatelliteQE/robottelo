cd2ec331cff2025ad493c9901c2e70af1fd0ee0a
Adarsh Dubey <addubey@redhat.com>
2023-12-14 15:13:29 +0530

    mend

--- tests/foreman/ui/test_registration.py
+++ tests/foreman/ui/test_registration.py
@@ -18,6 +18,7 @@
 """
 from robottelo.utils.datafactory import gen_string
 
+
 def test_positive_verify_default_values_for_global_registration(
     session,
     target_sat,
@@ -43,8 +44,11 @@ def test_positive_verify_default_values_for_global_registration(
     with session:
         session.organization.select(org_name=default_org.name)
         session.location.select(loc_name=default_location.name)
-        cmd = session.host.get_register_command({'general.activation_keys': ak.name, 'general.operating_system': default_os.title}, full_read=True)
-    
+        cmd = session.host.get_register_command(
+            {'general.activation_keys': ak.name, 'general.operating_system': default_os.title},
+            full_read=True,
+        )
+
     assert cmd['general']['organization'] == 'Default Organization'
     assert cmd['general']['location'] == 'Default Location'
     assert cmd['advanced']['setup_rex'] == 'Inherit from host parameter (yes)'
@@ -52,6 +56,6 @@ def test_positive_verify_default_values_for_global_registration(
     assert cmd['advanced']['token_life_time'] == '4'
     assert cmd['advanced']['rex_pull_mode'] == 'Inherit from host parameter (no)'
     assert cmd['general']['capsule'] == 'Nothing to select.'
-    assert cmd['advanced']['update_packages'] == False
-    assert cmd['advanced']['ignore_error'] == False
-    assert cmd['advanced']['force'] == False
+    assert cmd['advanced']['update_packages'] is False
+    assert cmd['advanced']['ignore_error'] is False
+    assert cmd['advanced']['force'] is False
