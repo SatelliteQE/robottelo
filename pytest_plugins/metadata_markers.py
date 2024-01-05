24b28154a16980703ddf9d67c9c53d04e2fd29b4
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2024-01-05 10:08:24 -0500

    [6.14.z] Fixing the missing markers as independent properties (#13605)
    Fixing the missing markers as independent properties (#13599)
    
    (cherry picked from commit ae99d0d0fe2ceda3d35c6586788e1587ab3c5ff6)
    
    Co-authored-by: Jitendra Yejare <jyejare@redhat.com>

--- pytest_plugins/metadata_markers.py
+++ pytest_plugins/metadata_markers.py
@@ -119,12 +119,15 @@ def pytest_collection_modifyitems(items, config):
         markers_prop_data = []
         exclude_markers = ['parametrize', 'skipif', 'usefixtures', 'skip_if_not_set']
         for marker in item.iter_markers():
-            prop = marker.name
-            if prop in exclude_markers:
+            proprty = marker.name
+            if proprty in exclude_markers:
                 continue
             if marker_val := next(iter(marker.args), None):
-                prop = '='.join([prop, str(marker_val)])
-            markers_prop_data.append(prop)
+                proprty = '='.join([proprty, str(marker_val)])
+            markers_prop_data.append(proprty)
+            # Adding independent marker as a property
+            item.user_properties.append((marker.name, marker_val))
+        # Adding all markers as a single property
         item.user_properties.append(("markers", ", ".join(markers_prop_data)))
 
         # Version specific user properties
