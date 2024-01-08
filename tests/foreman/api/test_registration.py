0da2a3a54b370a3acfd2ce5ce0036eda5670adb9
Shweta Singh <shwsingh@redhat.com>
2024-01-08 23:00:28 +0530

    Update manifest fixture in registration test to resolve conflict (#13616)
    Update manifest fixture in test
    
    (cherry picked from commit fb3453a38a3b44fae39024487d7c7306fb41f08e)

--- tests/foreman/api/test_registration.py
+++ tests/foreman/api/test_registration.py
@@ -31,7 +31,7 @@ pytestmark = pytest.mark.tier1
 @pytest.mark.e2e
 @pytest.mark.no_containers
 def test_host_registration_end_to_end(
-    module_entitlement_manifest_org,
+    module_sca_manifest_org,
     module_location,
     module_activation_key,
     module_target_sat,
@@ -51,7 +51,7 @@ def test_host_registration_end_to_end(
 
     :customerscenario: true
     """
-    org = module_entitlement_manifest_org
+    org = module_sca_manifest_org
     command = module_target_sat.api.RegistrationCommand(
         organization=org,
         activation_keys=[module_activation_key.name],
