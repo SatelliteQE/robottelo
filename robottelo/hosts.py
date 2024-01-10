574663c514c8ccb8f356114ca5418960ef889598
Jacob Callahan <jacob.callahan05@gmail.com>
2024-01-10 10:23:38 -0500

    Removing changes associated with Bugbear rule B019
    This PR reverts the change from lru_cache to cachedmethod.
    The primary goal of the original change was to avoid memory leaks, due
    to lru_cache holding instances of a class alive longer than they should
    be.
    However, for our test framework, the workaround to avoid these memory
    leaks really aren't worth it.
    
    I also added an lru_cache to cli_factory.__getattr__, improving its
    recurring lookup time substantially.
    
    Finally, I corrected a type check that was erroneously converted in the
    initial pass.
    
    (cherry picked from commit 8f7fe941d1f474328d6862692b20f9ed5d2f1b14)

--- robottelo/hosts.py
+++ robottelo/hosts.py
@@ -16,7 +16,6 @@ from urllib.parse import urljoin, urlparse, urlunsplit
 from box import Box
 from broker import Broker
 from broker.hosts import Host
-from cachetools import cachedmethod
 from dynaconf.vendor.box.exceptions import BoxKeyError
 from fauxfactory import gen_alpha, gen_string
 from manifester import Manifester
@@ -2296,7 +2295,7 @@ class SSOHost(Host):
                 break
         return client_id
 
-    @cachedmethod
+    @lru_cache
     def get_rhsso_user_details(self, username):
         """Getter method to receive the user id"""
         result = self.execute(
@@ -2305,7 +2304,7 @@ class SSOHost(Host):
         result_json = json.loads(result.stdout)
         return result_json[0]
 
-    @cachedmethod
+    @lru_cache
     def get_rhsso_groups_details(self, group_name):
         """Getter method to receive the group id"""
         result = self.execute(f"{KEY_CLOAK_CLI} get groups -r {settings.rhsso.realm}")
