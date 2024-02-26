d745d07d48ca882b44d9c5bb58d69fb22673ba06
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2024-02-26 12:38:40 -0500

    [6.13.z] Force keyscan to use ipv4 (#14178)

--- tests/foreman/cli/test_templatesync.py
+++ tests/foreman/cli/test_templatesync.py
@@ -160,7 +160,7 @@ class TestTemplateSyncTestCase:
                 'repo': url,
                 'branch': git_branch,
                 'organization-id': module_org.id,
-                'filter': 'Atomic Kickstart default',
+                'filter': 'User - Registered Users',
                 'dirname': dirname,
             }
         ).split('\n')
