56205ae7172108981bb446a70052dbf68c1a200c
Lukas Hellebrandt <lhellebr@redhat.com>
2024-03-11 16:21:51 +0100

    Add test for Webpack 5 presence

---
+++ tests/foreman/sys/test_webpack.py
@@ -0,0 +1,30 @@
+"""Test class for Webpack
+
+:CaseAutomation: Automated
+
+:CaseComponent: Installation
+
+:Requirement: Installation
+
+:Team: Endeavour
+
+:CaseImportance: High
+
+"""
+import pytest
+
+
+@pytest.mark.tier2
+def test_positive_webpack5(target_sat):
+    """Check whether Webpack 5 was used at packaging time
+
+    :id: b7f3fbb2-ef4b-4634-877f-b8ea10373e04
+
+    :expectedresults: There is a file "public/webpack/foreman_tasks/foreman_tasks_remoteEntry.js" when Webpack 5 has been used. It used to be "public/webpack/foreman-tasks-<ID>.js" before.
+    """
+    assert (
+        target_sat.execute(
+            "find /usr/share/gems | grep public/webpack/foreman_tasks/foreman_tasks_remoteEntry.js"
+        ).status
+        == 0
+    )
