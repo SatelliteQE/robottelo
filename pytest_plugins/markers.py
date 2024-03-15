ed8330b9b10c3668f83a5474784482f25ef88d7b
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2024-03-15 15:55:48 -0400

    [6.13.z] Add posibility to filter tests that require manifester (#14416)
    Add posibility to filter tests that require manifester (#14270)
    
    (cherry picked from commit 0397a0d1db879dccd84a099e7ee0765df53dda61)
    
    Co-authored-by: dosas <dosas@users.noreply.github.com>

--- pytest_plugins/markers.py
+++ pytest_plugins/markers.py
@@ -23,6 +23,7 @@ def pytest_configure(config):
         "no_containers: Disable container hosts from being used in favor of VMs",
         "include_capsule: For satellite-maintain tests to run on Satellite and Capsule both",
         "capsule_only: For satellite-maintain tests to run only on Capsules",
+        "manifester: Tests that require manifester",
     ]
     markers.extend(module_markers())
     for marker in markers:
