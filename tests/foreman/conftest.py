0397a0d1db879dccd84a099e7ee0765df53dda61
dosas <dosas@users.noreply.github.com>
2024-03-15 08:35:48 +0100

    Add posibility to filter tests that require manifester (#14270)

--- tests/foreman/conftest.py
+++ tests/foreman/conftest.py
@@ -30,6 +30,8 @@ def pytest_collection_modifyitems(session, items, config):
     deselected_items = []
 
     for item in items:
+        if any("manifest" in f for f in getattr(item, "fixturenames", ())):
+            item.add_marker("manifester")
         # 1. Deselect tests marked with @pytest.mark.deselect
         # WONTFIX BZs makes test to be dynamically marked as deselect.
         deselect = item.get_closest_marker('deselect')
