9bd99a910dcaf8b8a701f76ff7b4fa98c78def21
Shweta Singh <shwsingh@redhat.com>
2024-05-08 19:38:12 +0530

    Mark UI tests as end-to-end for Rocket Team Components (#14702)

--- tests/foreman/ui/test_operatingsystem.py
+++ tests/foreman/ui/test_operatingsystem.py
@@ -18,6 +18,7 @@ from robottelo.constants import HASH_TYPE
 from robottelo.utils.datafactory import gen_string
 
 
+@pytest.mark.e2e
 @pytest.mark.tier2
 def test_positive_end_to_end(session, module_org, module_location, target_sat):
     """Create all possible entities that required for operating system and then
