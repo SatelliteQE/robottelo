bf6dac83a1e700eea9d5dcb7e657e817304829ca
Tasos Papaioannou <tpapaioa@redhat.com>
2024-05-21 13:33:14 -0400

    Remove nailgun.entities imports in pytest_fixtures (#15084)
    (cherry picked from commit 1e952b13f3ed098bf8063c52703e8a4065d79619)

--- pytest_fixtures/component/hostgroup.py
+++ pytest_fixtures/component/hostgroup.py
@@ -1,5 +1,4 @@
 # Hostgroup Fixtures
-from nailgun import entities
 import pytest
 from requests.exceptions import HTTPError
 
@@ -7,14 +6,16 @@ from robottelo.logging import logger
 
 
 @pytest.fixture(scope='module')
-def module_hostgroup():
-    return entities.HostGroup().create()
+def module_hostgroup(module_target_sat):
+    return module_target_sat.api.HostGroup().create()
 
 
 @pytest.fixture(scope='class')
-def class_hostgroup(class_org, class_location):
+def class_hostgroup(class_target_sat, class_org, class_location):
     """Create a hostgroup linked to specific org and location created at the class scope"""
-    hostgroup = entities.HostGroup(organization=[class_org], location=[class_location]).create()
+    hostgroup = class_target_sat.api.HostGroup(
+        organization=[class_org], location=[class_location]
+    ).create()
     yield hostgroup
     try:
         hostgroup.delete()
