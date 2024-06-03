b4f994b198f30d14107499385d8882c654292cba
Peter Ondrejka <pondrejk@users.noreply.github.com>
2024-06-03 08:43:29 +0200

    eol version check (#13253)
    (cherry picked from commit e65bda0b00a5bd9d092b1469671ec0abe1fc77a7)

--- robottelo/constants/__init__.py
+++ robottelo/constants/__init__.py
@@ -2062,6 +2062,7 @@ WEBHOOK_METHODS = [
     "DELETE",
     "PATCH",
 ]
+LIFECYCLE_METADATA_FILE = '/usr/share/satellite/lifecycle-metadata.yml'
 
 OPENSSH_RECOMMENDATION = 'Decreased security: OpenSSH config permissions'
 DNF_RECOMMENDATION = (
