e08b9084605785f474b6999c9ccf647f1b0b17ca
Vladimir Sedmik <vsedmik@redhat.com>
2024-06-07 09:23:06 +0200

    Remove old skip_if for closed BZs

--- tests/foreman/api/test_reporttemplates.py
+++ tests/foreman/api/test_reporttemplates.py
@@ -21,7 +21,6 @@ from wait_for import wait_for
 from robottelo.constants import DEFAULT_SUBSCRIPTION_NAME, PRDS, REPOS, REPOSET
 from robottelo.hosts import ContentHost
 from robottelo.utils.datafactory import parametrized, valid_data_list
-from robottelo.utils.issue_handlers import is_open
 
 
 @pytest.fixture(scope='module')
@@ -279,14 +278,13 @@ def test_positive_lock_clone_nodelete_unlock_report(target_sat):
     assert template_clone_name == cloned_rt.name
     assert template1 == cloned_rt.template
     # 4. Try to delete template
-    if not is_open('BZ:1680458'):
-        with pytest.raises(HTTPError):
-            rt.delete()
-        # In BZ1680458, exception is thrown but template is deleted anyway
-        assert (
-            len(target_sat.api.ReportTemplate().search(query={'search': f'name="{template_name}"'}))
-            != 0
-        )
+    with pytest.raises(HTTPError):
+        rt.delete()
+    # In BZ1680458, exception is thrown but template is deleted anyway
+    assert (
+        len(target_sat.api.ReportTemplate().search(query={'search': f'name="{template_name}"'}))
+        != 0
+    )
     # 5. Try to edit template
     with pytest.raises(HTTPError):
         target_sat.api.ReportTemplate(id=rt.id, template=template2).update(["template"])
