bc83c264f8a6c5c60e49a9df65488e8d9c60fb4d
Jake Callahan <jacob.callahan05@gmail.com>
2024-06-11 02:51:04 -0400

    [6.15.z] Cherry-pick: Fix ssh command usage (#14703) (#15387)
    Fix ssh command usage (#14703)
    
    Co-authored-by: Tasos Papaioannou <tpapaioa@redhat.com>

--- robottelo/cli/base.py
+++ robottelo/cli/base.py
@@ -201,7 +201,7 @@ class Base:
             command,
         )
         response = ssh.command(
-            cmd.encode('utf-8'),
+            cmd,
             hostname=hostname or cls.hostname or settings.server.hostname,
             output_format=output_format,
             timeout=timeout,
