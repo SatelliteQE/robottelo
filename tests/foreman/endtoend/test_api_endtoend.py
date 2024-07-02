d168a079c67dca80be14661646f27c027edd4182
shwsingh <shwsingh@redhat.com>
2024-07-02 14:14:02 +0530

    Update API endpoint for host facts

--- tests/foreman/endtoend/test_api_endtoend.py
+++ tests/foreman/endtoend/test_api_endtoend.py
@@ -354,8 +354,10 @@ API_PATHS = {
         '/api/usergroups/:usergroup_id/external_usergroups/:id',
         '/api/usergroups/:usergroup_id/external_usergroups/:id/refresh',
     ),
-    'fact_values': ('/api/fact_values',),
-    'host_facts': ('/api/hosts/:host_id/facts',),
+    'fact_values': (
+        '/api/fact_values',
+        '/api/hosts/:host_id/facts',
+    ),
     'file_units': ('/katello/api/files', '/katello/api/files/compare', '/katello/api/files/:id'),
     'filters': (
         '/api/filters',
