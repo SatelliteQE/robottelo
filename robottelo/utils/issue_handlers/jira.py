fcb42b9a7de05c646cc753f3157bcf8c35aaba42
jyejare <jyejare@redhat.com>
2024-08-08 20:26:47 +0530

    Jira module restructured and dynamicitic  improvements

--- robottelo/utils/issue_handlers/jira.py
+++ robottelo/utils/issue_handlers/jira.py
@@ -20,6 +20,23 @@ from robottelo.logging import logger
 # The .version group being a `d.d` string that can be casted to Version()
 VERSION_RE = re.compile(r'(?:sat-)*?(?P<version>\d\.\d)\.\w*')
 
+mapped_response_fields = {
+    'key': "{obj_name}['key']",
+    'summary': "{obj_name}['fields']['summary']",
+    'status': "{obj_name}['fields']['status']['name']",
+    'labels': "{obj_name}['fields']['labels']",
+    'resolution': "{obj_name}['fields']['resolution']['name'] if {obj_name}['fields']['resolution'] else ''",
+    'fixVersions': "[ver['name'] for ver in {obj_name}['fields']['fixVersions']] if {obj_name}['fields']['fixVersions'] else []",
+    # Custom Field - SFDC Cases Counter
+    'customfield_12313440': "{obj_name}['fields']['customfield_12313440']",
+}
+
+
+def sanitized_issue_data(issue, out_fields):
+    return {
+        field: eval(mapped_response_fields[field].format(obj_name='issue')) for field in out_fields
+    }
+
 
 def is_open_jira(issue_id, data=None):
     """Check if specific Jira is open consulting a cached `data` dict or
@@ -129,10 +146,12 @@ def collect_data_jira(collected_data, cached_data):  # pragma: no cover
         collected_data {dict} -- dict with Jira issues collected by pytest
         cached_data {dict} -- Cached data previous loaded from API
     """
+    jira_fields = ['key', 'summary', 'status', 'labels', 'resolution', 'fixVersions']
     jira_data = (
         get_data_jira(
             [item for item in collected_data if item.startswith('SAT-')],
             cached_data=cached_data,
+            jira_fields=jira_fields,
         )
         or []
     )
@@ -165,26 +184,24 @@ def collect_dupes(jira, collected_data, cached_data=None):  # pragma: no cover
 CACHED_RESPONSES = defaultdict(dict)
 
 
-@retry(stop=stop_after_attempt(4), wait=wait_fixed(15))
-def get_jira_issue(id, fields=None):
-    jql = f'id = {id}'
-    response = requests.get(
-        f"{settings.jira.url}/rest/api/latest/search/",
-        params={
-            "jql": jql,
-            # "fields": ",".join(jira_fields),
-        },
-        headers={"Authorization": f"Bearer {settings.jira.api_key}"},
-    )
-    response.raise_for_status()
-    return response.json()
-
-
 @retry(
     stop=stop_after_attempt(4),  # Retry 3 times before raising
     wait=wait_fixed(20),  # Wait seconds between retries
 )
-def get_data_jira(issue_ids, cached_data=None):  # pragma: no cover
+def get_jira(jql, fields=None):
+    params = {"jql": jql}
+    if fields:
+        params.update({"fields": ",".join(fields)})
+    response = requests.get(
+        f"{settings.jira.url}/rest/api/latest/search/",
+        params=params,
+        headers={"Authorization": f"Bearer {settings.jira.api_key}"},
+    )
+    response.raise_for_status()
+    return response
+
+
+def get_data_jira(issue_ids, cached_data=None, jira_fields=None):  # pragma: no cover
     """Get a list of marked Jira data and query Jira REST API.
 
     Arguments:
@@ -219,47 +236,18 @@ def get_data_jira(issue_ids, cached_data=None):  # pragma: no cover
 
     # No cached data so Call Jira API
     logger.debug(f"Calling Jira API for {set(issue_ids)}")
-    jira_fields = [
-        "key",
-        "summary",
-        "status",
-        "labels",
-        "resolution",
-        "fixVersions",
-    ]
     # Following fields are dynamically calculated/loaded
     for field in ('is_open', 'version'):
         assert field not in jira_fields
 
     # Generate jql
+    if isinstance(issue_ids, str):
+        issue_ids = [issue_id.strip() for issue_id in issue_ids.split(',')]
     jql = ' OR '.join([f"id = {issue_id}" for issue_id in issue_ids])
-    response = requests.get(
-        f"{settings.jira.url}/rest/api/latest/search/",
-        params={
-            "jql": jql,
-            "fields": ",".join(jira_fields),
-        },
-        headers={"Authorization": f"Bearer {settings.jira.api_key}"},
-    )
-    response.raise_for_status()
+    response = get_jira(jql, jira_fields)
     data = response.json().get('issues')
     # Clean the data, only keep the required info.
-    data = [
-        {
-            'key': issue['key'],
-            'summary': issue['fields']['summary'],
-            'status': issue['fields']['status']['name'],
-            'labels': issue['fields']['labels'],
-            'resolution': issue['fields']['resolution']['name']
-            if issue['fields']['resolution']
-            else '',
-            'fixVersions': [ver['name'] for ver in issue['fields']['fixVersions']]
-            if issue['fields']['fixVersions']
-            else [],
-        }
-        for issue in data
-        if issue is not None
-    ]
+    data = [sanitized_issue_data(issue, jira_fields) for issue in data if issue is not None]
     CACHED_RESPONSES['get_data'][str(sorted(issue_ids))] = data
     return data
 
@@ -272,7 +260,8 @@ def get_single_jira(issue_id, cached_data=None):  # pragma: no cover
         try:
             jira_data = cached_data[f"{issue_id}"]['data']
         except (KeyError, TypeError):
-            jira_data = get_data_jira([str(issue_id)], cached_data)
+            jira_fields = ['key', 'summary', 'status', 'labels', 'resolution', 'fixVersions']
+            jira_data = get_data_jira([str(issue_id)], cached_data, jira_fields=jira_fields)
             jira_data = jira_data and jira_data[0]
         CACHED_RESPONSES['get_single'][issue_id] = jira_data
     return jira_data or get_default_jira(issue_id)
