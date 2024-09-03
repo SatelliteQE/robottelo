7686bdd4685dcfc109fe1e69b959e089d863a553
Cole Higgins <chiggins@redhat.com>
2024-09-03 09:46:06 -0400

    Update results to json for prt

--- tests/foreman/ui/test_reporttemplates.py
+++ tests/foreman/ui/test_reporttemplates.py
@@ -241,7 +241,7 @@ def test_positive_generate_registered_hosts_report(
     :expectedresults: The Host - Registered Content Hosts report is generated
                       and it contains created host with correct data
 
-    :CaseImportance: High
+    :CaseImportance: High 
     """
     client = rhel_contenthost
     org, ak, _, _ = module_setup_content
@@ -610,16 +610,18 @@ def test_positive_installable_errata_with_user(
     assert rhel_contenthost.execute(f'yum downgrade -y {FAKE_1_CUSTOM_PACKAGE}').status == 0
     # Generate the report
     with session:
-        result_html = session.reporttemplate.generate(
+        result_json = session.reporttemplate.generate(
             'Host - Available Errata',
-            values={'output_format': 'HTML', 'installability': 'installable'},
+            values={'output_format': 'JSON', 'installability': 'installable'},
         )
-    with open(result_html) as html_file:
-        parser = etree.HTMLParser()
-        tree = etree.parse(html_file, parser)
-        tree_result = etree.tostring(tree.getroot(), pretty_print=True, method='html').decode()
-    
+    # with open(result_html) as html_file:
+    #     parser = etree.HTMLParser()
+    #     tree = etree.parse(html_file, parser)
+    #     tree_result = etree.tostring(tree.getroot(), pretty_print=True, method='html').decode()
+        with open(result_json) as json_file:
+            data_json = json.load(json_file)
+        assert FAKE_1_CUSTOM_PACKAGE_NAME in data_json
     # with open(result_json) as json_file:
-    #     data_json = json.load(json_file)
-    assert FAKE_1_CUSTOM_PACKAGE_NAME in tree_result
-    assert ERRATUM_ID in tree_result
+    # #     data_json = json.load(json_file)
+    # assert FAKE_1_CUSTOM_PACKAGE_NAME in tree_result
+    # assert ERRATUM_ID in tree_result
