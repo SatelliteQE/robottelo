3afea68c1b393b2d53e07f3eb2268c9b8a50761a
Jameer Pathan <21165044+jameerpathan111@users.noreply.github.com>
2024-09-16 10:58:10 +0200

    Remove unused code related to is_open bz (#16347)
    * Remove unused code related to is_open bz
    
    * Remove skip_if_open marker from tests

--- tests/foreman/api/test_capsulecontent.py
+++ tests/foreman/api/test_capsulecontent.py
@@ -205,7 +205,6 @@ class TestCapsuleContentManagement:
         assert "sha1" in checksum_types
         assert "sha256" not in checksum_types
 
-    @pytest.mark.skip_if_open("BZ:2025494")
     @pytest.mark.e2e
     @pytest.mark.tier4
     @pytest.mark.skip_if_not_set('capsule')
@@ -720,7 +719,6 @@ class TestCapsuleContentManagement:
         caps_files = get_repo_files_by_url(caps_repo_url)
         assert len(caps_files) == packages_count
 
-    @pytest.mark.skip_if_open("BZ:2122780")
     @pytest.mark.tier4
     @pytest.mark.skip_if_not_set('capsule', 'fake_manifest')
     def test_positive_capsule_pub_url_accessible(self, module_capsule_configured):
