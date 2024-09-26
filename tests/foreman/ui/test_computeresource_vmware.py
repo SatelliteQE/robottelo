c2ed2f1a5c4b6902e636c2f4708aa4208a66dde1
amol patil <amolpati@redhat.com>
2024-09-26 18:27:50 +0530

    fix in CR for import ansible role

--- tests/foreman/ui/test_computeresource_vmware.py
+++ tests/foreman/ui/test_computeresource_vmware.py
@@ -607,9 +607,9 @@ def test_positive_provision_end_to_end(
             'network': f'VLAN {settings.provisioning.vlan_id}',
         }
     }
+    proxy_id = target_sat.nailgun_smart_proxy.id
+    target_sat.api.AnsibleRoles().sync(data={'proxy_id': proxy_id, 'role_names': [SELECTED_ROLE]})
     with target_sat.ui_session() as session:
-        session.ansibleroles.import_all_roles()
-        assert session.ansibleroles.import_all_roles() == session.ansibleroles.imported_roles_count
         session.location.select(module_location.name)
         session.organization.select(module_sca_manifest_org.name)
         session.hostgroup.assign_role_to_hostgroup(
@@ -649,7 +649,9 @@ def test_positive_provision_end_to_end(
             silent_failure=True,
             handle_exception=True,
         )
-        values = session.host_new.get_host_statuses(host_name)
+        values = session.host_new.get_host_statuses(
+            f'{host_name}.{module_vmware_hostgroup.domain.read().name}'
+        )
         assert values['Build']['Status'] == 'Installed'
         assert values['Execution']['Status'] == 'Last execution succeeded'
 
