7adee81b892968881602e7e780c2bbb8ca9df83a
vijaysawant <vijaysawant36@gmail.com>
2024-12-24 20:58:40 +0530

    update ak with cv and env to get rid out of error

--- tests/foreman/api/test_docker.py
+++ tests/foreman/api/test_docker.py
@@ -545,8 +545,8 @@ class TestDockerActivationKey:
             content_view=content_view, environment=module_lce, organization=module_org
         ).create()
         assert ak.content_view.id == content_view.id
-        ak.content_view = None
-        assert ak.update(['content_view']).content_view is None
+        ak.content_view_environments = None
+        assert ak.update(['content_view_environments']).content_view is None
 
     @pytest.mark.tier2
     def test_positive_add_docker_repo_ccv(
@@ -610,8 +610,8 @@ class TestDockerActivationKey:
             content_view=comp_content_view, environment=module_lce, organization=module_org
         ).create()
         assert ak.content_view.id == comp_content_view.id
-        ak.content_view = None
-        assert ak.update(['content_view']).content_view is None
+        ak.content_view_environments = None
+        assert ak.update(['content_view_environments']).content_view is None
 
 
 class TestPodman:
