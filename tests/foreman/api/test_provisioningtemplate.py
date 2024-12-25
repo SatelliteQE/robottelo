f3cd056a3258481953aaa6e01109311804282e26
Shubham Ganar <67952129+shubhamsg199@users.noreply.github.com>
2024-12-25 19:18:58 +0530

    Add RHEL10 support for Provisioning tests (#17192)
    * Bump ruff from 0.8.3 to 0.8.4 (#17209)
    
    Signed-off-by: Shubham Ganar <sganar@redhat.com>
    
    * Fix condition for skipping client repo sync for EL10
    
    * Fix condition for kickstart repo sync for EL10
    
    ---------
    
    Signed-off-by: Shubham Ganar <sganar@redhat.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Gaurav Talreja <gtalreja@redhat.com>

--- tests/foreman/api/test_provisioningtemplate.py
+++ tests/foreman/api/test_provisioningtemplate.py
@@ -242,7 +242,7 @@ class TestProvisioningTemplate:
                 )
 
     @pytest.mark.e2e
-    @pytest.mark.rhel_ver_match('[^6]')
+    @pytest.mark.rhel_ver_match(r'^(?!.*fips).*$')
     def test_positive_template_check_ipxe(
         self,
         module_sync_kickstart_content,
@@ -290,7 +290,7 @@ class TestProvisioningTemplate:
         ks_param = 'ks=' if module_sync_kickstart_content.rhel_ver <= 8 else 'inst.ks='
         assert ipxe_template.count(ks_param) == 1
 
-    @pytest.mark.rhel_ver_match('[^6]')
+    @pytest.mark.rhel_ver_match(r'^(?!.*fips).*$')
     def test_positive_template_check_vlan_parameter(
         self,
         module_sync_kickstart_content,
@@ -359,7 +359,7 @@ class TestProvisioningTemplate:
 
     @pytest.mark.parametrize('pxe_loader', ['uefi'], indirect=True)
     @pytest.mark.parametrize('boot_mode', ['Static', 'DHCP'])
-    @pytest.mark.rhel_ver_match('[^6]')
+    @pytest.mark.rhel_ver_match(r'^(?!.*fips).*$')
     def test_positive_template_subnet_with_boot_mode(
         self,
         module_sync_kickstart_content,
@@ -511,7 +511,7 @@ class TestProvisioningTemplate:
         assert f'"host_config_key":"{config_key}"' in render
         assert '{"package_install": "zsh"}' in render
 
-    @pytest.mark.rhel_ver_match('[^6]')
+    @pytest.mark.rhel_ver_match(r'^(?!.*fips).*$')
     def test_positive_template_check_rex_snippet(
         self,
         module_sync_kickstart_content,
@@ -590,7 +590,7 @@ class TestProvisioningTemplate:
         )
         assert ssh_key in rex_snippet
 
-    @pytest.mark.rhel_ver_match('[^6]')
+    @pytest.mark.rhel_ver_match(r'^(?!.*fips).*$')
     def test_positive_template_check_rex_pull_mode_snippet(
         self,
         module_sync_kickstart_content,
@@ -651,7 +651,7 @@ class TestProvisioningTemplate:
         assert 'yggdrasil status' in rex_snippet
         assert 'Remote execution pull provider successfully configured!' in rex_snippet
 
-    @pytest.mark.rhel_ver_match('[^6]')
+    @pytest.mark.rhel_ver_match(r'^(?!.*fips).*$')
     def test_positive_template_check_fips_enabled(
         self,
         module_sync_kickstart_content,
@@ -698,7 +698,7 @@ class TestProvisioningTemplate:
             render = host.read_template(data={'template_kind': kind})['template']
             assert 'fips=1' in render
 
-    @pytest.mark.rhel_ver_match('[^6]')
+    @pytest.mark.rhel_ver_match(r'^(?!.*fips).*$')
     def test_positive_verify_chronyd_timesource_kickstart_template(
         self,
         module_sync_kickstart_content,
