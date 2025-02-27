25b4b7eda5dca6a0c8d774af76cdc9ca669e8096
Ladislav Vasina <lvasina@redhat.com>
2025-02-27 15:48:31 +0100

    Multiple PhoenixSubs test fixes

--- tests/foreman/cli/test_docker.py
+++ tests/foreman/cli/test_docker.py
@@ -925,7 +925,10 @@ class TestDockerActivationKey:
                 'organization-id': module_org.id,
             }
         )
-        assert activation_key['content-view'] == comp_content_view['name']
+        assert (
+            activation_key['content-view-environments'][0]['label'].split('/')[-1]
+            == comp_content_view['name']
+        )
 
     @pytest.mark.tier2
     def test_positive_remove_docker_repo_ccv(
@@ -973,7 +976,10 @@ class TestDockerActivationKey:
                 'organization-id': module_org.id,
             }
         )
-        assert activation_key['content-view'] == comp_content_view['name']
+        assert (
+            activation_key['content-view-environments'][0]['label'].split('/')[-1]
+            == comp_content_view['name']
+        )
 
         # Create another content view replace with
         another_cv = module_target_sat.cli_factory.make_content_view(
@@ -994,4 +1000,7 @@ class TestDockerActivationKey:
             }
         )
         activation_key = module_target_sat.cli.ActivationKey.info({'id': activation_key['id']})
-        assert activation_key['content-view'] != comp_content_view['name']
+        assert (
+            activation_key['content-view-environments'][0]['label'].split('/')[-1]
+            != comp_content_view['name']
+        )
