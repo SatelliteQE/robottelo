0f4fc423028cfb3e74a7d48d05a38a2f93650b37
vsedmik <46570670+vsedmik@users.noreply.github.com>
2025-03-20 09:36:02 +0100

    Fix a couple of long-failing repo tests (#17928)
    * Fix test_positive_check_package_details
    
    * Stub out ostree UI tests
    
    * Fix test_positive_sync_rh_repos
    
    and make it better readable.
    
    * Remove test_negative_update_label
    
    since the bug was CLOSED WONTFIX
    
    * Fix test_negative_synchronize_auth_yum_repo
    
    Similar as #16640.
    
    * Isolate org in test_positive_disable_rh_repo_with_basearch

--- tests/foreman/cli/test_repositories.py
+++ tests/foreman/cli/test_repositories.py
@@ -85,7 +85,7 @@ def test_negative_invalid_repo_fails_publish(
     assert 'Remove the invalid repository before publishing again' in context.value.response.text
 
 
-def test_positive_disable_rh_repo_with_basearch(module_target_sat, module_sca_manifest_org):
+def test_positive_disable_rh_repo_with_basearch(module_target_sat, function_sca_manifest_org):
     """Verify that users can disable Red Hat Repositories with basearch
 
     :id: dd3b63b7-1dbf-4d8a-ab66-348de0ad7cf3
@@ -106,7 +106,7 @@ def test_positive_disable_rh_repo_with_basearch(module_target_sat, module_sca_ma
     """
     rh_repo_id = module_target_sat.api_factory.enable_rhrepo_and_fetchid(
         basearch=DEFAULT_ARCHITECTURE,
-        org_id=module_sca_manifest_org.id,
+        org_id=function_sca_manifest_org.id,
         product=REPOS['kickstart']['rhel8_aps']['product'],
         repo=REPOS['kickstart']['rhel8_aps']['name'],
         reposet=REPOS['kickstart']['rhel8_aps']['reposet'],
@@ -119,7 +119,7 @@ def test_positive_disable_rh_repo_with_basearch(module_target_sat, module_sca_ma
             'basearch': DEFAULT_ARCHITECTURE,
             'name': REPOSET['kickstart']['rhel8_bos'],
             'product-id': repo.product.id,
-            'organization-id': module_sca_manifest_org.id,
+            'organization-id': function_sca_manifest_org.id,
             'releasever': REPOS['kickstart']['rhel8_aps']['version'],
             'repository-id': rh_repo_id,
         }
