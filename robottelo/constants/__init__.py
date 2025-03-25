9919c94bfcddd9db1faafb36461921b52a9db664
vsedmik <vsedmik@redhat.com>
2025-03-25 09:49:37 +0100

    Remove comments

--- robottelo/constants/__init__.py
+++ robottelo/constants/__init__.py
@@ -331,8 +331,8 @@ RECOMMENDED_REPOS = [
     'rhel-10-for-x86_64-appstream-eus-rpms',
     'rhel-9-for-x86_64-baseos-rpms',
     'rhel-9-for-x86_64-appstream-rpms',
-    'rhel-9-for-x86_64-baseos-eus-rpms',  # 6.17+
-    'rhel-9-for-x86_64-appstream-eus-rpms',  # 6.17+
+    'rhel-9-for-x86_64-baseos-eus-rpms',
+    'rhel-9-for-x86_64-appstream-eus-rpms',
     'rhel-8-for-x86_64-baseos-rpms',
     'rhel-8-for-x86_64-appstream-rpms',
     'rhel-8-for-x86_64-baseos-eus-rpms',
@@ -343,9 +343,9 @@ RECOMMENDED_REPOS = [
 ]
 
 VERSIONED_REPOS = [
-    'satellite-capsule-{}-for-rhel-9-x86_64-rpms',  # 6.16+
-    'satellite-maintenance-{}-for-rhel-9-x86_64-rpms',  # 6.16+
-    'satellite-utils-{}-for-rhel-9-x86_64-rpms',  # 6.16+
+    'satellite-capsule-{}-for-rhel-9-x86_64-rpms',
+    'satellite-maintenance-{}-for-rhel-9-x86_64-rpms',
+    'satellite-utils-{}-for-rhel-9-x86_64-rpms',
 ]
 
 SM_OVERALL_STATUS = {
