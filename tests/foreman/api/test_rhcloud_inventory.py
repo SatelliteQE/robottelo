2277de30c407d69aba24fcc787f2e2341e9e7e11
Cole Higgins <chiggins@redhat.com>
2025-03-26 15:57:57 -0400

    update pytest marker

--- tests/foreman/api/test_rhcloud_inventory.py
+++ tests/foreman/api/test_rhcloud_inventory.py
@@ -333,7 +333,7 @@ def test_rhcloud_scheduled_insights_sync(
 
 @pytest.mark.no_containers
 @pytest.mark.run_in_one_thread
-@pytest.mark.rhel_ver_match('[8,9]')
+@pytest.mark.rhel_ver_list('[8,9]')
 @pytest.mark.tier3
 def test_rhcloud_compliance_policies(
     inventory_settings,
