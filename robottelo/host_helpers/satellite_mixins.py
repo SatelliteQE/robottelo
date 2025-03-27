14494b9a59c926860c0ad0d4830ac7a7b5ddfc6c
Lukáš Hellebrandt <lhellebr@redhat.com>
2025-03-27 16:53:57 +0100

    Fix netcat on ipv6 (#18059)
    * Fix netcat on ipv6
    
    * Address the comment
    
    (cherry picked from commit 7fb0f2b6b7d229fcd0eae2f93396464cb2338bd5)

--- robottelo/host_helpers/satellite_mixins.py
+++ robottelo/host_helpers/satellite_mixins.py
@@ -297,7 +297,8 @@ class SystemInfo:
             pre_ncat_procs = self.execute('pgrep ncat').stdout.splitlines()
             with self.session.shell() as channel:
                 # if ncat isn't backgrounded, it prevents the channel from closing
-                command = f'ncat -kl -p {newport} -c "ncat {self.hostname} {oldport}" &'
+                nwtype = '6' if self.ipv6 else ''
+                command = f'ncat -{nwtype}kl -p {newport} -c "ncat {self.hostname} {oldport}" &'
                 logger.debug(f'Creating tunnel: {command}')
                 channel.send(command)
 
