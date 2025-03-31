6044fff2d3e13ea86a1098f72ff958d88169db13
dosas <dosas@users.noreply.github.com>
2025-03-31 15:30:25 +0200

    Add permissions for foreman_statistics plugin (#18086)
    (cherry picked from commit d3cd194df823215809af9b20f5878248a2d330ea)

--- robottelo/constants/__init__.py
+++ robottelo/constants/__init__.py
@@ -994,6 +994,7 @@ PERMISSIONS = {
         'destroy_snapshots',
         'revert_snapshots',
         'edit_snapshots',
+        'view_statistics',
     ],
     'AnsibleRole': ['view_ansible_roles', 'destroy_ansible_roles', 'import_ansible_roles'],
     'AnsibleVariable': [
@@ -1076,6 +1077,13 @@ PERMISSIONS = {
         'edit_salt_modules',
         'view_salt_modules',
         'destroy_salt_modules',
+    ],
+    'ForemanStatistics::Trend': [
+        'create_trends',
+        'view_trends',
+        'edit_trends',
+        'update_trends',
+        'destroy_trends',
     ],
     'ForemanTasks::RecurringLogic': [
         'create_recurring_logics',
