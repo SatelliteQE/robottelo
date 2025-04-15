8fc07455a1cddfd488bc517addc8379cd2eda363
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2025-04-15 16:38:15 -0400

    [6.15.z] ACS - remove invalid parametrization combination (rhui-file) (#18250)

--- tests/foreman/cli/test_acs.py
+++ tests/foreman/cli/test_acs.py
@@ -31,8 +31,15 @@ VAL_CANNOT_BE = 'cannot be'
 @pytest.mark.e2e
 @pytest.mark.tier2
 @pytest.mark.pit_server
-@pytest.mark.parametrize('cnt_type', ['yum', 'file'])
-@pytest.mark.parametrize('acs_type', ['custom', 'simplified', 'rhui'])
+@pytest.mark.parametrize(
+    ('acs_type', 'cnt_type'),
+    [
+        (acs, cnt)
+        for acs in ['custom', 'simplified', 'rhui']
+        for cnt in ['yum', 'file']
+        if not (acs == 'rhui' and cnt == 'file')  # invalid combination: 'rhui-file'
+    ],
+)
 def test_positive_CRUD_all_types(
     request, module_target_sat, acs_type, cnt_type, module_yum_repo, module_file_repo
 ):
@@ -56,8 +63,6 @@ def test_positive_CRUD_all_types(
         4. ACS can be refreshed.
         5. ACS can be deleted.
     """
-    if 'rhui' in request.node.name and 'file' in request.node.name:
-        pytest.skip('unsupported parametrize combination')
 
     # Create
     params = {
