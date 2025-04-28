57f34d8b93e846638639cbba89afb5509ea6163a
amol patil <amolpati@redhat.com>
2025-04-28 22:39:25 +0530

    test libvirt image based provisioning

--- pytest_fixtures/component/provision_libvirt.py
+++ pytest_fixtures/component/provision_libvirt.py
@@ -1,17 +1,39 @@
 # Compute resource - Libvirt entities
+from fauxfactory import gen_string
 import pytest
+
+from robottelo.config import settings
+from robottelo.constants import FOREMAN_PROVIDERS, LIBVIRT_RESOURCE_URL
+
+LIBVIRT_URL = LIBVIRT_RESOURCE_URL % settings.libvirt.libvirt_hostname
 
 
 @pytest.fixture(scope='module')
 def module_cr_libvirt(module_target_sat, module_org, module_location):
     return module_target_sat.api.LibvirtComputeResource(
-        organization=[module_org], location=[module_location]
+        name=gen_string('alpha'),
+        provider=FOREMAN_PROVIDERS['libvirt'],
+        display_type='VNC',
+        organization=[module_org],
+        location=[module_location],
+        url=LIBVIRT_URL,
     ).create()
 
 
 @pytest.fixture(scope='module')
-def module_libvirt_image(module_target_sat, module_cr_libvirt):
-    return module_target_sat.api.Image(compute_resource=module_cr_libvirt).create()
+def module_libvirt_image(
+    module_target_sat, module_provisioning_rhel_content, module_cr_libvirt, default_architecture
+):
+    return module_target_sat.api.Image(
+        compute_resource=module_cr_libvirt,
+        name=gen_string('alpha'),
+        operatingsystem=module_provisioning_rhel_content.os,
+        architecture=default_architecture,
+        username=settings.server.SSH_USERNAME,
+        password=settings.server.SSH_PASSWORD,
+        user_data=False,
+        uuid=settings.libvirt.LIBVIRT_IMAGE_PATH,
+    ).create()
 
 
 @pytest.fixture(scope='module')
