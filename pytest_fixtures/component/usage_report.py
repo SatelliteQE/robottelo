03c8cabda16888b98211c98d33cf50c22c889b58
Peter Ondrejka <pondrejk@redhat.com>
2025-04-30 15:45:35 +0200

    test condensed usage report

--- pytest_fixtures/component/usage_report.py
+++ pytest_fixtures/component/usage_report.py
@@ -13,18 +13,23 @@ def process_keys(keys):
 
 
 @pytest.fixture(scope='module')
-def module_generate_report(module_target_sat):
+def module_generated_report(module_target_sat):
     filename = f'usage_report-{datetime.timestamp(datetime.now())}-{gen_string("alphanumeric")}.yml'
     result = module_target_sat.cli.SatelliteMaintainReport.generate({'output': filename})
     assert result.status == 0, 'failed to generate report'
-    return module_target_sat.load_remote_yaml_file(filename)
+    return filename
 
 
 @pytest.fixture(scope='module')
-def module_report_keys(module_generate_report):
-    return process_keys(module_generate_report.keys())
+def module_report_keys(module_target_sat, module_generated_report):
+    return process_keys(module_target_sat.load_remote_yaml_file(module_generated_report).keys())
 
 
 @pytest.fixture(scope='module')
 def module_expected_keys():
     return process_keys(yaml.safe_load(DataFile.USAGE_REPORT_ITEMS.read_text()).keys())
+
+
+@pytest.fixture(scope='module')
+def module_expected_keys_condensed():
+    return process_keys(yaml.safe_load(DataFile.USAGE_REPORT_ITEMS_CONDENSED.read_text()).keys())
