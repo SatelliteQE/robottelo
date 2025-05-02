4d15e028eb85b1dcd2d829068de02a6909343199
synkd <48261305+synkd@users.noreply.github.com>
2025-05-02 11:58:32 -0400

    Convert Discovery upgrade scenario to new format (#16898)
    * Convert Discovery upgrade scenario to new format
    
    * Add new test module to CODEOWNERS file
    
    * Fix typo in CODEOWNERS
    
    ---------
    
    Co-authored-by: vsedmik <46570670+vsedmik@users.noreply.github.com>

--- tests/new_upgrades/conftest.py
+++ tests/new_upgrades/conftest.py
@@ -98,3 +98,38 @@ def content_upgrade_shared_satellite():
     ) as test_duration:
         yield sat_instance
         test_duration.ready()
+
+
+@pytest.fixture
+def search_upgrade_shared_satellite():
+    """Mark tests using this fixture with pytest.mark.search_upgrades."""
+    sat_instance = shared_checkout("search_upgrade")
+    with SharedResource(
+        "search_upgrade_tests", shared_checkin, sat_instance=sat_instance
+    ) as test_duration:
+        yield sat_instance
+        test_duration.ready()
+
+
+@pytest.fixture
+def errata_upgrade_shared_satellite():
+    """Mark tests using this fixture with pytest.mark.search_upgrades."""
+    sat_instance = shared_checkout("errata_upgrade")
+    with SharedResource(
+        "errata_upgrade_tests",
+        shared_checkin,
+        sat_instance=sat_instance,
+    ) as test_duration:
+        yield sat_instance
+        test_duration.ready()
+
+
+@pytest.fixture
+def fdi_upgrade_shared_satellite():
+    """Mark tests using this fixture with pytest.mark.discovery_upgrades."""
+    sat_instance = shared_checkout("fdi_upgrade")
+    with SharedResource(
+        "fdi_upgrade_tests", shared_checkin, sat_instance=sat_instance
+    ) as test_duration:
+        yield sat_instance
+        test_duration.ready()
