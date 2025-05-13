90ecf04996636656c6d726469058d4d70fb8eea6
vsedmik <vsedmik@redhat.com>
2025-05-13 19:33:38 +0200

    Add organization scoping

--- tests/foreman/ui/test_flatpak.py
+++ tests/foreman/ui/test_flatpak.py
@@ -19,17 +19,20 @@ def test_flatpak_remotes(target_sat, function_org, function_flatpak_remote):
     :id: c757094c-e817-4a1b-a8b3-d2ce3f9d6b0e
 
     :setup:
-        1. Create a flatpak remote and scan it.
+        1. Create a flatpak remote in an Organization and scan it.
 
     :steps:
-        1. Navigate to Flatpak Remotes page and ensure the remote is displayed.
+        1. Select the Organization owning the remote.
+        2. Navigate to Flatpak Remotes page and read the table.
 
     :expectedresults:
         1. The remote is displayed correctly.
 
     """
     with target_sat.ui_session() as session:
+        session.organization.select(function_org.name)
         remotes = session.flatpak_remotes.read_table()
-        assert len(remotes) > 0
-        remote = next(r for r in remotes if r['Name'] == function_flatpak_remote.remote.name)
+        assert len(remotes) == 1
+        remote = remotes[0]
+        assert remote['Name'] == function_flatpak_remote.remote.name
         assert remote['URL'] == function_flatpak_remote.remote.flatpak_index_url
