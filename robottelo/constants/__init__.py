683275bee16f52accf7ed6dc7db6d540eb558aa5
dosas <dosas@users.noreply.github.com>
2025-05-15 11:42:48 +0200

    Add missing permissions for foreman_salt plugin (#18434)
    https://github.com/theforeman/foreman_salt/blob/master/lib/foreman_salt/plugin.rb#L110
    (cherry picked from commit 59f3cbc2878bf3c33370e1ba6b5c3f3cfb4aad1d)

--- robottelo/constants/__init__.py
+++ robottelo/constants/__init__.py
@@ -1237,6 +1237,7 @@ PERMISSIONS = {
         'lock_report_templates',
     ],
     'Role': ['view_roles', 'create_roles', 'edit_roles', 'destroy_roles'],
+    'Report': ['create_reports'],
     'SccAccount': [
         "delete_scc_accounts",
         "edit_scc_accounts",
