ed3a2a44e5b74de2fc7792e6b3f0f4244e934dec
Ondřej Gajdušek <ogajduse@redhat.com>
2025-05-28 13:32:37 +0200

    Allow NetworkType to be serialized by ruamel.yaml

--- robottelo/enums.py
+++ robottelo/enums.py
@@ -6,7 +6,12 @@ and configurations used in Robottelo tests and utilities.
 
 from enum import StrEnum
 
+import ruamel.yaml
 
+yaml = ruamel.yaml.YAML()
+
+
+@yaml.register_class
 class NetworkType(StrEnum):
     """
     Enumeration of host network addressing types.
@@ -26,3 +31,12 @@ class NetworkType(StrEnum):
     @property
     def has_ipv6(self):
         return self in (self.IPV6, self.DUALSTACK)
+
+    @classmethod
+    def to_yaml(cls, representer, node):
+        return representer.represent_scalar('!NetworkType', node.value)
+
+    @classmethod
+    def from_yaml(cls, constructor, node):
+        value = constructor.construct_scalar(node)
+        return cls(value)
