e8ff3dd44355a3744bfdccd2579d140bf13c067c
Danny Synk <dsynk@redhat.com>
2025-06-13 16:20:22 -0400

    Address reviewer feedback

--- tests/new_upgrades/conftest.py
+++ tests/new_upgrades/conftest.py
@@ -189,7 +189,7 @@ def perf_tuning_upgrade_shared_satellite():
 @pytest.fixture
 def sync_plan_upgrade_shared_satellite():
     """Mark tests using this fixture with pytest.mark.sync_plan_upgrades."""
-    sat_instance = shared_checkout("perf_tuning_upgrade")
+    sat_instance = shared_checkout("sync_plan_upgrade")
     with SharedResource(
         "perf_tuning_upgrade_tests", shared_checkin, sat_instance=sat_instance
     ) as test_duration:
