ddc30124f8e315b0101cfe4d6abf6e7baec81ecc
Peter Ondrejka <pondrejk@users.noreply.github.com>
2025-07-11 15:08:44 +0200

    more fine-grained reporting tests (#18951)

--- pytest_fixtures/component/usage_report.py
+++ pytest_fixtures/component/usage_report.py
@@ -15,7 +15,7 @@ def process_keys(keys):
 @pytest.fixture(scope='module')
 def module_generate_report(module_target_sat):
     filename = f'usage_report-{datetime.timestamp(datetime.now())}-{gen_string("alphanumeric")}.yml'
-    result = module_target_sat.execute(f'satellite-maintain report generate --output {filename}')
+    result = module_target_sat.cli.SatelliteMaintainReport.generate({'output': filename})
     assert result.status == 0, 'failed to generate report'
     return module_target_sat.load_remote_yaml_file(filename)
 
