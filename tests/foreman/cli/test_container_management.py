fa018367d589bb5cc2c0924bc00b344313f996af
Ondřej Gajdušek <ogajduse@redhat.com>
2025-07-18 17:19:25 +0200

    Rename ContainerManagement-Content to ContainerImageManagement (#19017)

--- tests/foreman/cli/test_container_management.py
+++ tests/foreman/cli/test_container_management.py
@@ -1,12 +1,12 @@
 """Tests for the Container Management Content
 
-:Requirement: ContainerManagement-Content
+:Requirement: ContainerImageManagement
 
 :CaseAutomation: Automated
 
 :Team: Phoenix-content
 
-:CaseComponent: ContainerManagement-Content
+:CaseComponent: ContainerImageManagement
 
 """
 
