36830feb989ebb5829b5cb6f6b06a264f3585df6
Peter Ondrejka <pondrejk@redhat.com>
2025-08-12 10:10:07 +0200

    test mcp image pulled from downstream registry

--- pytest_fixtures/component/mcp.py
+++ pytest_fixtures/component/mcp.py
@@ -46,11 +46,53 @@ def module_mcp_target_sat(module_target_sat):
     result = module_target_sat.execute('firewall-cmd --reload')
     assert result.status == 0
     module_target_sat.ensure_podman_installed()
-    result = module_target_sat.execute(f'podman pull {settings.foreman_mcp.registry}')
+    result = module_target_sat.execute(
+        f'podman pull {settings.foreman_mcp.registry}{settings.foreman_mcp.image_path}'
+    )
     assert result.status == 0
     module_target_sat.execute(
         f'podman run --name {container_name} -d --pull=never -it -p {settings.foreman_mcp.port}:8080 \
-            foreman-mcp-server --foreman-url https://{settings.server.hostname} --host 0.0.0.0 --no-verify-ssl'
+            foreman-mcp-server --foreman-url https://{module_target_sat.hostname} --host 0.0.0.0 --no-verify-ssl'
+    )
+    sleep(20)
+    result = module_target_sat.execute(
+        f'podman inspect -f "{{{{.State.Status}}}}" {container_name}'
+    )
+    log = module_target_sat.execute(f'podman logs {container_name}')
+    assert result.stdout.strip() == 'running', (
+        f'failed to start container {container_name}: {log.stdout}'
+        f'failed to start container {container_name}: {log.stderr}'
+    )
+    yield module_target_sat
+    module_target_sat.execute(f'podman kill {container_name}')
+    result = module_target_sat.execute(
+        f'podman inspect -f "{{{{.State.Status}}}}" {container_name}'
+    )
+    assert result.stdout.strip() == 'exited', f'failed to clean up container {container_name}'
+
+
+@pytest.fixture(scope='module')
+def module_downstream_mcp_target_sat(module_target_sat):
+    """A module-level fixture to provide an downstream distribution of MCP server configured on Satellite"""
+    container_name = f'mcp_server_downstream-{datetime.timestamp(datetime.now())}'
+    result = module_target_sat.execute(
+        f'firewall-cmd --permanent --add-port="{settings.foreman_mcp_downstream.port}/tcp"'
+    )
+    assert result.status == 0
+    result = module_target_sat.execute('firewall-cmd --reload')
+    assert result.status == 0
+    module_target_sat.podman_login(
+        settings.foreman_mcp_downstream.registry_stage_username,
+        settings.foreman_mcp_downstream.registry_stage_password,
+        settings.foreman_mcp_downstream.registry_stage,
+    )
+    result = module_target_sat.execute(
+        f'podman pull {settings.foreman_mcp_downstream.registry_stage}{settings.foreman_mcp_downstream.image_path}'
+    )
+    assert result.status == 0
+    module_target_sat.execute(
+        f'podman run --name {container_name} -d --pull=never -it -p {settings.foreman_mcp_downstream.port}:8080 \
+            foreman-mcp-server --foreman-url https://{module_target_sat.hostname} --host 0.0.0.0 --no-verify-ssl'
     )
     sleep(20)
     result = module_target_sat.execute(
