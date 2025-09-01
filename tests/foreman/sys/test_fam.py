d879230ea7f0f2fd2c4efe087737e8161a22c1f7
Evgeni Golov <evgeni@redhat.com>
2025-09-01 11:42:50 +0200

    Use fixtures.pulpproject.org instead of repos.fedorapeople.org for FAM (#19313)
    Use fixtures.pulpproject.org instead of repos.fedorapeople.org for FAM (#19309)
    
    repos.fedorapeople.org/pulp is gone and the tests that tried to sync
    content from there fail. fixtures.pulpproject.org is a drop-in
    replacement which we can use.
    
    (cherry picked from commit 536e24306e2e34ff24d01cee5e2db823b9d7c263)

--- tests/foreman/sys/test_fam.py
+++ tests/foreman/sys/test_fam.py
@@ -121,6 +121,12 @@ def setup_fam(module_target_sat, module_sca_manifest, install_import_ansible_rol
         f"sed -i '/inventory_use_container/ s#true#false#' {FAM_ROOT_DIR}/tests/test_playbooks/vars/inventory.yml"
     )
 
+    # Edit repos used in tests
+    # Until https://github.com/theforeman/foreman-ansible-modules/pull/1899 is in
+    module_target_sat.execute(
+        f"sed -i 's#https://repos.fedorapeople.org/pulp/pulp/demo_repos/zoo/#https://fixtures.pulpproject.org/rpm-signed/#' {FAM_ROOT_DIR}/tests/test_playbooks/*.yml"
+    )
+
     # Upload manifest to test playbooks directory
     module_target_sat.put(str(module_sca_manifest.path), str(module_sca_manifest.name))
     module_target_sat.execute(
