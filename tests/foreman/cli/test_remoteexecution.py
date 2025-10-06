abbbc72137fed808b59f92ee4b349332a740a069
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2025-10-06 09:34:43 -0400

    [6.18.z] conversion to pull provider not for rhel10 (#19856)
    conversion to pull provider not for rhel10 (#19849)
    
    (cherry picked from commit f5f63fc8bd627d12b91e601dbb426d9e960e8180)
    
    Co-authored-by: Peter Ondrejka <pondrejk@users.noreply.github.com>

--- tests/foreman/cli/test_remoteexecution.py
+++ tests/foreman/cli/test_remoteexecution.py
@@ -1211,6 +1211,7 @@ class TestPullProviderRex:
         ids=["no_global_proxy"],
         indirect=True,
     )
+    @pytest.mark.rhel_ver_match('[^10].*')
     def test_positive_run_job_on_host_converted_to_pull_provider(
         self,
         module_org,
