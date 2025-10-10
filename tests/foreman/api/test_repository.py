b5886d2f3a70d889917efc370aa0d077953e4984
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2025-10-10 07:52:43 -0400

    [6.18.z] Remove invalid / unnecessary skip decorators for SRPM (#19938)
    Remove invalid / unnecessary skip decorators for SRPM (#19932)
    
    (cherry picked from commit 599bc2065d41bd9f4631667cacc4f273031ccff0)
    
    Co-authored-by: Vladimir Sedmik <46570670+vsedmik@users.noreply.github.com>

--- tests/foreman/api/test_repository.py
+++ tests/foreman/api/test_repository.py
@@ -2178,9 +2178,6 @@ class TestSRPMRepositoryIgnoreContent:
     :BZ: 1673215
     """
 
-    @pytest.mark.skipif(
-        (not settings.robottelo.REPOS_HOSTING_URL), reason='Missing repos_hosting_url'
-    )
     @pytest.mark.parametrize(
         'repo_options',
         **datafactory.parametrized(
@@ -2206,9 +2203,6 @@ class TestSRPMRepositoryIgnoreContent:
         repo = repo.read()
         assert repo.content_counts['srpm'] == 0
 
-    @pytest.mark.skipif(
-        (not settings.robottelo.REPOS_HOSTING_URL), reason='Missing repos_hosting_url'
-    )
     @pytest.mark.parametrize(
         'repo_options',
         **datafactory.parametrized(
@@ -2230,10 +2224,6 @@ class TestSRPMRepositoryIgnoreContent:
         repo = repo.read()
         assert repo.content_counts['srpm'] == 2
 
-    @pytest.mark.skip('Uses deprecated SRPM repository')
-    @pytest.mark.skipif(
-        (not settings.robottelo.REPOS_HOSTING_URL), reason='Missing repos_hosting_url'
-    )
     @pytest.mark.parametrize(
         'repo_options',
         **datafactory.parametrized(
