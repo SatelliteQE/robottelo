SatelliteQE/robottelo

commit 78b8783a4869f2e206bca09eb037d2179b6dd383

Enable ipv6 for rhel7_contenthost and rhel8_contenthost (#19950)
* enable ipv6 for rhel7_contenthost and rhel8_contenthost

* Added  ipv6 condition check

* Update tests/foreman/cli/test_host.py

Co-authored-by: Gaurav Talreja <gauravtalreja1@gmail.com>

* Added  ipv6 condition check

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

---------

Co-authored-by: Gaurav Talreja <gauravtalreja1@gmail.com>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

pytest_fixtures/core/contenthosts.py
@@ -20,7 +20,7 @@ def host_conf(request):
     if hasattr(request, 'param'):
         params = request.param
     distro = params.get('distro', 'rhel')
-    network = params.get('network')
+    network = params.get('network') or settings.content_host.network_type
     _rhelver = f"{distro}{params.get('rhel_version', settings.content_host.default_rhel_version)}"
 
     # check to see if no-containers is passed as an argument to pytest

tests/foreman/cli/test_host.py
@@ -29,6 +29,7 @@ from robottelo.constants import (
     FAKE_8_CUSTOM_PACKAGE,
     FAKE_8_CUSTOM_PACKAGE_NAME,
 )
+from robottelo.enums import NetworkType
 from robottelo.exceptions import CLIFactoryError, CLIReturnCodeError
 from robottelo.logging import logger
 from robottelo.utils.datafactory import (
@@ -2216,7 +2217,8 @@ def test_positive_dump_enc_yaml(target_sat):
     """
     enc_dump = target_sat.cli.Host.enc_dump({'name': target_sat.hostname})
     assert f'fqdn: {target_sat.hostname}' in enc_dump
-    assert f'ip: {target_sat.ip_addr}' in enc_dump
+    ip_prefix = 'ip6' if target_sat.network_type == NetworkType.IPV6 else 'ip'
+    assert f'{ip_prefix}: {target_sat.ip_addr}' in enc_dump
     assert 'ssh-rsa' in enc_dump
 
 

commit ea06c96c712f1fcd5a998a1f1ec3f1e8d5dbc21b

don't pass deploy_network_type in centos_host and oracle_host
Since 78b8783a4869f2e206bca09eb037d2179b6dd383 host_conf does set
deploy_network_type itself, which results in the following error when
trying to use the oracle_host and centos_host fixtures:

    ../../lib64/python3.12/site-packages/pytest_asyncio/plugin.py:733: in pytest_fixture_setup
        return (yield)
                ^^^^^
    pytest_fixtures/core/contenthosts.py:290: in oracle_host
        with Broker(
    E   TypeError: broker.broker.Broker() got multiple values for keyword argument 'deploy_network_type'V

Fixes: 78b8783a4869f2e206bca09eb037d2179b6dd383

pytest_fixtures/core/contenthosts.py
@@ -275,7 +275,6 @@ def centos_host(request, version):
     with Broker(
         **host_conf(request),
         host_class=ContentHost,
-        deploy_network_type=settings.content_host.network_type,
     ) as host:
         yield host
 
@@ -290,7 +289,6 @@ def oracle_host(request, version):
     with Broker(
         **host_conf(request),
         host_class=ContentHost,
-        deploy_network_type=settings.content_host.network_type,
     ) as host:
         yield host
 