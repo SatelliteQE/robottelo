18d078f6427add407da77fa001e24eb5b0d377da
Gaurav Talreja <gtalreja@redhat.com>
2025-11-22 22:08:50 +0530

    Add initial tests to validate foremanctl
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>

--- pytest_fixtures/core/xdist.py
+++ pytest_fixtures/core/xdist.py
@@ -14,7 +14,7 @@ from robottelo.logging import logger
 def align_to_satellite(request, worker_id, satellite_factory):
     """Attempt to align a Satellite to the current xdist worker"""
     if 'build_sanity' in request.config.option.markexpr:
-        settings.set("server.hostname", None)
+        settings.set('server.hostname', None)
         yield
         # Checkout Sanity Capsule finally
         for host in [settings.capsule.hostname, settings.server.hostname]:
---
+++ tests/foreman/installer/test_new_installer.py
@@ -0,0 +1,123 @@
+"""Smoke tests to check installation health
+
+:Requirement: Installation
+
+:CaseAutomation: Automated
+
+:CaseComponent: Installation
+
+:Team: Rocket
+
+:CaseImportance: Critical
+
+"""
+
+from broker import Broker
+import pytest
+
+from robottelo.config import settings
+from robottelo.hosts import Satellite, get_sat_rhel_version
+
+pytestmark = [pytest.mark.new_installer, pytest.mark.build_sanity, pytest.mark.upgrade]
+
+SATELLITE_SERVICES = [
+    'dynflow-sidekiq@orchestrator',
+    'dynflow-sidekiq@worker',
+    'dynflow-sidekiq@worker-hosts-queue',
+    'foreman-proxy',
+    'foreman',
+    'httpd',
+    'postgresql',
+    'pulp-api',
+    'pulp-content',
+    'pulp-worker@*',
+    'candlepin',
+]
+
+
+def common_sat_install_assertions(satellite):
+    # no errors/failures in journald
+    result = satellite.execute(
+        r'journalctl --quiet --no-pager --boot --priority err -u "dynflow-sidekiq*" -u "foreman-proxy" -u "foreman" -u "httpd" -u "postgresql" -u "pulp-api" -u "pulp-content" -u "pulp-worker*" -u "redis" -u "candlepin"'
+    )
+    assert not result.stdout
+    # no errors/failures in /var/log/httpd/*
+    result = satellite.execute(r'grep -iR "error" /var/log/httpd/*')
+    assert not result.stdout
+    # # no errors/failures in /var/log/candlepin/*
+    result = satellite.execute(r'grep -iR "error" /var/log/candlepin/*')
+    assert not result.stdout
+    httpd_log = satellite.execute('journalctl --unit=httpd')
+    assert 'WARNING' not in httpd_log.stdout
+
+
+@pytest.fixture(scope='module')
+def module_sat_ready_rhel(request):
+    with Broker(
+        workflow=settings.server.deploy_workflows.os,
+        deploy_rhel_version=get_sat_rhel_version().major,
+        deploy_flavor=settings.flavors.default,
+        deploy_network_type=settings.server.network_type,
+        host_class=Satellite,
+    ) as sat:
+        sat.install_satellite_foremanctl(
+            enable_fapolicyd=(request.param == 'fapolicyd'), enable_fips=(request.param == 'fips')
+        )
+        yield sat
+
+
+@pytest.mark.first_sanity
+@pytest.mark.parametrize('module_sat_ready_rhel', ['default', 'fips', 'fapolicyd'], indirect=True)
+def test_satellite_installation_with_foremanctl(module_sat_ready_rhel):
+    """Run a basic Satellite installation
+
+    :id: 661206f3-2eec-403c-af26-3c5cadcd5769
+
+    :steps:
+        1. Get RHEL Host
+        2. Configure satellite repos
+        3. Install satellite using foremanctl
+        4. Run foremanctl deploy
+
+    :expectedresults:
+        1. foremanctl deploy runs successfully
+        2. no unexpected errors in logs
+    """
+    common_sat_install_assertions(module_sat_ready_rhel)
+
+
+@pytest.mark.parametrize('module_sat_ready_rhel', ['default', 'fips', 'fapolicyd'], indirect=True)
+def test_positive_check_installer_service_running(module_sat_ready_rhel):
+    """Check if all Satellite services is running
+
+    :id: 5389c174-7ab1-4e9d-b2aa-66d80fd6dc5h
+
+    :steps:
+        1. Verify a service is active with systemctl is-active
+
+    :expectedresults: All Satellite services are active
+    """
+    for service in SATELLITE_SERVICES:
+        is_active = module_sat_ready_rhel.execute(f'systemctl is-active {service}')
+        status = module_sat_ready_rhel.execute(f'systemctl status {service}')
+        assert is_active.status == 0, status.stdout
+
+
+@pytest.mark.parametrize('module_sat_ready_rhel', ['default', 'fips', 'fapolicyd'], indirect=True)
+def test_positive_check_installer_hammer_ping(module_sat_ready_rhel):
+    """Check if hammer ping reports all services as ok
+
+    :id: 85fd4388-6d94-42f5-bed2-24be38e9f111
+
+    :steps:
+        1. Run the 'hammer ping' command on satellite.
+
+    :expectedresults: All services are active (running)
+    """
+    assert module_sat_ready_rhel.execute('foremanctl setup-hammer').status == 0
+    # check status reported by hammer ping command
+    result = module_sat_ready_rhel.execute('hammer ping')
+    assert result.status == 0
+    for line in result.stdout.split('\n'):
+        if 'Status' in line:
+            assert 'ok' in line
