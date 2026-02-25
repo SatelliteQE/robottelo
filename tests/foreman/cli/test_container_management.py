023de1e2173cef03238a9b64ba034bd74f4a5d90
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2026-02-25 08:20:22 -0500

    [6.19.z] Replace library/busybox with lighter container repo (#20853)
    Replace library/busybox with lighter container repo (#20671)
    
    (cherry picked from commit 8ac73bc97a2531bb9b282f00adced329b16c0c93)
    
    Co-authored-by: Lukas Pramuk <lpramuk@redhat.com>

--- tests/foreman/cli/test_container_management.py
+++ tests/foreman/cli/test_container_management.py
@@ -11,6 +11,7 @@
 """
 
 from datetime import UTC, datetime
+import re
 
 from box import Box
 from fauxfactory import gen_string
@@ -94,16 +95,18 @@ class TestDockerClient:
             )
             assert result.status == 0
             try:
-                result = module_container_contenthost.execute(f'docker run {repo["published-at"]}')
+                result = module_container_contenthost.execute(
+                    f'docker run -d {repo["published-at"]}'
+                )
                 assert result.status == 0
+                match = re.match(r'^[0-9a-f]+$', result.stdout)
+                if match:
+                    container_id = match.group(0)
             finally:
                 # Stop and remove the container
-                result = module_container_contenthost.execute(
-                    f'docker ps -a | grep {repo["published-at"]}'
-                )
-                container_id = result.stdout[0].split()[0]
-                module_container_contenthost.execute(f'docker stop {container_id}')
-                module_container_contenthost.execute(f'docker rm {container_id}')
+                if container_id:
+                    module_container_contenthost.execute(f'docker stop {container_id}')
+                    module_container_contenthost.execute(f'docker rm {container_id}')
         finally:
             # Remove docker image
             module_container_contenthost.execute(f'docker rmi {repo["published-at"]}')
