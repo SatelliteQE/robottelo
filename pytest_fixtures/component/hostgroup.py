bdbb8f5e81d57543e56f1949f18ac9d2245c70e4
Pavel Novotny <pnovotny@users.noreply.github.com>
2026-02-26 10:07:43 +0100

    hostgroup nonadmin viewer read (#20875)
    hostgroups: new test for non-admin viewer role
    
    Verifies: SAT-38451
    
    New test for bug "Non-admin users on Satellite with viewer role, unable to see the hostgroup."
    Ensure that non-admin user with viewer role can see hostgroup created by admin user.
    
    Also added new `UserFactory` class to help with more reusable user fixtures.
    
    (cherry picked from commit 0da20aba3a316b090dff2355ba0b152b2a213a94)

--- pytest_fixtures/component/hostgroup.py
+++ pytest_fixtures/component/hostgroup.py
@@ -10,6 +10,13 @@ def module_hostgroup(module_target_sat):
     return module_target_sat.api.HostGroup().create()
 
 
+@pytest.fixture(scope='module')
+def module_hostgroup_with_org_loc(module_target_sat, module_org, module_location):
+    return module_target_sat.api.HostGroup(
+        organization=[module_org], location=[module_location]
+    ).create()
+
+
 @pytest.fixture(scope='class')
 def class_hostgroup(class_target_sat, class_org, class_location):
     """Create a hostgroup linked to specific org and location created at the class scope"""
