c52325f01d7d8c0c440544c7c61cda2920ee4a88
Ladislav Vasina <lvasina@redhat.com>
2026-02-26 10:31:28 +0100

    AutomationSAT-32425

--- tests/foreman/ui/test_host.py
+++ tests/foreman/ui/test_host.py
@@ -4248,3 +4248,29 @@ def test_assign_different_cv_from_same_env(
 
         cv_env = session.host_new.get_content_view_envs(rhel_contenthost.hostname)[0]
         assert cv_env['content_view'] == cv.name
+
+
+def test_positive_only_single_library_option_in_create_form(target_sat):
+    """
+    Ensure that only the Library option is displayed in the Create Host form
+
+    :id: 559f6324-dc17-4274-99f5-957ef0a2faf0
+
+    :steps:
+        1. Set location to "Any location"
+        2. Read LCE dropdown options in the Create host form
+
+    :expectedresults:
+        1. Only 1 Library option is displayed
+
+    :verifies: SAT-42710, SAT-32425
+
+    :customerscenario: true
+    """
+    with target_sat.ui_session() as session:
+        session.organization.select(org_name='Default Organization')
+        session.location.select(loc_name='Any location')
+        create_form = session.host.get_create_form()
+        create_form.host.lce.open_filter.click()
+        # Check that 'Library' appears just once
+        assert create_form.host.lce.filter_content.read().count('Library') == 1
