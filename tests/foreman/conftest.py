59c0ddd8009f63fb96f8c1b47ecd735a9dc3467c
Lukas Pramuk <lpramuk@redhat.com>
2026-03-26 11:12:41 +0100

    Fix automatic vault login

--- tests/foreman/conftest.py
+++ tests/foreman/conftest.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+from robottelo.config import settings
 from robottelo.hosts import Satellite
 from robottelo.logging import collection_logger
 
@@ -75,3 +76,14 @@ def ui_session_record_property(request, record_property):
             ):
                 sat = request.getfixturevalue(fixture)
                 sat.record_property = record_property
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_protocol(item, nextitem):
+    """Set version source to upstream for tests marked with foremanctl."""
+    if item.get_closest_marker('foremanctl'):
+        settings.set('server.version.source', 'upstream')
+        yield
+        settings.set('server.version.source', 'internal')
+    else:
+        yield
