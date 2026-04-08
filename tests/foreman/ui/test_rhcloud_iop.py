0593b6ffaf836d7790870d5db02313cc206e173e
synkd <48261305+synkd@users.noreply.github.com>
2026-04-08 10:42:53 -0400

    Automate re-registering host to Insights after enabling IoP (#21054)
    * Automate re-registering host to Insights after enabling IoP
    
    This PR adds a test that registers a host to hosted Insights via
    Satellite, then enables IoP, then re-registers the host to IoP and runs
    a remediation job. This operation was previously failing due to the bug
    tracked in SAT-43307.
    
    * Address reviewer feedback
    
    (cherry picked from commit dc9c7905a60452ec6430781c6bbe08fe3782deb9)

--- tests/foreman/ui/test_rhcloud_iop.py
+++ tests/foreman/ui/test_rhcloud_iop.py
@@ -619,3 +619,54 @@ def test_iop_insights_rbac_no_permissions(
         assert permission == "You do not have access to Advisor"
         permission = session.cloudvulnerability.read_no_authorized_message()
         assert permission == "You do not have access to Vulnerability"
+
+
+@pytest.mark.no_containers
+@pytest.mark.rhel_ver_list([settings.content_host.default_rhel_version])
+def test_re_register_after_enabling_iop(
+    rhel_insights_vm,
+    rhcloud_manifest_org,
+    module_target_sat_insights,
+):
+    """
+    Verify that a remediation job can be run successfully on a host that was originally registered
+    to hosted Insights after enabling IoP on Satellite and re-registering the host to IoP.
+
+    :id: 84fbc3b1-1aaa-4ca2-aa90-c606eb863c61
+
+    :steps:
+        1. Register a host to hosted Insights via Satellite.
+        2. Enable IoP on Satellite.
+        3. Re-run `insights-client --register` on the host.
+        4. Create a recommendation on the host.
+        5. Attempt to run a remediation job for the recommendation.
+
+    :expectedresults:
+        The remediation job runs successfully on the host.
+    """
+    assert rhel_insights_vm.execute('insights-client --test-connection').status == 0
+    module_target_sat_insights.configure_iop()
+    assert rhel_insights_vm.execute('insights-client --register').status == 0
+    create_insights_recommendation(rhel_insights_vm)
+
+    with module_target_sat_insights.ui_session() as session:
+        session.organization.select(org_name=rhcloud_manifest_org.name)
+
+        # Search for the recommendation.
+        result = session.recommendationstab.search(OPENSSH_RECOMMENDATION)
+        assert result[0]['Name'] == OPENSSH_RECOMMENDATION
+
+        # Remediate the Affected System.
+        result = session.recommendationstab.remediate_affected_system(
+            OPENSSH_RECOMMENDATION, rhel_insights_vm.hostname
+        )
+
+        # Verify that the job Succeeded
+        assert result['status']['Succeeded'] != 0
+        assert result['overall_status']['is_success']
+
+        # Verify that the recommendation is not listed anymore.
+        assert (
+            'No recommendations None of your connected systems are affected by enabled recommendations'
+            in session.recommendationstab.search(OPENSSH_RECOMMENDATION)[0]['Name']
+        )
