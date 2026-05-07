db96b263f6074341bdd0c727494b29c2ae6f5dc6
rmynar <64528205+rmynar@users.noreply.github.com>
2026-05-07 13:36:32 +0200

    [6.18.z] check logout redirection (#20718) (#21451)
    * check logout redirection (#20718)
    
    (cherry picked from commit 90b1295834d8db7767932fbcaa6eceeb6257a7da)
    
    * set the logout redirect setting within the test body (#20824)
    
    ---------
    
    Co-authored-by: Peter Ondrejka <pondrejk@users.noreply.github.com>

--- tests/foreman/destructive/test_ldap_authentication.py
+++ tests/foreman/destructive/test_ldap_authentication.py
@@ -20,7 +20,13 @@ import pyotp
 import pytest
 
 from robottelo.config import settings
-from robottelo.constants import CERT_PATH, HAMMER_CONFIG, HAMMER_SESSIONS, LDAP_ATTR
+from robottelo.constants import (
+    CERT_PATH,
+    HAMMER_CONFIG,
+    HAMMER_SESSIONS,
+    LDAP_ATTR,
+    LOGIN_DELEGATION_LOGOUT_URL,
+)
 from robottelo.exceptions import CLIReturnCodeError
 from robottelo.logging import logger
 from robottelo.utils.datafactory import gen_string
@@ -265,6 +271,8 @@ def test_single_sign_on_using_rhsso(
 
     :setup: Enroll the RH-SSO Configuration for External Authentication
 
+    :verifies: SAT-40322
+
     :steps:
         1. Create Mappers on RHSSO Instance and Update the Settings in Satellite
         2. Login into Satellite using RHSSO login page redirected by Satellite
@@ -280,6 +288,24 @@ def test_single_sign_on_using_rhsso(
             session.user.search('')
         actual_user = session.task.read_all(widget_names="current_user")['current_user']
         assert settings.rhsso.rhsso_user in actual_user
+        # logout verification for SAT-40322
+        setting_object = module_target_sat.api.Setting().search(
+            query={'search': 'name=login_delegation_logout_url'}
+        )[0]
+        default_setting_value = '' if setting_object.value is None else setting_object.value
+        setting_object.value = LOGIN_DELEGATION_LOGOUT_URL
+        setting_object.update({'value'})
+        try:
+            session.rhsso_login.logout()
+            assert session.browser.url == LOGIN_DELEGATION_LOGOUT_URL, (
+                "Unsuccessful logout redirect"
+            )
+            with pytest.raises(NavigationTriesExceeded) as error:
+                session.task.read_all(widget_names='current_user')['current_user']
+            assert error.typename == 'NavigationTriesExceeded'
+        finally:
+            setting_object.value = default_setting_value
+            setting_object.update({'value'})
 
 
 def test_external_logout_rhsso(rhsso_setting_setup, enable_external_auth_rhsso, module_target_sat):
