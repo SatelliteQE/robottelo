6581edef5303040a46c00dff7c1f8383df0c3f68
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2026-05-14 11:04:19 -0400

    [6.19.z] Remove unused func_shared (#21557)
    Remove unused func_shared (#21553)
    
    (cherry picked from commit 64e5a79ba13d1a7bd5921efba4cf15cadffa7e12)
    
    Co-authored-by: Lukas Pramuk <lpramuk@redhat.com>

--- robottelo/utils/decorators/func_shared/__init__.py
+++
@@ -1,3 +0,0 @@
-from robottelo.utils.decorators.func_shared.shared import shared  # noqa
-from robottelo.utils.decorators.func_shared.shared import SharedFunctionError  # noqa
-from robottelo.utils.decorators.func_shared.shared import SharedFunctionException  # noqa
