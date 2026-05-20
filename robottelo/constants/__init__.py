7830775251d4f281f9fce2247d5e46b988626282
Jameer Pathan <21165044+jameerpathan111@users.noreply.github.com>
2026-05-20 22:52:06 +0200

    Fix weekly_checks github action workflow (#21509)

--- robottelo/constants/__init__.py
+++ robottelo/constants/__init__.py
@@ -1909,6 +1909,7 @@ JIRA_WONTFIX_RESOLUTIONS = (
     "Not a Bug",
     "MirrorOrphan",
 )
+JIRA_COMMON_FIELDS = ['key', 'status', 'labels', 'resolution']
 
 GROUP_MEMBERSHIP_MAPPER = {
     "config": {
