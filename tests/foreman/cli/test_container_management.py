697b35fee9b872f5867863bb4b0b340714bae65a
Ladislav Vašina <ladislavvasina@gmail.com>
2026-06-02 16:15:47 +0200

    617zupdates_to_calling_wait_for

--- tests/foreman/cli/test_container_management.py
+++ tests/foreman/cli/test_container_management.py
@@ -87,7 +87,7 @@ class TestDockerClient:
             # publishing takes few seconds sometimes
             result, _ = wait_for(
                 lambda: module_container_contenthost.execute(f'docker pull {repo["published-at"]}'),
-                num_sec=60,
+                timeout=60,
                 delay=2,
                 fail_condition=lambda out: out.status != 0,
                 logger=logger,
@@ -305,7 +305,7 @@ class TestDockerClient:
         # publishing takes few seconds sometimes
         result, _ = wait_for(
             lambda: module_container_contenthost.execute(docker_pull_command),
-            num_sec=60,
+            timeout=60,
             delay=2,
             fail_condition=lambda out: out.status != 0,
             logger=logger,
