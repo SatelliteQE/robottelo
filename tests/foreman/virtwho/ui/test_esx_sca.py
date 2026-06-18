f7fef45655057094622e04f42938ca0a2bc2f3ba
jnagare <jnagare@redhat.com>
2026-06-18 18:56:12 +0530

    Fix test_positive_remove_env_option: correct error message format
    The expected error message had incorrect f-string formatting with
    double curly braces that produced literal braces in the output.
    
    Changed from:
      option {'env'} is not exist or not be enabled in {'/etc/virt-who.d/virt-who-config-18.conf'}
    
    To:
      option env is not exist or not be enabled in /etc/virt-who.d/virt-who-config-18.conf
    
    This matches the actual error message from get_configure_option().

--- tests/foreman/virtwho/ui/test_esx_sca.py
+++ tests/foreman/virtwho/ui/test_esx_sca.py
@@ -308,7 +308,7 @@ class TestVirtwhoConfigforEsx:
         option = "env"
         config_id = get_configure_id(name, target_sat)
         config_file = get_configure_file(config_id)
-        env_error = f"option {{'{option}'}} is not exist or not be enabled in {{'{config_file}'}}"
+        env_error = f"option {option} is not exist or not be enabled in {config_file}"
         with pytest.raises(Exception) as exc_info:  # noqa: PT011 - TODO determine better exception
             get_configure_option(option, config_file)
         assert str(exc_info.value) == env_error
