a9f708350ee8627d6cd344936f236ec6a9579455
Jameer Pathan <21165044+jameerpathan111@users.noreply.github.com>
2026-07-07 11:28:23 +0200

    Add install-method-aware test collection (#22007)

--- pytest_plugins/marker_deselection.py
+++ pytest_plugins/marker_deselection.py
@@ -1,5 +1,7 @@
 import pytest
 
+from robottelo.config import settings
+from robottelo.enums import InstallMethod
 from robottelo.logging import collection_logger as logger
 
 
@@ -20,6 +22,41 @@ def pytest_addoption(parser):
         parser.addoption(opt, action='store_true', default=False, help=help_text)
 
 
+def _install_method_for_collection():
+    """Resolve install method from server settings for test collection.
+
+    Uses ``server.install_method`` from conf/server.yaml (or
+    ``ROBOTTELO_SERVER__INSTALL_METHOD``). When set to ``auto``, falls back to
+    ``server.deploy_arguments.deploy_container``.
+    """
+    install_method = settings.server.get('install_method', InstallMethod.AUTO)
+    if install_method != InstallMethod.AUTO:
+        return InstallMethod(install_method)
+    if settings.server.deploy_arguments.get('deploy_container'):
+        return InstallMethod.FOREMANCTL
+    return None
+
+
+INCOMPATIBLE_MARKERS = {
+    InstallMethod.FOREMANCTL: 'foreman_installer',
+    InstallMethod.INSTALLER: 'foremanctl',
+}
+DEPLOYMENT_MARKERS = INCOMPATIBLE_MARKERS.values()
+
+
+def _markexpr_selects_marker(markexpr, marker_name):
+    if marker_name not in markexpr:
+        return False
+    return f'not{marker_name}' not in markexpr.replace(' ', '')
+
+
+def _bypass_install_method_filter(config):
+    markexpr = config.option.markexpr
+    if not markexpr:
+        return False
+    return any(_markexpr_selects_marker(markexpr, marker) for marker in DEPLOYMENT_MARKERS)
+
+
 @pytest.hookimpl(tryfirst=True)
 def pytest_collection_modifyitems(items, config):
     """
@@ -27,25 +64,37 @@ def pytest_collection_modifyitems(items, config):
     """
     include_onprem_provision = config.getoption('include_onprem_provisioning', False)
     include_ipv6_provisioning = config.getoption('include_ipv6_provisioning', False)
+    install_method = _install_method_for_collection()
+    bypass = _bypass_install_method_filter(config)
+    incompatible_marker = None if bypass else INCOMPATIBLE_MARKERS.get(install_method)
+    if install_method and bypass and INCOMPATIBLE_MARKERS.get(install_method):
+        logger.info(
+            'Skipping install method deselection due to manual marker selection '
+            f'(install method: {install_method.value}, markexpr: {config.option.markexpr})'
+        )
 
     selected = []
     deselected = []
-    # Cloud Provisioning Test can be run on new pipeline
     for item in items:
-        item_marks = [m.name for m in item.iter_markers()]
-        # Include / Exclude On Premises Provisioning Tests
+        item_marks = [mark.name for mark in item.iter_markers()]
+
+        if incompatible_marker and incompatible_marker in item_marks:
+            logger.info(
+                f'Deselected test {item.nodeid} due to install method '
+                f'{install_method.value} (marker: {incompatible_marker})'
+            )
+            deselected.append(item)
+            continue
         if 'on_premises_provisioning' in item_marks:
             selected.append(item) if include_onprem_provision else deselected.append(item)
             continue
-        # Include / Exclude IPv6 Provisioning Tests
         if 'ipv6_provisioning' in item_marks:
             selected.append(item) if include_ipv6_provisioning else deselected.append(item)
             continue
-        # This Plugin does not applies to this test
         selected.append(item)
     logger.debug(
         f'Selected {len(selected)} and deselected {len(deselected)} '
         'tests based on auto un-collectable markers and pytest options.'
     )
-    items[:] = selected or items
+    items[:] = selected
     config.hook.pytest_deselected(items=deselected)
