de44cd99d5e467700b96b98179a816caed264c85
Sam Bible <sjbible@gmail.com>
2026-07-12 21:58:23 -0500

    Add new file to codeowners, and address review comments.

--- tests/foreman/installer/foremanctl/test_foremanctl_backup.py
+++ tests/foreman/installer/foremanctl/test_foremanctl_backup.py
@@ -21,26 +21,16 @@ from robottelo.hosts import Satellite
 pytestmark = [pytest.mark.foremanctl]
 
 BACKUP_DIR = '/tmp/'
-IOP_FILES = {
-    'iop_inventory.dump',
-    'iop_vmaas.dump',
-    'iop_advisor.dump',
-    'iop_remediations.dump',
-    'iop_vulnerability.dump',
-}
-BASIC_FILES = {'config_files.tar.gz', '.config.snar', 'metadata.yml'}
-SAT_FILES = {'candlepin.dump', 'foreman.dump', 'pulpcore.dump'} | BASIC_FILES
+BASIC_FILES = {'foremanctl-state.tar.gz', 'metadata.yml'}
+SAT_FILES = {'candlepin.dump', 'foreman.dump', 'pulp.dump'} | BASIC_FILES
 CAPS_FILES = {'pulpcore.dump'} | BASIC_FILES
-CONTENT_FILES = {'pulp_data.tar', '.pulp.snar'}
+CONTENT_FILES = {'pulp-content.tar.gz'}
 
 
 def get_exp_files(module_target_sat, skip_pulp=False):
     expected_files = SAT_FILES if type(module_target_sat) is Satellite else CAPS_FILES
     if not skip_pulp:
         expected_files = expected_files | CONTENT_FILES
-    # IoP support will come when it is supported in CI
-    # if type(module_target_sat) is Satellite:
-    #    expected_files = expected_files | IOP_FILES if module_target_sat.iop_enabled else expected_files
     return expected_files
 
 
@@ -76,10 +66,7 @@ def test_positive_offline_backup(module_target_sat, setup_backup_tests):
     assert result.status == 0, f'Backup directory {subdir} was not created'
 
     # Get list of files in backup directory
-    # result = module_target_sat.execute(f'ls -a {backup_dir}')
-    # assert result.status == 0
-    # files = set(result.stdout.split())
-    files = module_target_sat.execute(f'ls -a {subdir}').stdout.split('\n')
+    files = module_target_sat.execute(f'ls -a {subdir}/*').stdout.split('\n')
     files = [i for i in files if not re.compile(r'^\.*$').search(i)]
     expected_files = get_exp_files(module_target_sat)
 
