ad736f21bcc9ede76d18fc9f2c3a73d33d7a42ae
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2026-09-07 06:36:50 -0400

    [6.20.z] Update content source naming scheme for host group tests (#22526)
    Update content source naming scheme for host group tests (#22464)
    
    Update content source name for host group tests
    
    Foremanctl introduces a new naming scheme for smart proxies, which
    causes a variety of test failures. This PR adjusts the naming scheme for
    content source smart proxies in host group tests to address failures
    caused by the old content source smart proxy naming scheme
    (`satellite.example.com:9090`).
    
    (cherry picked from commit 74282e71d85efe517a91d3c1e4b0a09541f5e8a1)
    
    Co-authored-by: synkd <48261305+synkd@users.noreply.github.com>

--- tests/foreman/api/test_hostgroup.py
+++ tests/foreman/api/test_hostgroup.py
@@ -540,7 +540,7 @@ class TestHostGroup:
 
         """
         new_content_source = target_sat.api.SmartProxy().search(
-            query={'search': f'url = {target_sat.url}:9090'}
+            query={'search': f'feature = "Pulpcore" and url ~ {target_sat.hostname}'}
         )[0]
         hostgroup.content_source = new_content_source
         hostgroup = hostgroup.update(['content_source'])
@@ -707,7 +707,7 @@ class TestHostGroup:
 
         # Get content source, architecture, and operating system objects for hostgroup creation
         content_source = module_target_sat.api.SmartProxy().search(
-            query={'search': f'url = {module_target_sat.url}:9090'}
+            query={'search': f'feature = "Pulpcore" and url ~ {module_target_sat.hostname}'}
         )[0]
         old_os_id = (
             module_target_sat.api.OperatingSystem()
