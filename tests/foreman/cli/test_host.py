8350f67ef86e74f1f6032235b4acff6b272e8fce
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2026-09-15 14:04:35 -0400

    [6.20.z] Update Hosts CLI tests for foremanctl (#22760)

--- tests/foreman/cli/test_host.py
+++ tests/foreman/cli/test_host.py
@@ -12,6 +12,7 @@
 
 """
 
+import json
 from random import choice
 import re
 
@@ -28,6 +29,7 @@ from robottelo.constants import (
     FAKE_7_CUSTOM_PACKAGE,
     FAKE_8_CUSTOM_PACKAGE,
     FAKE_8_CUSTOM_PACKAGE_NAME,
+    DataFile,
 )
 from robottelo.enums import NetworkType
 from robottelo.exceptions import CLIFactoryError, CLIReturnCodeError
@@ -68,6 +70,45 @@ def function_host(target_sat):
     )
     yield host
     target_sat.cli.Host.delete({'id': host['id']})
+
+
+@pytest.fixture
+def function_enc_host(target_sat):
+    """Create a managed host with the fields asserted by the ENC test."""
+    host_template = target_sat.api.Host()
+    host_template.create_missing()
+    ip_prefix = 'ip6' if target_sat.network_type == NetworkType.IPV6 else 'ip'
+    ip_address = gen_ipaddr(ipv6=target_sat.network_type.has_ipv6)
+    ssh_key = json.loads(DataFile.SSH_KEYS_JSON.read_bytes())['ssh_keys']['ed']
+
+    host_options = {
+        'architecture-id': host_template.architecture.id,
+        'domain-id': host_template.domain.id,
+        'location-id': host_template.location.id,
+        'mac': host_template.mac,
+        'medium-id': host_template.medium.id,
+        'name': host_template.name,
+        'operatingsystem-id': host_template.operatingsystem.id,
+        'organization-id': host_template.organization.id,
+        'partition-table-id': host_template.ptable.id,
+        'root-password': host_template.root_pass,
+        ip_prefix: ip_address,
+    }
+    host = None
+    try:
+        host = target_sat.cli_factory.make_host(host_options)
+        target_sat.cli.Host.set_parameter(
+            {
+                'host-id': host['id'],
+                'name': 'remote_execution_ssh_keys',
+                'value': ssh_key,
+            }
+        )
+
+        yield host, ip_prefix, ip_address, ssh_key
+    finally:
+        if host is not None:
+            target_sat.cli.Host.delete({'id': host['id']})
 
 
 @pytest.fixture
@@ -1058,7 +1099,7 @@ def test_negative_update_os(target_sat, function_host, module_architecture):
 
 @pytest.mark.run_in_one_thread
 @pytest.mark.cli_host_update
-def test_hammer_host_info_output(target_sat, module_user):
+def test_hammer_host_info_output(target_sat, module_user, function_host):
     """Verify re-add of 'owner-id' in `hammer host info` output
 
     :id: 03468516-0ebb-11eb-8ad8-0c7a158cbff4
@@ -1079,12 +1120,13 @@ def test_hammer_host_info_output(target_sat, module_user):
     user = target_sat.api.User().search(
         query={'search': f'login={settings.server.admin_username}'}
     )[0]
+    host_id = function_host['id']
     target_sat.cli.Host.update(
-        {'owner': settings.server.admin_username, 'owner-type': 'User', 'id': '1'}
+        {'owner': settings.server.admin_username, 'owner-type': 'User', 'id': host_id}
     )
-    result_info = target_sat.cli.Host.info(options={'id': '1', 'fields': 'Additional info'})
+    result_info = target_sat.cli.Host.info(options={'id': host_id, 'fields': 'Additional info'})
     assert int(result_info['additional-info']['owner-id']) == user.id
-    host = target_sat.cli.Host.info({'id': '1'})
+    host = target_sat.cli.Host.info({'id': host_id})
     target_sat.cli.User.update(
         {
             'id': module_user.id,
@@ -1092,8 +1134,8 @@ def test_hammer_host_info_output(target_sat, module_user):
             'locations': [host['location']['name']],
         }
     )
-    target_sat.cli.Host.update({'owner-id': module_user.id, 'id': '1'})
-    result_info = target_sat.cli.Host.info(options={'id': '1', 'fields': 'Additional info'})
+    target_sat.cli.Host.update({'owner-id': module_user.id, 'id': host_id})
+    result_info = target_sat.cli.Host.info(options={'id': host_id, 'fields': 'Additional info'})
     assert int(result_info['additional-info']['owner-id']) == module_user.id
 
 
@@ -2097,7 +2139,7 @@ def test_positive_multi_cv_host_repo_availability(
 
 
 # -------------------------- HOST ENC SUBCOMMAND SCENARIOS -------------------------
-def test_positive_dump_enc_yaml(target_sat):
+def test_positive_dump_enc_yaml(function_enc_host, target_sat):
     """Dump host's ENC YAML. Check BZ for details.
 
     :id: 50bf2530-788c-4710-a382-d034d73d5d4d
@@ -2110,12 +2152,11 @@ def test_positive_dump_enc_yaml(target_sat):
 
     :CaseImportance: Critical
     """
-    enc_dump = target_sat.cli.Host.enc_dump({'name': target_sat.hostname})
-    assert f'fqdn: {target_sat.hostname}' in enc_dump
-    ip_prefix = 'ip6' if target_sat.network_type == NetworkType.IPV6 else 'ip'
-    assert f'{ip_prefix}: {target_sat.ip_addr}' in enc_dump
-    # Check for SSH key (either RSA or Ed25519)
-    assert 'ssh-rsa' in enc_dump or 'ssh-ed25519' in enc_dump
+    host, ip_prefix, ip_address, ssh_key = function_enc_host
+    enc_dump = target_sat.cli.Host.enc_dump({'name': host['name']})
+    assert f'fqdn: {host["name"]}' in enc_dump
+    assert f'{ip_prefix}: {ip_address}' in enc_dump
+    assert ssh_key in enc_dump
 
 
 # -------------------------- HOST TRACE SUBCOMMAND SCENARIOS -------------------------
