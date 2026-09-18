d7a9f77c39dab80ba29540d809a96191fd2c0e37
jnagare-redhat <jnagare@redhat.com>
2026-09-18 12:23:52 +0530

    Update OS name search to use exact match
    Change OS name search from partial match to exact match.

--- pytest_fixtures/component/os.py
+++ pytest_fixtures/component/os.py
@@ -13,7 +13,7 @@ def default_os(
 ):
     """Returns an Operating System entity read from searching for supportability.content_host.default_os_name"""
     # Use partial match (~) instead of exact match to handle OS names like "RedHat 9.4"
-    search_string = f'name ~ {settings.supportability.content_hosts.default_os_name}'
+    search_string = f'name="{settings.supportability.content_hosts.default_os_name}"'
 
     try:
         os = (
